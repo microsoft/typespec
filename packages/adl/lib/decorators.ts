6e06caacd20ab968e1eb61b9703a71ded463f3a0
David Wilson <daviwil@microsoft.com>
2021-01-14 16:17:10 -0800

    Enable @format, @minLength, @maxLength decorators; fix OpenAPI types

--- packages/adl/lib/decorators.ts
+++ packages/adl/lib/decorators.ts
@@ -29,3 +29,85 @@ export function intrinsic(program: Program, target: Type) {
 export function isIntrinsic(target: Type) {
   return intrinsics.has(target);
 }
+
+// Walks the assignmentType chain to find the core intrinsic type, if any
+export function getIntrinsicType(target: Type | undefined): string | undefined {
+  while (target) {
+    if (target.kind === "Model") {
+      if (isIntrinsic(target)) {
+        return target.name;
+      }
+
+      target =
+        (target.assignmentType?.kind === "Model" && target.assignmentType)
+        || undefined;
+    } else {
+      break;
+    }
+  }
+
+  return undefined;
+}
+
+// -- @format decorator ---------------------
+
+const formatValues = new Map<Type, string>();
+
+export function format(program: Program, target: Type, format: string) {
+  if (target.kind === "Model") {
+    // Is it a model type that ultimately derives from 'string'?
+    if (getIntrinsicType(target) === "string") {
+      formatValues.set(target, format);
+    } else {
+      throw new Error("Cannot apply @format to a non-string type");
+    }
+  } else {
+    throw new Error("Cannot apply @format to anything that isn't a Model");
+  }
+}
+
+export function getFormat(target: Type): string | undefined {
+  return formatValues.get(target);
+}
+
+// -- @minLength decorator ---------------------
+
+const minLengthValues = new Map<Type, number>();
+
+export function minLength(program: Program, target: Type, minLength: number) {
+  if (target.kind === "Model") {
+    // Is it a model type that ultimately derives from 'string'?
+    if (getIntrinsicType(target) === "string") {
+      minLengthValues.set(target, minLength);
+    } else {
+      throw new Error("Cannot apply @minLength to a non-string type");
+    }
+  } else {
+    throw new Error("Cannot apply @format to anything that isn't a Model");
+  }
+}
+
+export function getMinLength(target: Type): number | undefined {
+  return minLengthValues.get(target);
+}
+
+// -- @maxLength decorator ---------------------
+
+const maxLengthValues = new Map<Type, number>();
+
+export function maxLength(program: Program, target: Type, maxLength: number) {
+  if (target.kind === "Model") {
+    // Is it a model type that ultimately derives from 'string'?
+    if (getIntrinsicType(target) === "string") {
+      maxLengthValues.set(target, maxLength);
+    } else {
+      throw new Error("Cannot apply @maxLength to a non-string type");
+    }
+  } else {
+    throw new Error("Cannot apply @format to anything that isn't a Model");
+  }
+}
+
+export function getMaxLength(target: Type): number | undefined {
+  return maxLengthValues.get(target);
+}
