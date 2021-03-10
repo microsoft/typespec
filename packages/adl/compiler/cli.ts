49385ddfb51e8bdf8a288035c10b4221cab0135a
Nick Guerrera <nicholg@microsoft.com>
2021-03-09 18:27:40 -0800

    Introduce language server and add live parse errors to VS Code

--- packages/adl/compiler/cli.ts
+++ packages/adl/compiler/cli.ts
@@ -4,13 +4,11 @@ import url from "url";
 import yargs from "yargs";
 import mkdirp from "mkdirp";
 import * as path from "path";
-import { readFileSync } from "fs";
 import { compile } from "../compiler/program.js";
 import { spawnSync } from "child_process";
 import { CompilerOptions } from "../compiler/options.js";
 import { DiagnosticError, dumpError, logDiagnostics } from "./diagnostics.js";
-
-const adlVersion = getVersion();
+import { adlVersion } from "./util.js";
 
 const args = yargs(process.argv.slice(2))
   .help()
@@ -98,12 +96,6 @@ async function getCompilerOptions(): Promise<CompilerOptions> {
     swaggerOutputFile: path.resolve(args["output-path"], "openapi.json"),
     nostdlib: args["nostdlib"],
   };
-}
-
-function getVersion(): string {
-  const packageJsonPath = new url.URL(`../../package.json`, import.meta.url);
-  const packageJson = JSON.parse(readFileSync(url.fileURLToPath(packageJsonPath), "utf-8"));
-  return packageJson.version;
 }
 
 async function main() {
--- packages/adl/compiler/util.ts
+++ packages/adl/compiler/util.ts
@@ -1,4 +1,13 @@
+import fs from "fs";
 import url from "url";
+
+export const adlVersion = getVersion();
+
+function getVersion(): string {
+  const packageJsonPath = resolvePath(import.meta.url, "../../package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+  return packageJson.version;
+}
 
 export function resolvePath(basePath: string, ...parts: string[]): string {
   const resolvedPath = new url.URL(parts.join(""), basePath);
