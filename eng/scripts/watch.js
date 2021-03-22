479a13d5bbeba064ebbae1b06afc031cca518f15
Nick Guerrera <nicholg@microsoft.com>
2021-03-22 10:09:25 -0700

    Use single process for `rush watch` as in VS Code

--- eng/scripts/watch.js
+++ eng/scripts/watch.js
@@ -1,31 +1,6 @@
-var cp = require("child_process");
-
-require("./for-each").forEachProject((packageName, projectFolder, project) => {
-  if (project.scripts && project.scripts.watch) {
-    // NOTE: We deliberately use `tsc --watch --project ${projectFolder}` here
-    // with cwd at the repo root instead of `npm run watch` with cwd at project
-    // folder. This ensures that error messages put source file paths relative
-    // to the repo root, which then allows VS Code to navigate to error
-    // locations correctly.
-    const tsc = `${projectFolder}/node_modules/.bin/tsc`;
-    const args = ["--watch", "--project", projectFolder];
-    console.log(`${tsc} ${args.join(" ")}`);
-
-    const proc = cp.spawn(tsc, args, { cwd: `${__dirname}/../`, shell: true, stdio: "inherit" });
-    proc.on("error", (c, s) => {
-      console.log(packageName);
-      console.error(c);
-      console.error(s);
-    });
-    proc.on("exit", (c, s) => {
-      console.log(packageName);
-      console.error(c);
-      console.error(s);
-    });
-    proc.on("message", (c, s) => {
-      console.log(packageName);
-      console.error(c);
-      console.error(s);
-    });
-  }
-});
+const { spawn } = require("child_process");
+const { resolve } = require("path");
+const root = resolve(__dirname, "../../");
+const tsc = resolve(root, "packages/adl/node_modules/.bin/tsc");
+const args = ["--build", "--watch"];
+spawn(tsc, args, { cwd: root, shell: true, stdio: "inherit" });
