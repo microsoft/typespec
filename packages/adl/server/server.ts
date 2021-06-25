5c349e7eedf068080e98bb940a7c57bd64627b40
Nick Guerrera <nicholg@microsoft.com>
2021-06-25 06:54:53 -0700

    Use LSP to log messages from client to server (#598)
    This removes the need for a custom output window pane in Visual Studio.
    
    Also, start tracking workspace folders which will be needed soon, but just
    log them for now.

--- packages/adl/server/server.ts
+++ packages/adl/server/server.ts
@@ -1,17 +1,21 @@
-import { fileURLToPath } from "url";
+import { fileURLToPath, pathToFileURL } from "url";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import {
   Connection,
   createConnection,
   Diagnostic,
   DiagnosticSeverity,
+  InitializedParams,
   InitializeParams,
   InitializeResult,
   ProposedFeatures,
   Range,
+  ServerCapabilities,
   TextDocumentChangeEvent,
   TextDocuments,
   TextDocumentSyncKind,
+  WorkspaceFolder,
+  WorkspaceFoldersChangeEvent,
 } from "vscode-languageserver/node.js";
 import { createSourceFile } from "../compiler/diagnostics.js";
 import { parse } from "../compiler/parser.js";
@@ -19,28 +23,86 @@ import { adlVersion } from "../compiler/util.js";
 
 let connection: Connection;
 let documents: TextDocuments<TextDocument>;
-main();
+let clientHasWorkspaceFolderCapability = false;
+let workspaceFolders: WorkspaceFolder[] = [];
+let isInitialized = false;
+let pendingMessages: string[] = [];
+
+process.on("unhandledRejection", fatalError);
+try {
+  main();
+} catch (e) {
+  fatalError(e);
+}
 
 function main() {
-  log(`ADL language server v${adlVersion}\n`);
-  log(`Module: ${fileURLToPath(import.meta.url)}`);
-  log(`Command Line: ${JSON.stringify(process.argv)}`);
+  log(`ADL language server v${adlVersion}`);
+  log("Module", fileURLToPath(import.meta.url));
+  log("Command Line", process.argv);
 
   connection = createConnection(ProposedFeatures.all);
   documents = new TextDocuments(TextDocument);
 
   connection.onInitialize(initialize);
+  connection.onInitialized(initialized);
   documents.onDidChangeContent(checkChange);
   documents.listen(connection);
   connection.listen();
 }
 
 function initialize(params: InitializeParams): InitializeResult {
-  return {
-    capabilities: {
-      textDocumentSync: TextDocumentSyncKind.Incremental,
-    },
+  const capabilities: ServerCapabilities = {
+    textDocumentSync: TextDocumentSyncKind.Incremental,
   };
+
+  if (params.capabilities.workspace?.workspaceFolders) {
+    clientHasWorkspaceFolderCapability = true;
+    workspaceFolders = params.workspaceFolders ?? [];
+    capabilities.workspace = {
+      workspaceFolders: {
+        supported: true,
+        changeNotifications: true,
+      },
+    };
+  } else if (params.rootUri) {
+    workspaceFolders = [
+      {
+        name: "<root>",
+        uri: params.rootUri?.toString(),
+      },
+    ];
+  } else if (params.rootPath) {
+    workspaceFolders = [
+      {
+        name: "<root>",
+        uri: pathToFileURL(params.rootPath).href,
+      },
+    ];
+  }
+
+  log("Workspace Folders", workspaceFolders);
+  return { capabilities };
+}
+
+function initialized(params: InitializedParams): void {
+  if (clientHasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders(workspaceFoldersChanged);
+  }
+  isInitialized = true;
+  log("Initialization complete.");
+}
+
+function workspaceFoldersChanged(e: WorkspaceFoldersChangeEvent) {
+  log("Workspace Folders Changed", e);
+  const map = new Map(workspaceFolders.map((f) => [f.uri, f]));
+  for (const folder of e.removed) {
+    map.delete(folder.uri);
+  }
+  for (const folder of e.added) {
+    map.set(folder.uri, folder);
+  }
+  workspaceFolders = Array.from(map.values());
+  log("Workspace Folders", workspaceFolders);
 }
 
 function checkChange(change: TextDocumentChangeEvent<TextDocument>) {
@@ -70,7 +132,29 @@ function convertSeverity(severity: "warning" | "error"): DiagnosticSeverity {
   }
 }
 
-function log(...args: any[]) {
-  // NB: We must log to stderr because stdin/out are used as the LSP pipe.
-  console.error(...args);
+function log(message: string, details: any = undefined) {
+  if (details) {
+    message += ": " + JSON.stringify(details, undefined, 2);
+  }
+
+  if (!isInitialized) {
+    pendingMessages.push(message);
+    return;
+  }
+
+  for (const pending of pendingMessages) {
+    connection.console.log(pending);
+  }
+
+  pendingMessages = [];
+  connection.console.log(message);
+}
+
+function fatalError(e: any) {
+  // If we failed to send any log messages over LSP pipe, send them to stderr before exiting.
+  for (const pending of pendingMessages) {
+    console.error(pending);
+  }
+  console.error(e);
+  process.exit(1);
 }
