f0ab71d9ec3733fed69d20046eb9f4002423c2a8
markcowl <markcowl@microsoft.com>
2021-07-27 11:54:34 -0700

    Adjusting to new decorators property on types

--- packages/adl-rest/src/rest.ts
+++ packages/adl-rest/src/rest.ts
@@ -22,6 +22,7 @@ export function getHttpOperation(
     returnType: operation.returnType,
     namespace: operation.namespace,
     parameters: operation.parameters,
+    decorators: operation.decorators,
   };
 }
 
