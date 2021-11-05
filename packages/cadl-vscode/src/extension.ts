0a612a66839e2717ef3f1b635f76cbcca7669a42
Timothee Guerin <tiguerin@microsoft.com>
2021-11-05 10:14:23 -0700

    Allow path to cadl-server as well

--- packages/cadl-vscode/src/extension.ts
+++ packages/cadl-vscode/src/extension.ts
@@ -1,3 +1,4 @@
+import { stat } from "fs/promises";
 import { join } from "path";
 import { ExtensionContext, workspace } from "vscode";
 import {
@@ -9,8 +10,8 @@ import {
 
 let client: LanguageClient | undefined;
 
-export function activate(context: ExtensionContext) {
-  const exe = resolveCadlServer(context);
+export async function activate(context: ExtensionContext) {
+  const exe = await resolveCadlServer(context);
   const options: LanguageClientOptions = {
     synchronize: {
       fileEvents: [
@@ -30,7 +31,7 @@ export function activate(context: ExtensionContext) {
   client.start();
 }
 
-function resolveCadlServer(context: ExtensionContext): Executable {
+async function resolveCadlServer(context: ExtensionContext): Promise<Executable> {
   const nodeOptions = process.env.CADL_SERVER_NODE_OPTIONS;
   const args = ["--stdio"];
   // In development mode (F5 launch from source), resolve to locally built server.js.
@@ -42,23 +43,18 @@ function resolveCadlServer(context: ExtensionContext): Executable {
     return { command: "node", args: [...options, script, ...args] };
   }
 
-  // In production, first try VS Code configuration, which allows a global machine
-  // location that is not on PATH, or a workspace-specific installation.
-  let serverPath = workspace.getConfiguration().get("cadl.cadl-server.path") as string;
-  if (serverPath && typeof serverPath !== "string") {
-    throw new Error("VS Code configuration option 'cadl.cadl-server.path' must be a string");
-  }
-
-  if (serverPath?.includes("${workspaceRoot}")) {
-    const workspaceRoot = workspace.workspaceFolders?.[0]?.uri?.fsPath ?? "";
-    serverPath = serverPath.replace("${workspaceRoot}", workspaceRoot);
-  }
-
   let options: ExecutableOptions | undefined;
   if (nodeOptions) {
     options = {
       env: { ...process.env, NODE_OPTIONS: nodeOptions },
     };
+  }
+
+  // In production, first try VS Code configuration, which allows a global machine
+  // location that is not on PATH, or a workspace-specific installation.
+  let serverPath = workspace.getConfiguration().get("cadl.cadl-server.path") as string;
+  if (serverPath && typeof serverPath !== "string") {
+    throw new Error("VS Code configuration option 'cadl.cadl-server.path' must be a string");
   }
 
   // Default to cadl-server on PATH, which would come from `npm install -g
@@ -68,11 +64,35 @@ function resolveCadlServer(context: ExtensionContext): Executable {
     return { command: executable, args, options };
   }
 
+  if (serverPath.includes("${workspaceRoot}")) {
+    const workspaceRoot = workspace.workspaceFolders?.[0]?.uri?.fsPath ?? "";
+    serverPath = serverPath.replace("${workspaceRoot}", workspaceRoot);
+  }
+
   if (!serverPath.endsWith(".js")) {
-    serverPath = join(serverPath, "cmd/cadl-server.js");
+    // Allow path to cadl-server.cmd to be passed.
+    if (await isFile(serverPath)) {
+      const command =
+        process.platform === "win32" && !serverPath.endsWith(".cmd")
+          ? `${serverPath}.cmd`
+          : "cadl-server";
+
+      return { command, args, options };
+    } else {
+      serverPath = join(serverPath, "cmd/cadl-server.js");
+    }
   }
 
   return { command: "node", args: [serverPath, ...args], options };
+}
+
+async function isFile(path: string) {
+  try {
+    const stats = await stat(path);
+    return stats.isFile();
+  } catch {
+    return false;
+  }
 }
 
 export async function deactivate() {
