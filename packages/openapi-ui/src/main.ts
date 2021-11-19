259f7178c7b406d06e2d2449122110cfb5abd96e
Timothee Guerin <tiguerin@microsoft.com>
2021-11-19 10:41:25 -0800

    .

--- packages/openapi-ui/src/main.ts
+++ packages/openapi-ui/src/main.ts
@@ -1,21 +1,12 @@
 import express from "express";
-import fs from "fs";
-import swagerUiDist from "swagger-ui-dist";
-const pathToSwaggerUi = swagerUiDist.absolutePath();
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 const app = express();
 
 const localSpec = process.argv[2];
 console.log("Local file for spec is", localSpec);
 
-const indexContent = fs
-  .readFileSync(`${pathToSwaggerUi}/index.html`)
-  .toString()
-  .replace("https://petstore.swagger.io/v2/swagger.json", "http://localhost:3000/openapi.json");
-
-app.get("/", (req, res) => res.send(indexContent));
-app.get("/index.html", (req, res) => res.send(indexContent)); // you need to do this since the line below serves `index.html` at both routes
-app.use(express.static(pathToSwaggerUi));
-
+app.use(express.static(join(dirname(fileURLToPath(import.meta.url)), "../../app")));
 app.get("/openapi.json", (req, res) => {
   res.sendFile(localSpec);
 });
