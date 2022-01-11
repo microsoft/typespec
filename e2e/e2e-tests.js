8c9d2c15024be3e1eecf1d7f5f9c5dbe1d9f49ae
Timothee Guerin <tiguerin@microsoft.com>
2022-01-11 13:23:54 -0800

    e2e test

--- e2e/e2e-tests.js
+++ e2e/e2e-tests.js
@@ -1,16 +1,16 @@
 // @ts-check
-import { repoRoot, run } from "./helpers.js";
+import { repoRoot, run } from "../eng/scripts/helpers.js";
 import { join } from "path";
-import { existsSync, readdirSync, rmSync } from "fs";
+import { existsSync, readdirSync, rmSync, writeFileSync } from "fs";
 
 const e2eTestDir = join(repoRoot, "e2e");
 const cadlCmd = process.platform === "win32" ? "cadl.cmd" : "cadl";
 
 function main() {
   const packages = packPackages();
-  console.log("Pack", packages);
   installCompiler(packages["@cadl-lang/compiler"]);
   testBasicLatest();
+  testBasicCurrentTgz(packages);
 }
 main();
 
@@ -53,6 +53,41 @@ function testBasicLatest() {
 
   console.log("Running cadl compile .");
   run(cadlCmd, ["compile", "."], { cwd: basicLatestDir });
+  console.log("Completed cadl compile .");
+
+  if (existsSync(join(outputDir, "openapi.json"))) {
+    console.log("Output created successfully.");
+  } else {
+    throw new Error("Test basic latest failed to produce output openapi.json");
+  }
+}
+
+function testBasicCurrentTgz(packages) {
+  const basicCurrentDir = join(e2eTestDir, "basic-current");
+  const outputDir = join(basicCurrentDir, "cadl-output");
+  console.log("Clearing basic-current");
+  rmSync(basicCurrentDir, { recursive: true });
+  console.log("Cleared basic-current");
+
+  console.log("Generating package.json for basic-current");
+  const packageJson = {
+    name: "basic-latest",
+    dependencies: {
+      "@cadl-lang/compiler": packages["@cadl-lang/compiler"],
+      "@cadl-lang/rest": packages["@cadl-lang/rest"],
+      "@cadl-lang/openapi3": packages["@cadl-lang/openapi3"],
+    },
+    private: true,
+  };
+  writeFileSync(join(basicCurrentDir, "pacakge.json"), JSON.stringify(packageJson, null, 2));
+  console.log("Generatedpackage.json for basic-current");
+
+  console.log("Installing basic-latest dependencies");
+  run(cadlCmd, ["install"], { cwd: basicCurrentDir });
+  console.log("Installed basic-latest dependencies");
+
+  console.log("Running cadl compile .");
+  run(cadlCmd, ["compile", "."], { cwd: basicCurrentDir });
   console.log("Completed cadl compile .");
 
   if (existsSync(join(outputDir, "openapi.json"))) {
