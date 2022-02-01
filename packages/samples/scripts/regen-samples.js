ba5d64e80a21b0dfe070af6e1ae80d398d9dba3c
Timothee Guerin <tiguerin@microsoft.com>
2022-02-01 09:36:20 -0800

    Update prettier import plugin

--- packages/samples/scripts/regen-samples.js
+++ packages/samples/scripts/regen-samples.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import mkdirp from "mkdirp";
-import { run } from "../../../eng/scripts/helpers.js";
 import { readdirSync, rmdirSync } from "fs";
-import { fileURLToPath } from "url";
+import mkdirp from "mkdirp";
 import { dirname, join, normalize, resolve } from "path";
+import { fileURLToPath } from "url";
+import { run } from "../../../eng/scripts/helpers.js";
 
 const excludedSamples = [
   // fails compilation by design to demo language server
