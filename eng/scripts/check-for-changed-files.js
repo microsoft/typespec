1e4a645b550ae8b0c98ba8ba6f8789136e424972
Nick Guerrera <nicholg@microsoft.com>
2022-02-17 11:31:05 -0800

    Validate that no changes were made after publish was prepared (#268)

--- eng/scripts/check-for-changed-files.js
+++ eng/scripts/check-for-changed-files.js
@@ -14,9 +14,17 @@ if (proc.stderr) {
 }
 
 if (proc.stdout || proc.stderr) {
-  console.error(
-    `ERROR: Files above were changed during PR validation, but not included in the PR.
+  if (process.argv[2] !== "publish") {
+    console.error(
+      `ERROR: Files above were changed during PR validation, but not included in the PR.
 Include any automated changes such as sample output, spec.html, and ThirdPartyNotices.txt in your PR.`
-  );
+    );
+  } else {
+    console.error(
+      `ERROR: Changes have been made since this publish PR was prepared.
+In the future, remember to alert coworkers to avoid merging additional changes while publish PRs are in progress.
+Close this PR, run prepare-publish again.`
+    );
+  }
   process.exit(1);
 }
