c254610eb3309bfb140cd38bb0a0991b253d58ea
Timothee Guerin <tiguerin@microsoft.com>
2022-03-07 12:46:38 -0800

    Tweak prettier plugin rollout to fix tree shaking

--- packages/prettier-plugin-cadl/rollup.config.js
+++ packages/prettier-plugin-cadl/rollup.config.js
@@ -5,13 +5,14 @@ import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 
 export default {
-  input: "src/index.js",
+  input: "src/index.mjs",
   output: {
     file: "dist/index.js",
     format: "commonjs",
     sourcemap: true,
     exports: "default",
   },
+  inlineDynamicImports: true,
   context: "this",
   external: ["fs/promises", "prettier"],
   plugins: [
