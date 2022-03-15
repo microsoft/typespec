03b0d469618b666c034f6b80c432e2a0e623dc21
Timothee Guerin <tiguerin@microsoft.com>
2022-03-15 15:31:48 -0700

    rollup normalize path

--- packages/cadl-vscode/rollup.config.js
+++ packages/cadl-vscode/rollup.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import path from "path";
 import { defineConfig } from "rollup";
 
 export default defineConfig({
@@ -18,7 +17,8 @@ export default defineConfig({
     if (warning.code === "CIRCULAR_DEPENDENCY") {
       // filter out warnings about circular dependencies out of our control
       for (const each of ["node_modules/semver"]) {
-        if (warning.importer.includes(path.normalize(each))) {
+        if (warning.importer.includes(each)) {
+          console.log("SKIPPP");
           return;
         }
       }
