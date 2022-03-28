003854cf43bef8534042714f036915bb0d698a90
Timothee Guerin <tiguerin@microsoft.com>
2022-03-28 14:42:56 -0600

    Fix eslint in vscode (#369)

--- packages/eslint-config-cadl/index.js
+++ packages/eslint-config-cadl/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./tsconfig.json" },
   plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   env: {
