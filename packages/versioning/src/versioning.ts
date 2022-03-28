9dc494eb60138524a06f3fd99faf7fc8e72cd7ad
Timothee Guerin <tiguerin@microsoft.com>
2022-03-28 13:05:04 -0700

    Remove console.log for testing

--- packages/versioning/src/versioning.ts
+++ packages/versioning/src/versioning.ts
@@ -17,7 +17,6 @@ const renamedFromKey = Symbol();
 const madeOptionalKey = Symbol();
 
 export function $added({ program }: DecoratorContext, t: Type, v: string) {
-  console.log("Abc");
   if (typeof v !== "string") {
     reportDiagnostic(program, { code: "version-must-be-string", target: t });
     return;
