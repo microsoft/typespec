fc4c8a006ea930544e49e14c1f80a2680b6e9f95
Timothee Guerin <tiguerin@microsoft.com>
2022-04-08 12:53:20 -0700

    Fix issue with using as default value of template arg & internal template instance duplication

--- packages/compiler/core/checker.ts
+++ packages/compiler/core/checker.ts
@@ -499,10 +499,7 @@ export function createChecker(program: Program): Checker {
         );
       }
     } else {
-      return (
-        templateInstantiation[index] ??
-        getResolvedTypeParameterDefault(links.declaredType as TemplateParameterType)
-      );
+      return templateInstantiation[index];
     }
 
     return type;
@@ -536,7 +533,7 @@ export function createChecker(program: Program): Checker {
             program.reportDiagnostic(
               createDiagnostic({ code: "invalid-template-default", target: node })
             );
-            return errorType;
+            return undefined;
           }
         }
         return type;
@@ -544,14 +541,14 @@ export function createChecker(program: Program): Checker {
 
       visitChildren(node, (x) => {
         const visited = visit(x);
-        if (visited === errorType) {
+        if (visited === undefined) {
           hasError = true;
         }
       });
 
-      return hasError ? errorType : type;
+      return hasError ? undefined : type;
     }
-    return visit(nodeDefault);
+    return visit(nodeDefault) ?? errorType;
   }
 
   function checkTypeReference(
@@ -563,6 +560,64 @@ export function createChecker(program: Program): Checker {
     }
 
     return checkTypeReferenceSymbol(sym, node);
+  }
+
+  function checkTypeReferenceArgs(
+    node: TypeReferenceNode | MemberExpressionNode | IdentifierNode
+  ): Type[] {
+    const args: Type[] = [];
+    if (node.kind !== SyntaxKind.TypeReference) {
+      return args;
+    }
+
+    for (const arg of node.arguments) {
+      args.length++;
+      const value = getTypeForNode(arg);
+      args.push(value);
+    }
+    return args;
+  }
+
+  function checkTemplateInstantiationArgs(
+    node: Node,
+    args: Type[],
+    declarations: readonly TemplateParameterDeclarationNode[]
+  ): Type[] {
+    if (args.length > declarations.length) {
+      program.reportDiagnostic(
+        createDiagnostic({ code: "invalid-template-args", messageId: "tooMany", target: node })
+      );
+    }
+
+    const values: Type[] = [];
+    let tooFew = false;
+    for (let i = 0; i < declarations.length; i++) {
+      const declaration = declarations[i];
+      if (i < args.length) {
+        values.push(args[i]);
+      } else {
+        const declaredType = getTypeForNode(declaration)! as TemplateParameterType;
+        const defaultValue = getResolvedTypeParameterDefault(declaredType);
+        if (defaultValue) {
+          values.push(defaultValue);
+        } else {
+          tooFew = true;
+          values.push(errorType);
+        }
+      }
+    }
+
+    if (tooFew) {
+      program.reportDiagnostic(
+        createDiagnostic({
+          code: "invalid-template-args",
+          messageId: "tooFew",
+          target: node,
+        })
+      );
+    }
+
+    return values;
   }
 
   function checkTypeReferenceSymbol(
@@ -587,7 +642,7 @@ export function createChecker(program: Program): Checker {
 
     const symbolLinks = getSymbolLinks(sym);
     let baseType;
-    let args = node.kind === SyntaxKind.TypeReference ? node.arguments.map(getTypeForNode) : [];
+    const args = checkTypeReferenceArgs(node);
     if (
       sym.flags &
       (SymbolFlags.Model | SymbolFlags.Alias | SymbolFlags.Interface | SymbolFlags.Union)
@@ -635,35 +690,8 @@ export function createChecker(program: Program): Checker {
         }
 
         const templateParameters = decl.templateParameters;
-        if (args.length < templateParameters.length) {
-          let tooFew = false;
-          for (let i = args.length; i < templateParameters.length; i++) {
-            if (!templateParameters[i].default) {
-              tooFew = true;
-              args.push(errorType);
-            }
-          }
-
-          if (tooFew) {
-            program.reportDiagnostic(
-              createDiagnostic({
-                code: "invalid-template-args",
-                messageId: "tooFew",
-                target: node,
-              })
-            );
-          }
-        } else if (args.length > templateParameters.length) {
-          program.reportDiagnostic(
-            createDiagnostic({
-              code: "invalid-template-args",
-              messageId: "tooMany",
-              target: node,
-            })
-          );
-          args = args.slice(0, templateParameters.length);
-        }
-        baseType = instantiateTemplate(decl, args);
+        const instantiationArgs = checkTemplateInstantiationArgs(node, args, templateParameters);
+        baseType = instantiateTemplate(decl, instantiationArgs);
       }
     } else {
       // some other kind of reference
