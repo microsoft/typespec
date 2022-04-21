0e9519c504beeb3ae177354fce4dc6e405590f3a
Timothee Guerin <tiguerin@microsoft.com>
2022-04-21 15:37:37 -0700

    Add logic to publish

--- packages/internal-build-utils/src/cli.ts
+++ packages/internal-build-utils/src/cli.ts
@@ -1,5 +1,6 @@
 import yargs from "yargs";
 import { generateThirdPartyNotice } from "./generate-third-party-notice.js";
+import { bumpVersionsForPrerelease } from "./prerelease.js";
 
 main().catch((e) => {
   // eslint-disable-next-line no-console
@@ -17,5 +18,15 @@ async function main() {
       "Generate the third party notice",
       () => {},
       () => generateThirdPartyNotice()
-    );
+    )
+    .command(
+      "bump-version-preview <workspaceRoot>",
+      "Bump all package version for the preview",
+      (cmd) =>
+        cmd.positional("workspaceRoot", {
+          type: "string",
+          demandOption: true,
+        }),
+      (args) => bumpVersionsForPrerelease(args.workspaceRoot)
+    ).argv;
 }
