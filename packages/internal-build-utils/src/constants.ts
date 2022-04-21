e966efd30e552f4e84d5ae7224515199738ddd8e
Timothee Guerin <tiguerin@microsoft.com>
2022-04-21 08:16:10 -0700

    New `@cadl-lang/internal-build-utils` package for build helpers shared across pkgs (#465)

---
+++ packages/internal-build-utils/src/constants.ts
@@ -0,0 +1,6 @@
+export const vsMinimumVersion = "16.9";
+
+export const minimumDotnetVersion = {
+  major: 5,
+  minor: 0,
+};
---
+++ packages/internal-build-utils/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./common.js";
+export * from "./dotnet.js";
+export * from "./visualstudio.js";
+export * from "./watch.js";
