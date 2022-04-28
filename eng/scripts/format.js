c72e9d6e6e00794b8867b71a7ada2f648df830b8
Timothee Guerin <tiguerin@microsoft.com>
2022-04-28 14:42:27 -0700

    Add dotnet format to formatting

--- eng/scripts/format.js
+++ eng/scripts/format.js
@@ -1,2 +1,5 @@
+// @ts-check
+import { runDotnetFormat } from "../../packages/internal-build-utils/dist/src/index.js";
 import { runPrettier } from "./helpers.js";
 runPrettier("--write");
+await runDotnetFormat();
