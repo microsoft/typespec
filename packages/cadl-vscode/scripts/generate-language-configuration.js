a540b272777e928ffe934098011760dad0fe9be3
Timothee Guerin <tiguerin@microsoft.com>
2022-05-04 13:28:53 -0700

    languge-config in dist folder

--- packages/cadl-vscode/scripts/generate-language-configuration.js
+++ packages/cadl-vscode/scripts/generate-language-configuration.js
@@ -1,7 +1,11 @@
 // @ts-check
 import { CadlLanguageConfiguration } from "@cadl-lang/compiler";
-import { writeFile } from "fs/promises";
+import { mkdir, writeFile } from "fs/promises";
+import { dirname, join, resolve } from "path";
+import { fileURLToPath } from "url";
 
+export const root = resolve(dirname(fileURLToPath(import.meta.url)), "..");
+const distFolder = resolve(root, "dist");
 main().catch((err) => {
   console.error(err.stack);
   process.exit(1);
@@ -9,5 +13,6 @@ main().catch((err) => {
 
 async function main() {
   const content = JSON.stringify(CadlLanguageConfiguration, null, 2);
-  await writeFile("language-configuration.json", content);
+  await mkdir(distFolder, { recursive: true });
+  await writeFile(join(distFolder, "language-configuration.json"), content);
 }
