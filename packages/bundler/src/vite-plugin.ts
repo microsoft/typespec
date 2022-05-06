3ae46c3eab54bbaf47a970a05000fcafe5675eab
Timothee Guerin <tiguerin@microsoft.com>
2022-05-06 13:14:51 -0700

    .

--- packages/bundler/src/vite-plugin.ts
+++ packages/bundler/src/vite-plugin.ts
@@ -1,5 +1,5 @@
 import { resolve } from "path";
-import { Plugin } from "vite";
+import type { Plugin } from "vite";
 import { createCadlBundleFile } from "./bundler.js";
 
 export interface CadlBundlePluginOptions {
