ea695e146397562e7d4065c66d314e6ec6aa6c11
Timothee Guerin <tiguerin@microsoft.com>
2022-05-10 09:20:42 -0700

    Enable having an emitter with cadl files

--- packages/compiler/test/e2e/scenarios/scenarios.e2e.ts
+++ packages/compiler/test/e2e/scenarios/scenarios.e2e.ts
@@ -1,14 +1,15 @@
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import { createProgram, NodeHost, Program } from "../../../core/index.js";
+import { CompilerOptions } from "../../../core/options.js";
 import { expectDiagnosticEmpty, expectDiagnostics } from "../../../testing/expect.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const scenarioRoot = resolve(__dirname, "../../../../test/e2e/scenarios");
 
 describe("compiler: entrypoints", () => {
-  async function compileScenario(name: string): Promise<Program> {
-    return createProgram(NodeHost, resolve(scenarioRoot, name));
+  async function compileScenario(name: string, options: CompilerOptions = {}): Promise<Program> {
+    return createProgram(NodeHost, resolve(scenarioRoot, name), { ...options });
   }
 
   describe("compile library", () => {
@@ -28,6 +29,13 @@ describe("compiler: entrypoints", () => {
         code: "invalid-main",
         message: "Main file must either be a .cadl file or a .js file.",
       });
+    });
+
+    it.only("compile library with Cadl entrypoint and emitter", async () => {
+      const program = await compileScenario("emitter-with-cadl", {
+        emitters: ["@cadl-lang/test-emitter-with-cadl"],
+      });
+      expectDiagnosticEmpty(program.diagnostics);
     });
   });
 });
