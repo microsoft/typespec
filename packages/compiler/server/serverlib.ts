f80fe6b554d12f18a79947cda59fdbea50997ae0
Jg1255 <Josueg1255@gmail.com>
2022-07-05 12:40:35 -0400

    new changes to servlib.ts

--- packages/compiler/server/serverlib.ts
+++ packages/compiler/server/serverlib.ts
@@ -1,5 +1,3 @@
-import { not } from "ajv/dist/compile/codegen/index.js";
-import { start } from "repl";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import {
   CompletionItem,
@@ -94,7 +92,7 @@ export interface Server {
   getSemanticTokens(params: SemanticTokensParams): Promise<SemanticToken[]>;
   buildSemanticTokens(params: SemanticTokensParams): Promise<SemanticTokens>;
   checkChange(change: TextDocumentChangeEvent<TextDocument>): Promise<void>;
-  getFoldingRanges(getFoldingRanges: FoldingRangeParams) : Promise<FoldingRange[]>;
+  getFoldingRanges(getFoldingRanges: FoldingRangeParams): Promise<FoldingRange[]>;
   documentClosed(change: TextDocumentChangeEvent<TextDocument>): void;
   log(message: string, details?: any): void;
 }
@@ -416,32 +414,32 @@ export function createServer(host: ServerHost): Server {
     const ranges: FoldingRange[] = [];
     getRangesForNode(ast);
 
-    function getRangesForNode(node: Node){
-          if ("decorators" in node && node.decorators.length > 0) {
-            const startPos = node.decorators[0].pos
-            const endPos = node.decorators[node.decorators.length-1].end
-            const nodeEndPos = node.end
-            let startLine = file.getLineAndCharacterOfPosition(startPos).line;
-            let endLine = file.getLineAndCharacterOfPosition(endPos).line;
-            if (startLine !== endLine){
-              ranges.push({startLine,endLine});
-            }
-            startLine = file.getLineAndCharacterOfPosition(skipTrivia(file.text,endPos)).line;
-            endLine = file.getLineAndCharacterOfPosition(nodeEndPos).line;
-            if (startLine !== endLine){
-              ranges.push({startLine,endLine});
-            }
-            visitChildren(node,getRangesForNode);
-            return
-          }
-          const startPos = node.pos;
-          const endPos = node.end;
-          const startLine = file.getLineAndCharacterOfPosition(startPos).line;
-          const endLine = file.getLineAndCharacterOfPosition(endPos).line;
-          if (startLine !== endLine && node.kind !== SyntaxKind.CadlScript){
-            ranges.push({startLine,endLine});
-          }
-          visitChildren(node,getRangesForNode);
+    function getRangesForNode(node: Node) {
+      if ("decorators" in node && node.decorators.length > 0) {
+        const startPos = node.decorators[0].pos;
+        const endPos = node.decorators[node.decorators.length - 1].end;
+        const nodeEndPos = node.end;
+        let startLine = file.getLineAndCharacterOfPosition(startPos).line;
+        let endLine = file.getLineAndCharacterOfPosition(endPos).line;
+        if (startLine !== endLine) {
+          ranges.push({ startLine, endLine });
+        }
+        startLine = file.getLineAndCharacterOfPosition(skipTrivia(file.text, endPos)).line;
+        endLine = file.getLineAndCharacterOfPosition(nodeEndPos).line;
+        if (startLine !== endLine) {
+          ranges.push({ startLine, endLine });
+        }
+        visitChildren(node, getRangesForNode);
+        return;
+      }
+      const startPos = node.pos;
+      const endPos = node.end;
+      const startLine = file.getLineAndCharacterOfPosition(startPos).line;
+      const endLine = file.getLineAndCharacterOfPosition(endPos).line;
+      if (startLine !== endLine && node.kind !== SyntaxKind.CadlScript) {
+        ranges.push({ startLine, endLine });
+      }
+      visitChildren(node, getRangesForNode);
     }
     return ranges;
   }
--- packages/compiler/test/server/folding.test.ts
+++ packages/compiler/test/server/folding.test.ts
@@ -1,27 +1,20 @@
 import { deepStrictEqual } from "assert";
-import {
-  FoldingRange,
-} from "vscode-languageserver/node.js";
-import { createTestServerHost} from "../../testing/test-server-host.js";
+import { FoldingRange } from "vscode-languageserver/node.js";
+import { createTestServerHost } from "../../testing/test-server-host.js";
 
 describe("compiler: server: foldingRange", () => {
-
   it("testing for foldingRange with doc", async () => {
     const ranges = await getFoldingRanges(`@doc("Error")
     @doc("Foo")
     @doc("bar")
     @doc("FooBar")`);
-    deepStrictEqual(ranges,[ 
-      { startLine: 0, endLine: 3 },
-     ]);
+    deepStrictEqual(ranges, [{ startLine: 0, endLine: 3 }]);
   });
 
   it("testing for foldingRange with model", async () => {
     const ranges = await getFoldingRanges(`model Error {
       code: int32;}`);
-    deepStrictEqual(ranges,[ 
-      { startLine: 0, endLine: 1 },
-     ]);
+    deepStrictEqual(ranges, [{ startLine: 0, endLine: 1 }]);
   });
 
   it("testing for foldingRange with doc outside of model", async () => {
@@ -32,40 +25,38 @@ describe("compiler: server: foldingRange", () => {
       code: int32;
       message: string;
     }`);
-    deepStrictEqual(ranges,[ 
+    deepStrictEqual(ranges, [
       { startLine: 0, endLine: 2 },
-      { startLine: 3, endLine: 6 }
-     ]);
+      { startLine: 3, endLine: 6 },
+    ]);
   });
 
   it("testing for foldingRange with op", async () => {
     const ranges = await getFoldingRanges(`op delete(...PetId): {
       ...Response<200>;
     } | Error;`);
-    deepStrictEqual(ranges,     [ 
+    deepStrictEqual(ranges, [
       { startLine: 0, endLine: 2 },
       { startLine: 0, endLine: 2 },
       { startLine: 0, endLine: 2 },
-      { startLine: 0, endLine: 2 }
-     ]);
+      { startLine: 0, endLine: 2 },
+    ]);
   });
 
   it("testing for foldingRange with of namespace", async () => {
     const ranges = await getFoldingRanges(`namespace Foo {
     }`);
-    deepStrictEqual(ranges,     [ 
-      { startLine: 0, endLine: 1 }
-     ]);
+    deepStrictEqual(ranges, [{ startLine: 0, endLine: 1 }]);
   });
 
   it("testing for foldingRange with doc inside of namespace", async () => {
     const ranges = await getFoldingRanges(`namespace Foo {
       @doc("Delete Foo")
       @doc("Create Foo") }`);
-    deepStrictEqual(ranges,     [ 
+    deepStrictEqual(ranges, [
       { startLine: 0, endLine: 2 },
-      { startLine: 1, endLine: 2 }
-     ]);
+      { startLine: 1, endLine: 2 },
+    ]);
   });
 
   it("testing for foldingRange with doc outside of namespace", async () => {
@@ -73,24 +64,22 @@ describe("compiler: server: foldingRange", () => {
     @doc("Create Foo") 
     namespace Foo {
  }`);
-    deepStrictEqual(ranges,     [ 
+    deepStrictEqual(ranges, [
       { startLine: 0, endLine: 1 },
-      { startLine: 2, endLine: 3 }
-     ]);
+      { startLine: 2, endLine: 3 },
+    ]);
   });
 
   it("testing folding does not occur with one line", async () => {
     const ranges = await getFoldingRanges(`@doc("Delete Foo")`);
-    deepStrictEqual(ranges,[]);
+    deepStrictEqual(ranges, []);
   });
 
-async function getFoldingRanges(
-  source: string,
-): Promise<FoldingRange[]> {
-  const testHost = await createTestServerHost();
-  const textDocument = testHost.addOrUpdateDocument("test/test.cadl", source);
-  return await testHost.server.getFoldingRanges({
-    textDocument
-  });
-}
+  async function getFoldingRanges(source: string): Promise<FoldingRange[]> {
+    const testHost = await createTestServerHost();
+    const textDocument = testHost.addOrUpdateDocument("test/test.cadl", source);
+    return await testHost.server.getFoldingRanges({
+      textDocument,
+    });
+  }
 });
