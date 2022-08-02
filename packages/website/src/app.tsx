243fce2c848a5751927e6fda1ecc2d93e02c522d
Descartes <t-dtuyishime@microsoft.com>
2022-08-02 16:28:32 -0700

    Add BrowserRouter and routes to pages/containers

--- packages/website/src/app.tsx
+++ packages/website/src/app.tsx
@@ -1,5 +1,21 @@
 import { FunctionComponent } from "react";
+import {Home, Blog, Community, Docs, Download, Playground, PageNotFound} from "./containers";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import "./app.css"; 
 
 export const App: FunctionComponent = () => {
-  return <>Cadl Website</>;
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/download" element={<Download/>}/>
+      <Route path="/docs" element={<Docs/>}/>
+      <Route path="/blog" element={<Blog/>}/>
+      <Route path="/play" element={<Playground/>}/>
+      <Route path="/community" element={<Community/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+  </BrowserRouter>
+  );  
 };
+export default App
