3da3bcbbf8925443f583a17349f55bed1b97325f
Descartes <t-dtuyishime@microsoft.com>
2022-08-02 18:59:35 -0700

    Remove unnecessary div.

--- packages/website/src/containers/playground/playground.tsx
+++ packages/website/src/containers/playground/playground.tsx
@@ -1,18 +1,15 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent} from "react";
 import { Footer, Navbar } from "../../components";
-//import { Grid } from  'react-loader-spinner'; 
 import "./playground.css";
 
 export const Playground: FunctionComponent = () => {
-  //const [load, setLoad] = useState(true);
-  //<Grid height="100" width="100" color= 'var(--main-color)' ariaLabel='loading'/>
   return (
     <div className="playground">
-      <div></div><Navbar/>
+      <Navbar/>
       <div className="play-iframe">
-      <iframe title="Cadl Playground" src="https://cadlplayground.z22.web.core.windows.net/" height="100%"></iframe>
+      <iframe title="Cadl Playground" src="https://cadlplayground.z22.web.core.windows.net/"/>
       </div>
-      <div><Footer/></div>
+      <Footer/>
     </div>
   );
 };
