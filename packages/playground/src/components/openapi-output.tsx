99273cb8add89da80af11fb3daa4f936f7a43c81
Descartes <t-dtuyishime@microsoft.com>
2022-08-03 11:01:59 -0700

    add openapi-output

---
+++ packages/playground/src/components/openapi-output.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent, useCallback, useState } from "react";
+import { OutputEditor } from "./cadl-editor";
+import { PlaygroundManifest } from "../manifest";
+import { SwaggerUI } from "./swagger-ui";
+export interface OpenAPIOutputProps {
+  content: string;
+}
+
+export const OpenAPIOutput: FunctionComponent<OpenAPIOutputProps> = (props)=> {
+  const[selected, setSelected] = useState<"raw"|"swagger-ui">("raw");
+  const options = [
+    {},
+    {},
+  ];
+  const handleSelected = useCallback(
+    () => { setSelected(selected === "raw"?("swagger-ui"):("raw"));
+    },
+    [selected],
+  );
+  return(
+    <>
+    {PlaygroundManifest.enableSwaggerUI? (
+    <select className="output-dropdown" onChange={handleSelected} value={selected}>
+      <option>OpenApi</option>
+      <option >Swagger-UI</option>
+    </select>
+    ):(<></>)}
+    
+    {selected === "raw"? (<OutputEditor value={props.content}/>):(<SwaggerUI spec={props.content} />)}
+    </>);
+}
+
