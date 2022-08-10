e25ec19409be8105ccc428b920b7357fcf231b81
dtuyishime <106712287+dtuyishime@users.noreply.github.com>
2022-08-10 15:52:17 -0700

    Add Swagger-UI Visualization to playground (#716)
    * Add swagger-ui and @types/swagger-ui dependency.
    
    * Add swagger-ui type.
    
    * Add swagger-ui visualization tab
    
    * Add loadSwaggerUIforFile function to load swggerui
    
    * Add condition that call load swagger function.
    
    * Add condition to load swaggerui function w/ tabid
    
    * Add Swaggerui output in the content of output file
    
    * Create swaggerUI output and sync OApi json changes
    
    * Removed space
    
    * Add swagger-ui and @types/swagger-ui dependencies
    
    * Update swagger-ui.tsx
    
    Replace <any | null> with <any>.
    
    * Replace any | null with any.
    
    * Add swagger-ui packages to devDependencies.
    
    * Update package.json
    
    Add Swagger-UI packages to devDependencies.
    
    * Update package.json
    
    * Add OpenAPI/ Swagger-ui output component.
    
    * Remove Swagger-ui spefic functions and call-ups.
    
    * Add enableSwaggerUI in playgroundManifest.
    
    * Add condition to decide OpenAPI/SwaggerUI dropdown
    
    * Add Launch.json to gitignore.
    
    * Set value on the options from selected variable.

---
+++ packages/playground/src/components/swagger-ui.tsx
@@ -0,0 +1,21 @@
+import { FunctionComponent, useEffect, useRef } from "react";
+import CreateSwaggerUI from "swagger-ui";
+export interface SwaggerUIProps {
+  spec: string;
+}
+export const SwaggerUI: FunctionComponent<SwaggerUIProps> = (props) => {
+  const uiRef = useRef(null);
+  const uiInstance = useRef<any>(null);
+  useEffect(() => {
+    if (uiInstance.current === null) {
+      uiInstance.current = CreateSwaggerUI({
+        domNode: uiRef.current,
+        spec: JSON.parse(props.spec),
+      });
+    } else {
+      uiInstance.current.specActions.updateSpec(props.spec);
+    }
+  }, [props.spec]);
+
+  return <div ref={uiRef}></div>;
+};
--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -2,4 +2,5 @@ export interface PlaygroundConfig {
   defaultEmitter: string;
   libraries: string[];
   samples: Record<string, string>;
+  enableSwaggerUI: boolean;
 }
