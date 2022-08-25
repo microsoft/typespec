531d86bd346a72b6e4d28a476eb4824c6fae6b77
Brian Terlson <brian.terlson@microsoft.com>
2022-08-25 17:19:15 -0500

    Add a discriminated union sample (#919)

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -10,9 +10,10 @@ export default definePlaygroundViteConfig({
     "@cadl-lang/openapi3",
   ],
   samples: {
-    Http: "samples/http.cadl",
-    "Rest framework": "samples/rest.cadl",
-    "Versioned Rest framework": "samples/versioning.cadl",
+    "API versioning": "samples/versioning.cadl",
+    "Discriminated unions": "samples/unions.cadl",
+    "Http service": "samples/http.cadl",
+    "REST framework": "samples/rest.cadl",
   },
   enableSwaggerUI: true,
 });
