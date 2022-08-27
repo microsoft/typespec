f868ef828a33131c0ee396ad7d0de56330e69d94
Timothee Guerin <tiguerin@microsoft.com>
2022-08-27 07:36:45 -0700

    UI testing

--- packages/playground/e2e/playwright.config.ts
+++ packages/playground/e2e/playwright.config.ts
@@ -27,6 +27,10 @@ const config: PlaywrightTestConfig = {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
   testMatch: "*.e2e.ts",
 };
