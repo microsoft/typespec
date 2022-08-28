0fa0fc0cd179d56715d6f120eab686f83105a301
Timothee Guerin <tiguerin@microsoft.com>
2022-08-27 20:34:12 -0700

    Cadl syntax highlighitn

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -1,9 +1,16 @@
 // @ts-check
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxhighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
+const cadlPrismDefinition = require("./cadl-prism-lang.js");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxhighlightPlugin, {
+    init: ({ Prism }) => {
+      Prism.languages.cadl = cadlPrismDefinition;
+    },
+  });
 
   return {
     markdownTemplateEngine: "njk",
