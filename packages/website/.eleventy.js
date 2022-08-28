d72fb25d0d8cc7b267e12232e7485a954c76ed66
Timothee Guerin <tiguerin@microsoft.com>
2022-08-28 08:09:30 -0700

    .

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -3,8 +3,10 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxhighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 const cadlPrismDefinition = require("./cadl-prism-lang.js");
 
+const prNumber = process.env["SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"];
+
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(syntaxhighlightPlugin, {
     init: ({ Prism }) => {
@@ -20,5 +22,6 @@ module.exports = (eleventyConfig) => {
       input: "src",
       output: "dist",
     },
+    pathPrefix: prNumber ? `/prs/${prNumber}/` : "/",
   };
 };
