412b93eb775f7a12bef5127d3c7882436caeec0a
Timothee Guerin <tiguerin@microsoft.com>
2022-08-30 20:19:57 -0700

    Load swagger UI async

--- packages/playground/src/components/swagger-ui.tsx
+++ packages/playground/src/components/swagger-ui.tsx
@@ -1,22 +1,34 @@
-import { FunctionComponent, useEffect, useRef } from "react";
-import CreateSwaggerUI from "swagger-ui";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+
 export interface SwaggerUIProps {
   spec: string;
 }
+
 export const SwaggerUI: FunctionComponent<SwaggerUIProps> = (props) => {
   const uiRef = useRef(null);
   const uiInstance = useRef<any>(null);
+  const [swaggerUI, setSwaggerUILib] = useState<{createSwaggerUI: typeof import("swagger-ui")} | undefined>(
+    undefined
+  );
+  useEffect(() => {
+    void import("swagger-ui").then((lib) => {
+      setSwaggerUILib({createSwaggerUI: lib.default});
+    });
+  }, [setSwaggerUILib]);
 
   useEffect(() => {
+    if (swaggerUI === undefined) {
+      return;
+    }
     if (uiInstance.current === null) {
-      uiInstance.current = CreateSwaggerUI({
+      uiInstance.current = swaggerUI.createSwaggerUI({
         domNode: uiRef.current,
         spec: props.spec as any,
       });
     } else {
       uiInstance.current.specActions.updateSpec(props.spec);
     }
-  }, [props.spec]);
+  }, [swaggerUI, props.spec]);
 
   return <div className="swagger-ui-container" ref={uiRef}></div>;
 };
