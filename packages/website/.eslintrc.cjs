623a47a59e31c307981815f211a48278bc360167
Timothee Guerin <tiguerin@microsoft.com>
2022-09-02 09:05:07 -0400

    Add missing scripts

---
+++ packages/website/.eslintrc.cjs
@@ -0,0 +1,9 @@
+require("@cadl-lang/eslint-config-cadl/patch/modern-module-resolution");
+
+module.exports = {
+  extends: ["@cadl-lang/eslint-config-cadl"],
+  parserOptions: { tsconfigRootDir: __dirname },
+  env: {
+    browser: true,
+  },
+};
