40fef155ef9d5f8141051a1972b3b5203c327429
Timothee Guerin <tiguerin@microsoft.com>
2022-09-08 19:26:12 -0400

    Remove some exclusion

--- eng/scripts/cspell.js
+++ eng/scripts/cspell.js
@@ -1,8 +1,10 @@
 // @ts-check
 import { resolve } from "path";
 import { repoRoot } from "./helpers.js";
-import { run } from "../../packages/internal-build-utils/dist/src/index.js";
-export const cspell = resolve(repoRoot, "packages/internal-build-utils/node_modules/.bin/cspell");
+import { run, xplatCmd } from "../../packages/internal-build-utils/dist/src/index.js";
+export const cspell = xplatCmd(
+  resolve(repoRoot, "packages/internal-build-utils/node_modules/.bin/cspell")
+);
 
 await run(
   cspell,
