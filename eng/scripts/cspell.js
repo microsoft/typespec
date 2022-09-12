7b55eeb08f9f464525dae21685adffc77df53ca9
Timothee Guerin <tiguerin@microsoft.com>
2022-09-12 08:37:45 -0400

    Fix prettier import format

--- eng/scripts/cspell.js
+++ eng/scripts/cspell.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { resolve } from "path";
-import { repoRoot } from "./helpers.js";
 import { run, xplatCmd } from "../../packages/internal-build-utils/dist/src/index.js";
+import { repoRoot } from "./helpers.js";
 export const cspell = xplatCmd(
   resolve(repoRoot, "packages/internal-build-utils/node_modules/.bin/cspell")
 );
