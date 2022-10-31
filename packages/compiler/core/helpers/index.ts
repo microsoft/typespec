3821377da79a548f84b97b559a939716333c857d
Nick Guerrera <nicholg@microsoft.com>
2022-10-31 14:40:57 -0500

    Remove findStatementsIn helper

--- packages/compiler/core/helpers/index.ts
+++ packages/compiler/core/helpers/index.ts
@@ -1,4 +1,3 @@
-export * from "./ast-utils.js";
 export { DiscriminatedUnion, getDiscriminatedUnion } from "./discriminator-utils.js";
 export * from "./operation-utils.js";
 export * from "./projected-names-utils.js";
