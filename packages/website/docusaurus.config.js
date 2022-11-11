a2cd67ebf8318c38671dd2a6e168e60a376da938
Timothee Guerin <tiguerin@microsoft.com>
2022-11-11 14:49:47 -0800

    Atom one theme

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -1,7 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
+// const lightCodeTheme = require("prism-react-renderer/themes/github");
+const lightCodeTheme = require("./themes/prism/atom-one-light.js");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
