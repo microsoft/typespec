21275197565806d282d42a18386ab19e4f645833
Timothee Guerin <tiguerin@microsoft.com>
2022-12-02 10:55:25 -0800

    Fix swagger ui

--- packages/playground/src/components/swagger-ui.tsx
+++ packages/playground/src/components/swagger-ui.tsx
@@ -4,41 +4,6 @@ export interface SwaggerUIProps {
 }
 
 export const SwaggerUI: FunctionComponent<SwaggerUIProps> = (props) => {
-  // const uiRef = useRef(null);
-  // const uiInstance = useRef<any>(null);
-  // const [swaggerUI, setSwaggerUILib] = useState<
-  //   { createSwaggerUI: typeof import("swagger-ui") } | undefined
-  // >(undefined);
-  // useEffect(() => {
-  //   void import("swager-ui" as any).then((lib) => {
-  //     setSwaggerUILib({ createSwaggerUI: lib.default });
-  //   });
-  // }, [setSwaggerUILib]);
-
-  // useEffect(() => {
-  //   if (swaggerUI === undefined) {
-  //     return;
-  //   }
-  //   if (uiInstance.current === null) {
-  //     uiInstance.current = swaggerUI.createSwaggerUI({
-  //       domNode: uiRef.current,
-  //       spec: {},
-  //     });
-  //   }
-  //   uiInstance.current.specActions.updateSpec(props.spec);
-  // }, [uiRef.current, swaggerUI, props.spec]);
-
-  // return (
-  //   <div
-  //     css={{
-  //       width: "100%",
-  //       height: "100%",
-  //       overflow: "auto",
-  //     }}
-  //     ref={uiRef}
-  //   ></div>
-  // );
-
   const uiRef = useRef(null);
   const uiInstance = useRef<any>(null);
   const [swaggerUI, setSwaggerUILib] = useState<
@@ -50,29 +15,6 @@ export const SwaggerUI: FunctionComponent<SwaggerUIProps> = (props) => {
     });
   }, [setSwaggerUILib]);
 
-  // useEffect(() => {
-  //   if (swaggerUI === undefined) {
-  //     return;
-  //   }
-  //   if (uiInstance.current === null) {
-  //     uiInstance.current = swaggerUI.createSwaggerUI({
-  //       domNode: uiRef.current,
-  //       spec: {},
-  //     });
-  //   }
-  //   uiInstance.current.specActions.updateSpec(props.spec);
-  // }, [uiRef.current, swaggerUI, props.spec]);
-
-  // return (
-  //   <div
-  //     css={{
-  //       width: "100%",
-  //       height: "100%",
-  //       overflow: "auto",
-  //     }}
-  //     ref={uiRef}
-  //   ></div>
-  // );
 
   if (swaggerUI === undefined) {
     return null;
