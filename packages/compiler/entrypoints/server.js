fc90d0b0fa14fe6f90c24d6b473f9b0c5869c8c3
Timothee Guerin <tiguerin@microsoft.com>
2022-12-09 11:34:42 -0800

    Fix cli

---
+++ packages/compiler/entrypoints/server.js
@@ -0,0 +1,5 @@
+/**
+ * File serving as an entrypoint to resolve a local cadl install from a global install.
+ * DO NOT MOVE or this will create a breaking change for user of global cli.
+ */
+import "./dist/server/server.js";
