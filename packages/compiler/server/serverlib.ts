f12746b9c0930659401838dcf9f4ab2f8f05f1af
Timothee Guerin <tiguerin@microsoft.com>
2023-01-03 11:42:12 -0800

    Fix: Import completion (#1483)

--- packages/compiler/server/serverlib.ts
+++ packages/compiler/server/serverlib.ts
@@ -80,20 +80,17 @@ import {
   DecoratorExpressionNode,
   Diagnostic as CadlDiagnostic,
   DiagnosticTarget,
-  DocContent,
   IdentifierNode,
   Node,
   SourceFile,
   StringLiteralNode,
   SyntaxKind,
   TextRange,
-  Type,
 } from "../core/types.js";
 import { doIO, getNormalizedRealPath, getSourceFileKindFromExt, loadFile } from "../core/util.js";
-import { getDoc } from "../lib/decorators.js";
-import { resolveCompletions } from "./completion.js";
+import { resolveCompletion } from "./completion.js";
 import { getSymbolStructure } from "./symbol-structure.js";
-import { getTypeSignature } from "./type-signature.js";
+import { getParameterDocumentation, getTypeDetails } from "./type-details.js";
 
 export interface ServerHost {
   compilerHost: CompilerHost;
@@ -267,7 +264,6 @@ export function createServer(host: ServerHost): Server {
       completionProvider: {
         resolveProvider: false,
         triggerCharacters: [".", "@", "/"],
-        allCommitCharacters: [".", ",", ";", "("],
       },
       semanticTokensProvider: {
         full: true,
@@ -596,87 +592,6 @@ export function createServer(host: ServerHost): Server {
     }
   }
 
-  /**
-   * Get the detailed documentation of a type.
-   * @param program The program
-   */
-  function getTypeDetails(
-    program: Program,
-    type: Type,
-    options = {
-      includeSignature: true,
-      includeParameterTags: true,
-    }
-  ): string {
-    // BUG: https://github.com/microsoft/cadl/issues/1348
-    // We've already resolved to a Type and lost the alias node so we don't show doc comments on aliases or alias signatures, currently.
-
-    if (type.kind === "Intrinsic") {
-      return "";
-    }
-    const lines = [];
-    if (options.includeSignature) {
-      lines.push(getTypeSignature(type));
-    }
-    const doc = getTypeDocumentation(program, type);
-    if (doc) {
-      lines.push(doc);
-    }
-    for (const doc of type?.node?.docs ?? []) {
-      for (const tag of doc.tags) {
-        if (tag.tagName.sv === "param" && !options.includeParameterTags) {
-          continue;
-        }
-        lines.push(
-          //prettier-ignore
-          `_@${tag.tagName.sv}_${"paramName" in tag ? ` \`${tag.paramName.sv}\`` : ""} —\n${getDocContent(tag.content)}`
-        );
-      }
-    }
-    return lines.join("\n\n");
-  }
-
-  function getTypeDocumentation(program: Program, type: Type) {
-    const docs: string[] = [];
-
-    // Add /** ... */ developer docs
-    for (const d of type?.node?.docs ?? []) {
-      docs.push(getDocContent(d.content));
-    }
-
-    // Add @doc(...) API docs
-    const apiDocs = getDoc(program, type);
-    if (apiDocs) {
-      docs.push(apiDocs);
-    }
-
-    return docs.join("\n\n");
-  }
-
-  function getParameterDocumentation(program: Program, type: Type): Map<string, string> {
-    const map = new Map<string, string>();
-    for (const d of type?.node?.docs ?? []) {
-      for (const tag of d.tags) {
-        if (tag.kind === SyntaxKind.DocParamTag) {
-          map.set(tag.paramName.sv, getDocContent(tag.content));
-        }
-      }
-    }
-    return map;
-  }
-
-  function getDocContent(content: readonly DocContent[]) {
-    const docs = [];
-    for (const node of content) {
-      compilerAssert(
-        node.kind === SyntaxKind.DocText,
-        "No other doc content node kinds exist yet. Update this code appropriately when more are added."
-      );
-      docs.push(node.text);
-    }
-    return docs.join("");
-  }
-
   async function getHover(params: HoverParams): Promise<Hover> {
     const docString = await compile(params.textDocument, (program, document, file) => {
       const id = getNodeAtPosition(file, document.offsetAt(params.position));
@@ -820,7 +735,6 @@ export function createServer(host: ServerHost): Server {
 
     return getLocations(sym?.declarations);
   }
-
   async function complete(params: CompletionParams): Promise<CompletionList> {
     const completions: CompletionList = {
       isIncomplete: false,
@@ -828,9 +742,17 @@ export function createServer(host: ServerHost): Server {
     };
     await compile(params.textDocument, async (program, document, file) => {
       const node = getCompletionNodeAtPosition(file, document.offsetAt(params.position));
-      await resolveCompletions(program, file, completions, node);
-    });
 
+      await resolveCompletion(
+        {
+          program,
+          file,
+          completions,
+          params,
+        },
+        node
+      );
+    });
     return completions;
   }
 
