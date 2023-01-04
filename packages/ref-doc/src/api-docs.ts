920bc86d8e07ed148b09cb24faef3f1e2d9e5451
Timothee Guerin <tiguerin@microsoft.com>
2023-01-04 09:28:18 -0800

    Cleaner

--- packages/ref-doc/src/api-docs.ts
+++ packages/ref-doc/src/api-docs.ts
@@ -7,6 +7,7 @@ export async function generateJsApiDocs(libraryPath: string, outputDir: string) 
 
   const markdownPluginOptions: any = {
     entryDocument: "index.md",
+    readme: "none",
   };
 
   app.bootstrap({
