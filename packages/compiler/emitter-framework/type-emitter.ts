18e6a3a9c25756bd86f90554466dd776396fd3e8
Brian Terlson <brian.terlson@microsoft.com>
2023-01-23 16:21:49 -0800

    make TypeEmitter abstract

--- packages/compiler/emitter-framework/type-emitter.ts
+++ packages/compiler/emitter-framework/type-emitter.ts
@@ -144,7 +144,7 @@ export type EmitterOutput<T> = EmitEntity<T> | Placeholder<T> | T;
  * And we set reference context for the Person model, Pet will be emitted twice,
  * once without context and once with the reference context.
  */
-export class TypeEmitter<T> {
+export abstract class TypeEmitter<T> {
   /**
    * @private
    *
