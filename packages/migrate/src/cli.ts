ea285ee17ccf967a611a98aa21d9baadaaddbb1a
Allen Zhang <allenzhang@live.com>
2023-03-07 10:57:02 -0800

    finishing up yargs

--- packages/migrate/src/cli.ts
+++ packages/migrate/src/cli.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-console */
-import { NodePackage, resolvePath } from "@typespec/compiler";
+import { MANIFEST, NodePackage, resolvePath } from "@typespec/compiler";
+import * as fs from "fs";
 import { readFile } from "fs/promises";
 import * as semver from "semver";
+import yargs from "yargs";
 import { migrationConfigurations } from "./migration-config.js";
 import {
   migrateFileRename,
@@ -13,46 +15,73 @@ import {
 import { MigrationKind } from "./migration-types.js";
 import { findTypeSpecFiles } from "./utils.js";
 
+interface Options {
+  path: string;
+  tspVersion: string;
+}
+
 async function main() {
-  let changesMake = false;
-  const workingFolder = process.cwd();
+  console.log(`TypeSpec migration tool v${MANIFEST.version}\n`);
 
-  // Locate current package.json
-  const pkgFile = resolvePath(workingFolder, "package.json");
-  const packageJson: NodePackage = JSON.parse(await readFile(pkgFile, "utf-8"));
+  const cliOptions: Options = await yargs(process.argv.slice(2))
+    .option("path", {
+      alias: "p",
+      describe: "Path to the input file. Default to current folder.",
+      type: "string",
+      default: process.cwd(),
+    })
+    .option("tspVersion", {
+      alias: "t",
+      describe: "Specifies the version of TypeSpec doc. Default will be loading from package.json",
+      type: "string",
+      default: "",
+    }).argv;
 
-  // Locate current compiler version
-  const CadlCompiler = "@cadl-lang/compiler";
-  const TypeSpecCompiler = "@typespec/compiler";
-  let packageTypeSpecVersion: string;
-  if (
-    packageJson?.devDependencies !== undefined &&
-    packageJson?.devDependencies[CadlCompiler] !== undefined
-  ) {
-    packageTypeSpecVersion = packageJson.devDependencies[CadlCompiler];
-  } else if (
-    packageJson?.devDependencies !== undefined &&
-    packageJson?.devDependencies[TypeSpecCompiler] !== undefined
-  ) {
-    packageTypeSpecVersion = packageJson.devDependencies[TypeSpecCompiler];
-  } else {
-    console.error("Unable to find TypeSpec compiler version in package.json.");
+  const PackageJsonFile = "package.json";
+  if (cliOptions.tspVersion.length === 0) {
+    // Locate current package.json
+    const pkgFile = resolvePath(cliOptions.path, PackageJsonFile);
+    const packageJson: NodePackage = JSON.parse(await readFile(pkgFile, "utf-8"));
+
+    // Locate current compiler version
+    const CadlCompiler = "@cadl-lang/compiler";
+    const TypeSpecCompiler = "@typespec/compiler";
+    if (
+      packageJson?.devDependencies !== undefined &&
+      packageJson?.devDependencies[CadlCompiler] !== undefined
+    ) {
+      cliOptions.tspVersion = packageJson.devDependencies[CadlCompiler];
+    } else if (
+      packageJson?.devDependencies !== undefined &&
+      packageJson?.devDependencies[TypeSpecCompiler] !== undefined
+    ) {
+      cliOptions.tspVersion = packageJson.devDependencies[TypeSpecCompiler];
+    } else {
+      console.error("Unable to find TypeSpec compiler version in package.json.");
+      return;
+    }
+  }
+
+  if (!fs.existsSync(cliOptions.path)) {
+    console.error(`Path not found. ${cliOptions.path}`);
     return;
   }
 
+  let changesMake = false;
+
   // Iterate thru migration configuration and invoke
-  console.log(`Current Typespec version ${packageTypeSpecVersion}.`);
+  console.log(`Current Typespec version ${cliOptions.tspVersion}.`);
   const stepKeys = Object.keys(migrationConfigurations);
   for (const key of stepKeys) {
-    if (semver.gt(key, packageTypeSpecVersion)) {
+    if (semver.gt(key, cliOptions.tspVersion)) {
       console.log(
-        `Migration step found to upgrade from ${packageTypeSpecVersion} to ${key}. Migrating...`
+        `Migration step found to upgrade from ${cliOptions.tspVersion} to ${key}. Migrating...`
       );
 
       for (const migrationStep of migrationConfigurations[key]) {
         switch (migrationStep.kind) {
           case MigrationKind.Content:
-            const files = await findTypeSpecFiles(workingFolder);
+            const files = await findTypeSpecFiles(cliOptions.path);
             const result = await migrateTypeSpecFiles(files, migrationStep);
             // If migration has been performed log status
             if (result.fileChanged.length > 0) {
@@ -64,10 +93,11 @@ async function main() {
             }
             break;
           case MigrationKind.FileRename:
-            const srcFiles = await findTypeSpecFiles(workingFolder);
+            const srcFiles = await findTypeSpecFiles(cliOptions.path);
             await migrateFileRename(srcFiles, migrationStep);
             break;
           case MigrationKind.PackageVersionUpdate:
+            const pkgFile = resolvePath(cliOptions.path, PackageJsonFile);
             await migratePackageVersion(pkgFile, migrationStep);
             break;
           default:
@@ -75,10 +105,10 @@ async function main() {
         }
       }
 
-      packageTypeSpecVersion = key;
+      cliOptions.tspVersion = key;
     } else {
       console.log(
-        `${packageTypeSpecVersion} is already greater than or equal to ${key}. Migration step skipped...`
+        `${cliOptions.tspVersion} is already greater than or equal to ${key}. Migration step skipped...`
       );
     }
   }
