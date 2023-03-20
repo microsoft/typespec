36c504d166375ebbcf0e52e50088f41fa31b9b31
Timothee Guerin <tiguerin@microsoft.com>
2023-03-20 09:15:11 -0700

    Revert algolia to use cadl index

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -157,7 +157,7 @@ const config = {
         // cspell:disable-next-line
         appId: "WQXW45O51C",
         apiKey: "9bfd71f94d516117e3022e788f2cc83c",
-        indexName: "typespec",
+        indexName: "cadl",
       },
     }),
 };
