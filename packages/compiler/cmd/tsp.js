fbf8ce18155385708342d1b3d6158d3590210f89
Nick Guerrera <nicholg@microsoft.com>
2023-05-19 14:35:50 -0500

    Add runtime check for minimum node version

--- packages/compiler/cmd/tsp.js
+++ packages/compiler/cmd/tsp.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
-import { runScript } from "../dist/cmd/runner.js";
-await runScript("entrypoints/cli.js", "dist/core/cli/cli.js");
+
+// WARNING: Be careful editing this file. For the node version runtime
+// check below to work, we must avoid using JS features that would trigger a
+// failure at parse time before it runs.
+//
+// We deliberately do not use `const/let`, `?.`, and backticked string
+// templates, and we obfuscate the dynamic import to avoid triggering a
+// reserved word.
+//
+// This has been tested back to Node.js v0.12.18
+
+var nodeVersion = Number(process.versions && process.versions.node && process.versions.node.split(".")[0]);
+if (!nodeVersion || nodeVersion < 16) {
+  console.log("error: TypeSpec requires Node.js version 16.0 or higher.");
+  process.exit(1);
+}
+
+// Wrap dynamic import to get around reserved word use
+function _import(module) {
+  var f = new Function("module", "return import(module)");
+  return f(module);
+}
+
+_import("../dist/cmd/runner.js").then(
+  function (r) {
+    r.runScript("entrypoints/cli.js", "dist/core/cli/cli.js")
+  });
