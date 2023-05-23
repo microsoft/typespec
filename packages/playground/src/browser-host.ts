65f51921c6a896ae79dd9714e263751fbafa8722
Timothee Guerin <tiguerin@microsoft.com>
2023-05-22 20:17:41 -0700

    Don't need importmap shim anymore

--- packages/playground/src/browser-host.ts
+++ packages/playground/src/browser-host.ts
@@ -4,7 +4,7 @@ import {
   getSourceFileKindFromExt,
   resolvePath,
 } from "@typespec/compiler";
-import { importShim } from "./core.js";
+import { importLibrary } from "./core.js";
 import { PlaygroundManifest } from "./manifest.js";
 
 export interface BrowserHost extends CompilerHost {}
@@ -19,7 +19,7 @@ export async function createBrowserHost(): Promise<BrowserHost> {
   const libsToLoad = PlaygroundManifest.libraries;
 
   for (const libName of libsToLoad) {
-    const { _TypeSpecLibrary_ } = (await importShim(libName)) as any;
+    const { _TypeSpecLibrary_ } = (await importLibrary(libName)) as any;
     for (const [key, value] of Object.entries<any>(_TypeSpecLibrary_.typespecSourceFiles)) {
       virtualFs.set(`/test/node_modules/${libName}/${key}`, value);
     }
--- packages/playground/src/core.ts
+++ packages/playground/src/core.ts
@@ -3,20 +3,16 @@ export async function importTypeSpecCompiler(): Promise<typeof import("@typespec
   if (cachedCompiler === undefined) {
     // We need to do this so the compiler loaded is the same as the one loaded by the bundled libraries.
     const name = "@typespec/compiler";
-    cachedCompiler = (await importShim(name)) as any;
+    cachedCompiler = (await importLibrary(name)) as any;
   }
 
   return cachedCompiler;
 }
 
 /**
- * Dynamic imports with importmap unfortunately do not work on all browsers.
- * Safari and Firefox lack support https://caniuse.com/import-maps.
- *
- * A polyfill for importmap is loaded but required calling importShim instead of the native import https://github.com/guybedford/es-module-shims
  * @param name Import name.
  * @returns Promise with the loaded module.
  */
-export async function importShim(name: string): Promise<unknown> {
-  return (window as any).importShim(name);
+export async function importLibrary(name: string): Promise<unknown> {
+  return import(name);
 }
