8478b7e626d5bcb1797239595833f447e8527966
Travis Prescott <trpresco@microsoft.com>
2023-06-02 07:21:03 -0700

    Support .env file for docusaurus build.

--- packages/website/.scripts/docusaurus-build.mjs
+++ packages/website/.scripts/docusaurus-build.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 // @ts-check
 
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { run } from "../../../eng/scripts/helpers.js";
+
+loadDotenv();
 
 if (process.env.TYPESPEC_SKIP_DOCUSAURUS_BUILD?.toLowerCase() === "true") {
   console.log("Skipping docusaurus build: TYPESPEC_SKIP_DOCUSAURUS_BUILD=true");
@@ -9,3 +14,11 @@ if (process.env.TYPESPEC_SKIP_DOCUSAURUS_BUILD?.toLowerCase() === "true") {
 }
 
 run("docusaurus", ["build"]);
+
+function loadDotenv() {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const dotenvPath = path.resolve(dirname, "../../../.env");
+  dotenv.config({
+    path: dotenvPath,
+  });
+}
