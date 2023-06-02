5d6cf76ab2a6fbfa23f02e20827892a1fb56f5a4
Brian Terlson <brian.terlson@microsoft.com>
2023-06-02 15:29:23 -0700

    Add descriptions to options json schema

--- packages/json-schema/src/lib.ts
+++ packages/json-schema/src/lib.ts
@@ -5,7 +5,7 @@ export type Int64Strategy = "string" | "number";
 
 export interface JSONSchemaEmitterOptions {
   /**
-   * If the content should be serialized as YAML or JSON.
+   * Serialize the schema as either yaml or json.
    * @default yaml, it not specified infer from the `output-file` extension
    */
   "file-type"?: FileType;
@@ -36,10 +36,33 @@ const EmitterOptionsSchema: JSONSchemaType<JSONSchemaEmitterOptions> = {
   type: "object",
   additionalProperties: false,
   properties: {
-    "file-type": { type: "string", enum: ["yaml", "json"], nullable: true },
-    "int64-strategy": { type: "string", enum: ["string", "number"], nullable: true },
-    bundleId: { type: "string", nullable: true },
-    emitAllModels: { type: "boolean", nullable: true },
+    "file-type": {
+      type: "string",
+      enum: ["yaml", "json"],
+      nullable: true,
+      description: "Serialize the schema as either yaml or json.",
+    },
+    "int64-strategy": {
+      type: "string",
+      enum: ["string", "number"],
+      nullable: true,
+      description: `How to handle 64 bit integers on the wire. Options are:
+
+* string: serialize as a string (widely interoperable)
+* number: serialize as a number (not widely interoperable)`,
+    },
+    bundleId: {
+      type: "string",
+      nullable: true,
+      description:
+        "When provided, bundle all the schemas into a single json schema document with schemas under $defs. The provided id is the id of the root document and is also used for the file name.",
+    },
+    emitAllModels: {
+      type: "boolean",
+      nullable: true,
+      description:
+        "When true, emit all model declarations to JSON Schema without requiring the @jsonSchema decorator.",
+    },
   },
   required: [],
 };
