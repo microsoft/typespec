452cee956642d6cbe4a24a954c8c5ecca2208e89
Travis Prescott <tjprescott@users.noreply.github.com>
2023-06-02 14:53:52 -0700

    Support .env file for docusaurus build (#2007)
    * Support .env file for docusaurus build.
    
    * Code review feedback.

--- packages/website/.scripts/docusaurus-build.mjs
+++ packages/website/.scripts/docusaurus-build.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 // @ts-check
 
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { run } from "../../../eng/scripts/helpers.js";
+
+loadDotenv();
 
 if (process.env.TYPESPEC_SKIP_DOCUSAURUS_BUILD?.toLowerCase() === "true") {
   console.log("Skipping docusaurus build: TYPESPEC_SKIP_DOCUSAURUS_BUILD=true");
@@ -9,3 +14,19 @@ if (process.env.TYPESPEC_SKIP_DOCUSAURUS_BUILD?.toLowerCase() === "true") {
 }
 
 run("docusaurus", ["build"]);
+
+function loadDotenv() {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const searchPaths = ["../../../.env", "../../../../.env"];
+  for (const searchPath of searchPaths) {
+    const dotenvPath = path.resolve(dirname, searchPath);
+    console.log(`Searching dotEnvPath: ${dotenvPath}`);
+    const result = dotenv.config({
+      path: dotenvPath,
+    });
+    if (result.parsed !== undefined) {
+      console.log(`Loaded dotenv: ${dotenvPath}`);
+      return;
+    }
+  }
+}
