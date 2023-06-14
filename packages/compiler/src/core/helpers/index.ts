19e5b445ee2b6df9c2132105eee77ebf52511a57
Timothee Guerin <tiguerin@microsoft.com>
2023-06-14 10:15:39 -0700

    Missing getTypeLocationContext

--- packages/compiler/src/core/helpers/index.ts
+++ packages/compiler/src/core/helpers/index.ts
@@ -2,6 +2,6 @@ export { DiscriminatedUnion, getDiscriminatedUnion } from "./discriminator-utils
 export * from "./operation-utils.js";
 export * from "./path-interpolation.js";
 export * from "./projected-names-utils.js";
-export { getTypeDeclerationContext } from "./type-declaration-context.js";
+export { getTypeLocationContext } from "./type-location-context.js";
 export * from "./type-name-utils.js";
 export * from "./usage-resolver.js";
