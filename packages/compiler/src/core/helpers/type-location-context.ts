8ac02df1cf12b43333231b291bba4d53047f3ff5
Timothee Guerin <tiguerin@microsoft.com>
2023-06-14 10:17:20 -0700

    missing file

---
+++ packages/compiler/src/core/helpers/type-location-context.ts
@@ -0,0 +1,12 @@
+import { getSourceLocation } from "../index.js";
+import { Program } from "../program.js";
+import { LocationContext, Type } from "../types.js";
+
+export function getTypeLocationContext(program: Program, type: Type): LocationContext {
+  const sourceLocation = getSourceLocation(type);
+
+  if (sourceLocation.isSynthetic) {
+    return { type: "synthetic" };
+  }
+  return program.getSourceFileLocationContext(sourceLocation.file);
+}
