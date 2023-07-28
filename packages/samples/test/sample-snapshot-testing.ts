f4b2e75142cf33f792c14451bcbe36e336c046ec
Timothee Guerin <tiguerin@microsoft.com>
2023-07-27 18:05:35 -0700

    Separate in other util

---
+++ packages/samples/test/sample-snapshot-testing.ts
@@ -0,0 +1,145 @@
+import {
+  CompilerHost,
+  NodeHost,
+  compile,
+  getDirectoryPath,
+  joinPaths,
+  resolveCompilerOptions,
+  resolvePath,
+} from "@typespec/compiler";
+import { expectDiagnosticEmpty } from "@typespec/compiler/testing";
+import { fail, ok, strictEqual } from "assert";
+import { readdirSync } from "fs";
+import { mkdir, readFile, readdir, writeFile } from "fs/promises";
+
+const shouldUpdateSnapshots = process.env.RECORD === "true";
+
+export interface SampleSnapshotTestOptions {
+  /**  Sample root directory. */
+  sampleDir: string;
+
+  /** Output directory for snapshots. */
+  outputDir: string;
+  /** Folders to exclude from testing. */
+  exclude?: string[];
+}
+
+export function defineSampleSnaphotTests(config: SampleSnapshotTestOptions) {
+  resolveSamples(config).forEach((samples) => defineSampleSnaphotTest(config, samples));
+}
+
+function defineSampleSnaphotTest(config: SampleSnapshotTestOptions, sample: Sample) {
+  it(sample.name, async () => {
+    const host = createSampleSnapshotTestHost(config);
+
+    const [options, diagnostics] = await resolveCompilerOptions(host, {
+      entrypoint: sample.entrypoint,
+    });
+    expectDiagnosticEmpty(diagnostics);
+
+    const emit = options.emit;
+    if (emit === undefined || emit.length === 0) {
+      fail(
+        `No emitters configured for sample "${sample.name}". Make sure the  config at: "${options.config}" is correct.`
+      );
+    }
+
+    const program = await compile(host, sample.entrypoint, {
+      ...options,
+      outputDir: "/out/",
+    });
+    expectDiagnosticEmpty(program.diagnostics);
+
+    const outputDir = resolvePath(config.outputDir, sample.name);
+
+    if (shouldUpdateSnapshots) {
+      try {
+        await host.rm(outputDir, { recursive: true });
+      } catch (e) {}
+      await host.mkdirp(outputDir);
+
+      for (const [filename, content] of host.outputs.entries()) {
+        const snapshotPath = resolvePath(outputDir, filename);
+
+        await mkdir(getDirectoryPath(snapshotPath), { recursive: true });
+        await writeFile(snapshotPath, content);
+      }
+    } else {
+      for (const [filename, content] of host.outputs.entries()) {
+        const snapshotPath = resolvePath(outputDir, filename);
+        const existingContent = await readFile(snapshotPath);
+        strictEqual(content, existingContent.toString());
+      }
+
+      for (const filename of await readFilesInDirRecursively(outputDir)) {
+        ok(
+          host.outputs.has(filename),
+          `Snapshot for "${filename}" is missing. Run with RECORD=true to regenerate it.`
+        );
+      }
+    }
+  });
+}
+
+interface SampleSnapshotTestHost extends CompilerHost {
+  outputs: Map<string, string>;
+}
+function createSampleSnapshotTestHost(config: SampleSnapshotTestOptions): SampleSnapshotTestHost {
+  const outputs = new Map<string, string>();
+  return {
+    ...NodeHost,
+    outputs,
+    mkdirp: (path: string) => Promise.resolve(path),
+    writeFile: async (path: string, content: string) => {
+      if (path.startsWith("/out/")) {
+        outputs.set(path.slice("/out/".length), content);
+      }
+    },
+  };
+}
+async function readFilesInDirRecursively(dir: string): Promise<string[]> {
+  const files = [];
+  const entries = await readdir(dir, { withFileTypes: true });
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      for (const file of await readFilesInDirRecursively(resolvePath(dir, entry.name))) {
+        files.push(resolvePath(entry.name, file));
+      }
+    } else {
+      files.push(entry.name);
+    }
+  }
+  return files;
+}
+
+interface Sample {
+  name: string;
+  /** Sample folder */
+  fullPath: string;
+  entrypoint: string;
+}
+
+function resolveSamples(config: SampleSnapshotTestOptions): Sample[] {
+  const samples: Sample[] = [];
+  const excludes = new Set(config.exclude);
+  walk("");
+  return samples;
+
+  async function walk(relativeDir: string) {
+    if (excludes.has(relativeDir)) {
+      return;
+    }
+    const fullDir = joinPaths(config.sampleDir, relativeDir);
+    for (const entry of readdirSync(fullDir, { withFileTypes: true })) {
+      if (entry.isDirectory()) {
+        walk(joinPaths(relativeDir, entry.name));
+      } else if (relativeDir && entry.name === "main.tsp") {
+        samples.push({
+          name: relativeDir,
+          fullPath: joinPaths(config.sampleDir, relativeDir),
+          entrypoint: joinPaths(config.sampleDir, relativeDir, entry.name),
+        });
+      }
+    }
+  }
+}
