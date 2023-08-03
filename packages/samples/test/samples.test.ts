04c62b3d230d22396203337dddbe0094a432624b
Timothee Guerin <tiguerin@microsoft.com>
2023-08-03 08:02:28 -0700

    Move samples to specs/ dir

--- packages/samples/test/samples.test.ts
+++ packages/samples/test/samples.test.ts
@@ -5,18 +5,11 @@ import { defineSampleSnaphotTests } from "../src/sample-snapshot-testing.js";
 const excludedSamples = [
   // fails compilation by design to demo language server
   "local-typespec",
-
-  // no actual samples in these dirs
-  "node_modules",
-  "dist",
-  "scratch",
-  "scripts",
-  "test",
-  ".rush",
 ];
 
-const samplesRoot = resolvePath(fileURLToPath(import.meta.url), "../../..");
-const rootOutputDir = resolvePath(samplesRoot, "test/output");
+const pkgRoot = resolvePath(fileURLToPath(import.meta.url), "../../..");
+const samplesRoot = resolvePath(pkgRoot, "specs");
+const rootOutputDir = resolvePath(pkgRoot, "test/output");
 
 describe("TypeSpec Samples", () => {
   defineSampleSnaphotTests({
