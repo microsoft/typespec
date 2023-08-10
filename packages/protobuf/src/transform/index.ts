1b8b27e3d916d2672139ae7992e585908b0beca1
Will Temple <witemple@microsoft.com>
2023-08-10 16:12:21 -0400

    [protobuf] Add `omit-unreachable-types` (#2281)
    This PR tweaks the way that models are included in output specifications
    when they are not referenced by operations and adds an option for
    controlling this behavior.
    
    The option is `omit-unreachable-types` after the OpenAPI v3 emitter
    which uses the same name, and the behavior in the protobuf emitter is
    similar. If a message is expliclty decorated with `@message` or is
    referenced direclty or indirectly by an operation/method, it will be
    emitted. Otherwise, it will be emitted if (1) it is a direct child of a
    `package` namespace AND (2) ALL of its fields are annotated with
    `@field` AND (3) the `omit-unreachable-types` option is not set to true.
    
    Closes #1879
    
    ---------
    
    Co-authored-by: Will Temple <will@wtemple.net>

--- packages/protobuf/src/transform/index.ts
+++ packages/protobuf/src/transform/index.ts
@@ -58,7 +58,7 @@ export function createProtobufEmitter(
 ): (outDir: string, options: ProtobufEmitterOptions) => Promise<void> {
   return async function doEmit(outDir, options) {
     // Convert the program to a set of proto files.
-    const files = tspToProto(program);
+    const files = tspToProto(program, options);
 
     if (!program.compilerOptions.noEmit && !options?.noEmit && !program.hasError()) {
       for (const file of files) {
@@ -85,12 +85,14 @@ export function createProtobufEmitter(
  *
  * This is the meat of the emitter.
  */
-function tspToProto(program: Program): ProtoFile[] {
+function tspToProto(program: Program, emitterOptions: ProtobufEmitterOptions): ProtoFile[] {
   const packages = new Set<Namespace>(
     program.stateMap(state.package).keys() as Iterable<Namespace>
   );
 
   const serviceInterfaces = [...(program.stateSet(state.service) as Set<Interface>)];
+
+  const declaredMessages = [...(program.stateSet(state.message) as Set<Model>)];
 
   const declarationMap = new Map<Namespace, ProtoTopLevelDeclaration[]>(
     [...packages].map((p) => [p, []])
@@ -218,11 +220,23 @@ function tspToProto(program: Program): ProtoFile[] {
    * @returns an array of declarations
    */
   function addDeclarationsOfPackage(namespace: Namespace) {
-    const models = [...namespace.models.values()];
+    const eagerModels = new Set([
+      ...declaredMessages.filter((m) => isDeclaredInNamespace(m, namespace)),
+    ]);
 
-    // Eagerly visit all models in the namespace.
-    for (const model of models) {
-      // Don't eagerly visit externs
+    if (!emitterOptions["omit-unreachable-types"]) {
+      // Add all models in the namespace that have `@field` on every property.
+      for (const model of namespace.models.values()) {
+        if (
+          [...model.properties.values()].every((p) => program.stateMap(state.fieldIndex).has(p)) ||
+          program.stateSet(state.message).has(model)
+        ) {
+          eagerModels.add(model);
+        }
+      }
+    }
+
+    for (const model of eagerModels) {
       if (
         // Don't eagerly visit externs
         !program.stateMap(state.externRef).has(model) &&
