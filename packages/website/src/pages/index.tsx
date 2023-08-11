50d0e116613dc5730cd0a5fdd3b8a2b0015bbd47
Timothee Guerin <tiguerin@microsoft.com>
2023-08-11 14:14:37 -0700

    better no flashing

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,7 +1,7 @@
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import { useColorMode } from "@docusaurus/theme-common";
 import { FluentProvider, webDarkTheme, webLightTheme } from "@fluentui/react-components";
 import Layout from "@theme/Layout";
-import { useEffect, useState } from "react";
 import { HomeContent } from "../components/homepage/homepage";
 
 export default function Home() {
@@ -14,14 +14,18 @@ export default function Home() {
 
 const HomeWithFluent = () => {
   const { colorMode } = useColorMode();
-  const [_, setInitialized] = useState(false);
+  // const [_, setInitialized] = useState(false);
 
-  useEffect(() => {
-    setInitialized(true); // Doing this to force the theme to update.
-  }, []);
+  // useEffect(() => {
+  //   setInitialized(true); // Doing this to force the theme to update.
+  // }, []);
   return (
-    <FluentProvider theme={colorMode === "dark" ? webDarkTheme : webLightTheme}>
-      <HomeContent />
-    </FluentProvider>
+    <BrowserOnly>
+      {() => (
+        <FluentProvider theme={colorMode === "dark" ? webDarkTheme : webLightTheme}>
+          <HomeContent />
+        </FluentProvider>
+      )}
+    </BrowserOnly>
   );
 };
