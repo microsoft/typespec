13a784b1e4476a5ddd9d0bbeca3a4a7c0c790196
Timothee Guerin <tiguerin@microsoft.com>
2023-10-04 11:46:01 -0700

    Cleaner

--- packages/playground/definitions/manifest.d.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-import { PlaygroundConfig } from "../src/vite/types.js";
-
-const config: PlaygroundConfig = {} as any;
-export default config;
