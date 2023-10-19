1eba8e090dee031ef65423fdc81dda3a73f46d8a
Timothee Guerin <tiguerin@microsoft.com>
2023-10-19 15:16:23 -0700

    hero

---
+++ packages/website/src/components/hero-tabs/hero-tabs.tsx
@@ -0,0 +1,39 @@
+import { SelectTabData, SelectTabEvent, Tab, TabList } from "@fluentui/react-components";
+import Lottie from "lottie-react";
+import { useCallback, useState } from "react";
+import style from "./hero-tabs.module.css";
+
+export interface HeroProps {
+  tabs: HeroTab[];
+}
+
+interface HeroTab {
+  value: string;
+  content: string;
+}
+export const HeroTabs = ({ tabs }: HeroProps) => {
+  const [selected, setSelected] = useState<string>(tabs[0].value);
+  const handleTabSelection = useCallback(
+    (event: SelectTabEvent, data: SelectTabData) => {
+      setSelected(data.value);
+    },
+    [setSelected]
+  );
+  const content = tabs.find((tab) => tab.value === selected)?.content;
+  return (
+    <>
+      <div className={style["illustration-card"]}>
+        <Lottie animationData={content} loop={true} />
+      </div>
+      <TabList selectedValue={selected} onTabSelect={handleTabSelection}>
+        {tabs.map((tab) => {
+          return (
+            <Tab key={tab.value} value={tab.value}>
+              {tab.value}
+            </Tab>
+          );
+        })}
+      </TabList>
+    </>
+  );
+};
