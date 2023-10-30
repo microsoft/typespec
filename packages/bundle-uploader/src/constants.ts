801680e116e12f3b8960cdecd0d3d9988ff63262
Timothee Guerin <tiguerin@microsoft.com>
2023-10-30 08:49:08 -0700

    Local

--- packages/bundle-uploader/src/constants.ts
+++ packages/bundle-uploader/src/constants.ts
@@ -2,5 +2,5 @@ import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
 export const pkgRoot = resolve(dirname(fileURLToPath(import.meta.url)), "../..");
-export const storageAccountName = "tsppackages";
+export const storageAccountName = "typespec";
 export const pkgsContainer = "pkgs";
