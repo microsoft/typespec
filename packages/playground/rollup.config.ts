043ab929256ba7919fc249c682a14868914e704f
Timothee Guerin <tiguerin@microsoft.com>
2023-11-01 09:52:45 -0700

    Use rollup in playground (#2584)
    Use rollup to bundle the playground UI library package.
    
    This allows to bundle the css with watch.

---
+++ packages/playground/rollup.config.ts
@@ -0,0 +1,54 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "fs";
+import { dirname, resolve } from "path";
+import { defineConfig } from "rollup";
+// To handle css files
+import postcss from "rollup-plugin-postcss";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const packageJson = JSON.parse(readFileSync(resolve(__dirname, "package.json")).toString());
+const dependencies = Object.keys(packageJson.dependencies);
+const external = [
+  ...dependencies,
+  "swagger-ui-react/swagger-ui.css",
+  "@emotion/react/jsx-runtime",
+  "@typespec/bundler/vite",
+  "react-dom/client",
+  "vite",
+  "@vitejs/plugin-react",
+];
+export default defineConfig([
+  {
+    input: {
+      index: "src/index.ts",
+      "react/index": "src/react/index.ts",
+      "react/viewers": "src/react/viewers.tsx",
+      "vite/index": "src/vite/index.ts",
+    },
+    treeshake: false,
+    output: {
+      dir: "dist",
+      format: "esm",
+      sourcemap: true,
+      // preserveModules: true,
+      // preserveModulesRoot: ".",
+      exports: "named",
+    },
+    plugins: [
+      (commonjs as any)(),
+      (typescript as any)({
+        tsconfig: "./tsconfig.build.json",
+        declaration: true,
+        declarationDir: "./dist",
+        sourceMap: true,
+        inlineSources: true,
+      }),
+      (postcss as any)({
+        extract: true,
+      }),
+    ],
+    external,
+  },
+]);
