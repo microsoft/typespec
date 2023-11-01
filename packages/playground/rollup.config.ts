706f04147efdafd124478ac4012e9fcb0b3c536c
Timothee Guerin <tiguerin@microsoft.com>
2023-11-01 09:20:48 -0700

    fix warning

--- packages/playground/rollup.config.ts
+++ packages/playground/rollup.config.ts
@@ -1,10 +1,14 @@
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "fs";
+import { dirname, resolve } from "path";
 import { defineConfig } from "rollup";
 // To handle css files
 import postcss from "rollup-plugin-postcss";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-import packageJson from "./package.json" assert { type: "json" };
+const packageJson = JSON.parse(readFileSync(resolve(__dirname, "package.json")).toString());
 const dependencies = Object.keys(packageJson.dependencies);
 const external = [
   ...dependencies,
