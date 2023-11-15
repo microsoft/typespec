38aa3200025f8ed3123b31081148efa2829c6a76
Timothee Guerin <tiguerin@microsoft.com>
2023-11-15 13:03:36 -0800

    Add ref docs for linter rules (#2668)
    Add some generated doc for the linter capability of a library.
    
    Each linter rule can also provide a url pointing to the the full
    documentation where we can get a more detailed set of examples of what
    is good and what is bad.
    
    Example for http linter 
    
    https://tspwebsitepr.z22.web.core.windows.net/prs/2668/next/standard-library/http/reference/linter.html
    
    
    
    ![image](https://github.com/microsoft/typespec/assets/1031227/81c2dddc-fa0f-40dd-bad7-9d193f764c55)
    
    
    ![image](https://github.com/microsoft/typespec/assets/1031227/36652cd5-418b-43b9-bd9a-aff046cbe538)
    
    ---------
    
    Co-authored-by: Mark Cowlishaw <markcowl@microsoft.com>

--- packages/tspd/src/ref-doc/emitters/markdown.ts
+++ packages/tspd/src/ref-doc/emitters/markdown.ts
@@ -1,15 +1,18 @@
 import { resolvePath } from "@typespec/compiler";
 import { readFile } from "fs/promises";
+import { stringify } from "yaml";
 import {
   DecoratorRefDoc,
   EmitterOptionRefDoc,
   EnumRefDoc,
   ExampleRefDoc,
   InterfaceRefDoc,
+  LinterRuleRefDoc,
   ModelRefDoc,
   NamedTypeRefDoc,
   NamespaceRefDoc,
   OperationRefDoc,
+  ReferencableElement,
   ScalarRefDoc,
   TemplateParameterRefDoc,
   TypeSpecRefDoc,
@@ -20,6 +23,7 @@ import {
   MarkdownDoc,
   codeblock,
   inlinecode,
+  link,
   renderMarkdowDoc,
   section,
   table,
@@ -56,6 +60,10 @@ export async function renderReadme(refDoc: TypeSpecRefDoc, projectRoot: string) 
     content.push(renderer.emitterUsage(refDoc));
   }
 
+  if (refDoc.linter) {
+    content.push(renderer.linterUsage(refDoc));
+  }
+
   if (refDoc.namespaces.some((x) => x.decorators.length > 0)) {
     content.push(section("Decorators", renderer.decoratorsSection(refDoc, { includeToc: true })));
   }
@@ -85,7 +93,7 @@ export class MarkdownRenderer {
     return inlinecode(item.name);
   }
 
-  anchorId(item: NamedTypeRefDoc): string {
+  anchorId(item: ReferencableElement): string {
     return `${item.name.toLowerCase().replace(/ /g, "-")}`;
   }
 
@@ -233,7 +241,7 @@ export class MarkdownRenderer {
     });
   }
 
-  toc(items: NamedTypeRefDoc[], filename?: string) {
+  toc(items: ReferencableElement[], filename?: string) {
     return items.map(
       (item) => ` - [${inlinecode(item.name)}](${filename ?? ""}#${this.anchorId(item)})`
     );
@@ -269,5 +277,38 @@ export class MarkdownRenderer {
       content.push(option.doc);
     }
     return section("Emitter options", content);
+  }
+
+  linterUsage(refDoc: TypeSpecRefDoc) {
+    if (refDoc.linter === undefined) {
+      return [];
+    }
+    const setupExample = stringify({
+      linter: refDoc.linter.ruleSets
+        ? { extends: [refDoc.linter.ruleSets[0].name] }
+        : { rules: {} },
+    });
+    return section("Linter", [
+      section("Usage", ["Add the following in `tspconfig.yaml`:", codeblock(setupExample, "yaml")]),
+      refDoc.linter.ruleSets
+        ? section("RuleSets", ["Available ruleSets:", this.toc(refDoc.linter.ruleSets)])
+        : [],
+      section("Rules", this.linterRuleToc(refDoc.linter.rules)),
+    ]);
+  }
+
+  linterRuleToc(rules: LinterRuleRefDoc[]) {
+    return table([
+      ["Name", "Description"],
+      ...rules.map((rule) => {
+        const name = inlinecode(rule.name);
+        const nameCell = rule.rule.url ? link(name, this.linterRuleLink(rule.rule.url)) : name;
+        return [nameCell, rule.rule.description];
+      }),
+    ]);
+  }
+
+  linterRuleLink(url: string) {
+    return url;
   }
 }
