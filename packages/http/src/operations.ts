c5d9db3de0a62f2726a75f195d1c4a98a96b6634
Travis Prescott <trpresco@microsoft.com>
2023-11-16 13:36:41 -0800

    Code review feedback.

--- packages/http/src/operations.ts
+++ packages/http/src/operations.ts
@@ -119,24 +119,19 @@ export function getAllRoutes(
 
 export function reportIfNoRoutes(program: Program, routes: HttpOperation[]) {
   if (routes.length === 0) {
-    const namespaceCounts = new Map<Namespace, number>();
     navigateProgram(program, {
       namespace: (namespace) => {
-        namespaceCounts.set(namespace, namespace.operations.size);
+        if (namespace.operations.size > 0) {
+          reportDiagnostic(program, {
+            code: "no-service-found",
+            format: {
+              namespace: namespace.name,
+            },
+            target: namespace,
+          });
+        }
       },
     });
-    // if there is any namespace with namespaceCounts > 0, then report the diagnostic
-    for (const [namespace, count] of namespaceCounts) {
-      if (count > 0) {
-        reportDiagnostic(program, {
-          code: "no-service-found",
-          format: {
-            namespace: namespace.name,
-          },
-          target: namespace,
-        });
-      }
-    }
   }
 }
 
