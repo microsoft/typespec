a7ce090791059bffeba3121b9de3b408372ed80e
Timothee Guerin <tiguerin@microsoft.com>
2023-12-05 08:23:08 -0800

    test

--- packages/website/src/theme/typespec-lang-prism.ts
+++ packages/website/src/theme/typespec-lang-prism.ts
@@ -33,8 +33,39 @@ const lang = {
     alias: "property",
   },
 
+  // TEST swift regex to see if they cause issue ___ REMOVE ___
+  'string-literal': [
+		// https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html
+		{
+			pattern: RegExp(
+				/(^|[^"#])/.source
+				+ '(?:'
+				// single-line string
+				+ /"(?:\\(?:\((?:[^()]|\([^()]*\))*\)|\r\n|[^(])|[^\\\r\n"])*"/.source
+				+ '|'
+				// multi-line string
+				+ /"""(?:\\(?:\((?:[^()]|\([^()]*\))*\)|[^(])|[^\\"]|"(?!""))*"""/.source
+				+ ')'
+				+ /(?!["#])/.source
+			),
+			lookbehind: true,
+			greedy: true,
+			inside: {
+				'interpolation': {
+					pattern: /(\\\()(?:[^()]|\([^()]*\))*(?=\))/,
+					lookbehind: true,
+					inside: null // see below
+				},
+				'interpolation-punctuation': {
+					pattern: /^\)|\\\($/,
+					alias: 'punctuation'
+				},
+				'punctuation': /\\(?=[\r\n])/,
+				'string': /[\s\S]+/
+			}
+		},
+
   string: [
-    // https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html
     {
       pattern: new RegExp(
         /(^|[^"#])/.source +
