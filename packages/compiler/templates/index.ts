f8c62cd0aa69d6a90033102512f73a8c757b8b7d
Timothee Guerin <tiguerin@microsoft.com>
2023-12-06 12:23:01 -0800

    Add eslint and prettier

--- packages/compiler/templates/index.ts
+++ packages/compiler/templates/index.ts
@@ -2,6 +2,7 @@ import { resolvePath } from "../src/index.js";
 import { InitTemplate } from "../src/init/init-template.js";
 import { MANIFEST } from "../src/manifest.js";
 
+import { readdir } from "fs/promises";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -9,6 +10,39 @@ const templateDir = resolvePath(dirname(fileURLToPath(import.meta.url)), "..", "
 
 function localFile(templateName: string, path: string) {
   return { path: resolvePath(templateDir, templateName, path), destination: path };
+}
+
+function isEnoentError(e: unknown): e is { code: "ENOENT" } {
+  return typeof e === "object" && e !== null && "code" in e;
+}
+
+async function readFilesInDirRecursively(dir: string): Promise<string[]> {
+  let entries;
+  try {
+    entries = await readdir(dir, { withFileTypes: true });
+  } catch (e) {
+    if (isEnoentError(e)) {
+      return [];
+    } else {
+      throw new Error(`Failed to read dir "${dir}"\n Error: ${e}`);
+    }
+  }
+  const files: string[] = [];
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      for (const file of await readFilesInDirRecursively(resolvePath(dir, entry.name))) {
+        files.push(resolvePath(entry.name, file));
+      }
+    } else {
+      files.push(entry.name);
+    }
+  }
+  return files;
+}
+
+async function localDir(templateName: string) {
+  const files = await readFilesInDirRecursively(resolvePath(templateDir, templateName));
+  return files.map((f) => localFile(templateName, f));
 }
 
 export const builtInTemplates: Record<string, InitTemplate> = {
@@ -33,15 +67,6 @@ export const builtInTemplates: Record<string, InitTemplate> = {
     compilerVersion: MANIFEST.version,
     libraries: [],
     config: undefined,
-    files: [
-      localFile("emitter-ts", "package.json"),
-      localFile("emitter-ts", "tsconfig.json"),
-      localFile("emitter-ts", "src/index.ts"),
-      localFile("emitter-ts", "src/lib.ts"),
-      localFile("emitter-ts", "src/emitter.ts"),
-      localFile("emitter-ts", "src/testing/index.ts"),
-      localFile("emitter-ts", "test/test-host.ts"),
-      localFile("emitter-ts", "test/hello.test.ts"),
-    ],
+    files: await localDir("emitter-ts"),
   },
 };
