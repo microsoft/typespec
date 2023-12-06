585a0920109b8d2329389b8221f5190c9c22a0b1
Timothee Guerin <tiguerin@microsoft.com>
2023-12-06 12:39:33 -0800

    respect no emit

--- packages/compiler/templates/emitter-ts/src/emitter.ts
+++ packages/compiler/templates/emitter-ts/src/emitter.ts
@@ -1,8 +1,10 @@
 import { EmitContext, emitFile, resolvePath } from "@typespec/compiler";
 
 export async function $onEmit(context: EmitContext) {
-  await emitFile(context.program, {
-    path: resolvePath(context.emitterOutputDir, "output.txt"),
-    content: "Hello world\n",
-  });
+  if (!context.program.compilerOptions.noEmit) {
+    await emitFile(context.program, {
+      path: resolvePath(context.emitterOutputDir, "output.txt"),
+      content: "Hello world\n",
+    });
+  }
 }
