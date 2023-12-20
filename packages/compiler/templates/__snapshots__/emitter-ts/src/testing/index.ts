940d83afc2258c304ad328fd357264a17601a844
Timothee Guerin <tiguerin@microsoft.com>
2023-12-20 14:35:47 -0800

    Fix snapshot

--- packages/compiler/templates/__snapshots__/emitter-ts/src/testing/index.ts
+++ packages/compiler/templates/__snapshots__/emitter-ts/src/testing/index.ts
@@ -2,7 +2,7 @@ import { resolvePath } from "@typespec/compiler";
 import { createTestLibrary, TypeSpecTestLibrary } from "@typespec/compiler/testing";
 import { fileURLToPath } from "url";
 
-export const TestLibrary: TypeSpecTestLibrary = createTestLibrary({
+export const EmitterTsTestLibrary: TypeSpecTestLibrary = createTestLibrary({
   name: "emitter-ts",
   packageRoot: resolvePath(fileURLToPath(import.meta.url), "../../../../"),
 });
