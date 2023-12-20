3220a606ab6c14979891eed5b576ba6b4ab04846
Timothee Guerin <tiguerin@microsoft.com>
2023-12-20 10:39:04 -0800

    Provide naming utils

--- packages/compiler/src/init/file-templating.ts
+++ packages/compiler/src/init/file-templating.ts
@@ -1,9 +1,11 @@
+import { camelCase, kebabCase, pascalCase } from "change-case";
 import Mustache from "mustache";
 import { InitTemplate } from "./init-template.js";
 import { ScaffoldingConfig } from "./scaffold.js";
 
 export type FileTemplatingContext = Omit<InitTemplate, "libraries"> &
   ScaffoldingConfig & {
+    naming: NamingUtils;
     /**
      * NormalizeVersion function replaces `-` with `_`.
      */
@@ -20,6 +22,12 @@ export type FileTemplatingContext = Omit<InitTemplate, "libraries"> &
     normalizePackageName: () => (text: string, render: any) => string;
   };
 
+export interface NamingUtils {
+  readonly camelCase: () => (text: string, render: (...args: any) => string) => string;
+  readonly pascalCase: () => (text: string, render: (...args: any) => string) => string;
+  readonly kebabCase: () => (text: string, render: (...args: any) => string) => string;
+}
+
 export function createFileTemplatingContext(config: ScaffoldingConfig): FileTemplatingContext {
   return {
     ...config.template,
@@ -27,12 +35,24 @@ export function createFileTemplatingContext(config: ScaffoldingConfig): FileTemp
     normalizeVersion,
     toLowerCase,
     normalizePackageName,
+    naming,
   };
 }
 
 export function render(content: string, context: FileTemplatingContext): string {
   return Mustache.render(content, context);
 }
+
+const naming: NamingUtils = {
+  camelCase: createNamingUtils(camelCase),
+  kebabCase: createNamingUtils(kebabCase),
+  pascalCase: createNamingUtils(pascalCase),
+};
+
+function createNamingUtils(fn: (text: string) => string) {
+  return () => (text: string, render: (...args: any) => string) => fn(render(text));
+}
+
 const normalizeVersion = function () {
   return function (text: string, render: any): string {
     return render(text).replaceAll("-", "_");
