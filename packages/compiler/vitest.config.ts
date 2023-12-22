53c016ad98c076eb2bc8fc00838acb99d68ba349
Timothee Guerin <tiguerin@microsoft.com>
2023-12-21 17:25:19 -0800

    Some cleanup

--- packages/compiler/vitest.config.ts
+++ packages/compiler/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    watch: false,
+
     include: ["test/**/*.test.ts"],
   },
 });
--- packages/openapi3/vitest.config.ts
+++ packages/openapi3/vitest.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    watch: false,
   },
 });
