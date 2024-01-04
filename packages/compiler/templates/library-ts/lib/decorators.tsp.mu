8c80e0727a5608640238b9609c4c411c92f6e10b
Timothee Guerin <tiguerin@microsoft.com>
2024-01-04 10:20:54 -0800

    Code review comments

--- packages/compiler/templates/library-ts/lib/decorators.tsp.mu
+++ packages/compiler/templates/library-ts/lib/decorators.tsp.mu
@@ -5,6 +5,7 @@ using TypeSpec.Reflection;
 namespace {{#casing.pascalCase}}{{name}}{{/casing.pascalCase}};
 
 /**
+ * __Example Decorator__
  * Provide an alternate name for an operation.
  * @param name The alternate name.
  */
--- packages/compiler/templates/library-ts/src/decorators.ts
+++ packages/compiler/templates/library-ts/src/decorators.ts
@@ -1,9 +1,15 @@
 import { DecoratorContext, Operation, Program } from "@typespec/compiler";
-import { createStateSymbol, reportDiagnostic } from "./lib.js";
+import { StateKeys, reportDiagnostic } from "./lib.js";
 
 export const namespace = "{{#casing.pascalCase}}{{name}}{{/casing.pascalCase}}";
 
-const alternateNameKey = createStateSymbol("alternateName");
+/**
+ * __Example implementation of the `@alternateName` decorator.__
+ *
+ * @param context Decorator context.
+ * @param target Decorator target. Must be an operation.
+ * @param name Alternate name.
+ */
 export function $alternateName(context: DecoratorContext, target: Operation, name: string) {
   if (name === "banned") {
     reportDiagnostic(context.program, {
@@ -12,9 +18,16 @@ export function $alternateName(context: DecoratorContext, target: Operation, nam
       format: { name },
     });
   }
-  context.program.stateMap(alternateNameKey).set(target, name);
+  context.program.stateMap(StateKeys.alternateName).set(target, name);
 }
 
+/**
+ * __Example accessor for  the `@alternateName` decorator.__
+ *
+ * @param program TypeSpec program.
+ * @param target Decorator target. Must be an operation.
+ * @returns Altenate name if provided on the given operation or undefined
+ */
 export function getAlternateName(program: Program, target: Operation): string | undefined {
-  return program.stateMap(alternateNameKey).get(target);
+  return program.stateMap(StateKeys.alternateName).get(target);
 }
