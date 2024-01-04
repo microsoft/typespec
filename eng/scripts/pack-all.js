659964622cccb58688979a958bfe28f4101d3157
Timothee Guerin <tiguerin@microsoft.com>
2024-01-04 13:48:09 -0800

    .

--- eng/scripts/pack-all.js
+++ eng/scripts/pack-all.js
@@ -4,10 +4,13 @@
  */
 
 import { findWorkspacePackagesNoCheck } from "@pnpm/find-workspace-packages";
-import { repoRoot, run } from "./helpers.js";
+import { runOrExit } from "../../packages/internal-build-utils/dist/src/common.js";
+import { repoRoot } from "./helpers.js";
 
 const projects = await findWorkspacePackagesNoCheck(repoRoot);
 
 for (const project of projects) {
-  run("pnpm", ["pack", `--pack-destination`, `${repoRoot}/temp/artifacts`], { cwd: project.dir });
+  runOrExit("pnpm", ["pack", `--pack-destination`, `${repoRoot}/temp/artifacts`], {
+    cwd: project.dir,
+  });
 }
