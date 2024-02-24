7a52d77952372df1dea7d83c4ddf4f46cbbcee2e
Brian Terlson <brian.terlson@microsoft.com>
2024-02-23 16:21:12 -0800

    Add test

---
+++ packages/openapi3/test/get-openapi.test.ts
@@ -0,0 +1,30 @@
+import { strictEqual } from "assert";
+import { it } from "vitest";
+import { getOpenAPI3 } from "../src/openapi.js";
+import { createOpenAPITestHost } from "./test-host.js";
+
+it("can get openapi as an object", async () => {
+  const host = await createOpenAPITestHost();
+  host.addTypeSpecFile(
+    "./main.tsp",
+    `import "@typespec/http";
+    import "@typespec/rest";
+    import "@typespec/openapi";
+    import "@typespec/openapi3";
+    using TypeSpec.Rest;
+    using TypeSpec.Http;
+    using TypeSpec.OpenAPI;
+
+    @service
+    namespace Foo;
+    
+    @get op get(): Item;
+
+    model Item { x: true }
+    model Bar { }; // unreachable
+   `
+  );
+  await host.compile("main.tsp");
+  const output = await getOpenAPI3(host.program, { "omit-unreachable-types": false });
+  strictEqual(output[0].document.components!.schemas!["Item"].type, "object");
+});
