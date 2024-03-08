c154accbef5fe6a101a60fa81e6ff190c63b17b1
Timothee Guerin <tiguerin@microsoft.com>
2024-03-08 14:13:48 -0800

    ,

--- packages/http/generated-defs/TypeSpec.Http.Private.ts
+++
@@ -1,3 +0,0 @@
-import { DecoratorContext, Model } from "@typespec/compiler";
-
-export type PlainDataDecorator = (context: DecoratorContext, target: Model) => void;
--- packages/json-schema/generated-defs/TypeSpec.JsonSchema.Private.ts
+++
@@ -1,7 +0,0 @@
-import { DecoratorContext, Model, Type } from "@typespec/compiler";
-
-export type ValidatesRawJsonDecorator = (
-  context: DecoratorContext,
-  target: Model,
-  value: Type
-) => void;
