077db13c8d3436f9a50fbb186f032af39418515f
Timothee Guerin <tiguerin@microsoft.com>
2024-03-19 16:08:59 -0700

    Generate TypeScript decorator signatures from `extern dec` (#2122)
    Note this is added to `tspd` which is not published yet so this can be
    iterated over without any issues.
    
    Adds a decorator signature generator. Generates 2 files:
    - `<namespace>.ts` : Contains the decorator signatures that can be
    imported and when declaring the decorators functions
    - `<namespace>.ts-test.ts`: Contains some test using typescript type
    system to make sure the package does reexport the right `$<name>` for
    each decorator
    
    ---------
    
    Co-authored-by: Brian Terlson <brian.terlson@microsoft.com>

---
+++ packages/compiler/generated-defs/TypeSpec.ts-test.ts
@@ -0,0 +1,168 @@
+/** An error here would mean that the decorator is not exported or doesn't have the right name. */
+import {
+  $deprecated,
+  $discriminator,
+  $doc,
+  $encode,
+  $encodedName,
+  $error,
+  $errorsDoc,
+  $format,
+  $friendlyName,
+  $inspectType,
+  $inspectTypeName,
+  $key,
+  $knownValues,
+  $list,
+  $maxItems,
+  $maxLength,
+  $maxValue,
+  $maxValueExclusive,
+  $minItems,
+  $minLength,
+  $minValue,
+  $minValueExclusive,
+  $overload,
+  $parameterVisibility,
+  $pattern,
+  $projectedName,
+  $returnTypeVisibility,
+  $returnsDoc,
+  $secret,
+  $service,
+  $summary,
+  $tag,
+  $visibility,
+  $withDefaultKeyVisibility,
+  $withOptionalProperties,
+  $withUpdateableProperties,
+  $withVisibility,
+  $withoutDefaultValues,
+  $withoutOmittedProperties,
+} from "../src/index.js";
+import {
+  DeprecatedDecorator,
+  DiscriminatorDecorator,
+  DocDecorator,
+  EncodeDecorator,
+  EncodedNameDecorator,
+  ErrorDecorator,
+  ErrorsDocDecorator,
+  FormatDecorator,
+  FriendlyNameDecorator,
+  InspectTypeDecorator,
+  InspectTypeNameDecorator,
+  KeyDecorator,
+  KnownValuesDecorator,
+  ListDecorator,
+  MaxItemsDecorator,
+  MaxLengthDecorator,
+  MaxValueDecorator,
+  MaxValueExclusiveDecorator,
+  MinItemsDecorator,
+  MinLengthDecorator,
+  MinValueDecorator,
+  MinValueExclusiveDecorator,
+  OverloadDecorator,
+  ParameterVisibilityDecorator,
+  PatternDecorator,
+  ProjectedNameDecorator,
+  ReturnTypeVisibilityDecorator,
+  ReturnsDocDecorator,
+  SecretDecorator,
+  ServiceDecorator,
+  SummaryDecorator,
+  TagDecorator,
+  VisibilityDecorator,
+  WithDefaultKeyVisibilityDecorator,
+  WithOptionalPropertiesDecorator,
+  WithUpdateablePropertiesDecorator,
+  WithVisibilityDecorator,
+  WithoutDefaultValuesDecorator,
+  WithoutOmittedPropertiesDecorator,
+} from "./TypeSpec.js";
+
+type Decorators = {
+  $encode: EncodeDecorator;
+  $doc: DocDecorator;
+  $withOptionalProperties: WithOptionalPropertiesDecorator;
+  $withUpdateableProperties: WithUpdateablePropertiesDecorator;
+  $withoutOmittedProperties: WithoutOmittedPropertiesDecorator;
+  $withoutDefaultValues: WithoutDefaultValuesDecorator;
+  $withDefaultKeyVisibility: WithDefaultKeyVisibilityDecorator;
+  $summary: SummaryDecorator;
+  $returnsDoc: ReturnsDocDecorator;
+  $errorsDoc: ErrorsDocDecorator;
+  $deprecated: DeprecatedDecorator;
+  $service: ServiceDecorator;
+  $error: ErrorDecorator;
+  $format: FormatDecorator;
+  $pattern: PatternDecorator;
+  $minLength: MinLengthDecorator;
+  $maxLength: MaxLengthDecorator;
+  $minItems: MinItemsDecorator;
+  $maxItems: MaxItemsDecorator;
+  $minValue: MinValueDecorator;
+  $maxValue: MaxValueDecorator;
+  $minValueExclusive: MinValueExclusiveDecorator;
+  $maxValueExclusive: MaxValueExclusiveDecorator;
+  $secret: SecretDecorator;
+  $list: ListDecorator;
+  $tag: TagDecorator;
+  $friendlyName: FriendlyNameDecorator;
+  $knownValues: KnownValuesDecorator;
+  $key: KeyDecorator;
+  $overload: OverloadDecorator;
+  $projectedName: ProjectedNameDecorator;
+  $encodedName: EncodedNameDecorator;
+  $discriminator: DiscriminatorDecorator;
+  $visibility: VisibilityDecorator;
+  $withVisibility: WithVisibilityDecorator;
+  $inspectType: InspectTypeDecorator;
+  $inspectTypeName: InspectTypeNameDecorator;
+  $parameterVisibility: ParameterVisibilityDecorator;
+  $returnTypeVisibility: ReturnTypeVisibilityDecorator;
+};
+
+/** An error here would mean that the exported decorator is not using the same signature. Make sure to have export const $decName: DecNameDecorator = (...) => ... */
+const _: Decorators = {
+  $encode,
+  $doc,
+  $withOptionalProperties,
+  $withUpdateableProperties,
+  $withoutOmittedProperties,
+  $withoutDefaultValues,
+  $withDefaultKeyVisibility,
+  $summary,
+  $returnsDoc,
+  $errorsDoc,
+  $deprecated,
+  $service,
+  $error,
+  $format,
+  $pattern,
+  $minLength,
+  $maxLength,
+  $minItems,
+  $maxItems,
+  $minValue,
+  $maxValue,
+  $minValueExclusive,
+  $maxValueExclusive,
+  $secret,
+  $list,
+  $tag,
+  $friendlyName,
+  $knownValues,
+  $key,
+  $overload,
+  $projectedName,
+  $encodedName,
+  $discriminator,
+  $visibility,
+  $withVisibility,
+  $inspectType,
+  $inspectTypeName,
+  $parameterVisibility,
+  $returnTypeVisibility,
+};
---
+++ packages/openapi/generated-defs/TypeSpec.OpenAPI.ts-test.ts
@@ -0,0 +1,32 @@
+/** An error here would mean that the decorator is not exported or doesn't have the right name. */
+import {
+  $defaultResponse,
+  $extension,
+  $externalDocs,
+  $info,
+  $operationId,
+} from "@typespec/openapi";
+import {
+  DefaultResponseDecorator,
+  ExtensionDecorator,
+  ExternalDocsDecorator,
+  InfoDecorator,
+  OperationIdDecorator,
+} from "./TypeSpec.OpenAPI.js";
+
+type Decorators = {
+  $operationId: OperationIdDecorator;
+  $extension: ExtensionDecorator;
+  $defaultResponse: DefaultResponseDecorator;
+  $externalDocs: ExternalDocsDecorator;
+  $info: InfoDecorator;
+};
+
+/** An error here would mean that the exported decorator is not using the same signature. Make sure to have export const $decName: DecNameDecorator = (...) => ... */
+const _: Decorators = {
+  $operationId,
+  $extension,
+  $defaultResponse,
+  $externalDocs,
+  $info,
+};
