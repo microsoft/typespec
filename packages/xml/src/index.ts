219afe582cb0580adb5e87b6d92a1a03a03ec4bc
Timothee Guerin <tiguerin@microsoft.com>
2024-03-20 09:54:09 -0700

    Tweaks

--- packages/xml/src/index.ts
+++ packages/xml/src/index.ts
@@ -1,1 +1,11 @@
-export * from "./decorators.js";
+export {
+  $attribute,
+  $name,
+  $ns,
+  $nsDeclarations,
+  $unwrapped,
+  getNs,
+  isAttribute,
+  isUnwrapped,
+} from "./decorators.js";
+export type { XmlNamespace } from "./types.js";
