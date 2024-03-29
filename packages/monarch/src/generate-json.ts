f168e7c97e7294de91e1634dafca22c36bb7aaa2
Timothee Guerin <tiguerin@microsoft.com>
2024-03-29 08:03:07 -0700

    Don't need json

--- packages/monarch/src/generate-json.ts
+++
@@ -1,7 +0,0 @@
-import { writeFile } from "fs/promises";
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
-import lang from "./typespec-monarch.js";
-
-const root = resolve(dirname(fileURLToPath(import.meta.url)), "..", "..", "..");
-await writeFile(resolve(root, "out", "typespec-monarch.json"), JSON.stringify(lang, null, 2));
