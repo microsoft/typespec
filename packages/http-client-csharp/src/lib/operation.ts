c42dd6912ab9be0d3e12a299e46de37c7a2d8c68
Timothee Guerin <tiguerin@microsoft.com>
2024-04-05 11:38:06 -0700

    Fix suppression

--- packages/http-client-csharp/src/lib/operation.ts
+++ packages/http-client-csharp/src/lib/operation.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 // TODO: stop using deprecated API
-/* eslint:disable deprecated */
+/* eslint-disable deprecation/deprecation */
 import { getLroMetadata } from "@azure-tools/typespec-azure-core";
 import {
   SdkContext,
