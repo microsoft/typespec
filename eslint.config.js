090e3ff6f076d330db9045448858b6221156fcef
Timothee Guerin <tiguerin@microsoft.com>
2024-04-05 16:06:47 -0700

    reusable pieces

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,107 @@ import { dirname } from "path";
 import tsEslint from "typescript-eslint";
 import { fileURLToPath } from "url";
 
+/** Config that will apply to all files */
+const allFilesConfig = tsEslint.config({
+  plugins: {
+    unicorn,
+  },
+  rules: {
+    /**
+     * Typescript plugin overrides
+     */
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { varsIgnorePattern: "^_", argsIgnorePattern: ".*", ignoreRestSiblings: true },
+    ],
+
+    // This rule is bugged https://github.com/typescript-eslint/typescript-eslint/issues/6538
+    "@typescript-eslint/no-misused-promises": "off",
+
+    /**
+     * Unicorn
+     */
+    "unicorn/filename-case": ["error", { case: "kebabCase" }],
+
+    /**
+     * Core
+     */
+    "no-inner-declarations": "off",
+    "no-empty": "off",
+    "no-constant-condition": "off",
+    "no-case-declarations": "off",
+    "no-ex-assign": "off",
+    "no-undef": "off",
+    "prefer-const": [
+      "warn",
+      {
+        destructuring: "all",
+      },
+    ],
+    eqeqeq: ["warn", "always", { null: "ignore" }],
+
+    // Do not want console.log left from debugging or using console.log for logging. Use the program logger.
+    "no-console": "warn",
+
+    // Symbols should have a description so it can be serialized.
+    "symbol-description": "warn",
+  },
+});
+
+/** Config that will apply to all typescript files only
+ * @param {string} root
+ */
+export function getTypeScriptProjectRules(root) {
+  return tsEslint.config({
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      deprecation,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: root,
+      },
+    },
+    rules: {
+      // Only put rules here that need typescript project information
+      "@typescript-eslint/no-floating-promises": "error",
+      "deprecation/deprecation": ["warn"],
+    },
+  });
+}
+
+/** Config that will apply to all test files only */
+const testFilesConfig = tsEslint.config({
+  /**
+   * Test files specific rules
+   */
+  files: ["**/*.test.ts"],
+  plugins: { vitest },
+  rules: {
+    "vitest/no-focused-tests": "warn",
+    "vitest/no-identical-title": "error",
+    "vitest/no-commented-out-tests": "warn",
+    "vitest/no-import-node-test": "warn",
+    "vitest/require-local-test-context-for-concurrent-snapshots": "warn",
+    "vitest/valid-describe-callback": "warn",
+    "vitest/valid-expect": "warn",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+  },
+});
+
+export const TypeSpecCommonEslintConfigs = [
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...allFilesConfig,
+  ...testFilesConfig,
+];
+
 export default tsEslint.config(
   {
     ignores: [
@@ -22,86 +123,6 @@ export default tsEslint.config(
       "packages/*/scripts/**/*",
     ],
   },
-  eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: dirname(fileURLToPath(import.meta.url)),
-      },
-    },
-    plugins: {
-      deprecation,
-      unicorn,
-    },
-    rules: {
-      /**
-       * Typescript plugin overrides
-       */
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-inferrable-types": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-empty-interface": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { varsIgnorePattern: "^_", argsIgnorePattern: ".*", ignoreRestSiblings: true },
-      ],
-      "@typescript-eslint/no-floating-promises": "error",
-
-      // This rule is bugged https://github.com/typescript-eslint/typescript-eslint/issues/6538
-      "@typescript-eslint/no-misused-promises": "off",
-
-      /**
-       * Unicorn
-       */
-      "deprecation/deprecation": ["warn"],
-
-      /**
-       * Unicorn
-       */
-      "unicorn/filename-case": ["error", { case: "kebabCase" }],
-
-      /**
-       * Core
-       */
-      "no-inner-declarations": "off",
-      "no-empty": "off",
-      "no-constant-condition": "off",
-      "no-case-declarations": "off",
-      "no-ex-assign": "off",
-      "no-undef": "off",
-      "prefer-const": [
-        "warn",
-        {
-          destructuring: "all",
-        },
-      ],
-      eqeqeq: ["warn", "always", { null: "ignore" }],
-
-      // Do not want console.log left from debugging or using console.log for logging. Use the program logger.
-      "no-console": "warn",
-
-      // Symbols should have a description so it can be serialized.
-      "symbol-description": "warn",
-    },
-  },
-  {
-    /**
-     * Test files specific rules
-     */
-    files: ["**/*.test.ts"],
-    plugins: { vitest },
-    rules: {
-      "vitest/no-focused-tests": "warn",
-      "vitest/no-identical-title": "error",
-      "vitest/no-commented-out-tests": "warn",
-      "vitest/no-import-node-test": "warn",
-      "vitest/require-local-test-context-for-concurrent-snapshots": "warn",
-      "vitest/valid-describe-callback": "warn",
-      "vitest/valid-expect": "warn",
-      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-    },
-  }
+  ...TypeSpecCommonEslintConfigs,
+  ...getTypeScriptProjectRules(dirname(fileURLToPath(import.meta.url)))
 );
