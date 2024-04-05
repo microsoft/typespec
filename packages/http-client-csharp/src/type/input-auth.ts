c8b61fb07b4992e52c952fecb3c8e645a0924b2d
Timothee Guerin <tiguerin@microsoft.com>
2024-04-05 11:34:01 -0700

    fix missed rename

--- packages/http-client-csharp/src/type/input-auth.ts
+++ packages/http-client-csharp/src/type/input-auth.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+import { InputApiKeyAuth } from "./input-api-key-auth.js";
 import { InputOAuth2Auth } from "./input-oauth2-auth.js";
-import { InputApiKeyAuth } from "./inputApiKeyAuth.js";
 
 export interface InputAuth {
   ApiKey?: InputApiKeyAuth;
