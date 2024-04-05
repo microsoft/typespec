51b2e57ac74fd7ea5386b0eadc4a76d77a59120a
Timothee Guerin <tiguerin@microsoft.com>
2024-04-05 15:00:39 -0700

    Pass compiler options for samples (#3124)

--- packages/playground/src/tooling/index.ts
+++ packages/playground/src/tooling/index.ts
@@ -20,6 +20,7 @@ export async function buildSamples_experimental(
       filename: config.filename,
       content,
       preferredEmitter: config.preferredEmitter,
+      compilerOptions: config.compilerOptions,
     };
   }
 
@@ -33,5 +34,5 @@ export async function buildSamples_experimental(
     `declare const samples: Record<string, PlaygroundSample>;`,
     `export default samples;`,
   ].join("\n");
-  await writeFile(resolvePath(dir, getBaseFileName(output)) + ".d.ts", dts);
+  await writeFile(resolvePath(dir, getBaseFileName(output)).replace(/\.js$/, ".d.ts"), dts);
 }
