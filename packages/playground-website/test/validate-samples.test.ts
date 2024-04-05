77d4e8412fe99cf818d4ae8b6df969d687376946
Timothee Guerin <tiguerin@microsoft.com>
2024-04-05 14:17:34 -0700

    fix

--- packages/playground-website/test/validate-samples.test.ts
+++ packages/playground-website/test/validate-samples.test.ts
@@ -8,8 +8,9 @@ const packageRoot = await findTestPackageRoot(import.meta.url);
 for (const [name, sample] of Object.entries(samples)) {
   it(`compile ${name}`, async () => {
     const program = await compile(NodeHost, resolvePath(packageRoot, sample.filename), {
+      ...(sample.compilerOptions ?? {}),
       noEmit: true,
-      emit: [sample.preferredEmitter],
+      emit: sample.preferredEmitter ? [sample.preferredEmitter] : [],
     });
     expectDiagnosticEmpty(program.diagnostics);
   });
