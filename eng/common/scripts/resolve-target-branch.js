a082998e0355a9e854a0be330fb8b38e43fca787
Timothee Guerin <tiguerin@microsoft.com>
2024-04-22 16:28:48 -0700

    missing

---
+++ eng/common/scripts/resolve-target-branch.js
@@ -0,0 +1,25 @@
+// @ts-check
+console.log("Process", process.env);
+
+const prTargetBranch = process.env["System_PullRequest_TargetBranch"];
+const currentBranch = process.env["Build_SourceBranch"];
+
+console.log("Branches:", {
+  prTargetBranch,
+  currentBranch,
+});
+
+if (prTargetBranch !== undefined) {
+  console.log("Target branch is", prTargetBranch);
+  console.log(`##vso[task.setvariable variable=TARGET_BRANCH]${prTargetBranch}`);
+} else if (currentBranch) {
+  const segments = currentBranch.split("/");
+  if (segments[0] === "github-readonly-queue") {
+    const targetBranch = segments.slice(1, segments.length - 2).join("/");
+    console.log("Target branch is", targetBranch);
+    console.log(`##vso[task.setvariable variable=TARGET_BRANCH]${targetBranch}`);
+  }
+} else {
+  console.log("Failed to resolve target branch.");
+  process.exit(1);
+}
