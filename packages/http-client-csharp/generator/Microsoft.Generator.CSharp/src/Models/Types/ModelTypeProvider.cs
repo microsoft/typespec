d3a4454fa0b14be847e49087d36b87a5dc8a8f1c
Jorge Rangel <102122018+jorgerangel-msft@users.noreply.github.com>
2024-04-29 00:51:42 -0500

    fix(http-client-csharp): remove setters for model properties collection types (#3233)
    This PR fixes the case where setters were being generated for model
    properties that were collection types. Model properties that are
    collection types will no longer generated a setter unless they are
    nullable.
    
    ---------
    
    Co-authored-by: ShivangiReja <45216704+ShivangiReja@users.noreply.github.com>
    Co-authored-by: Dapeng Zhang <ufo54153@gmail.com>
    Co-authored-by: ShivangiReja <shivangi.reja@microsoft.com>

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Models/Types/ModelTypeProvider.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Models/Types/ModelTypeProvider.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.Generator.CSharp.Expressions;
 using Microsoft.Generator.CSharp.Input;
 
@@ -48,19 +49,54 @@ namespace Microsoft.Generator.CSharp
         private PropertyDeclaration BuildPropertyDeclaration(InputModelProperty property)
         {
             var propertyType = CodeModelPlugin.Instance.TypeFactory.CreateCSharpType(property.Type);
-            MethodSignatureModifiers setterModifier = !property.IsReadOnly ? MethodSignatureModifiers.Public : MethodSignatureModifiers.None;
+            var propHasSetter = PropertyHasSetter(propertyType, property);
+            MethodSignatureModifiers setterModifier = propHasSetter ? MethodSignatureModifiers.Public : MethodSignatureModifiers.None;
 
-            // Exclude setter generation for collection properties https://github.com/Azure/autorest.csharp/issues/4616
             // Add Remarks and Example for BinaryData Properties https://github.com/Azure/autorest.csharp/issues/4617
             var propertyDeclaration = new PropertyDeclaration(
                     description: FormattableStringHelpers.FromString(property.Description),
                     modifiers: MethodSignatureModifiers.Public,
                     propertyType: propertyType,
                     name: property.Name.FirstCharToUpperCase(),
-                    propertyBody: new AutoPropertyBody(!property.IsReadOnly, setterModifier, GetPropertyInitializationValue(property, propertyType))
+                    propertyBody: new AutoPropertyBody(propHasSetter, setterModifier, GetPropertyInitializationValue(property, propertyType))
                     );
 
             return propertyDeclaration;
+        }
+
+        /// <summary>
+        /// Returns true if the property has a setter.
+        /// </summary>
+        /// <param name="type">The <see cref="CSharpType"/> of the property.</param>
+        /// <param name="prop">The <see cref="InputModelProperty"/>.</param>
+        private bool PropertyHasSetter(CSharpType type, InputModelProperty prop)
+        {
+            if (prop.IsDiscriminator)
+            {
+                return true;
+            }
+
+            if (prop.IsReadOnly)
+            {
+                return false;
+            }
+
+            if (IsStruct)
+            {
+                return false;
+            }
+
+            if (type.IsLiteral && prop.IsRequired)
+            {
+                return false;
+            }
+
+            if (type.IsCollection && !type.IsReadOnlyMemory)
+            {
+                return type.IsNullable;
+            }
+
+            return true;
         }
 
         private ConstantExpression? GetPropertyInitializationValue(InputModelProperty property, CSharpType propertyType)
