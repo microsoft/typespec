010691c76afcfc1795bc724aeeaa55c50cba6a96
m-nash <64171366+m-nash@users.noreply.github.com>
2024-05-06 18:15:24 -0700

    few more fixes

--- packages/http-client-csharp/eng/scripts/Functions.ps1
+++ packages/http-client-csharp/eng/scripts/Functions.ps1
@@ -1,25 +1,25 @@
 # Return list of nupkg artifacts
 function Get-Package-Artifacts ($location, $filter)
 {
-    $pkgs = Get-ChildItem -Path $location -Filter $filter -Recurse
-    if (!$pkgs)
+    $packages = Get-ChildItem -Path $location -Filter $filter -Recurse
+    if (!$packages)
     {
         Write-Host "$($location) does not have any packages matching filter $($filter)"
         return $null
     }
-    return $pkgs[0]
+    return $packages[0]
 }
 
 function Find-Artifacts-For-Apireview($artifactDir, $packageName)
 {
     # Find all nupkg files in given artifact directory
     $packageArtifactPath = Join-Path $artifactDir "packages"
-    $pkg = Get-Package-Artifacts $packageArtifactPath "$packageName*"
-    if (!$pkg)
+    $package = Get-Package-Artifacts $packageArtifactPath "$packageName*"
+    if (!$package)
     {
         Write-Host "Package is not available in artifact path $($packageArtifactPath)"
         return $null
     }
-    $packages = @{ $pkg.Name = $pkg.FullName }
+    $packages = @{ $package.Name = $package.FullName }
     return $packages
 }
