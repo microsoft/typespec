4fb29106f7d4491ee8155f5ab15ea3c057179271
m-nash <64171366+m-nash@users.noreply.github.com>
2024-05-08 12:47:54 -0700

    update build script

--- packages/http-client-csharp/eng/scripts/Build-Packages.ps1
+++ packages/http-client-csharp/eng/scripts/Build-Packages.ps1
@@ -42,7 +42,7 @@ function Pack-And-Write-Info {
     )
 
     $versionOption = $BuildNumber ? "/p:Version=$version" : ""
-    Invoke-LoggedCommand "dotnet pack $package/src/$package.csproj $versionOption -c Release -o $outputPath/packages"
+    Invoke-LoggedCommand "dotnet pack ./$package/src/$package.csproj $versionOption -c Release -o $outputPath/packages"
     Write-PackageInfo -packageName $package -directoryPath "packages/http-client-csharp/generator/$package/src"
 }
 
@@ -51,16 +51,16 @@ function Get-CsprojVersion {
         [string] $csprojFilePath
     )
 
-    Write-Host "Reading version from $csprojFilePath"
     $csprojContent = Get-Content -Path $csprojFilePath
     $versionElement = $csprojContent | Where-Object { $_ -match '<Version>(.*?)<\/Version>' }
 
     if ($versionElement) {
-        $version = $versionElement -replace '<Version>(.*?)<\/Version>', '$1'
+        $version = $versionElement -replace '<Version>([^-]*)-.*?<\/Version>', '$1'
         Write-Host "Version: $version"
     } else {
         Write-Host "Version not found in the .csproj file."
     }
+    return $version.Trim()
 }
 
 function Set-VersionVariable {
@@ -77,25 +77,12 @@ function Set-VersionVariable {
 $outputPath = New-Item -ItemType Directory -Force -Path $outputPath | Select-Object -ExpandProperty FullName
 New-Item -ItemType Directory -Force -Path "$outputPath/packages" | Out-Null
 
-# Read the contents of the .csproj file
-$csprojContent = Get-Content -Path $csprojFilePath
-
-# Search for the <Version> element in the .csproj file
-$versionElement = $csprojContent | Where-Object { $_ -match '<Version>(.*?)<\/Version>' }
-
-# Extract the version number from the <Version> element
-if ($versionElement) {
-    $version = $versionElement -replace '<Version>(.*?)<\/Version>', '$1'
-    Write-Output "Version: $version"
-} else {
-    Write-Output "Version not found in the .csproj file."
-}
-
+Write-Host "Getting existing versions"
 $emitterVersion = node -p -e "require('$packageRoot/package.json').version"
-$mgcVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp/Microsoft.Generator.CSharp.csproj"
-$mgcClientModelVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp.ClientModel/Microsoft.Generator.CSharp.ClientModel.csproj"
-$mgcInputVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp.Input/Microsoft.Generator.CSharp.Input.csproj"
-$mgcCustomizationVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp.Customization/Microsoft.Generator.CSharp.Customization.csproj"
+$mgcVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp/src/Microsoft.Generator.CSharp.csproj"
+$mgcClientModelVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp.ClientModel/src/Microsoft.Generator.CSharp.ClientModel.csproj"
+$mgcInputVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp.Input/src/Microsoft.Generator.CSharp.Input.csproj"
+$mgcCustomizationVersion = Get-CsprojVersion -csprojFilePath "$packageRoot/generator/Microsoft.Generator.CSharp.Customization/src/Microsoft.Generator.CSharp.Customization.csproj"
 
 if ($BuildNumber) {
     # set package versions
