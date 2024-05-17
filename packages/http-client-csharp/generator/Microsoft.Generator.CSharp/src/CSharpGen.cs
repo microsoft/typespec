e56969ca8acd7c905c8a157f05288311cc054154
m-nash <64171366+m-nash@users.noreply.github.com>
2024-05-17 14:30:22 -0700

    Reorganize folders and drop xml for now (#3394)
    Using a simple flat folder structure vs the deeply nested structure in
    autorest.csharp.
    
    Under src we will have
    
    - Expressions - Classes that inherit from ValueExpression
    - OutputTypes - Types constructed from output library like
    propertydeclaration, modeltypeprovider, etc
    - PostProcessing - Classes that deal with roslyn post processing of
    csharp files
    - Snippets - Helper classes for expressions / statements
    - SourceInput - Roslyn existing / custom code representation / converter
    / helper
    - StartUp - Start up process for loading plugins and initiating the
    generation
    - Statements - Classes that inherit from MethodBodyStatement
    - Utilities - Any extension on system types or general utilities
    - Writers - Writers to convert output types into csharp files

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/CSharpGen.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/CSharpGen.cs
@@ -6,13 +6,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Generator.CSharp.Writers;
 
 namespace Microsoft.Generator.CSharp
 {
     public sealed class CSharpGen
     {
-        private static readonly string[] _filesToKeep = [Constants.DefaultCodeModelFileName, Constants.DefaultConfigurationFileName];
+        private const string ConfigurationFileName = "Configuration.json";
+        private const string CodeModelFileName = "tspCodeModel.json";
+
+        private static readonly string[] _filesToKeep = [ConfigurationFileName, CodeModelFileName];
 
         /// <summary>
         /// Executes the generator task with the <see cref="CodeModelPlugin"/> instance.
@@ -21,7 +23,7 @@ namespace Microsoft.Generator.CSharp
         {
             GeneratedCodeWorkspace.Initialize();
             var outputPath = CodeModelPlugin.Instance.Configuration.OutputDirectory;
-            var generatedTestOutputPath = Path.Combine(outputPath, "..", "..", "tests", Constants.DefaultGeneratedCodeFolderName);
+            var generatedTestOutputPath = Path.Combine(outputPath, "..", "..", "tests", "Generated");
 
             GeneratedCodeWorkspace workspace = await GeneratedCodeWorkspace.Create();
 
