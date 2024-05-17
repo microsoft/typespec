04bd1be4e8fb295367079845f33a2ffa8fa12c3f
Timothee Guerin <tiguerin@microsoft.com>
2024-05-17 07:58:52 -0700

    remove path test

--- packages/http/vitest.config.ts
+++ packages/http/vitest.config.ts
@@ -2,5 +2,3 @@ import { defineConfig, mergeConfig } from "vitest/config";
 import { defaultTypeSpecVitestConfig } from "../../vitest.workspace.js";
 
 export default mergeConfig(defaultTypeSpecVitestConfig, defineConfig({}));
-
-// Test remove
