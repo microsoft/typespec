653e66bad469097bd06269480bc76e7b5f01aae2
Timothee Guerin <tiguerin@microsoft.com>
2024-05-21 16:04:52 -0700

    Fix

--- eng/common/scripts/utils/ado.ts
+++ eng/common/scripts/utils/ado.ts
@@ -1,4 +1,3 @@
 export function setOutputVariable(name: string, value: string) {
-  process.stdout.write("##vso[task.setvariable variable=RunCore;isOutput=true]true");
   process.stdout.write(`##vso[task.setvariable variable=${name};isOutput=true]${value}\n`);
 }
