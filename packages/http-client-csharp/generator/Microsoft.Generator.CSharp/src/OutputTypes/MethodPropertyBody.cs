4d880582fe0a5ebb236ed8930d38bbceb1fb2344
m-nash <64171366+m-nash@users.noreply.github.com>
2024-05-21 16:37:01 -0700

    fix a couple straggler namespaces

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/OutputTypes/MethodPropertyBody.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/OutputTypes/MethodPropertyBody.cs
@@ -1,9 +1,9 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using Microsoft.Generator.CSharp.Expressions;
 
-namespace Microsoft.Generator.CSharp.Models
+namespace Microsoft.Generator.CSharp
 {
     internal record MethodPropertyBody(MethodBodyStatement Getter, MethodBodyStatement? Setter = null, MethodSignatureModifiers SetterModifiers = MethodSignatureModifiers.None) : PropertyBody;
 }
