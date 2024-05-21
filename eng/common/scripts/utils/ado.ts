4c437718bc4fdfeb8225c04022ce0eb469852066
Timothee Guerin <tiguerin@microsoft.com>
2024-05-21 15:58:30 -0700

    test

--- eng/common/scripts/utils/ado.ts
+++ eng/common/scripts/utils/ado.ts
@@ -1,3 +1,4 @@
 export function setOutputVariable(name: string, value: string) {
-  process.stdout.write(`##vso[task.setvariable variable=${name};isOutput=true]${value}`);
+  process.stdout.write("##vso[task.setvariable variable=RunCore;isOutput=true]true");
+  process.stdout.write(`##vso[task.setvariable variable=${name};isOutput=true]${value}\n`);
 }
