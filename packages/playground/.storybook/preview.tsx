ff455c271b39c37cc94ff4d43a1b467a6b09fc35
Timothee Guerin <tiguerin@microsoft.com>
2024-05-22 11:51:35 -0700

    .

--- packages/playground/.storybook/preview.tsx
+++ packages/playground/.storybook/preview.tsx
@@ -4,7 +4,6 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
   decorators: [
     (Story, { parameters }) => {
-      console.log("PReview", parameters);
       return (
         <FluentProvider theme={webLightTheme}>
           <Story />
