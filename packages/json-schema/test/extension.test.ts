bd46e999765c5ac37262c81dc4c77d627dfe51f5
Christopher Radek <Christopher.Radek@microsoft.com>
2024-06-11 10:06:18 -0700

    remove unnecessary top-level describe

--- packages/json-schema/test/extension.test.ts
+++ packages/json-schema/test/extension.test.ts
@@ -4,9 +4,8 @@ import { describe, it } from "vitest";
 import { setExtension } from "../src/index.js";
 import { emitSchema } from "./utils.js";
 
-describe("emitting extensions", () => {
-  it("handles types", async () => {
-    const schemas = await emitSchema(`
+it("handles types", async () => {
+  const schemas = await emitSchema(`
       @extension("x-named-model", Thing)
       @extension("x-model-template", Collection<Thing>)
       @extension("x-record", Record<{ name: string }>)
@@ -31,57 +30,57 @@ describe("emitting extensions", () => {
         name: string;
       }
     `);
-    const Foo = schemas["Foo.json"];
+  const Foo = schemas["Foo.json"];
 
-    assert.deepStrictEqual(Foo["x-named-model"], { $ref: "Thing.json" });
-    assert.deepStrictEqual(Foo["x-model-template"], { $ref: "CollectionThing.json" });
-    assert.deepStrictEqual(Foo["x-record"], {
-      additionalProperties: {
-        properties: {
-          name: {
-            type: "string",
-          },
+  assert.deepStrictEqual(Foo["x-named-model"], { $ref: "Thing.json" });
+  assert.deepStrictEqual(Foo["x-model-template"], { $ref: "CollectionThing.json" });
+  assert.deepStrictEqual(Foo["x-record"], {
+    additionalProperties: {
+      properties: {
+        name: {
+          type: "string",
         },
-        required: ["name"],
-        type: "object",
       },
-      properties: {},
+      required: ["name"],
       type: "object",
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-bool"], { type: "boolean" });
-    assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], {
-      const: true,
-      type: "boolean",
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-int"], {
-      minimum: -128,
-      maximum: 127,
-      type: "integer",
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-int-literal"], {
-      const: 42,
-      type: "number",
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-string"], {
-      type: "string",
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-string-literal"], {
-      const: "hi",
-      type: "string",
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-union"], {
-      anyOf: [
-        { const: "one", type: "string" },
-        { const: "two", type: "string" },
-      ],
-    });
-    assert.deepStrictEqual(Foo.properties.x["x-null"], {
-      type: "null",
-    });
+    },
+    properties: {},
+    type: "object",
   });
+  assert.deepStrictEqual(Foo.properties.x["x-bool"], { type: "boolean" });
+  assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], {
+    const: true,
+    type: "boolean",
+  });
+  assert.deepStrictEqual(Foo.properties.x["x-int"], {
+    minimum: -128,
+    maximum: 127,
+    type: "integer",
+  });
+  assert.deepStrictEqual(Foo.properties.x["x-int-literal"], {
+    const: 42,
+    type: "number",
+  });
+  assert.deepStrictEqual(Foo.properties.x["x-string"], {
+    type: "string",
+  });
+  assert.deepStrictEqual(Foo.properties.x["x-string-literal"], {
+    const: "hi",
+    type: "string",
+  });
+  assert.deepStrictEqual(Foo.properties.x["x-union"], {
+    anyOf: [
+      { const: "one", type: "string" },
+      { const: "two", type: "string" },
+    ],
+  });
+  assert.deepStrictEqual(Foo.properties.x["x-null"], {
+    type: "null",
+  });
+});
 
-  it("handles Json-wrapped types", async () => {
-    const schemas = await emitSchema(`
+it("handles Json-wrapped types", async () => {
+  const schemas = await emitSchema(`
       @extension("x-anon-model", Json<{ name: "foo" }>)
       @extension("x-named-model", Json<Thing>)
       @extension("x-nested-anon-model", Json<{ items: [ {foo: "bar" }]}>)
@@ -101,23 +100,23 @@ describe("emitting extensions", () => {
         name: "thing";
       }
     `);
-    const Foo = schemas["Foo.json"];
+  const Foo = schemas["Foo.json"];
 
-    assert.deepStrictEqual(Foo["x-anon-model"], { name: "foo" });
-    assert.deepStrictEqual(Foo["x-named-model"], { name: "thing" });
-    assert.deepStrictEqual(Foo["x-nested-anon-model"], { items: [{ foo: "bar" }] });
+  assert.deepStrictEqual(Foo["x-anon-model"], { name: "foo" });
+  assert.deepStrictEqual(Foo["x-named-model"], { name: "thing" });
+  assert.deepStrictEqual(Foo["x-nested-anon-model"], { items: [{ foo: "bar" }] });
 
-    // Would expect this to be `{ size: 1, item: { name: "thing" }}` but it isn't.
-    // assert.deepStrictEqual(Foo["x-model-template"], { size: 1, item: {} });
+  // Would expect this to be `{ size: 1, item: { name: "thing" }}` but it isn't.
+  // assert.deepStrictEqual(Foo["x-model-template"], { size: 1, item: {} });
 
-    assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], true);
-    assert.deepStrictEqual(Foo.properties.x["x-int-literal"], 42);
-    assert.deepStrictEqual(Foo.properties.x["x-string-literal"], "hi");
-  });
+  assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], true);
+  assert.deepStrictEqual(Foo.properties.x["x-int-literal"], 42);
+  assert.deepStrictEqual(Foo.properties.x["x-string-literal"], "hi");
+});
 
-  // These tests are skipped - can enable if @extension is updated to support `valueof unknown`
-  it.skip("handles values", async () => {
-    const schemas = await emitSchema(`
+// These tests are skipped - can enable if @extension is updated to support `valueof unknown`
+it.skip("handles values", async () => {
+  const schemas = await emitSchema(`
       @extension("x-anon-model", #{ name: "foo" })
       @extension("x-nested-anon-model", #{ items: #[ #{foo: "bar" }]})
       model Foo {
@@ -128,21 +127,21 @@ describe("emitting extensions", () => {
         x: string;
       }
     `);
-    const Foo = schemas["Foo.json"];
+  const Foo = schemas["Foo.json"];
 
-    assert.deepStrictEqual(Foo["x-anon-model"], { name: "foo" });
-    assert.deepStrictEqual(Foo["x-nested-anon-model"], { items: [{ foo: "bar" }] });
+  assert.deepStrictEqual(Foo["x-anon-model"], { name: "foo" });
+  assert.deepStrictEqual(Foo["x-nested-anon-model"], { items: [{ foo: "bar" }] });
 
-    assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], true);
-    assert.deepStrictEqual(Foo.properties.x["x-int-literal"], 42);
-    assert.deepStrictEqual(Foo.properties.x["x-string-literal"], "hi");
-    assert.deepStrictEqual(Foo.properties.x["x-null"], null);
-  });
+  assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], true);
+  assert.deepStrictEqual(Foo.properties.x["x-int-literal"], 42);
+  assert.deepStrictEqual(Foo.properties.x["x-string-literal"], "hi");
+  assert.deepStrictEqual(Foo.properties.x["x-null"], null);
+});
 
-  describe("setExtension", () => {
-    it("handles values", async () => {
-      const schemas = await emitSchema(
-        `
+describe("setExtension", () => {
+  it("handles values", async () => {
+    const schemas = await emitSchema(
+      `
         extern dec setExtension(target: unknown, key: valueof string, value: (valueof unknown));
 
         @setExtension("x-anon-model", #{ name: "foo" })
@@ -155,55 +154,55 @@ describe("emitting extensions", () => {
           x: string;
         }
       `,
-        undefined,
-        {
-          emitNamespace: true,
-          decorators: {
-            namespace: "test",
-            $flags: { decoratorArgMarshalling: "new" },
-            $setExtension(context: DecoratorContext, target: Type, key: string, value: unknown) {
-              setExtension(context.program, target, key, value);
-            },
+      undefined,
+      {
+        emitNamespace: true,
+        decorators: {
+          namespace: "test",
+          $flags: { decoratorArgMarshalling: "new" },
+          $setExtension(context: DecoratorContext, target: Type, key: string, value: unknown) {
+            setExtension(context.program, target, key, value);
           },
-        }
-      );
+        },
+      }
+    );
 
-      const Foo = schemas["Foo.json"];
+    const Foo = schemas["Foo.json"];
 
-      assert.deepStrictEqual(Foo["x-anon-model"], { name: "foo" });
-      assert.deepStrictEqual(Foo["x-nested-anon-model"], { items: [{ foo: "bar" }] });
+    assert.deepStrictEqual(Foo["x-anon-model"], { name: "foo" });
+    assert.deepStrictEqual(Foo["x-nested-anon-model"], { items: [{ foo: "bar" }] });
 
-      assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], true);
-      assert.deepStrictEqual(Foo.properties.x["x-int-literal"], 42);
-      assert.deepStrictEqual(Foo.properties.x["x-string-literal"], "hi");
-      assert.deepStrictEqual(Foo.properties.x["x-null"], null);
-    });
+    assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], true);
+    assert.deepStrictEqual(Foo.properties.x["x-int-literal"], 42);
+    assert.deepStrictEqual(Foo.properties.x["x-string-literal"], "hi");
+    assert.deepStrictEqual(Foo.properties.x["x-null"], null);
   });
+});
 
-  // Expect these to fail if @extension is updated to support `valueof unknown`.
-  describe("breaking changes with value support", () => {
-    it("supports in-line models types", async () => {
-      const schemas = await emitSchema(`
+// Expect these to fail if @extension is updated to support `valueof unknown`.
+describe("breaking changes with value support", () => {
+  it("supports in-line models types", async () => {
+    const schemas = await emitSchema(`
         @extension("x-anon-model", { name: string })
         model Foo {
           x: string;
         }
       `);
 
-      // What used to be emitted
-      assert.deepStrictEqual(schemas["Foo.json"]["x-anon-model"], {
-        type: "object",
-        required: ["name"],
-        properties: {
-          name: {
-            type: "string",
-          },
+    // What used to be emitted
+    assert.deepStrictEqual(schemas["Foo.json"]["x-anon-model"], {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: {
+          type: "string",
         },
-      });
+      },
     });
+  });
 
-    it("treats scalar literals as types without 'typeof'", async () => {
-      const schemas = await emitSchema(`
+  it("treats scalar literals as types without 'typeof'", async () => {
+    const schemas = await emitSchema(`
         model Foo {
           @extension("x-int-literal", 42)
           @extension("x-string-literal", "hi")
@@ -212,23 +211,22 @@ describe("emitting extensions", () => {
           x: string;
         }
       `);
-      const Foo = schemas["Foo.json"];
+    const Foo = schemas["Foo.json"];
 
-      assert.deepStrictEqual(Foo.properties.x["x-int-literal"], {
-        const: 42,
-        type: "number",
-      });
-      assert.deepStrictEqual(Foo.properties.x["x-string-literal"], {
-        const: "hi",
-        type: "string",
-      });
-      assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], {
-        const: true,
-        type: "boolean",
-      });
-      assert.deepStrictEqual(Foo.properties.x["x-null"], {
-        type: "null",
-      });
+    assert.deepStrictEqual(Foo.properties.x["x-int-literal"], {
+      const: 42,
+      type: "number",
+    });
+    assert.deepStrictEqual(Foo.properties.x["x-string-literal"], {
+      const: "hi",
+      type: "string",
+    });
+    assert.deepStrictEqual(Foo.properties.x["x-bool-literal"], {
+      const: true,
+      type: "boolean",
+    });
+    assert.deepStrictEqual(Foo.properties.x["x-null"], {
+      type: "null",
     });
   });
 });
