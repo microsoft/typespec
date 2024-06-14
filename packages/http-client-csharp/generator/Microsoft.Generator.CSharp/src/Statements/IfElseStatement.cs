022c65b712fd7180bfc52b0b2273c1e7d48ab2f8
m-nash <64171366+m-nash@users.noreply.github.com>
2024-06-14 12:25:19 -0700

    relax constraint on condition to value expression

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Statements/IfElseStatement.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Statements/IfElseStatement.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Generator.CSharp.Snippets;
+using Microsoft.Generator.CSharp.Expressions;
 
 namespace Microsoft.Generator.CSharp.Statements
 {
@@ -16,7 +16,7 @@ namespace Microsoft.Generator.CSharp.Statements
             Else = elseStatement;
         }
 
-        public IfElseStatement(BoolSnippet condition, MethodBodyStatement ifStatement, MethodBodyStatement? elseStatement, bool inline = false, bool addBraces = true)
+        public IfElseStatement(ValueExpression condition, MethodBodyStatement ifStatement, MethodBodyStatement? elseStatement, bool inline = false, bool addBraces = true)
             : this(new IfStatement(condition, inline, addBraces) { ifStatement }, elseStatement) {}
 
         internal override void Write(CodeWriter writer)
