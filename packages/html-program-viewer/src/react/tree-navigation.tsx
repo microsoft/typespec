deffd08b54f5308e4c7b69a97807bb5f7aafef22
Timothee Guerin <tiguerin@microsoft.com>
2024-06-16 16:10:00 -0700

    Fix

--- packages/html-program-viewer/src/react/tree-navigation.tsx
+++ packages/html-program-viewer/src/react/tree-navigation.tsx
@@ -1,12 +1,7 @@
-import {
-  Tree,
-  TreeItem,
-  TreeItemLayout,
-  type TreeOpenChangeData,
-} from "@fluentui/react-components";
+import { type TreeOpenChangeData } from "@fluentui/react-components";
 import { useCallback, useMemo, useState } from "react";
-import style from "./tree-navigation.module.css";
-import { useTreeNavigator, type TypeGraphNode } from "./use-tree-navigation.js";
+import { Tree } from "./tree/tree.js";
+import { useTreeNavigator } from "./use-tree-navigation.js";
 
 export interface TreeNavigationProps {}
 
@@ -27,31 +22,4 @@ export const TreeNavigation = (_: TreeNavigationProps) => {
   }, [openItems, nav.selectedPath]);
 
   return <Tree tree={nav.tree} onSelect={nav.selectPath} />;
-};
-
-const TreeNodeItemsUI = ({ nodes }: { nodes: TypeGraphNode[] }) => {
-  return (
-    <>
-      {nodes.map((node) => {
-        return <TreeNodeUI key={node.id} node={node} />;
-      })}
-    </>
-  );
-};
-
-const TreeNodeUI = ({ node }: { node: TypeGraphNode }) => {
-  const nav = useTreeNavigator();
-  const selected = nav.selectedPath === node.id;
-  return (
-    <TreeItem itemType={node.children.length === 0 ? "leaf" : "branch"} value={node.id}>
-      <TreeItemLayout className={selected ? style["selected"] : undefined} aria-selected={selected}>
-        {node.name}
-      </TreeItemLayout>
-      {node.children.length > 0 && (
-        <Tree>
-          <TreeNodeItemsUI nodes={node.children} />
-        </Tree>
-      )}
-    </TreeItem>
-  );
 };
