831e0c8e0921ca5ab9c813fc74ece737b484193b
m-nash <64171366+m-nash@users.noreply.github.com>
2024-06-25 13:03:19 -0700

    remove new dictionaryexpression and keep as snippet only (#3652)
    Fixes https://github.com/microsoft/typespec/issues/3471

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Expressions/NewInstanceExpression.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Expressions/NewInstanceExpression.cs
@@ -5,20 +5,30 @@ using System.Collections.Generic;
 
 namespace Microsoft.Generator.CSharp.Expressions
 {
-    public sealed record NewInstanceExpression(CSharpType Type, IReadOnlyList<ValueExpression> Parameters, ObjectInitializerExpression? InitExpression = null) : ValueExpression
+    public sealed record NewInstanceExpression(CSharpType? Type, IReadOnlyList<ValueExpression> Parameters, ObjectInitializerExpression? InitExpression = null) : ValueExpression
     {
         private const int SingleLineParameterThreshold = 6;
 
         internal override void Write(CodeWriter writer)
         {
-            writer.Append($"new {Type}");
-            if (Parameters.Count > 0 || InitExpression is not { Parameters.Count: > 0 })
+            writer.Append($"new");
+            if (Type is not null)
+                writer.Append($" {Type}");
+            if (Parameters.Count > 0 || InitExpression is not { Values.Count: > 0 })
             {
                 writer.WriteArguments(Parameters, Parameters.Count < SingleLineParameterThreshold);
             }
 
-            if (InitExpression is { Parameters.Count: > 0 })
+            if (InitExpression is not null)
             {
+                if (!InitExpression.UseSingleLine)
+                {
+                    writer.WriteLine();
+                }
+                else
+                {
+                    writer.AppendRaw(" ");
+                }
                 InitExpression.Write(writer);
             }
         }
