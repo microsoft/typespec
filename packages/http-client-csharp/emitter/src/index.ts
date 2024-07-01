36e8a2c312902b14ff5ac91d57790e81c79d107e
FAREAST\chunyu <chunyu@microsoft.com>
2024-07-01 16:35:50 +0800

    export more api

--- packages/http-client-csharp/emitter/src/index.ts
+++ packages/http-client-csharp/emitter/src/index.ts
@@ -1,4 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+import exp from "constants";
+
 export * from "./emitter.js";
+export * from "./options.js";
+export * from "./constants.js";
+export * from "./lib/log-level.js";
+export * from "./lib/logger.js";
+export * from "./lib/lib.js";
