0d3894931e6c251bd8dd23886e706dd2c700b8c0
Christopher Radek <Christopher.Radek@microsoft.com>
2024-07-01 21:22:42 -0700

    rearrange imports

--- packages/openapi3/src/cli/actions/convert/convert.ts
+++ packages/openapi3/src/cli/actions/convert/convert.ts
@@ -1,11 +1,11 @@
 import oaParser from "@readme/openapi-parser";
+import { resolvePath } from "@typespec/compiler";
 import { OpenAPI3Document } from "../../../types.js";
 import { CliHost } from "../../types.js";
 import { handleInternalCompilerError } from "../../utils.js";
 import { ConvertCliArgs } from "./args.js";
 import { generateMain } from "./generators/generate-main.js";
 import { transform } from "./transforms/transforms.js";
-import { resolvePath } from "@typespec/compiler";
 
 export async function convertAction(host: CliHost, args: ConvertCliArgs & { path: string }) {
   // attempt to read the file
