43bdad253b6711d356414ef662646e2c1bdddff5
FAREAST\chunyu <chunyu@microsoft.com>
2024-07-02 13:46:40 +0800

    format

--- packages/http-client-csharp/emitter/src/index.ts
+++ packages/http-client-csharp/emitter/src/index.ts
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import exp from "constants";
-
-export * from "./emitter.js";
-export * from "./options.js";
 export * from "./constants.js";
+export * from "./emitter.js";
+export * from "./lib/lib.js";
 export * from "./lib/log-level.js";
 export * from "./lib/logger.js";
-export * from "./lib/lib.js";
+export * from "./options.js";
