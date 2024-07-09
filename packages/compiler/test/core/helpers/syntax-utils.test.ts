0af0bbdcc61ebe206c33dbf7f9fd78316812cd7e
Timothee Guerin <tiguerin@microsoft.com>
2024-07-09 15:42:25 -0700

    fix

--- packages/compiler/test/core/helpers/syntax-utils.test.ts
+++ packages/compiler/test/core/helpers/syntax-utils.test.ts
@@ -4,6 +4,8 @@ import { printIdentifier } from "../../../src/index.js";
 it.each([
   ["foo", "foo"],
   ["foo-bar", "`foo-bar`"],
+  ["9test", "`9test`"],
+  ["foo bar", "`foo bar`"],
   ["foo\nbar", "`foo\\nbar`"],
 ])("%s -> %s", (a, b) => {
   expect(printIdentifier(a)).toEqual(b);
