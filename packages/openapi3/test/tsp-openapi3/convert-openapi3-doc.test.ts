3bc34c737528f6c3ff5adbbc1f371597d9bc55d2
Christopher Radek <Christopher.Radek@microsoft.com>
2024-07-09 16:16:55 -0700

    remove top-level describe

--- packages/openapi3/test/tsp-openapi3/convert-openapi3-doc.test.ts
+++ packages/openapi3/test/tsp-openapi3/convert-openapi3-doc.test.ts
@@ -1,30 +1,29 @@
 import { formatTypeSpec } from "@typespec/compiler";
 import { strictEqual } from "node:assert";
-import { describe, it } from "vitest";
+import { it } from "vitest";
 import { convertOpenAPI3Document } from "../../src/index.js";
 
-describe("tsp-openapi: convertOpenAPI3Document", () => {
-  it("should convert an OpenAPI3 document to a formatted TypeSpec program", async () => {
-    const tsp = await convertOpenAPI3Document({
-      info: {
-        title: "Test Service",
-        version: "1.0.0",
-      },
-      openapi: "3.0.0",
-      paths: {},
-      components: {
-        schemas: {
-          Foo: {
-            type: "string",
-          },
+it("should convert an OpenAPI3 document to a formatted TypeSpec program", async () => {
+  const tsp = await convertOpenAPI3Document({
+    info: {
+      title: "Test Service",
+      version: "1.0.0",
+    },
+    openapi: "3.0.0",
+    paths: {},
+    components: {
+      schemas: {
+        Foo: {
+          type: "string",
         },
       },
-    });
+    },
+  });
 
-    strictEqual(
-      tsp,
-      await formatTypeSpec(
-        `
+  strictEqual(
+    tsp,
+    await formatTypeSpec(
+      `
       import "@typespec/http";
       import "@typespec/openapi";
       import "@typespec/openapi3";
@@ -42,8 +41,7 @@ describe("tsp-openapi: convertOpenAPI3Document", () => {
 
       scalar Foo extends string;
         `,
-        { printWidth: 100, tabWidth: 2 }
-      )
-    );
-  });
+      { printWidth: 100, tabWidth: 2 }
+    )
+  );
 });
