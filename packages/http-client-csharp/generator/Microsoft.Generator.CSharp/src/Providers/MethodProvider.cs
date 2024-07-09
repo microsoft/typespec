d4f6b14392d7eae8a977a7a63ec73513a173b1a5
m-nash <64171366+m-nash@users.noreply.github.com>
2024-07-09 12:35:33 -0700

    share common code

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Providers/MethodProvider.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Providers/MethodProvider.cs
@@ -30,9 +30,11 @@ namespace Microsoft.Generator.CSharp.Providers
         {
             Signature = signature;
             bool skipParamValidation = !signature.Modifiers.HasFlag(MethodSignatureModifiers.Public);
-            var paramHash = GetParamhash(skipParamValidation);
-            BodyStatements = GetBodyStatementWithValidation(bodyStatements, paramHash);
-            XmlDocs = xmlDocProvider ?? (IsMethodPublic(enclosingType.DeclarationModifiers, signature.Modifiers) ? BuildXmlDocs(paramHash) : null);
+            var paramHash = MethodProviderHelpers.GetParamhash(signature.Parameters, skipParamValidation);
+            BodyStatements = MethodProviderHelpers.GetBodyStatementWithValidation(signature.Parameters, bodyStatements, paramHash);
+            XmlDocs = xmlDocProvider ?? (MethodProviderHelpers.IsMethodPublic(enclosingType.DeclarationModifiers, signature.Modifiers)
+                ? MethodProviderHelpers.BuildXmlDocs(signature.Parameters, signature.Description, signature.ReturnDescription, paramHash)
+                : null);
         }
 
         /// <summary>
@@ -46,96 +48,9 @@ namespace Microsoft.Generator.CSharp.Providers
         {
             Signature = signature;
             BodyExpression = bodyExpression;
-            XmlDocs = xmlDocProvider ?? (IsMethodPublic(enclosingType.DeclarationModifiers, signature.Modifiers) ? BuildXmlDocs(null) : null);
-        }
-
-        private static bool IsMethodPublic(TypeSignatureModifiers typeModifiers, MethodSignatureModifiers methodModifiers)
-        {
-            if (!typeModifiers.HasFlag(TypeSignatureModifiers.Public))
-                return false;
-
-            if (methodModifiers.HasFlag(MethodSignatureModifiers.Public) || (methodModifiers.HasFlag(MethodSignatureModifiers.Protected) && !methodModifiers.HasFlag(MethodSignatureModifiers.Private)))
-                return true;
-
-            return false;
-        }
-
-        private Dictionary<ParameterValidationType, List<ParameterProvider>>? GetParamhash(bool skipParamValidation)
-        {
-            Dictionary<ParameterValidationType, List<ParameterProvider>>? paramHash = null;
-            if (!skipParamValidation)
-            {
-                paramHash = new();
-                foreach (var parameter in Signature.Parameters)
-                {
-                    if (parameter.Validation == ParameterValidationType.None)
-                        continue;
-
-                    if (!paramHash.ContainsKey(parameter.Validation))
-                        paramHash[parameter.Validation] = new List<ParameterProvider>();
-                    paramHash[parameter.Validation].Add(parameter);
-                }
-            }
-            return paramHash;
-        }
-
-        private MethodBodyStatement GetBodyStatementWithValidation(MethodBodyStatement bodyStatements, Dictionary<ParameterValidationType, List<ParameterProvider>>? paramHash)
-        {
-            if (paramHash is null)
-                return bodyStatements;
-
-            int count = 0;
-            foreach (var kvp in paramHash)
-            {
-                if (kvp.Key == ParameterValidationType.None)
-                    continue;
-                count += kvp.Value.Count;
-            }
-
-            if (count == 0)
-                return bodyStatements;
-
-            MethodBodyStatement[] statements = new MethodBodyStatement[count + 2];
-            int index = 0;
-            foreach (var parameter in Signature.Parameters)
-            {
-                if (parameter.Validation != ParameterValidationType.None)
-                {
-                    statements[index] = ValidateParameter(parameter);
-                    index++;
-                }
-            }
-            statements[index] = MethodBodyStatement.EmptyLine;
-            index++;
-
-            statements[index] = bodyStatements;
-
-            return statements;
-        }
-
-        private XmlDocProvider? BuildXmlDocs(Dictionary<ParameterValidationType, List<ParameterProvider>>? paramHash)
-        {
-            var docs = new XmlDocProvider();
-            if (Signature.Description is not null)
-                docs.Summary = new XmlDocSummaryStatement([Signature.Description]);
-
-            foreach (var parameter in Signature.Parameters)
-            {
-                docs.Params.Add(new XmlDocParamStatement(parameter.Name, parameter.Description));
-            }
-
-            if (paramHash is not null)
-            {
-                foreach (var kvp in paramHash)
-                {
-                    docs.Exceptions.Add(new XmlDocExceptionStatement(kvp.Key, kvp.Value));
-                }
-            }
-
-            if (Signature.ReturnDescription is not null)
-                docs.Returns = new XmlDocReturnsStatement(Signature.ReturnDescription);
-
-            return docs;
+            XmlDocs = xmlDocProvider ?? (MethodProviderHelpers.IsMethodPublic(enclosingType.DeclarationModifiers, signature.Modifiers)
+                ? MethodProviderHelpers.BuildXmlDocs(signature.Parameters, signature.Description, signature.ReturnDescription, null)
+                : null);
         }
     }
 }
