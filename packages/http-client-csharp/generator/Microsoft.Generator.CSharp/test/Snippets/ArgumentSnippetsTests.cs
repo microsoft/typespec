437c54d7f3dddb49983c46921dd56f8e24350113
m-nash <64171366+m-nash@users.noreply.github.com>
2024-07-09 09:42:31 -0700

    add missing argument snippet tests

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/Snippets/ArgumentSnippetsTests.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/Snippets/ArgumentSnippetsTests.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Generator.CSharp.Primitives;
+using Microsoft.Generator.CSharp.Providers;
+using Microsoft.Generator.CSharp.Snippets;
+using NUnit.Framework;
 
 namespace Microsoft.Generator.CSharp.Tests.Snippets
 {
-    internal class ArgumentSnippetsTests
+    public class ArgumentSnippetsTests
     {
+
+        public ArgumentSnippetsTests()
+        {
+            MockCodeModelPlugin.LoadMockPlugin();
+        }
+
+        [Test]
+        public void AssertNotNull()
+        {
+            using CodeWriter writer = new CodeWriter();
+            var p = new ParameterProvider("p1", $"p1", new CSharpType(typeof(bool)));
+
+
+            ArgumentSnippets.AssertNotNull(p).Write(writer);
+            Assert.AreEqual("global::Sample.Argument.AssertNotNull(p1, nameof(p1));\n", writer.ToString(false));
+        }
     }
 }
