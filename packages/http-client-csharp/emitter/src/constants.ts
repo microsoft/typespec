301151c93d80d3fa32c562c6ee061b5bcd458f03
FAREAST\chunyu <chunyu@microsoft.com>
2024-07-19 16:27:31 +0800

    remove predefined decorator list

--- packages/http-client-csharp/emitter/src/constants.ts
+++ packages/http-client-csharp/emitter/src/constants.ts
@@ -1,22 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+import { CreateSdkContextOptions } from "@azure-tools/typespec-client-generator-core";
+
 export const projectedNameJsonKey = "json";
 export const projectedNameCSharpKey = "csharp";
 export const projectedNameClientKey = "client";
 export const mockApiVersion = "0000-00-00";
 export const tspOutputFileName = "tspCodeModel.json";
 export const configurationFileName = "Configuration.json";
-export let createSDKContextoptions = {
-  additionalDecorators: ["TypeSpec\\.@projectedName"],
-};
-export function getSDKContextOptions(): any {
+export let createSDKContextoptions: CreateSdkContextOptions = {};
+export function getSDKContextOptions(): CreateSdkContextOptions {
   return createSDKContextoptions;
 }
-export function setSDKContextOptions(options: any): void {
+export function setSDKContextOptions(options: CreateSdkContextOptions): void {
   createSDKContextoptions = options;
 }
 export function addAdditionalDecorators(additionalDecorators: string[]): void {
-  createSDKContextoptions.additionalDecorators =
-    createSDKContextoptions.additionalDecorators.concat(additionalDecorators);
+  if (!createSDKContextoptions.additionalDecorators) {
+    createSDKContextoptions.additionalDecorators = additionalDecorators;
+  } else {
+    createSDKContextoptions.additionalDecorators =
+      createSDKContextoptions.additionalDecorators.concat(additionalDecorators);
+  }
 }
