microsoft/typespec

commit c4aa1abe171898a4075b697a18115e3bb3110010

openapi3 - fixes bug with circular references in unions (#3908)
Fixes #3811 

Root cause seemed to be that when encountering 2+ non-null items in a
union, placeholders weren't getting wrapped by ObjectBuilder and thus
the emitter wasn't resolving circular references correctly.

Co-authored-by: Christopher Radek <Christopher.Radek@microsoft.com>

packages/openapi3/src/schema-emitter.ts
@@ -521,25 +521,16 @@ export class OpenAPI3SchemaEmitter extends TypeEmitter<
       }
     }
 
-    if (schemaMembers.length === 0) {
-      if (nullable) {
-        // This union is equivalent to just `null` but OA3 has no way to specify
-        // null as a value, so we throw an error.
-        reportDiagnostic(program, { code: "union-null", target: union });
-        return new ObjectBuilder({});
-      } else {
-        // completely empty union can maybe only happen with bugs?
-        compilerAssert(false, "Attempting to emit an empty union");
-      }
-    }
-
-    if (schemaMembers.length === 1) {
+    const wrapWithObjectBuilder = (
+      schemaMember: { schema: any; type: Type | null },
+      { applyNullable }: { applyNullable: boolean }
+    ): ObjectBuilder<OpenAPI3Schema> => {
       // we can just return the single schema member after applying nullable
-      const schema = schemaMembers[0].schema;
-      const type = schemaMembers[0].type;
+      const schema = schemaMember.schema;
+      const type = schemaMember.type;
       const additionalProps: Partial<OpenAPI3Schema> = this.#applyConstraints(union, {});
 
-      if (nullable) {
+      if (applyNullable && nullable) {
         additionalProps.nullable = true;
       }
 
@@ -567,10 +558,26 @@ export class OpenAPI3SchemaEmitter extends TypeEmitter<
           return merged;
         }
       }
+    };
+
+    if (schemaMembers.length === 0) {
+      if (nullable) {
+        // This union is equivalent to just `null` but OA3 has no way to specify
+        // null as a value, so we throw an error.
+        reportDiagnostic(program, { code: "union-null", target: union });
+        return new ObjectBuilder({});
+      } else {
+        // completely empty union can maybe only happen with bugs?
+        compilerAssert(false, "Attempting to emit an empty union");
+      }
+    }
+
+    if (schemaMembers.length === 1) {
+      return wrapWithObjectBuilder(schemaMembers[0], { applyNullable: true });
     }
 
     const schema: OpenAPI3Schema = {
-      [ofType]: schemaMembers.map((m) => m.schema),
+      [ofType]: schemaMembers.map((m) => wrapWithObjectBuilder(m, { applyNullable: false })),
     };
 
     if (nullable) {

packages/openapi3/test/circular-references.test.ts
@@ -34,4 +34,20 @@ describe("openapi3: circular reference", () => {
       },
     });
   });
+
+  it("can reference itself via a union property", async () => {
+    const res = await oapiForModel(
+      "Pet",
+      `
+      model Pet { parents?: string | Pet };
+      `
+    );
+
+    deepStrictEqual(res.schemas.Pet, {
+      type: "object",
+      properties: {
+        parents: { anyOf: [{ type: "string" }, { $ref: "#/components/schemas/Pet" }] },
+      },
+    });
+  });
 });

commit d8a476032b34a08e1a9a473f7c28a5f372879d0c

openapi3 - fix bug where union-level docs were applied to members (#3912)
Fixes regression introduced in #3908 where docs set at the union level
were being applied to each union variant.

Co-authored-by: Christopher Radek <Christopher.Radek@microsoft.com>

packages/openapi3/src/schema-emitter.ts
@@ -523,14 +523,16 @@ export class OpenAPI3SchemaEmitter extends TypeEmitter<
 
     const wrapWithObjectBuilder = (
       schemaMember: { schema: any; type: Type | null },
-      { applyNullable }: { applyNullable: boolean }
+      { mergeUnionWideConstraints }: { mergeUnionWideConstraints: boolean }
     ): ObjectBuilder<OpenAPI3Schema> => {
       // we can just return the single schema member after applying nullable
       const schema = schemaMember.schema;
       const type = schemaMember.type;
-      const additionalProps: Partial<OpenAPI3Schema> = this.#applyConstraints(union, {});
+      const additionalProps: Partial<OpenAPI3Schema> = mergeUnionWideConstraints
+        ? this.#applyConstraints(union, {})
+        : {};
 
-      if (applyNullable && nullable) {
+      if (mergeUnionWideConstraints && nullable) {
         additionalProps.nullable = true;
       }
 
@@ -573,11 +575,13 @@ export class OpenAPI3SchemaEmitter extends TypeEmitter<
     }
 
     if (schemaMembers.length === 1) {
-      return wrapWithObjectBuilder(schemaMembers[0], { applyNullable: true });
+      return wrapWithObjectBuilder(schemaMembers[0], { mergeUnionWideConstraints: true });
     }
 
     const schema: OpenAPI3Schema = {
-      [ofType]: schemaMembers.map((m) => wrapWithObjectBuilder(m, { applyNullable: false })),
+      [ofType]: schemaMembers.map((m) =>
+        wrapWithObjectBuilder(m, { mergeUnionWideConstraints: false })
+      ),
     };
 
     if (nullable) {

packages/openapi3/test/union-schema.test.ts
@@ -441,4 +441,24 @@ describe("openapi3: union type", () => {
     strictEqual(res.schemas.Foo.title, "FooUnion");
     strictEqual(res.schemas.Bar.title, "BarUnion");
   });
+
+  it("does not duplicate top-level description on union members", async () => {
+    const res = await oapiForModel(
+      "Foo",
+      `
+      @doc("The possible types of things")
+      union Foo {
+        string,
+
+        bar: "bar",
+        buzz: "buzz",
+      }`
+    );
+
+    strictEqual(res.schemas.Foo.description, "The possible types of things");
+    strictEqual(res.schemas.Foo.anyOf.length, 2);
+    for (const variant of res.schemas.Foo.anyOf) {
+      strictEqual(variant.description, undefined);
+    }
+  });
 });