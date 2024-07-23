63668f0fedd00aebe9f52328175a59692c167f9a
m-nash <64171366+m-nash@users.noreply.github.com>
2024-07-23 12:07:48 -0700

    add validation for fields and properties in rest client provider

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/RestClientProviderTests.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/RestClientProviderTests.cs
@@ -1,21 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Generator.CSharp.ClientModel.Providers;
 using Microsoft.Generator.CSharp.Input;
+using Microsoft.Generator.CSharp.Primitives;
+using Microsoft.Generator.CSharp.Providers;
 using NUnit.Framework;
 
 namespace Microsoft.Generator.CSharp.ClientModel.Tests.Providers
 {
-    internal class RestClientProviderTests
+    public class RestClientProviderTests
     {
+        public RestClientProviderTests()
+        {
+            MockHelpers.LoadMockPlugin();
+        }
+
         [TestCaseSource(nameof(DefaultCSharpMethodCollectionTestCases))]
         public void TestRestClientMethods(InputOperation inputOperation)
         {
-            var inputClient = new InputClient("TestClient", "TestClient description", [inputOperation], new List<InputParameter>(), null);
-            var restClientProvider = new ClientProvider(inputClient).RestClient;
+            var restClientProvider = new ClientProvider(SingleOpInputClient).RestClient;
             MockHelpers.LoadMockPlugin(createMethods: (inputOperation, typeProvider) => new ScmMethodProviderCollection(inputOperation, restClientProvider.ClientProvider));
 
             var methods = restClientProvider.Methods;
@@ -32,34 +38,93 @@ namespace Microsoft.Generator.CSharp.ClientModel.Tests.Providers
             Assert.AreEqual(inputOperation.Parameters.Count + 1, parameters.Count);
         }
 
-        public static IEnumerable<TestCaseData> DefaultCSharpMethodCollectionTestCases
+        [Test]
+        public void ValidateFields()
         {
-            get
-            {
-                yield return new TestCaseData(new InputOperation(
-                    name: "CreateMessage",
-                    resourceName: null,
-                    deprecated: null,
-                    description: string.Empty,
-                    accessibility: null,
-                    parameters: new List<InputParameter>
-                    {
-                        new InputParameter("message", "message", "The message to create.", new InputPrimitiveType(InputPrimitiveTypeKind.Boolean), RequestLocation.Body, null, InputOperationParameterKind.Method, true, false, false, false, false, false, false, null, null)
-                    },
-                    responses: Array.Empty<OperationResponse>(),
-                    httpMethod: "GET",
-                    requestBodyMediaType: BodyMediaType.Json,
-                    uri: "localhost",
-                    path: "/api/messages",
-                    externalDocsUrl: null,
-                    requestMediaTypes: null,
-                    bufferResponse: false,
-                    longRunning: null,
-                    paging: null,
-                    generateProtocolMethod: true,
-                    generateConvenienceMethod: true
-                ));
-            }
+            var restClient = new ClientProvider(SingleOpInputClient).RestClient;
+            Dictionary<string, FieldProvider> fieldHash = restClient.Fields.ToDictionary(f => f.Name);
+
+            //validate _pipelineMessageClassifier200
+            Assert.IsTrue(fieldHash.ContainsKey("_pipelineMessageClassifier200"));
+            var pipelineMessageClassifier200 = fieldHash["_pipelineMessageClassifier200"];
+            Assert.AreEqual("PipelineMessageClassifier", pipelineMessageClassifier200.Type.Name);
+            Assert.AreEqual("_pipelineMessageClassifier200", pipelineMessageClassifier200.Name);
+            Assert.AreEqual(FieldModifiers.Private | FieldModifiers.Static, pipelineMessageClassifier200.Modifiers);
+
+            //validate _pipelineMessageClassifier204
+            Assert.IsTrue(fieldHash.ContainsKey("_pipelineMessageClassifier204"));
+            var pipelineMessageClassifier204 = fieldHash["_pipelineMessageClassifier204"];
+            Assert.AreEqual("PipelineMessageClassifier", pipelineMessageClassifier204.Type.Name);
+            Assert.AreEqual("_pipelineMessageClassifier204", pipelineMessageClassifier204.Name);
+            Assert.AreEqual(FieldModifiers.Private | FieldModifiers.Static, pipelineMessageClassifier204.Modifiers);
+
+            //validate _pipelineMessageClassifier2xxAnd4xx
+            Assert.IsTrue(fieldHash.ContainsKey("_pipelineMessageClassifier2xxAnd4xx"));
+            var pipelineMessageClassifier2xxAnd4xx = fieldHash["_pipelineMessageClassifier2xxAnd4xx"];
+            Assert.AreEqual("Classifier2xxAnd4xx", pipelineMessageClassifier2xxAnd4xx.Type.Name);
+            Assert.AreEqual("_pipelineMessageClassifier2xxAnd4xx", pipelineMessageClassifier2xxAnd4xx.Name);
+            Assert.AreEqual(FieldModifiers.Private | FieldModifiers.Static, pipelineMessageClassifier2xxAnd4xx.Modifiers);
         }
+
+        [Test]
+        public void ValidateProperties()
+        {
+            var restClient = new ClientProvider(SingleOpInputClient).RestClient;
+            Dictionary<string, PropertyProvider> propertyHash = restClient.Properties.ToDictionary(p => p.Name);
+
+            //validate _pipelineMessageClassifier200
+            Assert.IsTrue(propertyHash.ContainsKey("PipelineMessageClassifier200"));
+            var pipelineMessageClassifier200 = propertyHash["PipelineMessageClassifier200"];
+            Assert.AreEqual("PipelineMessageClassifier", pipelineMessageClassifier200.Type.Name);
+            Assert.AreEqual("PipelineMessageClassifier200", pipelineMessageClassifier200.Name);
+            Assert.AreEqual(MethodSignatureModifiers.Private | MethodSignatureModifiers.Static, pipelineMessageClassifier200.Modifiers);
+            Assert.IsFalse(pipelineMessageClassifier200.Body.HasSetter);
+
+            //validate _pipelineMessageClassifier204
+            Assert.IsTrue(propertyHash.ContainsKey("PipelineMessageClassifier204"));
+            var pipelineMessageClassifier204 = propertyHash["PipelineMessageClassifier204"];
+            Assert.AreEqual("PipelineMessageClassifier", pipelineMessageClassifier204.Type.Name);
+            Assert.AreEqual("PipelineMessageClassifier204", pipelineMessageClassifier204.Name);
+            Assert.AreEqual(MethodSignatureModifiers.Private | MethodSignatureModifiers.Static, pipelineMessageClassifier204.Modifiers);
+            Assert.IsFalse(pipelineMessageClassifier204.Body.HasSetter);
+
+            //validate _pipelineMessageClassifier2xxAnd4xx
+            Assert.IsTrue(propertyHash.ContainsKey("PipelineMessageClassifier2xxAnd4xx"));
+            var pipelineMessageClassifier2xxAnd4xx = propertyHash["PipelineMessageClassifier2xxAnd4xx"];
+            Assert.AreEqual("Classifier2xxAnd4xx", pipelineMessageClassifier2xxAnd4xx.Type.Name);
+            Assert.AreEqual("PipelineMessageClassifier2xxAnd4xx", pipelineMessageClassifier2xxAnd4xx.Name);
+            Assert.AreEqual(MethodSignatureModifiers.Private | MethodSignatureModifiers.Static, pipelineMessageClassifier2xxAnd4xx.Modifiers);
+            Assert.IsFalse(pipelineMessageClassifier2xxAnd4xx.Body.HasSetter);
+        }
+
+        private readonly static InputOperation BasicOperation = new InputOperation(
+            name: "CreateMessage",
+            resourceName: null,
+            deprecated: null,
+            description: string.Empty,
+            accessibility: null,
+            parameters:
+            [
+                new InputParameter("message", "message", "The message to create.", new InputPrimitiveType(InputPrimitiveTypeKind.Boolean), RequestLocation.Body, null, InputOperationParameterKind.Method, true, false, false, false, false, false, false, null, null)
+            ],
+            responses: [],
+            httpMethod: "GET",
+            requestBodyMediaType: BodyMediaType.Json,
+            uri: "localhost",
+            path: "/api/messages",
+            externalDocsUrl: null,
+            requestMediaTypes: null,
+            bufferResponse: false,
+            longRunning: null,
+            paging: null,
+            generateProtocolMethod: true,
+            generateConvenienceMethod: true);
+
+        private readonly static InputClient SingleOpInputClient = new InputClient("TestClient", "TestClient description", [BasicOperation], [], null);
+
+        private static IEnumerable<TestCaseData> DefaultCSharpMethodCollectionTestCases =>
+        [
+            new TestCaseData(BasicOperation)
+        ];
     }
 }
