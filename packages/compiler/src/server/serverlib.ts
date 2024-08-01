36727aac2a2aae5b04b5ea310e1f89d5ea71e114
Timothee Guerin <tiguerin@microsoft.com>
2024-08-01 10:00:00 -0700

    Import prettier dynamically

--- packages/compiler/src/server/serverlib.ts
+++ packages/compiler/src/server/serverlib.ts
@@ -1,5 +1,3 @@
-import { resolveConfig } from "prettier";
-import { inspect } from "util";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import {
   CodeAction,
@@ -593,17 +591,27 @@ export function createServer(host: ServerHost): Server {
       return [];
     }
     const path = await fileService.fileURLToRealPath(params.textDocument.uri);
-    const prettierConfig = await resolveConfig(path);
+    const prettierConfig = await resolvePrettierConfig(path);
     const resolvedConfig = prettierConfig ?? {
       tabWidth: params.options.tabSize,
       useTabs: !params.options.insertSpaces,
     };
     log({
       level: "info",
-      message: `Formatting TypeSpec document: ${inspect({ fileUri: params.textDocument.uri, vscodeOptions: params.options, prettierConfig, resolvedConfig })}`,
+      message: `Formatting TypeSpec document: ${JSON.stringify({ fileUri: params.textDocument.uri, vscodeOptions: params.options, prettierConfig, resolvedConfig }, null, 2)}`,
     });
     const formattedText = await formatTypeSpec(document.getText(), resolvedConfig);
     return [minimalEdit(document, formattedText)];
+  }
+
+  async function resolvePrettierConfig(path: string) {
+    try {
+      // Resolve prettier if it is installed.
+      const prettier = await import("prettier");
+      return prettier.resolveConfig(path);
+    } catch (e) {
+      return null;
+    }
   }
 
   function minimalEdit(document: TextDocument, string1: string): TextEdit {
