75e773cfa897c4fd538acaa5e188ae12bd7fa1ae
Timothee Guerin <tiguerin@microsoft.com>
2024-08-12 12:16:21 -0700

    Add new way to define decorator implementation with `$decorators` export  (#4139)
    fix [#3719](https://github.com/microsoft/typespec/issues/3719)

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -1,4 +1,6 @@
-export const namespace = "TypeSpec.Rest";
+export { $lib } from "./lib.js";
 export * from "./resource.js";
 export * from "./rest.js";
-export * from "./validate.js";
+
+/** @internal */
+export { $decorators } from "./tsp-index.js";
