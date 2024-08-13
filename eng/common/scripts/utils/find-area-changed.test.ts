495ec47026888289a1f3d26cddd359a4e3ea938d
iscai-msft <isabellavcai@gmail.com>
2024-08-13 15:10:14 -0400

    initial pass-through of top-level packages

--- eng/common/scripts/utils/find-area-changed.test.ts
+++ eng/common/scripts/utils/find-area-changed.test.ts
@@ -28,6 +28,18 @@ describe("paths that should trigger Java CI", () => {
   });
 });
 
+describe("paths that should trigger python CI", () => {
+  it.each([
+    ["packages/http-client-python/emitter/src/emitter.ts"],
+    [
+      "packages/http-client-python/package.json"
+    ],
+  ])("%s", (...paths) => {
+    const areas = findAreasChanged(paths);
+    expect(areas).toEqual(["Java"]);
+  });
+});
+
 describe("paths that should trigger Core CI", () => {
   it.each([
     "packages/compiler/package.json",
@@ -45,7 +57,7 @@ describe("paths that should trigger all isolated packages", () => {
     "eng/emitters/pipelines/templates/jobs/test-job.yml",
   ])("%s", (path) => {
     const areas = findAreasChanged([path]);
-    expect(areas).toEqual(["CSharp", "Java"]);
+    expect(areas).toEqual(["CSharp", "Java", "Python"]);
   });
 });
 
@@ -53,9 +65,10 @@ it("Should return a combination of core and isolated packages", () => {
   const areas = findAreasChanged([
     "packages/http-client-csharp/src/constants.ts",
     "packages/http-client-java/src/emitter.ts",
+    "packages/http-client-python/src/emitter.ts",
     "packages/compiler/package.json",
   ]);
-  expect(areas).toEqual(["CSharp", "Java", "Core"]);
+  expect(areas).toEqual(["CSharp", "Java", "Python", "Core"]);
 });
 
 it("Should return CSharp, Core and Java if .editorconfig is changed", () => {
@@ -71,8 +84,9 @@ it("Should not return Core for .prettierignore, .prettierrc.json, cspell.yaml, e
     "esling.config.json",
     "packages/http-client-csharp/emitter/src/constants.ts",
     "packages/http-client-java/emitter/src/emitter.ts",
+    "packages/http-client-python/emitter/src/emitter.ts",
   ]);
-  expect(areas).toEqual(["CSharp", "Java"]);
+  expect(areas).toEqual(["CSharp", "Java", "Python"]);
 });
 
 it("should return Core for random files at the root", () => {
