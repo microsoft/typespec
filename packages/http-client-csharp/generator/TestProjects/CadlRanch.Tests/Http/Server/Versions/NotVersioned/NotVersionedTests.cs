88a63541b38bfbfa3fb5821850a7cfce1ceb2011
FAREAST\chunyu <chunyu@microsoft.com>
2024-08-19 09:08:31 +0800

    set test as cadlReanchTest

--- packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Http/Server/Versions/NotVersioned/NotVersionedTests.cs
+++ packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Http/Server/Versions/NotVersioned/NotVersionedTests.cs
@@ -7,21 +7,21 @@ namespace TestProjects.CadlRanch.Tests.Http.Server.Versions.NotVersioned
 {
     public class NotVersionedTests : CadlRanchTestBase
     {
-        [Test]
+        [CadlRanchTest]
         public Task WithoutApiVersion() => Test(async (host) =>
         {
             var response = await new NotVersionedClient(host, null).WithoutApiVersionAsync();
             Assert.AreEqual(200, response.GetRawResponse().Status);
         });
 
-        [Test]
+        [CadlRanchTest]
         public Task WithQueryApiVersion() => Test(async (host) =>
         {
             var response = await new NotVersionedClient(host, null).WithQueryApiVersionAsync("v1.0");
             Assert.AreEqual(200, response.GetRawResponse().Status);
         });
 
-        [Test]
+        [CadlRanchTest]
         public Task WithPathApiVersion() => Test(async (host) =>
         {
             var response = await new NotVersionedClient(host, null).WithPathApiVersionAsync("v1.0");
