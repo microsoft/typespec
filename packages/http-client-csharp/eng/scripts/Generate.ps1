d6b36fbaefa126fd8b77c701305084af99713045
m-nash <64171366+m-nash@users.noreply.github.com>
2024-08-20 09:15:02 -0700

    Http type array (#4212)
    Fixes https://github.com/microsoft/typespec/issues/3992

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -72,7 +72,6 @@ $failingSpecs = @(
     Join-Path 'http' 'server' 'versions' 'versioned'
     Join-Path 'http' 'special-headers' 'conditional-request'
     Join-Path 'http' 'special-headers' 'repeatability'
-    Join-Path 'http' 'type' 'array'
     Join-Path 'http' 'type' 'dictionary'
     Join-Path 'http' 'type' 'scalar'
     Join-Path 'http' 'type' 'union'
