0272c64890d638bfc65d25339a7315f0135d581b
m-nash <64171366+m-nash@users.noreply.github.com>
2024-08-21 21:18:31 -0700

    use cadl ranch tests

--- packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Http/_Type/Model/Empty/EmptyTests.cs
+++ packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Http/_Type/Model/Empty/EmptyTests.cs
@@ -10,21 +10,21 @@ namespace TestProjects.CadlRanch.Tests.Http._Type.Model.Empty
 {
     internal class EmptyTests : CadlRanchTestBase
     {
-        [Test]
+        [CadlRanchTest]
         public Task Type_Model_Empty_putEmpty() => Test(async (host) =>
         {
             var response = await new EmptyClient(host, null).PutEmptyAsync(new EmptyInput());
             Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
-        [Test]
+        [CadlRanchTest]
         public Task Type_Model_Empty_getEmpty() => Test(async (host) =>
         {
             var response = await new EmptyClient(host, null).GetEmptyAsync();
             Assert.AreEqual(200, response.GetRawResponse().Status);
         });
 
-        [Test]
+        [CadlRanchTest]
         public Task Type_Model_Empty_postRoundTripEmpty() => Test(async (host) =>
         {
             var response = await new EmptyClient(host, null).PostRoundTripEmptyAsync(new EmptyInputOutput());
