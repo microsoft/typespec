7997d9dd44770202db0e3fd9c518ce574d967006
m-nash <64171366+m-nash@users.noreply.github.com>
2024-08-21 22:53:23 -0700

    Adopt http/type/model/usage (#4237)
    Fixes https://github.com/microsoft/typespec/issues/4000

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -74,7 +74,6 @@ $failingSpecs = @(
     Join-Path 'http' 'special-headers' 'repeatability'
     Join-Path 'http' 'type' 'dictionary'
     Join-Path 'http' 'type' 'model' 'flatten'
-    Join-Path 'http' 'type' 'model' 'usage'
     Join-Path 'http' 'type' 'model' 'visibility'
     Join-Path 'http' 'type' 'model' 'inheritance' 'enum-discriminator'
     Join-Path 'http' 'type' 'model' 'inheritance' 'nested-discriminator'
