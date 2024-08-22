fdb9a107bf004d10531f918014505f2b408b48d5
m-nash <64171366+m-nash@users.noreply.github.com>
2024-08-21 19:52:35 -0700

    update checked in state

--- packages/http-client-csharp/generator/TestProjects/CadlRanch/http/type/enum/extensible/src/Generated/String.cs
+++ packages/http-client-csharp/generator/TestProjects/CadlRanch/http/type/enum/extensible/src/Generated/String.cs
@@ -2,7 +2,6 @@
 
 #nullable disable
 
-using System;
 using System.ClientModel;
 using System.ClientModel.Primitives;
 using System.Threading.Tasks;
@@ -10,239 +9,42 @@ using _Type.Enum.Extensible.Models;
 
 namespace _Type.Enum.Extensible
 {
-    /// <summary></summary>
     public partial class String
     {
-        private readonly Uri _endpoint;
+        protected String() => throw null;
 
-        /// <summary> Initializes a new instance of String for mocking. </summary>
-        protected String()
-        {
-        }
+        public ClientPipeline Pipeline => throw null;
 
-        internal String(ClientPipeline pipeline, Uri endpoint)
-        {
-            _endpoint = endpoint;
-            Pipeline = pipeline;
-        }
+        public virtual ClientResult GetKnownValue(RequestOptions options) => throw null;
 
-        /// <summary> The HTTP pipeline for sending and receiving REST requests and responses. </summary>
-        public ClientPipeline Pipeline { get; }
+        public virtual Task<ClientResult> GetKnownValueAsync(RequestOptions options) => throw null;
 
-        /// <summary>
-        /// [Protocol Method] getKnownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual ClientResult GetKnownValue(RequestOptions options)
-        {
-            using PipelineMessage message = CreateGetKnownValueRequest(options);
-            return ClientResult.FromResponse(Pipeline.ProcessMessage(message, options));
-        }
+        public virtual ClientResult<DaysOfWeekExtensibleEnum> GetKnownValue() => throw null;
 
-        /// <summary>
-        /// [Protocol Method] getKnownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual async Task<ClientResult> GetKnownValueAsync(RequestOptions options)
-        {
-            using PipelineMessage message = CreateGetKnownValueRequest(options);
-            return ClientResult.FromResponse(await Pipeline.ProcessMessageAsync(message, options).ConfigureAwait(false));
-        }
+        public virtual Task<ClientResult<DaysOfWeekExtensibleEnum>> GetKnownValueAsync() => throw null;
 
-        /// <summary> getKnownValue. </summary>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual ClientResult<DaysOfWeekExtensibleEnum> GetKnownValue()
-        {
-            ClientResult result = GetKnownValue(null);
-            return ClientResult.FromValue(new DaysOfWeekExtensibleEnum(result.GetRawResponse().Content.ToObjectFromJson<string>()), result.GetRawResponse());
-        }
+        public virtual ClientResult GetUnknownValue(RequestOptions options) => throw null;
 
-        /// <summary> getKnownValue. </summary>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual async Task<ClientResult<DaysOfWeekExtensibleEnum>> GetKnownValueAsync()
-        {
-            ClientResult result = await GetKnownValueAsync(null).ConfigureAwait(false);
-            return ClientResult.FromValue(new DaysOfWeekExtensibleEnum(result.GetRawResponse().Content.ToObjectFromJson<string>()), result.GetRawResponse());
-        }
+        public virtual Task<ClientResult> GetUnknownValueAsync(RequestOptions options) => throw null;
 
-        /// <summary>
-        /// [Protocol Method] getUnknownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual ClientResult GetUnknownValue(RequestOptions options)
-        {
-            using PipelineMessage message = CreateGetUnknownValueRequest(options);
-            return ClientResult.FromResponse(Pipeline.ProcessMessage(message, options));
-        }
+        public virtual ClientResult<DaysOfWeekExtensibleEnum> GetUnknownValue() => throw null;
 
-        /// <summary>
-        /// [Protocol Method] getUnknownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual async Task<ClientResult> GetUnknownValueAsync(RequestOptions options)
-        {
-            using PipelineMessage message = CreateGetUnknownValueRequest(options);
-            return ClientResult.FromResponse(await Pipeline.ProcessMessageAsync(message, options).ConfigureAwait(false));
-        }
+        public virtual Task<ClientResult<DaysOfWeekExtensibleEnum>> GetUnknownValueAsync() => throw null;
 
-        /// <summary> getUnknownValue. </summary>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual ClientResult<DaysOfWeekExtensibleEnum> GetUnknownValue()
-        {
-            ClientResult result = GetUnknownValue(null);
-            return ClientResult.FromValue(new DaysOfWeekExtensibleEnum(result.GetRawResponse().Content.ToObjectFromJson<string>()), result.GetRawResponse());
-        }
+        public virtual ClientResult PutKnownValue(BinaryContent content, RequestOptions options) => throw null;
 
-        /// <summary> getUnknownValue. </summary>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual async Task<ClientResult<DaysOfWeekExtensibleEnum>> GetUnknownValueAsync()
-        {
-            ClientResult result = await GetUnknownValueAsync(null).ConfigureAwait(false);
-            return ClientResult.FromValue(new DaysOfWeekExtensibleEnum(result.GetRawResponse().Content.ToObjectFromJson<string>()), result.GetRawResponse());
-        }
+        public virtual Task<ClientResult> PutKnownValueAsync(BinaryContent content, RequestOptions options) => throw null;
 
-        /// <summary>
-        /// [Protocol Method] putKnownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="content"> The content to send as the body of the request. </param>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="content"/> is null. </exception>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual ClientResult PutKnownValue(BinaryContent content, RequestOptions options)
-        {
-            Argument.AssertNotNull(content, nameof(content));
+        public virtual ClientResult PutKnownValue(DaysOfWeekExtensibleEnum body) => throw null;
 
-            using PipelineMessage message = CreatePutKnownValueRequest(content, options);
-            return ClientResult.FromResponse(Pipeline.ProcessMessage(message, options));
-        }
+        public virtual Task<ClientResult> PutKnownValueAsync(DaysOfWeekExtensibleEnum body) => throw null;
 
-        /// <summary>
-        /// [Protocol Method] putKnownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="content"> The content to send as the body of the request. </param>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="content"/> is null. </exception>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual async Task<ClientResult> PutKnownValueAsync(BinaryContent content, RequestOptions options)
-        {
-            Argument.AssertNotNull(content, nameof(content));
+        public virtual ClientResult PutUnknownValue(BinaryContent content, RequestOptions options) => throw null;
 
-            using PipelineMessage message = CreatePutKnownValueRequest(content, options);
-            return ClientResult.FromResponse(await Pipeline.ProcessMessageAsync(message, options).ConfigureAwait(false));
-        }
+        public virtual Task<ClientResult> PutUnknownValueAsync(BinaryContent content, RequestOptions options) => throw null;
 
-        /// <summary> putKnownValue. </summary>
-        /// <param name="body"></param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual ClientResult PutKnownValue(DaysOfWeekExtensibleEnum body)
-        {
-            return PutKnownValue(BinaryContent.Create(BinaryData.FromObjectAsJson(body.ToString())), null);
-        }
+        public virtual ClientResult PutUnknownValue(DaysOfWeekExtensibleEnum body) => throw null;
 
-        /// <summary> putKnownValue. </summary>
-        /// <param name="body"></param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual async Task<ClientResult> PutKnownValueAsync(DaysOfWeekExtensibleEnum body)
-        {
-            return await PutKnownValueAsync(BinaryContent.Create(BinaryData.FromObjectAsJson(body.ToString())), null).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// [Protocol Method] putUnknownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="content"> The content to send as the body of the request. </param>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="content"/> is null. </exception>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual ClientResult PutUnknownValue(BinaryContent content, RequestOptions options)
-        {
-            Argument.AssertNotNull(content, nameof(content));
-
-            using PipelineMessage message = CreatePutUnknownValueRequest(content, options);
-            return ClientResult.FromResponse(Pipeline.ProcessMessage(message, options));
-        }
-
-        /// <summary>
-        /// [Protocol Method] putUnknownValue
-        /// <list type="bullet">
-        /// <item>
-        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="content"> The content to send as the body of the request. </param>
-        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="content"/> is null. </exception>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        /// <returns> The response returned from the service. </returns>
-        public virtual async Task<ClientResult> PutUnknownValueAsync(BinaryContent content, RequestOptions options)
-        {
-            Argument.AssertNotNull(content, nameof(content));
-
-            using PipelineMessage message = CreatePutUnknownValueRequest(content, options);
-            return ClientResult.FromResponse(await Pipeline.ProcessMessageAsync(message, options).ConfigureAwait(false));
-        }
-
-        /// <summary> putUnknownValue. </summary>
-        /// <param name="body"></param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual ClientResult PutUnknownValue(DaysOfWeekExtensibleEnum body)
-        {
-            return PutUnknownValue(BinaryContent.Create(BinaryData.FromObjectAsJson(body.ToString())), null);
-        }
-
-        /// <summary> putUnknownValue. </summary>
-        /// <param name="body"></param>
-        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
-        public virtual async Task<ClientResult> PutUnknownValueAsync(DaysOfWeekExtensibleEnum body)
-        {
-            return await PutUnknownValueAsync(BinaryContent.Create(BinaryData.FromObjectAsJson(body.ToString())), null).ConfigureAwait(false);
-        }
+        public virtual Task<ClientResult> PutUnknownValueAsync(DaysOfWeekExtensibleEnum body) => throw null;
     }
 }
