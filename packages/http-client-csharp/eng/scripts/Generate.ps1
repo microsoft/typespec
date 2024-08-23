23d00156cfdccc551cae71335318758823275fe9
ShivangiReja <45216704+ShivangiReja@users.noreply.github.com>
2024-08-23 10:16:14 -0700

    Adopt http\type\dictionary from cadl ranch (#4246)
    Fixes: https://github.com/microsoft/typespec/issues/3993
    
    ---------
    
    Co-authored-by: ShivangiReja <shivangi.reja@microsoft.com>

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -71,7 +71,6 @@ $failingSpecs = @(
     Join-Path 'http' 'server' 'versions' 'versioned'
     Join-Path 'http' 'special-headers' 'conditional-request'
     Join-Path 'http' 'special-headers' 'repeatability'
-    Join-Path 'http' 'type' 'dictionary'
     Join-Path 'http' 'type' 'model' 'flatten'
     Join-Path 'http' 'type' 'model' 'visibility'
     Join-Path 'http' 'type' 'model' 'inheritance' 'enum-discriminator'
