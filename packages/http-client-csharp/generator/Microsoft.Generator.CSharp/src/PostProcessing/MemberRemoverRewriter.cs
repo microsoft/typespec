d7808df911ec72ee27045130f82a1d6538e089ae
jolov <jolov@microsoft.com>
2024-08-29 12:38:41 -0700

    Remove suppressions

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/PostProcessing/MemberRemoverRewriter.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/PostProcessing/MemberRemoverRewriter.cs
@@ -1,31 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.Generator.CSharp.SourceInput;
 
 namespace Microsoft.Generator.CSharp
 {
-#pragma warning disable RS1024
     internal class MemberRemoverRewriter : CSharpSyntaxRewriter
     {
-        private static readonly SymbolDisplayFormat _fullyQualifiedNameFormat
-            = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces);
-
         private readonly Project _project;
         private readonly SemanticModel _semanticModel;
-        private readonly Dictionary<INamedTypeSymbol, List<Supression>> _suppressionCache;
 
         public MemberRemoverRewriter(Project project, SemanticModel semanticModel)
         {
             _project = project;
             _semanticModel = semanticModel;
-            _suppressionCache = new Dictionary<INamedTypeSymbol, List<Supression>>();
         }
 
         public override SyntaxNode? VisitCompilationUnit(CompilationUnitSyntax node)
@@ -79,89 +70,6 @@ namespace Microsoft.Generator.CSharp
             return ShouldRemoveMember(symbol) ? null : base.VisitFieldDeclaration(node);
         }
 
-        private List<Supression>? GetSuppressions(INamedTypeSymbol namedTypeSymbol)
-        {
-            if (_suppressionCache.TryGetValue(namedTypeSymbol, out var suppressions))
-            {
-                return suppressions;
-            }
-
-            foreach (var attributeData in namedTypeSymbol.GetAttributes())
-            {
-                if (attributeData.AttributeClass?.Name.Equals(CodeGenAttributes.CodeGenSuppressAttributeName) == true)
-                {
-                    ValidateArguments(namedTypeSymbol, attributeData);
-
-                    suppressions ??= new List<Supression>();
-                    var name = attributeData.ConstructorArguments[0].Value as string;
-                    var parameterTypes = attributeData.ConstructorArguments[1].Values.Select(v => (ISymbol?)v.Value).ToArray();
-                    suppressions.Add(new Supression(name, parameterTypes));
-                }
-            }
-
-            if (suppressions != null)
-            {
-                _suppressionCache.Add(namedTypeSymbol, suppressions);
-            }
-            return suppressions;
-
-            static void ValidateArguments(INamedTypeSymbol typeSymbol, AttributeData attributeData)
-            {
-                var arguments = attributeData.ConstructorArguments;
-                if (arguments.Length == 0)
-                {
-                    var fullName = typeSymbol.ToDisplayString(_fullyQualifiedNameFormat);
-                    throw new InvalidOperationException($"CodeGenSuppress attribute on {fullName} must specify a method name as its first argument.");
-                }
-
-                if (arguments.Length == 1 || arguments[0].Kind != TypedConstantKind.Primitive || arguments[0].Value is not string)
-                {
-                    var attribute = GetText(attributeData.ApplicationSyntaxReference);
-                    var fullName = typeSymbol.ToDisplayString(_fullyQualifiedNameFormat);
-                    throw new InvalidOperationException($"{attribute} attribute on {fullName} must specify a method name as its first argument.");
-                }
-
-                if (arguments.Length == 2 && arguments[1].Kind == TypedConstantKind.Array)
-                {
-                    ValidateTypeArguments(typeSymbol, attributeData, arguments[1].Values);
-                }
-                else
-                {
-                    ValidateTypeArguments(typeSymbol, attributeData, arguments.Skip(1));
-                }
-            }
-
-            static void ValidateTypeArguments(INamedTypeSymbol typeSymbol, AttributeData attributeData, IEnumerable<TypedConstant> arguments)
-            {
-                foreach (var argument in arguments)
-                {
-                    if (argument.Kind == TypedConstantKind.Type)
-                    {
-                        if (argument.Value is IErrorTypeSymbol errorType)
-                        {
-                            var attribute = GetText(attributeData.ApplicationSyntaxReference);
-                            var fileLinePosition = GetFileLinePosition(attributeData.ApplicationSyntaxReference);
-                            var filePath = fileLinePosition.Path;
-                            var line = fileLinePosition.StartLinePosition.Line + 1;
-                            throw new InvalidOperationException($"The undefined type '{errorType.Name}' is referenced in the '{attribute}' attribute ({filePath}, line: {line}). Please define this type or remove it from the attribute.");
-                        }
-                    }
-                    else
-                    {
-                        var fullName = typeSymbol.ToDisplayString(_fullyQualifiedNameFormat);
-                        var attribute = GetText(attributeData.ApplicationSyntaxReference);
-                        throw new InvalidOperationException($"Argument '{argument.ToCSharpString()}' in attribute '{attribute}' applied to '{fullName}' must be a type.");
-                    }
-                }
-            }
-        }
-
-        public static string GetText(SyntaxReference? syntaxReference)
-            => syntaxReference?.SyntaxTree.GetText().ToString(syntaxReference.Span) ?? string.Empty;
-
-        public static FileLinePositionSpan GetFileLinePosition(SyntaxReference? syntaxReference)
-            => syntaxReference?.SyntaxTree.GetLocation(syntaxReference.Span).GetLineSpan() ?? default;
-
         private bool ShouldRemoveMember(ISymbol? symbol)
         {
             if (symbol != null)
@@ -169,29 +77,18 @@ namespace Microsoft.Generator.CSharp
                 INamedTypeSymbol? containingType = symbol.ContainingType;
                 IMethodSymbol? methodSymbol = symbol as IMethodSymbol;
 
-                var suppressions = GetSuppressions(symbol.ContainingType);
-                if (suppressions != null)
-                {
-                    foreach (var suppression in suppressions)
-                    {
-                        if (suppression.Matches(symbol))
-                        {
-                            return true;
-                        }
-                    }
-                }
-
                 while (containingType != null)
                 {
                     var members = containingType.GetMembers(symbol.Name);
                     foreach (var member in members)
                     {
-                        if (!member.Equals(symbol) &&
+                        if (!SymbolEqualityComparer.Default.Equals(member, symbol) &&
                             IsDeclaredInNonGeneratedCode(member))
                         {
                             if (methodSymbol != null &&
                                 member is IMethodSymbol memberMethodSymbol &&
-                                !methodSymbol.Parameters.SequenceEqual(memberMethodSymbol.Parameters, (s1, s2) => s1.Type.Equals(s2.Type)))
+                                !methodSymbol.Parameters.SequenceEqual(
+                                    memberMethodSymbol.Parameters, (s1, s2) => SymbolEqualityComparer.Default.Equals(s1, s2.Type)))
                             {
                                 continue;
                             }
