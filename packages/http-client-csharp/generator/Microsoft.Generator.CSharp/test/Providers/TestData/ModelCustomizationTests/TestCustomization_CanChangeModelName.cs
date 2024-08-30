6f5dd1f5b22b9c2315d2a23adbbc4234713b5b71
Dapeng Zhang <dapzhang@microsoft.com>
2024-08-30 15:30:19 +0800

    add todo

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/Providers/TestData/ModelCustomizationTests/TestCustomization_CanChangeModelName.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/Providers/TestData/ModelCustomizationTests/TestCustomization_CanChangeModelName.cs
@@ -4,6 +4,7 @@ using System;
 
 namespace NewNamespace
 {
+    // TODO: if we decide to use the public APIs, we do not have to define this attribute here. Tracking: https://github.com/Azure/autorest.csharp/issues/4551
     [AttributeUsage(AttributeTargets.Class)]
     internal class CodeGenTypeAttribute : Attribute
     {
