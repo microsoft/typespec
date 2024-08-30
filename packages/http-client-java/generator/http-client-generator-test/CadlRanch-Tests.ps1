7fa45cb80e4c9d63994aa6220f89a29d95eac7bc
Srikanta Nagaraja <srnagar@microsoft.com>
2024-08-30 13:32:11 -0700

    fix filename

--- packages/http-client-java/generator/http-client-generator-test/CadlRanch-Tests.ps1
+++ packages/http-client-java/generator/http-client-generator-test/CadlRanch-Tests.ps1
@@ -7,13 +7,13 @@ try {
     Write-Host "Running cadl ranch tests"
 
     Write-Host "Starting the test server"
-    if (Test-Path node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.j) {
+    if (Test-Path node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.js) {
         Invoke-Expression "node node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.js serve node_modules/\@azure-tools/cadl-ranch-specs/http/ --coverageFile ./cadl-ranch-coverage-java.json &"
         mvn clean test
         Write-Host "Stopping the test server"
         Invoke-Expression "node node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.js server stop"
     } else {
-        Get-ChildItem -Path node_modules/\@azure-tools/ -Recurse
+        Get-ChildItem -Path node_modules/\@azure-tools/ -Recurse -Name
         Write-Host "cadl-ranch is not installed. Skipping the tests"
     }
 }
