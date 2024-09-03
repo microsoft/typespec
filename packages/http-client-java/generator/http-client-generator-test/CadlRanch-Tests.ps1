cbafd0f721d96529924754d92f5400d5193dbb38
Srikanta Nagaraja <srnagar@microsoft.com>
2024-09-02 23:58:10 -0700

    use npx commands to run cadl ranch server

--- packages/http-client-java/generator/http-client-generator-test/CadlRanch-Tests.ps1
+++ packages/http-client-java/generator/http-client-generator-test/CadlRanch-Tests.ps1
@@ -6,12 +6,12 @@ Set-StrictMode -Version 3.0
 try {
     Write-Host "Running cadl ranch tests"
 
-    Write-Host "Starting the test server"
     if (Test-Path node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.js) {
-        Invoke-Expression "node node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.js serve node_modules/\@azure-tools/cadl-ranch-specs/http/ --coverageFile ./cadl-ranch-coverage-java.json &"
+        Write-Host "Starting the test server"
+        npm run testserver-start
         mvn clean test
         Write-Host "Stopping the test server"
-        Invoke-Expression "node node_modules/\@azure-tools/cadl-ranch/dist/cli/cli.js server stop"
+        npm run testserver-stop
     } else {
         Get-ChildItem -Path node_modules/\@azure-tools/ -Recurse -Name
         Write-Host "cadl-ranch is not installed. Skipping the tests"
