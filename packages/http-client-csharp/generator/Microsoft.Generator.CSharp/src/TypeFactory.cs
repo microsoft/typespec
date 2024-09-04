4a4d0ff37129ca474087bdd0765dedb9760ddd35
jolov <jolov@microsoft.com>
2024-09-04 13:46:32 -0700

    Make privatge

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/TypeFactory.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/TypeFactory.cs
@@ -61,7 +61,7 @@ namespace Microsoft.Generator.CSharp
             return type;
         }
 
-        private protected virtual CSharpType? CreateCSharpTypeCore(InputType inputType)
+        private CSharpType? CreateCSharpTypeCore(InputType inputType)
         {
             CSharpType? type;
             switch (inputType)
