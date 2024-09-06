86cc361bf0a0ed29dbe80613cc2435331786cdce
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2024-09-06 15:56:05 -0700

    Add tests for changing property type/accessibility (#4365)
    Validates that https://github.com/microsoft/typespec/issues/4258 and
    https://github.com/microsoft/typespec/issues/4265 are fixed. They were
    fixed by https://github.com/microsoft/typespec/pull/4362.

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/Providers/ModelProviders/ModelCustomizationTests.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/Providers/ModelProviders/ModelCustomizationTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using Microsoft.Generator.CSharp.Input;
+using Microsoft.Generator.CSharp.Primitives;
 using Microsoft.Generator.CSharp.Providers;
 using Microsoft.Generator.CSharp.Tests.Common;
 using NUnit.Framework;
@@ -25,11 +26,7 @@ namespace Microsoft.Generator.CSharp.Tests.Providers // the namespace here is cr
             var modelTypeProvider = new ModelProvider(inputModel);
             var customCodeView = modelTypeProvider.CustomCodeView;
 
-            Assert.IsNotNull(customCodeView);
-            Assert.AreEqual("CustomizedModel", modelTypeProvider.Type.Name);
-            Assert.AreEqual("NewNamespace.Models", modelTypeProvider.Type.Namespace);
-            Assert.AreEqual(customCodeView?.Name, modelTypeProvider.Type.Name);
-            Assert.AreEqual(customCodeView?.Type.Namespace, modelTypeProvider.Type.Namespace);
+            AssertCommon(customCodeView, modelTypeProvider, "NewNamespace.Models", "CustomizedModel");
         }
 
         [Test]
@@ -46,11 +43,7 @@ namespace Microsoft.Generator.CSharp.Tests.Providers // the namespace here is cr
             var modelTypeProvider = new ModelProvider(inputModel);
             var customCodeView = modelTypeProvider.CustomCodeView;
 
-            Assert.IsNotNull(customCodeView);
-            Assert.AreEqual("MockInputModel", modelTypeProvider.Type.Name);
-            Assert.AreEqual("Sample.Models", modelTypeProvider.Type.Namespace);
-            Assert.AreEqual(customCodeView?.Name, modelTypeProvider.Type.Name);
-            Assert.AreEqual(customCodeView?.Type.Namespace, modelTypeProvider.Type.Namespace);
+            AssertCommon(customCodeView, modelTypeProvider, "Sample.Models", "MockInputModel");
 
             // the property should be filtered from the model provider
             Assert.AreEqual(0, modelTypeProvider.Properties.Count);
@@ -58,6 +51,65 @@ namespace Microsoft.Generator.CSharp.Tests.Providers // the namespace here is cr
             // the property should be added to the custom code view
             Assert.AreEqual(1, customCodeView!.Properties.Count);
             Assert.AreEqual("Prop2", customCodeView.Properties[0].Name);
+        }
+
+        [Test]
+        public void TestCustomization_CanChangePropertyType()
+        {
+            MockHelpers.LoadMockPlugin(customization: Helpers.GetCompilationFromFile());
+
+            var props = new[]
+            {
+                InputFactory.Property("Prop1", InputFactory.Array(InputPrimitiveType.String))
+            };
+
+            var inputModel = InputFactory.Model("mockInputModel", properties: props);
+            var modelTypeProvider = new ModelProvider(inputModel);
+            var customCodeView = modelTypeProvider.CustomCodeView;
+
+            AssertCommon(customCodeView, modelTypeProvider, "Sample.Models", "MockInputModel");
+
+            // the property should be filtered from the model provider
+            Assert.AreEqual(0, modelTypeProvider.Properties.Count);
+
+            // the property should be added to the custom code view
+            Assert.AreEqual(1, customCodeView!.Properties.Count);
+            // the property type should be changed
+            Assert.AreEqual(new CSharpType(typeof(int[])), customCodeView.Properties[0].Type);
+        }
+
+        [Test]
+        public void TestCustomization_CanChangePropertyAccessibility()
+        {
+            MockHelpers.LoadMockPlugin(customization: Helpers.GetCompilationFromFile());
+
+            var props = new[]
+            {
+                InputFactory.Property("Prop1", InputFactory.Array(InputPrimitiveType.String))
+            };
+
+            var inputModel = InputFactory.Model("mockInputModel", properties: props);
+            var modelTypeProvider = new ModelProvider(inputModel);
+            var customCodeView = modelTypeProvider.CustomCodeView;
+
+            AssertCommon(customCodeView, modelTypeProvider, "Sample.Models", "MockInputModel");
+
+            // the property should be filtered from the model provider
+            Assert.AreEqual(0, modelTypeProvider.Properties.Count);
+
+            // the property should be added to the custom code view
+            Assert.AreEqual(1, customCodeView!.Properties.Count);
+            // the property accessibility should be changed
+            Assert.IsTrue(customCodeView.Properties[0].Modifiers.HasFlag(MethodSignatureModifiers.Internal));
+        }
+
+        private static void AssertCommon(TypeProvider? customCodeView, ModelProvider modelTypeProvider, string expectedNamespace, string expectedName)
+        {
+            Assert.IsNotNull(customCodeView);
+            Assert.AreEqual(expectedNamespace, modelTypeProvider.Type.Namespace);
+            Assert.AreEqual(expectedName, modelTypeProvider.Type.Name);
+            Assert.AreEqual(customCodeView?.Name, modelTypeProvider.Type.Name);
+            Assert.AreEqual(customCodeView?.Type.Namespace, modelTypeProvider.Type.Namespace);
         }
     }
 }
