b247bc627d295638b9eefc0e191973abc1e56e33
Kyle Zhang <v-zhanh@microsoft.com>
2024-09-10 10:20:52 +0800

    revert e2e test

--- e2e/e2e-tests.js
+++ e2e/e2e-tests.js
@@ -57,7 +57,6 @@ async function packPackages() {
     "@typespec/http": resolvePackage("typespec-http-"),
     "@typespec/rest": resolvePackage("typespec-rest-"),
     "@typespec/versioning": resolvePackage("typespec-versioning-"),
-    "@typespec/xml": resolvePackage("typespec-xml-"),
   };
 }
 
@@ -106,7 +105,6 @@ async function testBasicCurrentTgz(packages) {
       "@typespec/openapi": packages["@typespec/openapi"],
       "@typespec/openapi3": packages["@typespec/openapi3"],
       "@typespec/versioning": packages["@typespec/versioning"],
-      "@typespec/xml": packages["@typespec/xml"],
     },
     private: true,
   };
