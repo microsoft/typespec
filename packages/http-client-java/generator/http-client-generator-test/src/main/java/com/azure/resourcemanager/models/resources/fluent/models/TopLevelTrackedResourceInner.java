d808138c3ddc61b15300039c65265cc155b47c48
Xiaofei Cao <92354331+XiaofeiCao@users.noreply.github.com>
2024-09-14 10:15:16 +0800

    java mgmt, skip validate on resource properties (#4431)
    If parent model doesn't have validate() method on it, don't validate
    parent model's properties.
    Specially for `Resource`'s `location` property.

--- packages/http-client-java/generator/http-client-generator-test/src/main/java/com/azure/resourcemanager/models/resources/fluent/models/TopLevelTrackedResourceInner.java
+++ packages/http-client-java/generator/http-client-generator-test/src/main/java/com/azure/resourcemanager/models/resources/fluent/models/TopLevelTrackedResourceInner.java
@@ -7,7 +7,6 @@ package com.azure.resourcemanager.models.resources.fluent.models;
 import com.azure.core.annotation.Fluent;
 import com.azure.core.management.Resource;
 import com.azure.core.management.SystemData;
-import com.azure.core.util.logging.ClientLogger;
 import com.azure.json.JsonReader;
 import com.azure.json.JsonToken;
 import com.azure.json.JsonWriter;
@@ -137,14 +136,7 @@ public final class TopLevelTrackedResourceInner extends Resource {
         if (properties() != null) {
             properties().validate();
         }
-        if (location() == null) {
-            throw LOGGER.atError()
-                .log(new IllegalArgumentException(
-                    "Missing required property location in model TopLevelTrackedResourceInner"));
-        }
     }
-
-    private static final ClientLogger LOGGER = new ClientLogger(TopLevelTrackedResourceInner.class);
 
     /**
      * {@inheritDoc}
