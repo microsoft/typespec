6274961e26209d5eae8f1dd49431eb6fc9252a27
Sarangan Rajamanickam <sarajama@microsoft.com>
2024-09-16 19:48:58 +0000

    Remove reference to repoUrl

--- packages/spec-lib/src/lib.ts
+++ packages/spec-lib/src/lib.ts
@@ -1,27 +1,25 @@
 import { createTypeSpecLibrary, paramMessage } from "@typespec/compiler";
 
-const repoUrl = `https://github.com/Microsoft/typespec`;
-
 export const $lib = createTypeSpecLibrary({
   name: "@azure-tools/cadl-ranch-expect",
   diagnostics: {
     "category-invalid": {
       severity: "error",
       messages: {
-        default: paramMessage`Category "${"category"}" is not one of the allowed category. Use ${"allowed"}. See ${repoUrl}/blob/main/docs/decorators.md#category`,
+        default: paramMessage`Category "${"category"}" is not one of the allowed category. Use ${"allowed"}. See https://github.com/Azure/cadl-ranch/blob/main/docs/decorators.md#category`,
       },
     },
 
     "missing-scenario": {
       severity: "warning",
       messages: {
-        default: `Operation doesn't belong to a scenario. Use @scenario(name?: string) to mark it as a scenario. See ${repoUrl}/blob/main/docs/decorators.md#scenario`,
+        default: `Operation doesn't belong to a scenario. Use @scenario(name?: string) to mark it as a scenario. See https://github.com/Azure/cadl-ranch/blob/main/docs/decorators.md#scenario`,
       },
     },
     "missing-scenario-doc": {
       severity: "warning",
       messages: {
-        default: `Operation is missing a scenario doc. Use @scenarioDoc to provide the name of the scenario. See ${repoUrl}/blob/main/docs/decorators.md#scenariodoc`,
+        default: `Operation is missing a scenario doc. Use @scenarioDoc to provide the name of the scenario. See https://github.com/Azure/cadl-ranch/blob/main/docs/decorators.md#scenariodoc`,
       },
     },
   },
