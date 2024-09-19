7f0536224523b57b6ed38d04a96dea03451c0378
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-09-19 09:56:59 -0700

    Add union create test

--- packages/compiler/test/experimental/typekit/union.test.ts
+++ packages/compiler/test/experimental/typekit/union.test.ts
@@ -1,7 +1,25 @@
 import { expect, it } from "vitest";
 import { $ } from "../../../src/experimental/typekit/index.js";
-import { Union } from "../../../src/index.js";
+import { StringLiteral, Union } from "../../../src/index.js";
 import { getTypes } from "./utils.js";
+
+it("can create a union", async () => {
+  await getTypes(
+    `
+    model Foo {}
+    `,
+    ["Foo"],
+  );
+
+  const union = $.union.create({
+    name: "Foo",
+    variants: { hi: "Hello", bye: "Goodbye" },
+  });
+  expect(union).toBeDefined();
+  expect(union.variants.size).toBe(2);
+  expect((union.variants.get("hi")?.type as StringLiteral).value).toBe("Hello");
+  expect((union.variants.get("bye")?.type as StringLiteral).value).toBe("Goodbye");
+});
 
 it("can check if the union is extensible", async () => {
   const { Foo, Bar } = await getTypes(
