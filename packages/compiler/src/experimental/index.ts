7e55550c09ecbdc7d56ae23c2d2976b1a8e9df29
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-09-19 10:26:30 -0700

    Explicit exports

--- packages/compiler/src/experimental/index.ts
+++ packages/compiler/src/experimental/index.ts
@@ -1,8 +1,3 @@
-export * from "./mutators.js";
-export * from "./realm.js";
-export { unsafe_useStateMap, unsafe_useStateSet } from "./state-accessor.js";
-export * from "./typekit/index.js";
-
 import { createConsoleSink } from "../core/logger/console-sink.js";
 import { createLogger } from "../core/logger/logger.js";
 
@@ -11,3 +6,17 @@ const logger = createLogger({ sink: logSink, level: "warning" });
 logger.warn(
   "Functionality from @typespec/compiler/experimental is highly likely to change or be removed.",
 );
+
+export {
+  MutableType,
+  mutateSubgraph,
+  Mutator,
+  MutatorFilterFn,
+  MutatorFlow,
+  MutatorFn,
+  MutatorRecord,
+  MutatorReplaceFn,
+} from "./mutators.js";
+export { Realm } from "./realm.js";
+export { unsafe_useStateMap, unsafe_useStateSet } from "./state-accessor.js";
+export { $ } from "./typekit/index.js";
