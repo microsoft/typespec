f97d912ad20662a11d2327e081ea1f9d87db7267
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-09-20 16:28:47 -0700

    fix format

--- packages/compiler/src/experimental/index.ts
+++ packages/compiler/src/experimental/index.ts
@@ -1,12 +1,12 @@
 export {
   MutableType as unsafe_MutableType,
-  mutateSubgraph as unsafe_mutateSubgraph,
   Mutator as unsafe_Mutator,
   MutatorFilterFn as unsafe_MutatorFilterFn,
   MutatorFlow as unsafe_MutatorFlow,
   MutatorFn as unsafe_MutatorFn,
   MutatorRecord as unsafe_MutatorRecord,
   MutatorReplaceFn as unsafe_MutatorReplaceFn,
+  mutateSubgraph as unsafe_mutateSubgraph,
 } from "./mutators.js";
 export { Realm as unsafe_Realm } from "./realm.js";
 export { unsafe_useStateMap, unsafe_useStateSet } from "./state-accessor.js";
