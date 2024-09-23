dc7dacf428d19fa47fef2cbdeb040502b4f38ac7
Timothee Guerin <tiguerin@microsoft.com>
2024-09-23 12:54:04 -0700

    clean

--- packages/typespec-vscode/rollup.config.ts
+++ packages/typespec-vscode/rollup.config.ts
@@ -2,37 +2,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import * as path from "path";
 
 import { defineConfig } from "rollup";
-
-/**
- * For web extension, all tests, including the test runner, need to be bundled into
- * a single module that has a exported `run` function .
- * This plugin bundles implements a virtual file extensionTests.ts that bundles all these together.
- * @type {import('esbuild').Plugin}
- */
-const testBundlePlugin = {
-  name: "testBundlePlugin",
-  setup(build: any) {
-    build.onResolve({ filter: /[/\\]suite\.ts$/ }, (args: any) => {
-      if (args.kind === "entry-point") {
-        return { path: path.resolve(args.path) };
-      }
-    });
-    build.onLoad({ filter: /[/\\]suite\.ts$/ }, async (args: any) => {
-      const testsRoot = path.join(__dirname, "test");
-      const files = ["web.test.js"];
-      return {
-        contents:
-          `export { run } from './mochaTestRunner.ts';` +
-          files.map((f) => `import('./${f}');`).join(""),
-        watchDirs: files.map((f) => path.dirname(path.resolve(testsRoot, f))),
-        watchFiles: files.map((f) => path.resolve(testsRoot, f)),
-      };
-    });
-  },
-};
 
 const baseConfig = defineConfig({
   input: "src/extension.ts",
@@ -94,6 +65,5 @@ export default defineConfig([
       sourcemap: true,
       inlineDynamicImports: true,
     },
-    plugins: [testBundlePlugin as any, ...(baseConfig.plugins as any)],
   },
 ]);
