44a0f7375e7b92ca0f024ef9481d9eff9e3652f3
Weidong Xu <weidxu@microsoft.com>
2024-09-24 13:54:41 +0800

    explicit exit

--- packages/http-client-java/generator/http-client-generator-test/CadlRanch-Tests.ps1
+++ packages/http-client-java/generator/http-client-generator-test/CadlRanch-Tests.ps1
@@ -9,6 +9,9 @@ Write-Host "Starting the test server"
 npm run testserver-start
 Write-Host "Compile and run the tests"
 mvn clean test --no-transfer-progress -T 1C
+if ($LASTEXITCODE -ne 0) {
+  exit $LASTEXITCODE
+}
 Write-Host "Stopping the test server"
 npm run testserver-stop
 
