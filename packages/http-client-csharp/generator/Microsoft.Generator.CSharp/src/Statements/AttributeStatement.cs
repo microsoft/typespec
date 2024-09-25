68db20ac9d8c83800099ad32f4ef11b947e9b78b
Dapeng Zhang <dapzhang@microsoft.com>
2024-09-25 14:53:13 +0800

    currently our attribute only support one argument

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Statements/AttributeStatement.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/src/Statements/AttributeStatement.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Generator.CSharp.Expressions;
 using Microsoft.Generator.CSharp.Primitives;
 
@@ -12,30 +11,41 @@ namespace Microsoft.Generator.CSharp.Statements
     {
         public CSharpType Type { get; }
         public IReadOnlyList<ValueExpression> Arguments { get; }
+        public IReadOnlyDictionary<string, ValueExpression> PositionalArguments { get; }
 
-        public AttributeStatement(CSharpType type, IReadOnlyList<ValueExpression> arguments)
+        public AttributeStatement(CSharpType type, IReadOnlyList<ValueExpression> arguments, IReadOnlyDictionary<string, ValueExpression> positionalArguments)
         {
             Type = type;
             Arguments = arguments;
+            PositionalArguments = positionalArguments;
         }
 
-        public AttributeStatement(CSharpType type, params ValueExpression[] arguments) : this(type, (IReadOnlyList<ValueExpression>)arguments) { }
+        public AttributeStatement(CSharpType type, params ValueExpression[] arguments) : this(type, arguments, new Dictionary<string, ValueExpression>()) { }
 
         internal override void Write(CodeWriter writer)
         {
-            if (Arguments.Any())
+            writer.Append($"[{Type}");
+            var hasArguments = Arguments.Count > 0 || PositionalArguments.Count > 0;
+            if (hasArguments)
             {
-                writer.Append($"[{Type}(");
-                foreach (var argument in Arguments)
+                writer.AppendRaw("(");
+            }
+            if (Arguments.Count > 0)
+            {
+                for (int i = 0; i < Arguments.Count; i++)
                 {
-                    argument.Write(writer);
+                    Arguments[i].Write(writer);
+                    if (i != Arguments.Count - 1)
+                    {
+                        writer.AppendRaw(", ");
+                    }
                 }
-                writer.WriteRawLine(")]");
             }
-            else
+            if (hasArguments)
             {
-                writer.WriteLine($"[{Type}]");
+                writer.AppendRaw(")");
             }
+            writer.AppendRaw("]");
         }
     }
 }
