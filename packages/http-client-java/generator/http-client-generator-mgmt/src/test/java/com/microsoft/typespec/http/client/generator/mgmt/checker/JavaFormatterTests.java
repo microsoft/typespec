fb0983859fb84b49eafdc68543f1b7f03cdfda70
Weidong Xu <weidxu@microsoft.com>
2024-09-29 14:38:31 +0800

    add mgmt unit tests

---
+++ packages/http-client-java/generator/http-client-generator-mgmt/src/test/java/com/microsoft/typespec/http/client/generator/mgmt/checker/JavaFormatterTests.java
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.typespec.http.client.generator.mgmt.checker;
+
+import com.microsoft.typespec.http.client.generator.mgmt.FluentGen;
+import com.microsoft.typespec.http.client.generator.mgmt.FluentGenAccessor;
+import com.microsoft.typespec.http.client.generator.mgmt.TestUtils;
+import org.junit.jupiter.api.BeforeAll;
+
+public class JavaFormatterTests {
+
+    private final String JAVA_CONTENT = "package com.azure.autorest.fluent.checker;\n" +
+            "\n" +
+            "import com.azure.autorest.extension.base.plugin.PluginLogger;\n" +
+            "import com.azure.autorest.fluent.FluentGen;\n" +
+            "import com.azure.core.util.CoreUtils;\n" +
+            "import org.slf4j.Logger;\n" +
+            "\n" +
+            "import java.lang.reflect.Method;\n" +
+            "import java.util.ArrayList;\n" +
+            "import java.util.Collections;\n" +
+            "import java.util.List;\n" +
+            "import java.util.regex.Pattern;\n" +
+            "\n" +
+            "public class JavaFormatter {\n" +
+            "    private final String content;\n" +
+            "    private final String path;\n" +
+            "\n" +
+            "    public JavaFormatter(String content, String path) {\n" +
+            "        this.content = content;\n" +
+            "        this.path = path;\n" +
+            "        String longString = \"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/virtualMachineScaleSets/{virtualMachineScaleSetName}/virtualMachines/{virtualmachineIndex}/networkInterfaces/{networkInterfaceName}/ipconfigurations/{ipConfigurationName}/publicipaddresses/{publicIpAddressName}\";\n" +
+            "    }\n" +
+            "}\n";
+
+    private static FluentGenAccessor fluentgenAccessor;
+
+    @BeforeAll
+    public static void ensurePlugin() {
+        FluentGen fluentgen = new TestUtils.MockFluentGen();
+        fluentgenAccessor = new FluentGenAccessor(fluentgen);
+    }
+
+//    @EnabledForJreRange(min = JRE.JAVA_11, max = JRE.JAVA_20)
+//    @Test
+//    public void testFormatter() {
+//        JavaFormatter formatter = new JavaFormatter(JAVA_CONTENT, "mock");
+//        String content = formatter.format();
+//        String[] lines = content.split("\r?\n", -1);
+//        Assertions.assertTrue(Arrays.stream(lines).noneMatch(s -> s.equals("import com.azure.autorest.extension.base.plugin.PluginLogger;")));
+//    }
+}
---
+++ packages/http-client-java/generator/http-client-generator-mgmt/src/test/java/com/microsoft/typespec/http/client/generator/mgmt/mapper/FluentPomMapperTests.java
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.typespec.http.client.generator.mgmt.mapper;
+
+import com.microsoft.typespec.http.client.generator.core.model.clientmodel.Pom;
+import com.microsoft.typespec.http.client.generator.mgmt.FluentGen;
+import com.microsoft.typespec.http.client.generator.mgmt.FluentGenAccessor;
+import com.microsoft.typespec.http.client.generator.mgmt.TestUtils;
+import com.microsoft.typespec.http.client.generator.mgmt.model.projectmodel.FluentProject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class FluentPomMapperTests {
+
+    private static FluentGenAccessor fluentgenAccessor;
+
+    @BeforeAll
+    public static void ensurePlugin() {
+        FluentGen fluentgen = new TestUtils.MockFluentGen();
+        fluentgenAccessor = new FluentGenAccessor(fluentgen);
+    }
+
+    @Test
+    public void testMergeDependencies() {
+        String additionalDependencyIdentifier = "com.azure.resourcemanager:azure-resourcemanager-resources:2.42.0:test";
+
+        FluentProject fluentProject = Mockito.mock(FluentProject.class);
+        Mockito.when(fluentProject.getGroupId()).thenReturn("com.azure.resourcemanager");
+        Mockito.when(fluentProject.getArtifactId()).thenReturn("azure-resourcemanager-mock");
+        Mockito.when(fluentProject.getVersion()).thenReturn("1.0.0-beta.1");
+        Mockito.when(fluentProject.getServiceName()).thenReturn("MockManagement");
+        Mockito.when(fluentProject.getServiceDescription()).thenReturn("MockManagement");
+        Mockito.when(fluentProject.getServiceDescriptionForPom()).thenReturn("MockManagement");
+        Mockito.when(fluentProject.getPomDependencyIdentifiers()).thenReturn(List.of(additionalDependencyIdentifier));
+
+        Pom pom = new FluentPomMapper().map(fluentProject);
+
+        Assertions.assertTrue(pom.getDependencyIdentifiers().stream().anyMatch(p -> p.equals(additionalDependencyIdentifier)));
+    }
+}
