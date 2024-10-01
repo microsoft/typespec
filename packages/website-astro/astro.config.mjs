b06505db3ace413757a7d9762a0416c3e0129ae9
Timothee Guerin <tiguerin@microsoft.com>
2024-10-01 15:25:32 -0700

    .

--- packages/website-astro/astro.config.mjs
+++ packages/website-astro/astro.config.mjs
@@ -17,4 +17,7 @@ export default defineConfig({
     }),
     react(),
   ],
+  redirects: {
+    "/docs": "/current/introduction/installation",
+  },
 });
--- packages/website-astro/src/content/config.ts
+++ packages/website-astro/src/content/config.ts
@@ -2,5 +2,5 @@ import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({ schema: docsSchema(), }),
 };
