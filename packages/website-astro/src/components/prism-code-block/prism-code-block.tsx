471a8f804367f5ea557d6189587dab5aa794b1e5
Timothee Guerin <tiguerin@microsoft.com>
2024-10-01 11:45:46 -0700

    basic

--- packages/website-astro/src/components/prism-code-block/prism-code-block.tsx
+++ packages/website-astro/src/components/prism-code-block/prism-code-block.tsx
@@ -3,7 +3,7 @@ import typespecPrismDefinition from "@site/src/prism/typespec-lang-prism";
 import clsx from "clsx";
 import { Highlight, Prism, themes } from "prism-react-renderer";
 import { useColorMode } from "../docusaurus/core/theme-common";
-import style from "./prism-code-block.module.css";
+import styles from "./prism-code-block.module.css";
 
 Prism.languages.tsp = typespecPrismDefinition;
 Prism.languages.typespec = typespecPrismDefinition;
@@ -15,24 +15,23 @@ const pickedThemes = {
 export const CodeBlock = (props: any) => {
   const { colorMode } = useColorMode();
 
+  const title = props.title;
   const theme = pickedThemes[colorMode];
   return (
-    <Highlight
-      {...props}
-      className={clsx(style["code-block"], props.className)}
-      theme={theme}
-      code={props.children}
-    >
+    <Highlight {...props} theme={theme} code={props.children}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={clsx(className)} style={style}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token })} />
-              ))}
-            </div>
-          ))}
-        </pre>
+        <div className={clsx(className, styles.codeblockContainer, props.className)}>
+          {title && <div className={styles["codeblock-title"]}>{title}</div>}
+          <pre className={clsx(className, styles.codeblock)} style={style}>
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        </div>
       )}
     </Highlight>
   );
