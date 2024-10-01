a695d29119410de86488fdf139005615b1f344d9
Timothee Guerin <tiguerin@microsoft.com>
2024-10-01 09:18:15 -0700

    progress

--- packages/website-astro/src/components/docusaurus/core/useBaseUrl.ts
+++ packages/website-astro/src/components/docusaurus/core/useBaseUrl.ts
@@ -1,4 +1,10 @@
 /* eslint-disable unicorn/filename-case */
 export default function useBaseUrl(input: string | undefined) {
+  if (input === undefined) {
+    return undefined;
+  }
+  if (input[0] === "/") {
+    input = input.slice(1);
+  }
   return import.meta.env.BASE_URL + input;
 }
