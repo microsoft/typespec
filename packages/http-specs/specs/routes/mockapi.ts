e805847a1cc3355da969e25780e0571147ab804d
Sarangan Rajamanickam <sarajama@microsoft.com>
2024-10-01 15:22:37 -0700

    Refactor code

--- packages/http-specs/specs/routes/mockapi.ts
+++ packages/http-specs/specs/routes/mockapi.ts
@@ -2,13 +2,18 @@ import { MockRequest, passOnSuccess, ScenarioMockApi, ValidationError } from "@t
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-function createTests(uri: string, params?: Record<string, any>) {
+function createTests(uri: string) {
   const url = new URL("http://example.com" + uri);
+  const searchParams = url.searchParams;
+  const params: Record<string, any> = {};
+  for (const [key, value] of searchParams) {
+    params[key] = value;
+  }
   return passOnSuccess({
     uri: url.pathname,
     method: "get",
     request: {
-      params: params,
+      params,
     },
     response: {
       status: 204,
@@ -34,18 +39,15 @@ function createTests(uri: string, params?: Record<string, any>) {
 
 Scenarios.Routes_InInterface = createTests("/routes/fixed");
 Scenarios.Routes_fixed = createTests("/routes/in-interface/fixed");
-
 Scenarios.Routes_PathParameters_templateOnly = createTests("/routes/path/template-only/a");
 Scenarios.Routes_PathParameters_explicit = createTests("/routes/path/explicit/a");
 Scenarios.Routes_PathParameters_annotationOnly = createTests("/routes/path/annotation-only/a");
-
 Scenarios.Routes_PathParameters_ReservedExpansion_template = createTests(
   "/routes/path/reserved-expansion/template/foo/bar%20baz",
 );
 Scenarios.Routes_PathParameters_ReservedExpansion_annotation = createTests(
   "/routes/path/reserved-expansion/annotation/foo/bar%20baz",
 );
-
 Scenarios.Routes_PathParameters_SimpleExpansion_Standard_primitive = createTests(
   "/routes/simple/standard/primitivea",
 );
@@ -64,7 +66,6 @@ Scenarios.Routes_PathParameters_SimpleExpansion_Explode_array = createTests(
 Scenarios.Routes_PathParameters_SimpleExpansion_Explode_record = createTests(
   "/routes/simple/standard/recorda=1,b=2",
 );
-
 Scenarios.Routes_PathParameters_PathExpansion_Standard_primitive = createTests(
   "/routes/path/standard/primitive/a",
 );
@@ -83,7 +84,6 @@ Scenarios.Routes_PathParameters_PathExpansion_Explode_array = createTests(
 Scenarios.Routes_PathParameters_PathExpansion_Explode_record = createTests(
   "/routes/path/standard/record/a=1/b=2",
 );
-
 Scenarios.Routes_PathParameters_LabelExpansion_Standard_primitive = createTests(
   "/routes/label/standard/primitive.a",
 );
@@ -102,7 +102,6 @@ Scenarios.Routes_PathParameters_LabelExpansion_Explode_array = createTests(
 Scenarios.Routes_PathParameters_LabelExpansion_Explode_record = createTests(
   "/routes/label/standard/record.a=1.b=2",
 );
-
 Scenarios.Routes_PathParameters_MatrixExpansion_Standard_primitive = createTests(
   "/routes/matrix/standard/primitive;a",
 );
@@ -121,98 +120,44 @@ Scenarios.Routes_PathParameters_MatrixExpansion_Explode_array = createTests(
 Scenarios.Routes_PathParameters_MatrixExpansion_Explode_record = createTests(
   "/routes/matrix/standard/record;a=1;b=2",
 );
-
-Scenarios.Routes_QueryParameters_templateOnly = createTests("/routes/query/template-only?param=a", {
-  param: "a",
-});
-Scenarios.Routes_QueryParameters_explicit = createTests("/routes/query/explicit?param=a", {
-  param: "a",
-});
+Scenarios.Routes_QueryParameters_templateOnly = createTests("/routes/query/template-only?param=a");
+Scenarios.Routes_QueryParameters_explicit = createTests("/routes/query/explicit?param=a");
 Scenarios.Routes_QueryParameters_annotationOnly = createTests(
   "/routes/query/annotation-only?param=a",
-  {
-    param: "a",
-  },
 );
-
 Scenarios.Routes_QueryParameters_QueryExpansion_Standard_primitive = createTests(
   "/routes/query/query-expansion/standard/primitive?param=a",
-  {
-    param: "a",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryExpansion_Standard_array = createTests(
   "/routes/query/query-expansion/standard/array?param=a,b",
-  {
-    param: "a,b",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryExpansion_Standard_record = createTests(
   "/routes/query/query-expansion/standard/record?param=a,1,b,2",
-  {
-    param: "a,1,b,2",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryExpansion_Explode_primitive = createTests(
   "/routes/query/query-expansion/explode/primitive?param=a",
-  {
-    param: "a",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryExpansion_Explode_array = createTests(
   "/routes/query/query-expansion/explode/array?param=a,b",
-  {
-    param: "a,b",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryExpansion_Explode_record = createTests(
   "/routes/query/query-expansion/explode/record?a=1&b=2",
-  {
-    a: "1",
-    b: "2",
-  },
 );
-
 Scenarios.Routes_QueryParameters_QueryContinuation_Standard_primitive = createTests(
   "/routes/query/query-continuation/standard/primitive?fixed=true&param=a",
-  {
-    fixed: true,
-    param: "a",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryContinuation_Standard_array = createTests(
   "/routes/query/query-continuation/standard/array?fixed=true&param=a,b",
-  {
-    fixed: true,
-    param: "a,b",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryContinuation_Standard_record = createTests(
   "/routes/query/query-continuation/standard/record?fixed=true&param=a,1,b,2",
-  {
-    fixed: true,
-    param: "a,1,b,2",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryContinuation_Explode_primitive = createTests(
   "/routes/query/query-continuation/explode/primitive?fixed=true&param=a",
-  {
-    fixed: true,
-    param: "a",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryContinuation_Explode_array = createTests(
   "/routes/query/query-continuation/explode/array?fixed=true&param=a,b",
-  {
-    fixed: true,
-    param: "a,b",
-  },
 );
 Scenarios.Routes_QueryParameters_QueryContinuation_Explode_record = createTests(
   "/routes/query/query-continuation/explode/record?fixed=true&a=1&b=2",
-  {
-    fixed: true,
-    a: "1",
-    b: "2",
-  },
 );
