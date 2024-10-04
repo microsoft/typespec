377c7467454d7c89ce185f07648b9ceab4eb96a7
Timothee Guerin <tiguerin@microsoft.com>
2024-10-04 11:37:26 -0700

    tweaks

--- packages/compiler/src/module-resolver/esm/resolve-package-imports-exports.ts
+++ packages/compiler/src/module-resolver/esm/resolve-package-imports-exports.ts
@@ -4,10 +4,12 @@ import { resolvePackageTarget } from "./resolve-package-target.js";
 import { EsmResolutionContext, InvalidModuleSpecifierError } from "./utils.js";
 
 interface ResolvePackageImportsExportsOptions {
-  matchKey: string;
-  matchObj: Record<string, Exports>;
-  isImports?: boolean;
+  readonly matchKey: string;
+  readonly matchObj: Record<string, Exports>;
+  readonly isImports?: boolean;
 }
+
+/** Implementation of PACKAGE_IMPORTS_EXPORTS_RESOLVE https://github.com/nodejs/node/blob/main/doc/api/esm.md */
 export async function resolvePackageImportsExports(
   context: EsmResolutionContext,
   { matchKey, matchObj, isImports }: ResolvePackageImportsExportsOptions,
