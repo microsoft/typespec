21735c49332cfa90174892fb57ea18050c18facd
Travis Prescott <trpresco@microsoft.com>
2024-10-09 10:05:54 -0700

    Work on emitter itself.

--- packages/efnext-pydantic-sketch/src/pydantic.tsx
+++ packages/efnext-pydantic-sketch/src/pydantic.tsx
@@ -1,13 +1,23 @@
 import { Output } from "@alloy-js/core";
 import { EmitContext } from "@typespec/compiler";
-import { PythonProject } from "@typespec/emitter-framework/python";
+import { $ } from "@typespec/compiler/typekit";
+import { ClassDeclaration, PythonModule, PythonPackage, PythonProject } from "@typespec/emitter-framework/python";
 
 export async function $onEmit(context: EmitContext) {
   if (context.program.compilerOptions.noEmit) return;
-
+  
+  const globalNamespace = context.program.getGlobalNamespaceType();
+  const models = [...globalNamespace.models.values()].filter((model) => !$.type.isTemplateDeclaration(model));
+  const modelComponents = models.map((model) => <ClassDeclaration type={model} />);
   return (
     <Output basePath={context.emitterOutputDir}>
-      <PythonProject name="testProject" path="testProject" version="0.1.0" />
+      <PythonProject name="pet_store" version="0.1.0">
+        <PythonPackage name="models">
+          <PythonModule name="models.py">
+            {modelComponents}
+          </PythonModule>
+        </PythonPackage>
+      </PythonProject>    
     </Output>
   );
 }
