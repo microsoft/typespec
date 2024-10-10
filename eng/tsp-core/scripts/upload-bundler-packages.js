0bb82e6a1bd40d20b2ae88b738a6329c8f5103c7
Christopher Radek <14189820+chrisradek@users.noreply.github.com>
2024-10-10 09:15:04 -0700

    fix upload bundler packages script (#4678)
    Fixes #4674
    
    Co-authored-by: Christopher Radek <Christopher.Radek@microsoft.com>

--- eng/tsp-core/scripts/upload-bundler-packages.js
+++ eng/tsp-core/scripts/upload-bundler-packages.js
@@ -3,20 +3,12 @@ import {
   bundleAndUploadPackages,
   getPackageVersion,
 } from "../../../packages/bundle-uploader/dist/src/index.js";
+import { TypeSpecPlaygroundConfig } from "../../../packages/playground-website/src/index.js";
 import { repoRoot } from "../../common/scripts/helpers.js";
 
 await bundleAndUploadPackages({
   repoRoot: repoRoot,
   indexName: "typespec",
   indexVersion: await getPackageVersion(repoRoot, "@typespec/compiler"),
-  packages: [
-    "@typespec/compiler",
-    "@typespec/http",
-    "@typespec/rest",
-    "@typespec/openapi",
-    "@typespec/versioning",
-    "@typespec/openapi3",
-    "@typespec/json-schema",
-    "@typespec/protobuf",
-  ],
+  packages: [...TypeSpecPlaygroundConfig.libraries],
 });
