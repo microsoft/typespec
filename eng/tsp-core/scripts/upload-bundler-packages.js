40e83d18102c90546b3584411e5decded5299b19
Christopher Radek <Christopher.Radek@microsoft.com>
2024-10-10 10:19:51 -0700

    add streams to upload-bundler

--- eng/tsp-core/scripts/upload-bundler-packages.js
+++ eng/tsp-core/scripts/upload-bundler-packages.js
@@ -3,12 +3,21 @@ import {
   bundleAndUploadPackages,
   getPackageVersion,
 } from "../../../packages/bundle-uploader/dist/src/index.js";
-import { TypeSpecPlaygroundConfig } from "../../../packages/playground-website/src/index.js";
 import { repoRoot } from "../../common/scripts/helpers.js";
 
 await bundleAndUploadPackages({
   repoRoot: repoRoot,
   indexName: "typespec",
   indexVersion: await getPackageVersion(repoRoot, "@typespec/compiler"),
-  packages: [...TypeSpecPlaygroundConfig.libraries],
+  packages: [
+    "@typespec/compiler",
+    "@typespec/http",
+    "@typespec/rest",
+    "@typespec/openapi",
+    "@typespec/versioning",
+    "@typespec/openapi3",
+    "@typespec/json-schema",
+    "@typespec/protobuf",
+    "@typespec/streams",
+  ],
 });
