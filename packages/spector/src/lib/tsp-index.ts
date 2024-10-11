54711a8f79049481f7c2ec1285d7883920d43411
Timothee Guerin <tiguerin@microsoft.com>
2024-10-11 10:15:18 -0700

    mroe rename

--- packages/spector/src/lib/tsp-index.ts
+++ packages/spector/src/lib/tsp-index.ts
@@ -1,4 +1,4 @@
-import type { TypeSpecSpecLibDecorators } from "../../generated-defs/TypeSpec.Spector.js";
+import type { TypeSpecSpectorDecorators } from "../../generated-defs/TypeSpec.Spector.js";
 import { $scenario, $scenarioDoc, $scenarioService } from "./decorators.js";
 export { $lib } from "./lib.js";
 
@@ -8,5 +8,5 @@ export const $decorators = {
     scenario: $scenario,
     scenarioDoc: $scenarioDoc,
     scenarioService: $scenarioService,
-  } satisfies TypeSpecSpecLibDecorators,
+  } satisfies TypeSpecSpectorDecorators,
 };
