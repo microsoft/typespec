277e1996651edb4c160ce5a0a08646b21b57c497
Yalin Li <yall@microsoft.com>
2024-10-17 14:07:53 -0700

    Support generating with Pyodide

--- packages/http-client-python/emitter/src/emitter.ts
+++ packages/http-client-python/emitter/src/emitter.ts
@@ -13,6 +13,9 @@ import { emitCodeModel } from "./code-model.js";
 import { saveCodeModelAsYaml } from "./external-process.js";
 import { PythonEmitterOptions, PythonSdkContext } from "./lib.js";
 import { removeUnderscoresFromNamespace } from "./utils.js";
+import { loadPyodide } from "pyodide";
+import jsyaml from "js-yaml";
+import { runPython3 } from "./run-python3.js";
 
 export function getModelsMode(context: SdkContext): "dpg" | "none" {
   const specifiedModelsMode = context.emitContext.options["models-mode"];
@@ -79,49 +82,125 @@ export async function $onEmit(context: EmitContext<PythonEmitterOptions>) {
   const outputDir = context.emitterOutputDir;
   const yamlMap = emitCodeModel(sdkContext);
   addDefaultOptions(sdkContext);
-  const yamlPath = await saveCodeModelAsYaml("python-yaml-path", yamlMap);
-  let venvPath = path.join(root, "venv");
-  if (fs.existsSync(path.join(venvPath, "bin"))) {
-    venvPath = path.join(venvPath, "bin", "python");
-  } else if (fs.existsSync(path.join(venvPath, "Scripts"))) {
-    venvPath = path.join(venvPath, "Scripts", "python.exe");
-  } else {
-    throw new Error("Virtual environment doesn't exist.");
-  }
-  const commandArgs = [
-    venvPath,
-    `${root}/eng/scripts/setup/run_tsp.py`,
-    `--output-folder=${outputDir}`,
-    `--cadl-file=${yamlPath}`,
-  ];
   const resolvedOptions = sdkContext.emitContext.options;
-  if (resolvedOptions["packaging-files-config"]) {
-    const keyValuePairs = Object.entries(resolvedOptions["packaging-files-config"]).map(
-      ([key, value]) => {
-        return `${key}:${value}`;
-      },
-    );
-    commandArgs.push(`--packaging-files-config='${keyValuePairs.join("|")}'`);
-    resolvedOptions["packaging-files-config"] = undefined;
-  }
-  if (
-    resolvedOptions["package-pprint-name"] !== undefined &&
-    !resolvedOptions["package-pprint-name"].startsWith('"')
-  ) {
-    resolvedOptions["package-pprint-name"] = `"${resolvedOptions["package-pprint-name"]}"`;
-  }
+  if (resolvedOptions["use-pyodide"]) {
+    const commandArgs: Record<string, string> = {}
+    if (resolvedOptions["packaging-files-config"]) {
+        const keyValuePairs = Object.entries(resolvedOptions["packaging-files-config"]).map(([key, value]) => {
+            return `${key}:${value}`;
+        });
+        commandArgs["packaging-files-config"] = keyValuePairs.join("|");
+        resolvedOptions["packaging-files-config"] = undefined;
+    }
+    if (
+        resolvedOptions["package-pprint-name"] !== undefined &&
+        !resolvedOptions["package-pprint-name"].startsWith('"')
+    ) {
+        resolvedOptions["package-pprint-name"] = `"${resolvedOptions["package-pprint-name"]}"`;
+    }
 
-  for (const [key, value] of Object.entries(resolvedOptions)) {
-    commandArgs.push(`--${key}=${value}`);
-  }
-  if (sdkContext.arm === true) {
-    commandArgs.push("--azure-arm=true");
-  }
-  if (resolvedOptions.flavor === "azure") {
-    commandArgs.push("--emit-cross-language-definition-file=true");
-  }
-  commandArgs.push("--from-typespec=true");
-  if (!program.compilerOptions.noEmit && !program.hasError()) {
-    execSync(commandArgs.join(" "));
+    for (const [key, value] of Object.entries(resolvedOptions)) {
+        commandArgs[key] = value;
+    }
+    if (sdkContext.arm === true) {
+        commandArgs["azure-arm"]="true";
+    }
+    if (resolvedOptions.flavor === "azure") {
+        commandArgs["emit-cross-language-definition-file"]="true";
+    }
+    commandArgs["from-typespec"] = "true";
+
+    if (!program.compilerOptions.noEmit && !program.hasError()) {
+        const outputFolder = path.relative(root, outputDir)
+        if (!fs.existsSync(outputFolder)) {
+          fs.mkdirSync(outputFolder, { recursive: true });
+        }
+        let pyodide = await loadPyodide({indexURL: path.join(root, "node_modules", "pyodide")});
+        pyodide.FS.mount(pyodide.FS.filesystems.NODEFS, { root: "." }, ".");
+        const yamlStr = jsyaml.dump(yamlMap);
+        if (!fs.existsSync("temp")) {
+          fs.mkdirSync("temp");
+        }      
+        const yamlPath = path.join("temp", "yamldata.yaml");
+        pyodide.FS.writeFile(yamlPath, yamlStr);
+        await pyodide.loadPackage("micropip");
+        const micropip = pyodide.pyimport("micropip");
+        await micropip.install(
+            [
+                "black",
+                "click",
+                "docutils",
+                "Jinja2",
+                "m2r2",
+                "MarkupSafe",
+                "pathspec",
+                "platformdirs",
+                "PyYAML",
+                "tomli",
+                "setuptools",
+            ]
+        );
+        await micropip.install("https://github.com/microsoft/typespec/releases/download/pygen%401.0.0/pygen-0.1.0-py3-none-any.whl");
+        const globals = pyodide.toPy({outputFolder, yamlPath, commandArgs});
+        const python = `
+        async def main():
+            from pygen import m2r, preprocess, codegen, black
+
+            m2r.M2R(output_folder=outputFolder, cadl_file=yamlPath, **commandArgs).process()
+            preprocess.PreProcessPlugin(output_folder=outputFolder, cadl_file=yamlPath, **commandArgs).process()
+            codegen.CodeGenerator(output_folder=outputFolder, cadl_file=yamlPath, **commandArgs).process()
+            black.BlackScriptPlugin(output_folder=outputFolder, **commandArgs).process()
+    
+        await main()
+        `
+        await pyodide.runPythonAsync(python, {globals});
+    }
+  } else {
+    await runPython3("./eng/scripts/setup/install.py");
+    await runPython3("./eng/scripts/setup/prepare.py");
+    let venvPath = path.join(root, "venv");
+    if (fs.existsSync(path.join(venvPath, "bin"))) {
+      venvPath = path.join(venvPath, "bin", "python");
+    } else if (fs.existsSync(path.join(venvPath, "Scripts"))) {
+      venvPath = path.join(venvPath, "Scripts", "python.exe");
+    } else {
+      throw new Error("Virtual environment doesn't exist.");
+    }
+    const yamlPath = await saveCodeModelAsYaml("python-yaml-path", yamlMap);
+    const commandArgs = [
+      venvPath,
+      `${root}/eng/scripts/setup/run_tsp.py`,
+      `--output-folder=${outputDir}`,
+      `--cadl-file=${yamlPath}`,
+    ];
+    if (resolvedOptions["packaging-files-config"]) {
+      const keyValuePairs = Object.entries(resolvedOptions["packaging-files-config"]).map(
+        ([key, value]) => {
+          return `${key}:${value}`;
+        },
+      );
+      commandArgs.push(`--packaging-files-config='${keyValuePairs.join("|")}'`);
+      resolvedOptions["packaging-files-config"] = undefined;
+    }
+    if (
+      resolvedOptions["package-pprint-name"] !== undefined &&
+      !resolvedOptions["package-pprint-name"].startsWith('"')
+    ) {
+      resolvedOptions["package-pprint-name"] = `"${resolvedOptions["package-pprint-name"]}"`;
+    }
+
+    for (const [key, value] of Object.entries(resolvedOptions)) {
+      commandArgs.push(`--${key}=${value}`);
+    }
+    if (sdkContext.arm === true) {
+      commandArgs.push("--azure-arm=true");
+    }
+    if (resolvedOptions.flavor === "azure") {
+      commandArgs.push("--emit-cross-language-definition-file=true");
+    }
+    commandArgs.push("--from-typespec=true");
+    if (!program.compilerOptions.noEmit && !program.hasError()) {
+      execSync(commandArgs.join(" "));
+    }
   }
 }
