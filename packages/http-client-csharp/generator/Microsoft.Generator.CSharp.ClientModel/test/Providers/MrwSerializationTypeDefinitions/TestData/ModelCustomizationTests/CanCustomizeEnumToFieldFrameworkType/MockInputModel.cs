7ca755ea878774f292bca950833cd1a1cd4fb8f9
jolov <jolov@microsoft.com>
2024-10-21 15:38:38 -0700

    PR fb

---
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/MrwSerializationTypeDefinitions/TestData/ModelCustomizationTests/CanCustomizeEnumToFieldFrameworkType/MockInputModel.cs
@@ -0,0 +1,11 @@
+#nullable disable
+
+using Microsoft.Generator.CSharp.Customization;
+
+namespace Sample.Models
+{
+    public partial class MockInputModel
+    {
+        private object _prop1;
+    }
+}
