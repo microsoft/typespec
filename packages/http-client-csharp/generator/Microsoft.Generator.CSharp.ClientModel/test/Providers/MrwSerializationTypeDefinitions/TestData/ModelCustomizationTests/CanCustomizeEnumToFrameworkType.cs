2119117994dc48ee3db6770b980d98e4d5793348
Jorge Rangel <102122018+jorgerangel-msft@users.noreply.github.com>
2024-10-23 19:43:21 -0500

    [http-client-csharp] Fix: Properly handle property wire nullability during deserialization (#4850)
    This PR adds a set of fixes related to the generated deserialization
    code and customization:
    - Properties will be set to null during deserialization if both the
    customized type and the wire type are nullable. Previously, this exposed
    a corner case where the wire type was nullable but the customized type
    was not.
    - Removed the unnecessary null check when a custom deserialization
    method hook is added for a property.
    
    fixes: https://github.com/microsoft/typespec/issues/4813

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/MrwSerializationTypeDefinitions/TestData/ModelCustomizationTests/CanCustomizeEnumToFrameworkType.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/MrwSerializationTypeDefinitions/TestData/ModelCustomizationTests/CanCustomizeEnumToFrameworkType.cs
@@ -81,7 +81,6 @@ namespace Sample.Models
                 {
                     if ((prop.Value.ValueKind == global::System.Text.Json.JsonValueKind.Null))
                     {
-                        prop1 = null;
                         continue;
                     }
                     prop1 = prop.Value.GetObject();
