eab45856d5ea906bbdd7d58b6e07023e46e4b1d7
nisha-bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2024-10-28 14:59:22 -0700

    wip

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -50,7 +50,6 @@ function IsSpecDir {
 
 $failingSpecs = @(
     Join-Path 'http' 'special-words'
-    Join-Path 'http' 'client' 'structure' 'default'
     Join-Path 'http' 'client' 'structure' 'two-operation-group'
     Join-Path 'http' 'encode' 'numeric'
     Join-Path 'http' 'parameters' 'body-optionality'
