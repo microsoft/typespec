2e5f531494b68c1dc41a2818f78007e8ac01b3d4
Timothee Guerin <tiguerin@microsoft.com>
2024-11-05 15:14:42 -0800

    New version banner

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -2,7 +2,19 @@ import { docsSchema } from "@astrojs/starlight/schema";
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: z.object({
+        version: z.string().optional(),
+        releaseDate: z.coerce
+          .date()
+          .optional()
+          .describe(
+            "A date string or YAML date that is compatible with JavaScript's `new Date()` constructor.",
+          ),
+      }),
+    }),
+  }),
   blog: defineCollection({
     type: "content",
     // Type-check frontmatter using a schema
