699cbbf31dad640d17e16cd6fcc1b293e52f0f08
swatikumar <swatikumar@pinterest.com>
2024-11-06 00:33:19 +0530

    address comments

--- packages/graphql/vitest.config.ts
+++ packages/graphql/vitest.config.ts
@@ -1,9 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
+import { defaultTypeSpecVitestConfig } from "../../vitest.workspace.js";
 
-export default defineConfig({
-  test: {
-    environment: "node",
-    // testTimeout: 10000, // Uncomment to increase the default timeout
-    isolate: false, // Your test shouldn't have side effects to this will improve performance.
-  },
-});
+export default mergeConfig(defaultTypeSpecVitestConfig, defineConfig({}));
