234e13736a00757aa50a45d61179e499ab3ca795
Swati Kumar <swati.kumar.a@gmail.com>
2024-11-05 16:14:33 -0800

    Initial setup for graphql emitter (#4978)

---
+++ packages/graphql/src/emitter.ts
@@ -0,0 +1,11 @@
+import type { EmitContext } from "@typespec/compiler";
+import { emitFile, resolvePath } from "@typespec/compiler";
+
+export async function $onEmit(context: EmitContext) {
+  if (!context.program.compilerOptions.noEmit) {
+    await emitFile(context.program, {
+      path: resolvePath(context.emitterOutputDir, "output.txt"),
+      content: "Hello world\n",
+    });
+  }
+}
