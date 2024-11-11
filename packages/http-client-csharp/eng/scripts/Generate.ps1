fe87f794c0b397b34416915b3483d48684ccf9d9
jolov <jolov@microsoft.com>
2024-11-11 15:34:22 -0800

    Adopt CadlRanch model-visibility spec

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -62,7 +62,6 @@ $failingSpecs = @(
     Join-Path 'http' 'special-headers' 'conditional-request'
     Join-Path 'http' 'special-headers' 'repeatability'
     Join-Path 'http' 'type' 'model' 'flatten'
-    Join-Path 'http' 'type' 'model' 'visibility'
     Join-Path 'http' 'type' 'model' 'inheritance' 'nested-discriminator'
     Join-Path 'http' 'type' 'model' 'inheritance' 'not-discriminated'
     Join-Path 'http' 'type' 'model' 'inheritance' 'recursive'
