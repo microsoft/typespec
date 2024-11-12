a21b0626b49ca457b61283b2f1916146874cc5a2
Jorge Rangel <102122018+jorgerangel-msft@users.noreply.github.com>
2024-11-12 12:07:02 -0600

    [http-client-csharp] onboard http/type/model/inheritance/recursive (#5069)
    fixes: https://github.com/microsoft/typespec/issues/4005

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -62,7 +62,6 @@ $failingSpecs = @(
     Join-Path 'http' 'special-headers' 'repeatability'
     Join-Path 'http' 'type' 'model' 'flatten'
     Join-Path 'http' 'type' 'model' 'inheritance' 'not-discriminated'
-    Join-Path 'http' 'type' 'model' 'inheritance' 'recursive'
     Join-Path 'http' 'type' 'model' 'templated'
 )
 
