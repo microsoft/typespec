d2401c7147f235ab3cb6ff6a6865e926a5f19a87
Jorge Rangel <jorgerangel@microsoft.com>
2024-11-12 11:43:42 -0600

    feat: onboard http/server/versions/versioned

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -57,7 +57,6 @@ $failingSpecs = @(
     Join-Path 'http' 'resiliency' 'srv-driven'
     Join-Path 'http' 'routes'
     Join-Path 'http' 'server' 'endpoint' 'not-defined'
-    Join-Path 'http' 'server' 'versions' 'versioned'
     Join-Path 'http' 'special-headers' 'conditional-request'
     Join-Path 'http' 'special-headers' 'repeatability'
     Join-Path 'http' 'type' 'model' 'flatten'
