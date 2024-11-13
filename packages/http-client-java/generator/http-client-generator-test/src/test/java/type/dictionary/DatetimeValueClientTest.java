5546545863dd6d7ca9f16a2f1ed2d0ccbdfb49e8
Weidong Xu <weidxu@microsoft.com>
2024-11-13 13:07:44 +0800

    update test

---
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/type/dictionary/DatetimeValueClientTest.java
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package type.dictionary;
+
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DatetimeValueClientTest {
+
+    DatetimeValueClient client = new DictionaryClientBuilder().buildDatetimeValueClient();
+
+    @Test
+    void get() {
+        Map<String, OffsetDateTime> response = client.get();
+        Assertions.assertTrue(response.containsKey("k1"));
+        Assertions.assertEquals(OffsetDateTime.parse("2022-08-26T18:38Z"), response.get("k1"));
+    }
+
+    @Test
+    void put() {
+        Map<String, OffsetDateTime> map = new HashMap<>();
+        map.put("k1", OffsetDateTime.parse("2022-08-26T18:38Z"));
+        client.put(map);
+    }
+}
