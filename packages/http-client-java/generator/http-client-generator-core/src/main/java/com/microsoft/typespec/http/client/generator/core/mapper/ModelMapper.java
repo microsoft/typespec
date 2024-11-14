60a540d92bcceb31ac183d1ed071b3d147aaa174
Weidong Xu <weidxu@microsoft.com>
2024-11-14 12:16:00 +0800

    comment

--- packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/mapper/ModelMapper.java
+++ packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/mapper/ModelMapper.java
@@ -527,7 +527,7 @@ public class ModelMapper implements IMapper<ObjectSchema, ClientModel>, NeedsPla
 
         List<ClientModelPropertyReference> propertyReferences = new ArrayList<>();
         ObjectSchema targetModelSchema = (ObjectSchema) property.getSchema();
-        // not modelProperty.getName()
+        // use "property.getLanguage().getJava().getName()", not "modelProperty.getName()"
         String originalFlattenedPropertyName = property.getLanguage().getJava().getName();
         ClientModel targetModel = this.map(targetModelSchema);
         if (targetModel != null && targetModel.getProperties() != null) {
