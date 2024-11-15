c52aa2bdb7cfae0a266abef6fe5f2e580f5fb0a2
Christopher Radek <Christopher.Radek@microsoft.com>
2024-11-15 15:18:02 -0800

    create test helper for worksFor versions

--- packages/openapi3/test/works-for.ts
+++ packages/openapi3/test/works-for.ts
@@ -10,38 +10,8 @@ import {
 } from "./test-host.js";
 
 export const OpenAPISpecHelpers: Record<OpenAPIVersion, SpecHelper> = {
-  "3.0.0": {
-    version: "3.0.0",
-    oapiForModel: (...[name, modelDef, options]: Parameters<typeof oapiForModel>) =>
-      oapiForModel(name, modelDef, { ...options, "openapi-versions": ["3.0.0"] }),
-    openApiFor: (...[code, versions, options]: Parameters<typeof openApiFor>) =>
-      openApiFor(code, versions, { ...options, "openapi-versions": ["3.0.0"] }),
-    openapiWithOptions: (...[code, options]: Parameters<typeof openapiWithOptions>) =>
-      openapiWithOptions(code, { ...options, "openapi-versions": ["3.0.0"] }),
-    checkFor: (...[code, options]: Parameters<typeof checkFor>) =>
-      checkFor(code, { ...options, "openapi-versions": ["3.0.0"] }),
-    diagnoseOpenApiFor: (...[code, options]: Parameters<typeof diagnoseOpenApiFor>) =>
-      diagnoseOpenApiFor(code, { ...options, "openapi-versions": ["3.0.0"] }),
-    emitOpenApiWithDiagnostics: (
-      ...[code, options]: Parameters<typeof emitOpenApiWithDiagnostics>
-    ) => emitOpenApiWithDiagnostics(code, { ...options, "openapi-versions": ["3.0.0"] }),
-  },
-  "3.1.0": {
-    version: "3.1.0",
-    oapiForModel: (...[name, modelDef, options]: Parameters<typeof oapiForModel>) =>
-      oapiForModel(name, modelDef, { ...options, "openapi-versions": ["3.1.0"] }),
-    openApiFor: (...[code, versions, options]: Parameters<typeof openApiFor>) =>
-      openApiFor(code, versions, { ...options, "openapi-versions": ["3.1.0"] }),
-    openapiWithOptions: (...[code, options]: Parameters<typeof openapiWithOptions>) =>
-      openapiWithOptions(code, { ...options, "openapi-versions": ["3.1.0"] }),
-    checkFor: (...[code, options]: Parameters<typeof checkFor>) =>
-      checkFor(code, { ...options, "openapi-versions": ["3.1.0"] }),
-    diagnoseOpenApiFor: (...[code, options]: Parameters<typeof diagnoseOpenApiFor>) =>
-      diagnoseOpenApiFor(code, { ...options, "openapi-versions": ["3.1.0"] }),
-    emitOpenApiWithDiagnostics: (
-      ...[code, options]: Parameters<typeof emitOpenApiWithDiagnostics>
-    ) => emitOpenApiWithDiagnostics(code, { ...options, "openapi-versions": ["3.1.0"] }),
-  },
+  "3.0.0": createSpecHelpers("3.0.0"),
+  "3.1.0": createSpecHelpers("3.1.0"),
 };
 
 export type SpecHelper = {
@@ -55,6 +25,25 @@ export type SpecHelper = {
 };
 
 export type WorksForCb = (specHelpers: SpecHelper) => void;
+
+function createSpecHelpers(version: OpenAPIVersion): SpecHelper {
+  return {
+    version,
+    oapiForModel: (...[name, modelDef, options]: Parameters<typeof oapiForModel>) =>
+      oapiForModel(name, modelDef, { ...options, "openapi-versions": [version] }),
+    openApiFor: (...[code, versions, options]: Parameters<typeof openApiFor>) =>
+      openApiFor(code, versions, { ...options, "openapi-versions": [version] }),
+    openapiWithOptions: (...[code, options]: Parameters<typeof openapiWithOptions>) =>
+      openapiWithOptions(code, { ...options, "openapi-versions": [version] }),
+    checkFor: (...[code, options]: Parameters<typeof checkFor>) =>
+      checkFor(code, { ...options, "openapi-versions": [version] }),
+    diagnoseOpenApiFor: (...[code, options]: Parameters<typeof diagnoseOpenApiFor>) =>
+      diagnoseOpenApiFor(code, { ...options, "openapi-versions": [version] }),
+    emitOpenApiWithDiagnostics: (
+      ...[code, options]: Parameters<typeof emitOpenApiWithDiagnostics>
+    ) => emitOpenApiWithDiagnostics(code, { ...options, "openapi-versions": [version] }),
+  };
+}
 
 export function worksFor(versions: OpenAPIVersion[], cb: WorksForCb) {
   const specsHelpers = versions.map((version) => OpenAPISpecHelpers[version]);
