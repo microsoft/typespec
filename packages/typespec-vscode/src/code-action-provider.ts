38497cb4d52b14072192adee053baeeead4e3a29
Zhonglei Ma (WICRESOFT NORTH AMERICA LTD) <v-zhongleima@microsoft.com>
2024-11-21 17:15:06 +0800

    Readjust the code

--- packages/typespec-vscode/src/code-action-provider.ts
+++ packages/typespec-vscode/src/code-action-provider.ts
@@ -1,5 +1,5 @@
 import vscode from "vscode";
-import { OPEN_URL_COMMAND } from "./const.js";
+import { OPEN_URL_COMMAND } from "./vscode-command.js";
 
 export function createCodeActionProvider() {
   return vscode.languages.registerCodeActionsProvider(
@@ -26,30 +26,32 @@ export class TypeSpecCodeActionProvider implements vscode.CodeActionProvider {
     // for each diagnostic entry that has the matching `code`, create a code action command
     // A CodeAction will only be created if it is a TypeSpec diagnostic and code is an object and has a target attribute
     // target attribute is the URL to open
-    return context.diagnostics
-      .filter(
-        (diagnostic) =>
-          diagnostic.source === "TypeSpec" &&
-          typeof diagnostic.code === "object" &&
-          "target" in diagnostic.code,
-      )
-      .map((diagnostic) => this.createOpenUrlCodeAction(diagnostic));
+
+    // target is a Uri type, which corresponds to diagnostic.codeDescription.href in compiler
+    // When target is empty, it does not exist in the code object, so the code action will not be created
+    const actions: vscode.CodeAction[] = [];
+    context.diagnostics.forEach((diagnostic) => {
+      if (
+        diagnostic.source === "TypeSpec" &&
+        diagnostic.code &&
+        typeof diagnostic.code === "object" &&
+        "target" in diagnostic.code
+      ) {
+        actions.push(this.createOpenUrlCodeAction(diagnostic, diagnostic.code.target.toString()));
+      }
+    });
+    return actions;
   }
 
-  private createOpenUrlCodeAction(diagnostic: vscode.Diagnostic): vscode.CodeAction {
-    // The target will only exist if the compiler's diagnosis contains the url, so no data will be stored.
-    if (typeof diagnostic.code === "object" && "target" in diagnostic.code) {
-      const action = new vscode.CodeAction("Open Document", vscode.CodeActionKind.QuickFix);
-      action.command = {
-        command: OPEN_URL_COMMAND,
-        title: "Learn more about details",
-        arguments: [diagnostic.code.target.toString()],
-      };
-      action.diagnostics = [diagnostic];
-      return action;
-    }
-
-    // Because it has been filtered above, this statement will never trigger
-    return new vscode.CodeAction("", vscode.CodeActionKind.Empty);
+  private createOpenUrlCodeAction(diagnostic: vscode.Diagnostic, url: string): vscode.CodeAction {
+    // vscode. CodeActionKind.Empty does not generate a Quick Fix menu
+    const action = new vscode.CodeAction("Open Document", vscode.CodeActionKind.QuickFix);
+    action.command = {
+      command: OPEN_URL_COMMAND,
+      title: "Learn more about details",
+      arguments: [url],
+    };
+    action.diagnostics = [diagnostic];
+    return action;
   }
 }
--- packages/typespec-vscode/src/const.ts
+++ packages/typespec-vscode/src/const.ts
@@ -1,5 +1,3 @@
 export const enum SettingName {
   TspServerPath = "typespec.tsp-server.path",
 }
-
-export const OPEN_URL_COMMAND = "typespec.openUrl";
