46c648a69e85df570a17fd780e7dd472a149b03b
jolov <jolov@microsoft.com>
2024-11-21 12:16:01 -0800

    revert usings

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/CSharpGenTests.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp/test/CSharpGenTests.cs
@@ -1,13 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Generator.CSharp.Input;
-using Microsoft.Generator.CSharp.Primitives;
-using Microsoft.Generator.CSharp.Tests.Common;
 using Moq;
 using NUnit.Framework;
 
