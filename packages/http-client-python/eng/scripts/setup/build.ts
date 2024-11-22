0f53dfb61c94f9e72049e359e7912e09ba6aef09
Yalin Li <yall@microsoft.com>
2024-11-22 13:16:50 -0800

    Fix format and lint

--- packages/http-client-python/eng/scripts/setup/build.ts
+++ packages/http-client-python/eng/scripts/setup/build.ts
@@ -1,13 +1,14 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 // Define the command you want to run
-const command = "tsx ./eng/scripts/setup/run-python3.ts ./eng/scripts/setup/build_pygen_wheel.py && rimraf ./venv_build_wheel"
+const command =
+  "tsx ./eng/scripts/setup/run-python3.ts ./eng/scripts/setup/build_pygen_wheel.py && rimraf ./venv_build_wheel";
 
 // Execute the command
 exec(command, (error, stdout, stderr) => {
   if (error) {
-    console.error(`Error executing command: ${error.message}`);
+    console.error(`Error executing command: ${error.message}`); // eslint-disable-line no-console
     return;
   }
-  console.log(`Command output:\n${stdout}`);
+  console.log(`Command output:\n${stdout}`); // eslint-disable-line no-console
 });
