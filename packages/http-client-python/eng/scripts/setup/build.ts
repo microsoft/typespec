9be5ab7470a0d032c7f1ab1b301500c0c6857a29
Yalin Li <yall@microsoft.com>
2024-11-22 13:05:42 -0800

    Cleanup scripts

---
+++ packages/http-client-python/eng/scripts/setup/build.ts
@@ -0,0 +1,13 @@
+import { exec } from 'child_process';
+
+// Define the command you want to run
+const command = "tsx ./eng/scripts/setup/run-python3.ts ./eng/scripts/setup/build_pygen_wheel.py && rimraf ./venv_build_wheel"
+
+// Execute the command
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error.message}`);
+    return;
+  }
+  console.log(`Command output:\n${stdout}`);
+});
