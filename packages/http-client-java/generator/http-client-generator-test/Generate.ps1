a5c064fd70424f1bc104ea4de22d48392057552a
Weidong Xu <weidxu@microsoft.com>
2024-11-26 13:40:31 +0800

    script

--- packages/http-client-java/generator/http-client-generator-test/Generate.ps1
+++ packages/http-client-java/generator/http-client-generator-test/Generate.ps1
@@ -121,7 +121,7 @@ $generateScript = {
 
 ./Setup.ps1
 
-New-Item -Path ./existingcode/src/main/java/com/cadl/ -ItemType Directory -Force | Out-Null
+New-Item -Path ./existingcode/src/main/java/tsptest/ -ItemType Directory -Force | Out-Null
 
 if (Test-Path ./src/main/java/tsptest/partialupdate) {
   Copy-Item -Path ./src/main/java/tsptest/partialupdate -Destination ./existingcode/src/main/java/tsptest/partialupdate -Recurse -Force
@@ -148,17 +148,18 @@ npx tsp compile ./tsp/partialupdate.tsp --option="@typespec/http-client-java.emi
 Copy-Item -Path ./existingcode/src/main/java/tsptest/partialupdate -Destination ./src/main/java/tsptest/partialupdate -Recurse -Force
 Remove-Item ./existingcode -Recurse -Force
 
-# run cadl ranch tests sources
-Copy-Item -Path node_modules/@azure-tools/cadl-ranch-specs/http -Destination ./ -Recurse -Force
+# run http-specs / azure-http-specs tests sources
+Copy-Item -Path node_modules/@typespec/http-specs/specs -Destination ./ -Recurse -Force
+Copy-Item -Path node_modules/@azure-tools/azure-http-specs/specs -Destination ./ -Recurse -Force
 # remove xml tests, emitter has not supported xml model
-Remove-Item ./http/payload/xml -Recurse -Force
+Remove-Item ./specs/payload/xml -Recurse -Force
 
-$job = (Get-ChildItem ./http -Include "main.tsp","old.tsp" -File -Recurse) | ForEach-Object -Parallel $generateScript -ThrottleLimit $Parallelization -AsJob
+$job = (Get-ChildItem ./specs -Include "main.tsp","old.tsp" -File -Recurse) | ForEach-Object -Parallel $generateScript -ThrottleLimit $Parallelization -AsJob
 
 $job | Wait-Job -Timeout 1200
 $job | Receive-Job
 
-Remove-Item ./http -Recurse -Force
+Remove-Item ./specs -Recurse -Force
 
 Copy-Item -Path ./tsp-output/*/src -Destination ./ -Recurse -Force -Exclude @("ReadmeSamples.java", "module-info.java")
 
