79b051a4a6201d3eda9f47ba4cc94d6664fdb979
tadelesh <tadelesh.shi@live.cn>
2024-11-28 11:30:13 +0800

    move to payload

--- packages/http-specs/specs/payload/pageable/mockapi.ts
+++ packages/http-specs/specs/payload/pageable/mockapi.ts
@@ -2,9 +2,9 @@ import { json, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Operations_List_ServerDrivenPagination_link = passOnSuccess([
+Scenarios.Payload_Pageable_ServerDrivenPagination_link = passOnSuccess([
   {
-    uri: "/operations/list/server-driven-pagination/link",
+    uri: "/payload/pageable/server-driven-pagination/link",
     method: "get",
     request: {},
     response: {
@@ -15,14 +15,14 @@ Scenarios.Operations_List_ServerDrivenPagination_link = passOnSuccess([
           { id: "2", name: "cat" },
         ],
         links: {
-          next: "/operations/list/server-driven-pagination/link/nextPage",
+          next: "/payload/pageable/server-driven-pagination/link/nextPage",
         },
       }),
     },
     kind: "MockApiDefinition",
   },
   {
-    uri: "/operations/list/server-driven-pagination/link/nextPage",
+    uri: "/payload/pageable/server-driven-pagination/link/nextPage",
     method: "get",
     request: {},
     response: {
