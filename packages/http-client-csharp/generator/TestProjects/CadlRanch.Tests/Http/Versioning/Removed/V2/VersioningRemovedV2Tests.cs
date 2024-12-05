45a2ed7f988370b670046d4f463a6f9965cc0e57
jolov <jolov@microsoft.com>
2024-12-05 13:33:14 -0800

    Update tests

--- packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Http/Versioning/Removed/V2/VersioningRemovedV2Tests.cs
+++ packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Http/Versioning/Removed/V2/VersioningRemovedV2Tests.cs
@@ -72,7 +72,7 @@ namespace TestProjects.CadlRanch.Tests.Http.Versioning.Removed.V2
         public Task Versioning_Removed_V3Model() => Test(async (host) =>
         {
             var model = new ModelV3("123", EnumV3.EnumMemberV1);
-            var response = await new RemovedClient(host, new RemovedClientOptions(RemovedClientOptions.ServiceVersion.V1)).ModelV3Async(model);
+            var response = await new RemovedClient(host).ModelV3Async(model);
             Assert.AreEqual(200, response.GetRawResponse().Status);
             Assert.AreEqual("123", response.Value.Id);
             Assert.AreEqual(EnumV3.EnumMemberV1, response.Value.EnumProp);
