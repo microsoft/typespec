31a88c8e569a4bf3bf4d1a78c1e4849b2f7a6e0f
Matthew Gertz <mattge@microsoft.com>
2024-12-09 10:33:37 -0800

    Finished primitives

--- packages/efv2-zod-sketch/sample/output/todo/@typespec/efv2-zod-sketch/src/models.ts
+++ packages/efv2-zod-sketch/sample/output/todo/@typespec/efv2-zod-sketch/src/models.ts
@@ -1,110 +1,150 @@
 import { z } from "zod";
 
-export const user = z.object({
-  id: z.any(),
-  username: z.any(),
-  email: z.any(),
-  password: z.any(),
-  validated: z.any(),
-});
+export const user = z.object(
+{
+id: z.string(),
+username: z.string(),
+email: z.string(),
+password: z.string(),
+accountCreated: z.string().datetime(),
+floaty: z.string(),
+validated: z.string()
+}
+);
 
-export const todoItem = z.object({
-  id: z.any(),
-  title: z.any(),
-  createdBy: z.any(),
-  assignedTo: z.any(),
-  description: z.any(),
-  status: z.any(),
-  createdAt: z.any(),
-  updatedAt: z.any(),
-  completedAt: z.any(),
-  labels: z.any(),
-  _dummy: z.any(),
-});
+export const todoItem = z.object(
+{
+id: z.string(),
+title: z.string(),
+createdBy: z.any(),
+assignedTo: z.any(),
+description: z.string(),
+status: z.any(),
+createdAt: z.string().datetime(),
+updatedAt: z.string().datetime(),
+completedAt: z.string().datetime(),
+labels: z.any(),
+_dummy: z.string()
+}
+);
 
-export const todoLabelRecord = z.object({
-  name: z.any(),
-  color: z.any(),
-});
+export const todoLabelRecord = z.object(
+{
+name: z.string(),
+color: z.string()
+}
+);
 
-export const todoFileAttachment = z.object({
-  filename: z.any(),
-  mediaType: z.any(),
-  contents: z.any(),
-});
+export const todoFileAttachment = z.object(
+{
+filename: z.string(),
+mediaType: z.string(),
+contents: z.string()
+}
+);
 
-export const todoUrlAttachment = z.object({
-  description: z.any(),
-  url: z.any(),
-});
+export const todoUrlAttachment = z.object(
+{
+description: z.string(),
+url: z.string()
+}
+);
 
-export const apiError = z.object({
-  code: z.any(),
-  message: z.any(),
-});
+export const apiError = z.object(
+{
+code: z.string(),
+message: z.string()
+}
+);
 
-export const standard4XxResponse = z.object({
-  statusCode: z.any(),
-});
+export const standard4XxResponse = z.object(
+{
+statusCode: z.string()
+}
+);
 
-export const standard5XxResponse = z.object({
-  statusCode: z.any(),
-});
+export const standard5XxResponse = z.object(
+{
+statusCode: z.string()
+}
+);
 
-export const userCreatedResponse = z.object({
-  id: z.any(),
-  username: z.any(),
-  email: z.any(),
-  password: z.any(),
-  validated: z.any(),
-  statusCode: z.number(),
-  token: z.any(),
-});
+export const userCreatedResponse = z.object(
+{
+id: z.string(),
+username: z.string(),
+email: z.string(),
+password: z.string(),
+accountCreated: z.string().datetime(),
+floaty: z.string(),
+validated: z.string(),
+statusCode: z.number(),
+token: z.string()
+}
+);
 
-export const userExistsResponse = z.object({
-  statusCode: z.number(),
-  code: z.string(),
-});
+export const userExistsResponse = z.object(
+{
+statusCode: z.number(),
+code: z.string()
+}
+);
 
-export const invalidUserResponse = z.object({
-  statusCode: z.number(),
-  code: z.string(),
-});
+export const invalidUserResponse = z.object(
+{
+statusCode: z.number(),
+code: z.string()
+}
+);
 
-export const paginationControls = z.object({
-  limit: z.any(),
-  offset: z.any(),
-});
+export const paginationControls = z.object(
+{
+limit: z.string(),
+offset: z.string()
+}
+);
 
-export const todoPage = z.object({
-  items: z.object(),
-  pageSize: z.any(),
-  totalSize: z.any(),
-  limit: z.any(),
-  offset: z.any(),
-  prevLink: z.any(),
-  nextLink: z.any(),
-});
+export const todoPage = z.object(
+{
+items: z.any(),
+pageSize: z.string(),
+totalSize: z.string(),
+limit: z.string(),
+offset: z.string(),
+prevLink: z.string(),
+nextLink: z.string()
+}
+);
 
-export const todoItemPatch = z.object({
-  title: z.any(),
-  assignedTo: z.any(),
-  description: z.any(),
-  status: z.any(),
-});
+export const todoItemPatch = z.object(
+{
+title: z.any(),
+assignedTo: z.any(),
+description: z.any(),
+status: z.any()
+}
+);
 
-export const invalidTodoItem = z.object({
-  statusCode: z.number(),
-});
+export const invalidTodoItem = z.object(
+{
+statusCode: z.number()
+}
+);
 
-export const notFoundResponse = z.object({
-  statusCode: z.number(),
-});
+export const notFoundResponse = z.object(
+{
+statusCode: z.number()
+}
+);
 
-export const noContentResponse = z.object({
-  statusCode: z.number(),
-});
+export const noContentResponse = z.object(
+{
+statusCode: z.number()
+}
+);
 
-export const page = z.object({
-  items: z.object(),
-});
+export const page = z.object(
+{
+items: z.any()
+}
+);
--- packages/efv2-zod-sketch/src/emitter.tsx
+++ packages/efv2-zod-sketch/src/emitter.tsx
@@ -86,7 +86,7 @@ function ZodType(props: ZodTypeProps) {
     case "Scalar":
     case "Intrinsic":
       // TODO: Handle Scalar intrinsic types. See packages/emitter-framework/src/typescript/components/type-expression.tsx
-      return <>{zod.z}.string()</>;
+      return getScalarIntrinsicZodType(props);
     case "Boolean":
       return <>{zod.z}.boolean()</>;
     case "String":
@@ -95,6 +95,198 @@ function ZodType(props: ZodTypeProps) {
       return <>{zod.z}.number()</>;
     default:
       return <>{zod.z}.any()</>;
+  }
+}
+
+function getScalarIntrinsicZodType(props: ZodTypeProps): string {
+  if ($.scalar.is(props.type)) {
+    // Types with parity in Zod
+    if ($.scalar.isBoolean(props.type)) {
+      return <>{zod.z}.boolean()</>;
+    }
+
+    if ($.scalar.isBytes(props.type)) {
+      return <>{zod.z}.string()</>;
+    }
+
+    // Numbers
+    if ($.scalar.isDecimal(props.type)) {
+      return (
+        <>
+          {zod.z}.number()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+
+    // isDecimal128 is problematic.  If intended to be a whole number (integer), it must be less than 2^53-1 and thus
+    // can't be represented as a number in JavaScript without using BigInt.  But BigInt
+    // makes no sense if this is a floating point number.  We will leave this as a number.
+    // Since Decimal128 is a 128-bit floating point number, we'll take the hit in
+    // precision if an integer.
+    if ($.scalar.isDecimal128(props.type)) {
+      return (
+        <>
+          {zod.z}.number()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+
+    // Bit limitations don't translate very well, since they really
+    // affect precision and not min/max values (i.e. a mismatch won't
+    // cause an overflow but just a truncation in accuracy).  We will leave these as
+    // numbers.
+    if ($.scalar.isFloat(props.type)) {
+      return (
+        <>
+          {zod.z}.number()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isFloat32(props.type)) {
+      return (
+        <>
+          {zod.z}.number()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isFloat64(props.type)) {
+      return (
+        <>
+          {zod.z}.number()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+
+    // With integers, we completely understand the range and can parse to it.
+    if ($.scalar.isInteger(props.type)) {
+      return (
+        <>
+          {zod.z}.number()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isInt8(props.type)) {
+      return (
+        <>
+          {zod.z}.number().min(-128).max(127)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isInt16(props.type)) {
+      return (
+        <>
+          {zod.z}.number().min(-32768).max(32767)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isInt32(props.type)) {
+      return (
+        <>
+          {zod.z}.number().min(-2,147,483,648).max(2,147,483,647)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isInt64(props.type)) {
+      return (
+        <>
+          {zod.z}.number().bigint().min(-9,223,372,036,854,775,808).max(9,223,372,036,854,775,807)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isSafeint(props.type)) {
+      return (
+        <>
+          {zod.z}.number().safe()
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isUint8(props.type)) {
+      return (
+        <>
+          {zod.z}.number().nonnegative().max(255)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isUint16(props.type)) {
+      return (
+        <>
+          {zod.z}.number().nonnegative().max(65535)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isUint32(props.type)) {
+      return (
+        <>
+          {zod.z}.number().nonnegative().nonnegative.max(4,294,967,295)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+    if ($.scalar.isUint64(props.type)) {
+      return (
+        <>
+          {zod.z}.number().bigint().nonnegative().max(18,446,744,073,709,551,615)
+          <ZodConstraints type={props.type} />
+        </>
+      );
+    }
+
+    if ($.scalar.isString(props.type)) {
+      return <>{zod.z}.string()</>;
+    }
+    if ($.scalar.isUrl(props.type)) {
+      return <>{zod.z}.string().url()</>;
+    }
+    if ($.scalar.isNumeric(props.type)) {
+      return <>{zod.z}.number()</>;
+    }
+
+    //Dates and times
+    if ($.scalar.isUtcDateTime(props.type)) {
+      return <>{zod.z}.string().datetime()</>;
+    }
+    if ($.scalar.isOffsetDateTime(props.type)) {
+      return <>{zod.z}.string().datetime( &#123;offset: true&#125;)</>;
+    }
+    if ($.scalar.isDuration(props.type)) {
+      return <>{zod.z}.duration()</>;
+    }
+    if ($.scalar.isPlainDate(props.type)) {
+      return <>{zod.z}.date()</>;
+    }
+    if ($.scalar.isPlainTime(props.type)) {
+      return <>{zod.z}.time()</>;
+    }
+
+    // Types without parity in Zod -- consider throwing an error instead
+  }
+  return <>{zod.z}.string()</>;
+}
+
+function ZodConstraints(props: ZodTypeProps) {
+  if ($.scalar.extendsNumeric(props.type)) {
+    const min = $.scalar.getMin(props.type);
+    const max = $.scalar.getMax(props.type);
+
+    return (
+      <>
+        {min !== undefined ? <>.min({min})</> : null}
+        {max !== undefined ? <>.max({max})</> : null}
+      </>
+    );
   }
 }
 
