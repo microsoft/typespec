5fed9555bde304358af9e9e6701cb5542d5c2966
Dapeng Zhang <dapzhang@microsoft.com>
2024-12-09 10:58:57 +0800

    move those utils back to clientprovidertests

--- packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/ClientProviders/ClientProviderTests.cs
+++ packages/http-client-csharp/generator/Microsoft.Generator.CSharp.ClientModel/test/Providers/ClientProviders/ClientProviderTests.cs
@@ -15,7 +15,6 @@ using Microsoft.Generator.CSharp.Snippets;
 using Microsoft.Generator.CSharp.Statements;
 using Microsoft.Generator.CSharp.Tests.Common;
 using NUnit.Framework;
-using static Microsoft.Generator.CSharp.ClientModel.Tests.Providers.ClientProviders.ClientProviderTestsUtils;
 using static Microsoft.Generator.CSharp.Snippets.Snippet;
 
 namespace Microsoft.Generator.CSharp.ClientModel.Tests.Providers.ClientProviders
@@ -1230,5 +1229,92 @@ namespace Microsoft.Generator.CSharp.ClientModel.Tests.Providers.ClientProviders
         }
 
         internal class FakeTokenCredential { }
+
+        public record ExpectedCSharpType
+        {
+            public string Name { get; }
+
+            public string Namespace { get; }
+
+            public bool IsFrameworkType { get; }
+
+            public Type FrameworkType => _frameworkType ?? throw new InvalidOperationException();
+
+            public bool IsNullable { get; }
+
+            private readonly Type? _frameworkType;
+
+            public ExpectedCSharpType(Type frameworkType, bool isNullable)
+            {
+                _frameworkType = frameworkType;
+                IsFrameworkType = true;
+                IsNullable = isNullable;
+                Name = frameworkType.Name;
+                Namespace = frameworkType.Namespace!;
+            }
+
+            public ExpectedCSharpType(string name, string ns, bool isNullable)
+            {
+                IsFrameworkType = false;
+                IsNullable = isNullable;
+                Name = name;
+                Namespace = ns;
+            }
+
+            public static implicit operator ExpectedCSharpType(CSharpType type)
+            {
+                if (type.IsFrameworkType)
+                {
+                    return new(type.FrameworkType, type.IsNullable);
+                }
+                else
+                {
+                    return new(type.Name, type.Namespace, type.IsNullable);
+                }
+            }
+        }
+
+        public record ExpectedFieldProvider(FieldModifiers Modifiers, ExpectedCSharpType Type, string Name);
+
+        private static void AssertCSharpTypeAreEqual(ExpectedCSharpType expected, CSharpType type)
+        {
+            if (expected.IsFrameworkType)
+            {
+                Assert.IsTrue(type.IsFrameworkType);
+                Assert.AreEqual(expected.FrameworkType, type.FrameworkType);
+            }
+            else
+            {
+                Assert.IsFalse(type.IsFrameworkType);
+                Assert.AreEqual(expected.Name, type.Name);
+                Assert.AreEqual(expected.Namespace, type.Namespace);
+            }
+            Assert.AreEqual(expected.IsNullable, type.IsNullable);
+        }
+
+        private static void AssertFieldAreEqual(ExpectedFieldProvider expected, FieldProvider field)
+        {
+            Assert.AreEqual(expected.Name, field.Name);
+            AssertCSharpTypeAreEqual(expected.Type, field.Type);
+            Assert.AreEqual(expected.Modifiers, field.Modifiers);
+        }
+
+        private static void AssertHasFields(TypeProvider provider, IReadOnlyList<ExpectedFieldProvider> expectedFields)
+        {
+            var fields = provider.Fields;
+
+            // validate the length of the result
+            Assert.GreaterOrEqual(fields.Count, expectedFields.Count);
+
+            // validate each of them
+            var fieldDict = fields.ToDictionary(f => f.Name);
+            for (int i = 0; i < expectedFields.Count; i++)
+            {
+                var expected = expectedFields[i];
+
+                Assert.IsTrue(fieldDict.TryGetValue(expected.Name, out var actual), $"Field {expected.Name} not present");
+                AssertFieldAreEqual(expected, actual!);
+            }
+        }
     }
 }
