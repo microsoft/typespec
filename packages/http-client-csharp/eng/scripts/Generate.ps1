c1655ef6d661e722da6edcb8988dbe0d3cc895ea
Dapeng Zhang <dapzhang@microsoft.com>
2024-12-10 15:20:37 +0800

    revert the cadl ranch onboarding and move it to another separated PR

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -50,6 +50,7 @@ function IsSpecDir {
 
 $failingSpecs = @(
     Join-Path 'http' 'payload' 'pageable'
+    Join-Path 'http' 'special-headers' 'conditional-request'
     Join-Path 'http' 'type' 'model' 'flatten'
     Join-Path 'http' 'type' 'model' 'templated'
 )
