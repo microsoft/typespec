e8f93f2ef91fe19a437d0b741d909d3941e8ba88
iscai-msft <isabellavcai@gmail.com>
2024-12-10 11:32:10 -0500

    update location of compiled files

--- packages/http-client-python/eng/scripts/setup/prepare.ts
+++ packages/http-client-python/eng/scripts/setup/prepare.ts
@@ -1,9 +1,10 @@
 import { runPython3 } from "./run-python3.js";
-
-async function preparePythonDeps() {
-  await runPython3("./eng/scripts/setup/prepare.py");
+async function main() {
+  try {
+    await runPython3("./eng/scripts/setup/prepare.py");
+  } catch (error) {
+    console.log("No Python found on your local environment. We will use Pyodide instead."); // eslint-disable-line no-console
+  }
 }
 
-preparePythonDeps()
-  .then(() => console.log("Successfully prepared all required Python packages")) // eslint-disable-line no-console
-  .catch((error) => console.log(`Preparation failed: ${error.message}`)); // eslint-disable-line no-console
+main();
