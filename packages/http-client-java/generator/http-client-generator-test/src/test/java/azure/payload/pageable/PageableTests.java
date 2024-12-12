c4e153f2a9bfda04b6aa8004566d222d444fd909
Weidong Xu <weidxu@microsoft.com>
2024-12-12 11:41:16 +0800

    move test

--- packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/payload/pageable/PageableTests.java
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/payload/pageable/PageableTests.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package payload.pageable;
+package azure.payload.pageable;
 
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import payload.pageable.models.User;
+import azure.payload.pageable.models.User;
 
 public class PageableTests {
 
