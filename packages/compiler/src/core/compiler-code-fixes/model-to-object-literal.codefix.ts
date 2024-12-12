3c720ea0090c5cc4fcb56d39ef7dc1481aef550d
Zhonglei Ma (WICRESOFT NORTH AMERICA LTD) <v-zhongleima@microsoft.com>
2024-12-12 10:31:23 +0800

    There will be two quick fixes for this issue, and only by clicking on the quick fix of the entire expression can you get the desired result; If you click on the quick fix for a partial expression, because there is no recursive part in the middle of the partial expression, you need to click twice to get the desired result

--- packages/compiler/src/core/compiler-code-fixes/model-to-object-literal.codefix.ts
+++ packages/compiler/src/core/compiler-code-fixes/model-to-object-literal.codefix.ts
@@ -1,5 +1,5 @@
 import { defineCodeFix, getSourceLocation } from "../diagnostics.js";
-import type { ModelExpressionNode } from "../types.js";
+import { SyntaxKind, type CodeFixEdit, type ModelExpressionNode } from "../types.js";
 
 /**
  * Quick fix that convert a model expression to an object value.
@@ -9,8 +9,26 @@ export function createModelToObjectValueCodeFix(node: ModelExpressionNode) {
     id: "model-to-object-value",
     label: `Convert to an object value \`#{}\``,
     fix: (context) => {
+      const result: CodeFixEdit[] = [];
+
       const location = getSourceLocation(node);
-      return context.prependText(location, "#");
+      result.push(context.prependText(location, "#"));
+      createChildModelToObjValCodeFix(node);
+
+      return result;
+
+      function createChildModelToObjValCodeFix(node: ModelExpressionNode) {
+        for (const prop of node.properties.values()) {
+          if ("value" in prop) {
+            const childNode = prop.value;
+            if (childNode.pos > node.pos && childNode.kind === SyntaxKind.ModelExpression) {
+              const locationChild = getSourceLocation(childNode);
+              result.push(context.prependText(locationChild, "#"));
+              createChildModelToObjValCodeFix(childNode);
+            }
+          }
+        }
+      }
     },
   });
 }
