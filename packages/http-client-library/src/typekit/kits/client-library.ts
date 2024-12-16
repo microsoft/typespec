4ce3158835e22f74612c77a2a5c7fb099f5e4229
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-12-16 14:27:12 -0800

    Add listDataTypes function to collect all data types from a client recursively

--- packages/http-client-library/src/typekit/kits/client-library.ts
+++ packages/http-client-library/src/typekit/kits/client-library.ts
@@ -5,12 +5,17 @@ import {
   listServices,
   Model,
   Namespace,
+  navigateType,
+  Operation,
+  Scalar,
+  Type,
+  Union,
 } from "@typespec/compiler";
-import { defineKit } from "@typespec/compiler/typekit";
+import { $, defineKit } from "@typespec/compiler/typekit";
 import { Client } from "../../interfaces.js";
 
 interface ClientLibraryKit {
-   /**
+  /**
    * Get the top-level namespaces that are used to generate the client library.
    *
    * @param namespace: If namespace param is given, we will return the children of the given namespace.
@@ -38,6 +43,7 @@ interface ClientLibraryKit {
    * @param namespace namespace to get the enums of
    */
   listEnums(namespace: Namespace): Enum[];
+  listDataTypes(namespace: Client): Type[];
 }
 
 interface TK {
@@ -118,5 +124,95 @@ defineKit<TK>({
     listEnums(namespace) {
       return [...namespace.enums.values()];
     },
+    listDataTypes(client: Client) {
+      return collectTypes(client, { includeTemplateDeclaration: false }).dataTypes;
+    },
   },
 });
+
+export interface TypeCollectorOptions {
+  includeTemplateDeclaration?: boolean;
+  includeTypeSpecTypes?: boolean;
+}
+
+export function collectTypes(client: Client, options: TypeCollectorOptions = {}) {
+  const dataTypes = new Set<Model | Enum | Union>();
+  const operations: Operation[] = [];
+  $.client.flat(client).forEach((c) => {
+    const ops = $.client.listServiceOperations(c);
+    operations.push(...ops);
+  });
+
+  for (const operation of operations) {
+    navigateType(
+      operation,
+      {
+        model(m) {
+          trackType(dataTypes, m);
+        },
+        modelProperty(p) {
+          trackType(dataTypes, p.type);
+        },
+        scalar(s) {
+          if (s.namespace?.name !== "TypeSpec") {
+            return;
+          }
+
+          trackType(dataTypes, s);
+        },
+        enum(e) {
+          trackType(dataTypes, e);
+        },
+        union(u) {
+          u.variants;
+          trackType(dataTypes, u);
+        },
+        unionVariant(v) {
+          trackType(dataTypes, v.type);
+        },
+      },
+      { includeTemplateDeclaration: options.includeTemplateDeclaration },
+    );
+  }
+
+  return {
+    dataTypes: [...dataTypes],
+    operations: [...operations],
+  };
+}
+
+type DataType = Model | Union | Enum | Scalar;
+
+function isDataType(type: Type): type is DataType {
+  return (
+    type.kind === "Model" || type.kind === "Union" || type.kind === "Enum" || type.kind === "Scalar"
+  );
+}
+
+function isDeclaredType(type: Type): boolean {
+  if ("namespace" in type && type.namespace?.name === "TypeSpec") {
+    return false;
+  }
+
+  if (!isDataType(type)) {
+    return false;
+  }
+
+  if (type.name === undefined || type.name === "") {
+    return false;
+  }
+
+  return true;
+}
+
+function trackType(types: Set<DataType>, type: Type) {
+  if (!isDataType(type)) {
+    return;
+  }
+
+  if (!isDeclaredType(type)) {
+    return;
+  }
+
+  types.add(type);
+}
