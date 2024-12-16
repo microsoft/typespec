e6df27f3c28aaa624317b230f6ad2063647acf11
Dapeng Zhang <dapzhang@microsoft.com>
2024-12-16 16:32:29 +0800

    Adopt http/special-headers/conditional-request from cadl ranch (#5318)
    Fixes https://github.com/microsoft/typespec/issues/3990
    
    cherry picked from @nisha-bhatia 's
    [PR](https://github.com/microsoft/typespec/pull/5214) for clarity.
    
    But this requires the update in that PR to work properly.
    
    ---------
    
    Co-authored-by: nisha-bhatia <67986960+nisha-bhatia@users.noreply.github.com>

--- packages/http-client-csharp/eng/scripts/Generate.ps1
+++ packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -51,7 +51,6 @@ function IsSpecDir {
 $failingSpecs = @(
     Join-Path 'http' 'payload' 'pageable'
     Join-Path 'http' 'payload' 'xml'
-    Join-Path 'http' 'special-headers' 'conditional-request'
     Join-Path 'http' 'type' 'model' 'flatten'
     Join-Path 'http' 'type' 'model' 'templated'
 )
