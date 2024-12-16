311ce09181d10f2132ec4061b9a3f224bd2c8a00
Weidong Xu <weidxu@microsoft.com>
2024-12-16 13:00:46 +0800

    skip emitter, if found dependency not ready

--- packages/http-client-java/emitter/src/emitter.ts
+++ packages/http-client-java/emitter/src/emitter.ts
@@ -120,74 +120,78 @@ export async function $onEmit(context: EmitContext<EmitterOptions>) {
   const program = context.program;
   await validateDependencies(program, true);
 
-  const options = context.options;
-  if (!options["flavor"]) {
-    if (options["package-dir"]?.toLocaleLowerCase().startsWith("azure")) {
-      // Azure package
-      options["flavor"] = "azure";
-    }
-  }
-  const builder = new CodeModelBuilder(program, context);
-  const codeModel = await builder.build();
-
-  if (!program.compilerOptions.noEmit && !program.hasError()) {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const moduleRoot = resolvePath(__dirname, "..", "..");
-
-    const outputPath = options["output-dir"] ?? context.emitterOutputDir;
-    options["output-dir"] = getNormalizedAbsolutePath(outputPath, undefined);
-
-    (options as any)["arm"] = codeModel.arm;
-
-    const codeModelFileName = resolvePath(outputPath, "./code-model.yaml");
-
-    await promises.mkdir(outputPath, { recursive: true }).catch((err) => {
-      if (err.code !== "EISDIR" && err.code !== "EEXIST") {
-        logError(program, `Failed to create output directory: ${outputPath}`);
-        return;
-      }
-    });
-
-    await program.host.writeFile(codeModelFileName, dump(codeModel));
-
-    program.trace("http-client-java", `Code model file written to ${codeModelFileName}`);
-
-    const emitterOptions = JSON.stringify(options);
-    program.trace("http-client-java", `Emitter options ${emitterOptions}`);
-
-    const jarFileName = resolvePath(
-      moduleRoot,
-      "generator/http-client-generator/target",
-      "emitter.jar",
-    );
-    program.trace("http-client-java", `Exec JAR ${jarFileName}`);
-
-    const javaArgs: string[] = [];
-    javaArgs.push(`-DemitterOptions=${emitterOptions}`);
-    if (options["dev-options"]?.debug) {
-      javaArgs.push("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005");
-    }
-    if (options["dev-options"]?.loglevel) {
-      javaArgs.push("-Dorg.slf4j.simpleLogger.defaultLogLevel=" + options["dev-options"]?.loglevel);
-    }
-    if (options["dev-options"]?.["java-temp-dir"]) {
-      javaArgs.push("-Dcodegen.java.temp.directory=" + options["dev-options"]?.["java-temp-dir"]);
-    }
-    javaArgs.push("-jar");
-    javaArgs.push(jarFileName);
-    javaArgs.push(codeModelFileName);
-    try {
-      await asyncSpawn("java", javaArgs);
-    } catch (error: any) {
-      if (error && "code" in error && error["code"] === "ENOENT") {
-        logError(program, "'java' is not on PATH. Please install JDK 11 or above.");
-      } else {
-        logError(program, error.message);
+  if (!program.hasError()) {
+    const options = context.options;
+    if (!options["flavor"]) {
+      if (options["package-dir"]?.toLocaleLowerCase().startsWith("azure")) {
+        // Azure package
+        options["flavor"] = "azure";
       }
     }
+    const builder = new CodeModelBuilder(program, context);
+    const codeModel = await builder.build();
 
-    if (!options["dev-options"]?.["generate-code-model"]) {
-      await program.host.rm(codeModelFileName);
+    if (!program.hasError() && !program.compilerOptions.noEmit) {
+      const __dirname = dirname(fileURLToPath(import.meta.url));
+      const moduleRoot = resolvePath(__dirname, "..", "..");
+
+      const outputPath = options["output-dir"] ?? context.emitterOutputDir;
+      options["output-dir"] = getNormalizedAbsolutePath(outputPath, undefined);
+
+      (options as any)["arm"] = codeModel.arm;
+
+      const codeModelFileName = resolvePath(outputPath, "./code-model.yaml");
+
+      await promises.mkdir(outputPath, { recursive: true }).catch((err) => {
+        if (err.code !== "EISDIR" && err.code !== "EEXIST") {
+          logError(program, `Failed to create output directory: ${outputPath}`);
+          return;
+        }
+      });
+
+      await program.host.writeFile(codeModelFileName, dump(codeModel));
+
+      program.trace("http-client-java", `Code model file written to ${codeModelFileName}`);
+
+      const emitterOptions = JSON.stringify(options);
+      program.trace("http-client-java", `Emitter options ${emitterOptions}`);
+
+      const jarFileName = resolvePath(
+        moduleRoot,
+        "generator/http-client-generator/target",
+        "emitter.jar",
+      );
+      program.trace("http-client-java", `Exec JAR ${jarFileName}`);
+
+      const javaArgs: string[] = [];
+      javaArgs.push(`-DemitterOptions=${emitterOptions}`);
+      if (options["dev-options"]?.debug) {
+        javaArgs.push("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005");
+      }
+      if (options["dev-options"]?.loglevel) {
+        javaArgs.push(
+          "-Dorg.slf4j.simpleLogger.defaultLogLevel=" + options["dev-options"]?.loglevel,
+        );
+      }
+      if (options["dev-options"]?.["java-temp-dir"]) {
+        javaArgs.push("-Dcodegen.java.temp.directory=" + options["dev-options"]?.["java-temp-dir"]);
+      }
+      javaArgs.push("-jar");
+      javaArgs.push(jarFileName);
+      javaArgs.push(codeModelFileName);
+      try {
+        await asyncSpawn("java", javaArgs);
+      } catch (error: any) {
+        if (error && "code" in error && error["code"] === "ENOENT") {
+          logError(program, "'java' is not on PATH. Please install JDK 11 or above.");
+        } else {
+          logError(program, error.message);
+        }
+      }
+
+      if (!options["dev-options"]?.["generate-code-model"]) {
+        await program.host.rm(codeModelFileName);
+      }
     }
   }
 }
