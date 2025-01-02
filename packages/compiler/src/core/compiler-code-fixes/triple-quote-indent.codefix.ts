880f6d3b78cde51eaee9502f44e760c813cbe0de
Zhonglei Ma (WICRESOFT NORTH AMERICA LTD) <v-zhongleima@microsoft.com>
2025-01-02 11:13:14 +0800

    更新的代码和测试

--- packages/compiler/src/core/compiler-code-fixes/triple-quote-indent.codefix.ts
+++ packages/compiler/src/core/compiler-code-fixes/triple-quote-indent.codefix.ts
@@ -1,6 +1,6 @@
-import { isLineBreak, isWhiteSpaceSingleLine } from "../charcode.js";
+import { CharCode, isWhiteSpaceSingleLine } from "../charcode.js";
 import { defineCodeFix, getSourceLocation } from "../diagnostics.js";
-import type { CodeFixEdit, DiagnosticTarget, SourceLocation } from "../types.js";
+import type { CodeFixEdit, DiagnosticTarget } from "../types.js";
 
 export function createTripleQuoteIndentCodeFix(diagnosticTarget: DiagnosticTarget) {
   return defineCodeFix({
@@ -10,143 +10,123 @@ export function createTripleQuoteIndentCodeFix(diagnosticTarget: DiagnosticTarge
       const result: CodeFixEdit[] = [];
 
       const location = getSourceLocation(diagnosticTarget);
-      const { startPos: startPosArr, indent } = findStartPositionAndIndent(location);
-      startPosArr.map((pos) => {
-        const updatedLocation = { ...location, pos };
-        result.push(context.prependText(updatedLocation, indent));
-      });
+      const startPos = location.pos;
+      const endPos = location.end;
+      const offSet = 3;
+      const splitOrIndentStr = "\r\n";
+
+      const arrSplintLines = splitLines(location.file.text, startPos + offSet, endPos - offSet);
+      const count = arrSplintLines.length;
+
+      const minIndentObj: { startPos: number; indent: number } = arrSplintLines.reduce(
+        (prev, curr) => {
+          return prev.indent < curr.indent ? prev : curr;
+        },
+      );
+
+      for (let i = 0; i < count; i++) {
+        const line = arrSplintLines[i];
+        const lastLine = arrSplintLines[arrSplintLines.length - 1];
+
+        if (i === 0 || i === count - 1) {
+          if (i === 0 && line.lineText.trim() !== "") {
+            // start triple quote is not on new line
+            [startPos + offSet].map((pos) => {
+              result.push(
+                context.prependText(
+                  { ...location, pos },
+                  splitOrIndentStr + " ".repeat(lastLine.indent - minIndentObj.indent),
+                ),
+              );
+            });
+          }
+
+          if (i === count - 1 && line.lineText.replace(splitOrIndentStr, "").trim() !== "") {
+            // end triple quote is not on new line
+            [endPos - offSet].map((pos) => {
+              result.push(
+                context.prependText(
+                  { ...location, pos },
+                  splitOrIndentStr +
+                    " ".repeat(
+                      lastLine.indent - (arrSplintLines.length === 1 ? 0 : minIndentObj.indent),
+                    ),
+                ),
+              );
+            });
+          }
+        } else {
+          // All triple quote is on new line, but content is not indented the same as the closing triple quote
+          // So take the difference between the last line and the smallest indentation in all lines,
+          // and supplement the indentation of each line (except the first and last lines)
+          result.push(
+            context.prependText(
+              { ...location, pos: arrSplintLines[i].startPos },
+              " ".repeat(lastLine.indent - minIndentObj.indent),
+            ),
+          );
+        }
+      }
 
       return result;
     },
   });
 }
 
-function isNoNewlineStartTripleQuote(start: number, end: number, input: string): boolean {
-  while (start < end && isWhiteSpaceSingleLine(input.charCodeAt(start))) {
-    start++;
-  }
-  return !isLineBreak(input.charCodeAt(start));
-}
+function splitLines(
+  text: string,
+  start: number,
+  end: number,
+): { startPos: number; indent: number; lineText: string }[] {
+  const lines: { startPos: number; indent: number; lineText: string }[] = [];
+  let pos = start;
 
-function isNoNewlineEndTripleQuote(start: number, end: number, input: string): boolean {
-  while (end > start && isWhiteSpaceSingleLine(input.charCodeAt(end - 1))) {
-    end--;
-  }
-  return !isLineBreak(input.charCodeAt(end - 1));
-}
-
-function getSpaceNumbBetweenStartPosAndVal(start: number, end: number, input: string): number {
-  while (start < end && isWhiteSpaceSingleLine(input.charCodeAt(start))) {
-    start++;
-  }
-  if (isLineBreak(input.charCodeAt(start))) {
-    start += 2;
-  }
-
-  let spaceNumb = 0;
-  while (start < end && isWhiteSpaceSingleLine(input.charCodeAt(start))) {
-    spaceNumb++;
-    start++;
-  }
-  return spaceNumb;
-}
-
-function getSpaceNumbBetweenEnterAndEndPos(start: number, end: number, input: string): number {
-  let spaceNumb = 0;
-  while (end > start && isWhiteSpaceSingleLine(input.charCodeAt(end - 1))) {
-    spaceNumb++;
-    end--;
-  }
-  return spaceNumb;
-}
-
-function findStartPositionAndIndent(location: SourceLocation): {
-  startPos: number[];
-  indent: string;
-} {
-  const text = location.file.text;
-  const splitOrIndentStr = "\r\n";
-  const startPos = location.pos;
-  const endPos = location.end;
-  const offSet = 3; // The length of `"""`
-
-  const noNewlineStart = isNoNewlineStartTripleQuote(startPos + offSet, endPos, text);
-  const noNewlineEnd = isNoNewlineEndTripleQuote(startPos, endPos - offSet, text);
-  if (noNewlineStart && noNewlineEnd) {
-    // eg. `"""  one   two   """`
-    return { startPos: [startPos + offSet, endPos - offSet], indent: splitOrIndentStr };
-  } else if (noNewlineStart) {
-    // eg. `""" one   two   \r\n"""`
-    const startSpaceNumb = getSpaceNumbBetweenStartPosAndVal(startPos + offSet, endPos, text);
-    const endSpaceNumb = getSpaceNumbBetweenEnterAndEndPos(startPos, endPos - offSet, text);
-
-    // Only in the case of equals, the `triple-quote-indent` warning will be triggered.
-    // The `no-new-line-start-triple-quote` warning is triggered when it is greater than
-    if (startSpaceNumb >= endSpaceNumb) {
-      return { startPos: [startPos + offSet], indent: splitOrIndentStr };
-    } else {
-      return {
-        startPos: [startPos + offSet],
-        indent: splitOrIndentStr + " ".repeat(endSpaceNumb - startSpaceNumb),
-      };
-    }
-  } else if (noNewlineEnd) {
-    // eg. `"""\r\n one   two   """`
-    const startSpaceNumb = getSpaceNumbBetweenStartPosAndVal(startPos + offSet, endPos, text);
-    const endSpaceNumb = getSpaceNumbBetweenEnterAndEndPos(startPos, endPos - offSet, text);
-    if (startSpaceNumb < endSpaceNumb) {
-      return {
-        startPos: [endPos - offSet],
-        indent: splitOrIndentStr + " ".repeat(startSpaceNumb),
-      };
-    } else {
-      // Detailed description: `no-new-line-start-triple-quote`, `no-new-line-end-triple-quote`
-      // and `triple-quote-indent` are all warnings about incorrect triple quote values.
-      // Currently, only `triple-quote-indent` has a quick fix.
-      // Todo: add codefix for  `no-new-line-start-triple-quote` and `no-new-line-end-triple-quote` warning
-
-      // It will only warn that the ending """ is not on a new line and will not trigger the triple quote indent warning.
-      return {
-        startPos: [endPos - offSet],
-        indent: splitOrIndentStr + " ".repeat(startSpaceNumb - endSpaceNumb),
-      };
-    }
-  } else {
-    // eg. `"""\r\none\r\n  two\r\n  """`
-    const endSpaceNumb = getSpaceNumbBetweenEnterAndEndPos(startPos, endPos - offSet, text);
-    const arrIndents: number[] = [];
-    let start = startPos + offSet;
-
-    // Calculate the number of spaces needed to align each line
-    while (start > 0 && start < endPos) {
-      const currLineSpaceNumb = getSpaceNumbBetweenStartPosAndVal(start, endPos, text);
-      arrIndents.push(currLineSpaceNumb);
-
-      // If it is 0, the method indexOf cannot get the next `\r\n` position, so add 1
-      start += currLineSpaceNumb === 0 ? 1 : currLineSpaceNumb;
-      start = text.indexOf(splitOrIndentStr, start);
-    }
-
-    // Find all the positions of `\r\n` and remove the last one because it is the position of `"""`
-    const arrStartPos: number[] = [];
-    start = startPos + offSet;
-    while (start < endPos) {
-      start = text.indexOf(splitOrIndentStr, start);
-      if (start < 0) {
+  while (pos <= end) {
+    const ch = text.charCodeAt(pos);
+    switch (ch) {
+      case CharCode.CarriageReturn:
+        if (text.charCodeAt(pos + 1) === CharCode.LineFeed) {
+          addObjToArray();
+          pos++;
+        } else {
+          addObjToArray();
+        }
         break;
-      }
-      start += 2;
-      if (start < endPos) {
-        arrStartPos.push(start);
-      }
+      case CharCode.LineFeed:
+        addObjToArray();
+        break;
     }
-    arrStartPos.pop();
-
-    //If minSpaceNumb is larger than endSpaceNumb, codefix will not be generated
-    const minSpaceNumb = Math.min(...arrIndents);
-    return {
-      startPos: arrStartPos,
-      indent: " ".repeat(endSpaceNumb - minSpaceNumb),
-    };
+    pos++;
   }
+
+  const lineText = text.slice(start, end);
+  const indentNumb = getIndentInLine(lineText);
+  lines.push({ startPos: start + 2, indent: indentNumb, lineText });
+
+  return lines;
+
+  function addObjToArray() {
+    const lineText = text.slice(start, pos);
+    const indentNumb = getIndentInLine(lineText);
+    lines.push({ startPos: start + 2, indent: indentNumb, lineText });
+    start = pos;
+  }
+}
+
+function getIndentInLine(lineText: string): number {
+  let curStart = 0;
+  const curEnd = lineText.length;
+  let indentNumb = 0;
+  if (
+    lineText.charCodeAt(curStart) === CharCode.CarriageReturn &&
+    lineText.charCodeAt(curStart + 1) === CharCode.LineFeed
+  ) {
+    curStart += 2;
+  }
+
+  while (curStart < curEnd && isWhiteSpaceSingleLine(lineText.charCodeAt(curStart))) {
+    indentNumb++;
+    curStart++;
+  }
+  return indentNumb;
 }
--- packages/compiler/test/core/compiler-code-fixes/triple-quote-indent.codefix.test.ts
+++ packages/compiler/test/core/compiler-code-fixes/triple-quote-indent.codefix.test.ts
@@ -29,11 +29,25 @@ describe("CodeFix: triple-quote-indent", () => {
         return createTripleQuoteIndentCodeFix(node);
       },
     ).toChangeTo(`
-      const a = """\r\n one\r\n  two    \r\n""";
+      const a = """\r\n  one\r\n  two    \r\n """;
     `);
   });
 
-  it("case 3: start triple-quote is not on a new line but end one is", async () => {
+  it("case 3: all triple-quote is on one line and is no carriage return in line", async () => {
+    await expectCodeFixOnAst(
+      `
+      const a = ┆""" one  two    """;
+    `,
+      (node) => {
+        strictEqual(node.kind, SyntaxKind.StringLiteral);
+        return createTripleQuoteIndentCodeFix(node);
+      },
+    ).toChangeTo(`
+      const a = """\r\n one  two    \r\n """;
+    `);
+  });
+
+  it("case 4: start triple-quote is not on a new line but end one is", async () => {
     await expectCodeFixOnAst(
       `
       const a = ┆"""one\r\n  two\r\n  """;
@@ -43,11 +57,11 @@ describe("CodeFix: triple-quote-indent", () => {
         return createTripleQuoteIndentCodeFix(node);
       },
     ).toChangeTo(`
-      const a = """\r\n  one\r\n  two\r\n  """;
+      const a = """\r\n  one\r\n    two\r\n  """;
     `);
   });
 
-  it("case 4: end triple-quote is not on a new line but start one is", async () => {
+  it("case 5: end triple-quote is not on a new line but start one is", async () => {
     await expectCodeFixOnAst(
       `
       const a = ┆"""\r\n  one\r\n  two   """;
@@ -57,7 +71,7 @@ describe("CodeFix: triple-quote-indent", () => {
         return createTripleQuoteIndentCodeFix(node);
       },
     ).toChangeTo(`
-      const a = """\r\n  one\r\n  two   \r\n  """;
+      const a = """\r\n    one\r\n  two   \r\n  """;
     `);
   });
 });
