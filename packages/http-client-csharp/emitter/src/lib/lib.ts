f25109fc9e79a88e887e8726df498eda69323bfc
Mingzhe Huang (from Dev Box) <mingzhehuang@microsoft.com>
2025-01-03 10:27:08 +0800

    refactor(http-client-csharp): improve error message of no root client
    - add a `no-root-client` diagnostic error
    - report diagnostic error instead of throwing exception when there is no root client
    
    resolve #5363

--- packages/http-client-csharp/emitter/src/lib/lib.ts
+++ packages/http-client-csharp/emitter/src/lib/lib.ts
@@ -37,6 +37,12 @@ const $lib = createTypeSpecLibrary({
         default: paramMessage`${"message"}`,
       },
     },
+    "no-root-client": {
+      severity: "error",
+      messages: {
+        default: "Cannot generate CSharp SDK since no public root client is defined in typespec file.",
+      },
+    },
   },
   emitter: {
     options: NetEmitterOptionsSchema,
