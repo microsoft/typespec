6507080cb945a4d6eb8c96131392e534a8f57f45
Weidong Xu <weidxu@microsoft.com>
2025-01-13 14:35:29 +0800

    fix lint and format

--- packages/http-specs/specs/response/status-code-range/mockapi.ts
+++ packages/http-specs/specs/response/status-code-range/mockapi.ts
@@ -1,4 +1,4 @@
-import { json, MockRequest, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
+import { json, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
@@ -12,7 +12,7 @@ Scenarios.Response_StatusCodeRange_errorResponse = passOnSuccess({
     status: 494,
     body: json({
       code: "request-header-too-large",
-      message: "Request header too large"
+      message: "Request header too large",
     }),
   },
   kind: "MockApiDefinition",
