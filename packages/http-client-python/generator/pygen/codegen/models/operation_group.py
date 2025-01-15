62b612d19c5e1384db0d18b4d90913e091ac68b8
Yuchao Yan <yuchaoyan@microsoft.com>
2025-01-15 14:33:34 +0800

    fix typing for operation group

--- packages/http-client-python/generator/pygen/codegen/models/operation_group.py
+++ packages/http-client-python/generator/pygen/codegen/models/operation_group.py
@@ -159,7 +159,11 @@ class OperationGroup(BaseModel):
                 ImportType.SDKCORE,
             )
             file_import.add_submodule_import(
-                ".._configuration",
+                self.code_model.get_relative_import_path(
+                    serialize_namespace,
+                    self.code_model.get_imported_namespace_for_client(self.client.client_namespace, async_mode),
+                    module_name="_configuration",
+                ),
                 f"{self.client.name}Configuration",
                 ImportType.LOCAL,
             )
