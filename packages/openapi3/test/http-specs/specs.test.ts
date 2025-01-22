cbe99cb111f1c4ad9e0b895d76d53a81ae39d84a
Kyle Zhang <v-zhanh@microsoft.com>
2025-01-22 15:29:36 +0800

    update case

--- packages/openapi3/test/http-specs/specs.test.ts
+++ packages/openapi3/test/http-specs/specs.test.ts
@@ -1,31 +1,48 @@
 import { resolvePath } from "@typespec/compiler";
 import { findTestPackageRoot } from "@typespec/compiler/testing";
-import { strictEqual } from "assert";
+import { deepStrictEqual, ok, strictEqual } from "assert";
 import { describe, it } from "vitest";
 import { worksFor } from "./../works-for.js";
 import { defineSpecTests } from "./utils/spec-snapshot-testing.js";
 
 const pkgRoot = await findTestPackageRoot(import.meta.url);
 const specsRoot = resolvePath(pkgRoot, "node_modules", "@typespec", "http-specs", "specs");
+const rootOutputDir = resolvePath(pkgRoot, "test", "http-specs", "output");
 
 describe("http-specs convert", () => {
   defineSpecTests({
     specDir: specsRoot,
+    outputDir: rootOutputDir,
     exclude: ["parameters/collection-format", "payload/xml", "routes"],
   });
 });
 
 describe("http-specs cases", () => {
   worksFor(["3.0.0", "3.1.0"], ({ openApiForFile }) => {
-    it("authentication/api-key", async () => {
-      const curr = {
-        name: "authentication/api-key",
-        fullPath: resolvePath(specsRoot, "authentication/api-key"),
-      };
-      const results = await openApiForFile(curr);
-      const res = Object.values(results)[0] as any;
-      const getThing = res.paths["/authentication/api-key/invalid"].get;
-      strictEqual(getThing.operationId, "invalid");
+    describe("AuthApiKeyClient Rest Client", () => {
+      it("should return 204 when the apiKey is valid", async () => {
+        const curr = {
+          name: "authentication/api-key",
+          fullPath: resolvePath(specsRoot, "authentication/api-key"),
+        };
+        const results = await openApiForFile(curr);
+        const res = Object.values(results)[0] as any;
+        const getThing = res.paths["/authentication/api-key/valid"].get;
+        ok(getThing.responses["204"]);
+      });
+
+      it("should return 403 when the apiKey is invalid", async () => {
+        const curr = {
+          name: "authentication/api-key",
+          fullPath: resolvePath(specsRoot, "authentication/api-key"),
+        };
+        const results = await openApiForFile(curr);
+        const res = Object.values(results)[0] as any;
+        const getThing = res.paths["/authentication/api-key/invalid"].get;
+        deepStrictEqual(getThing.responses["403"].content["application/json"].schema, {
+          $ref: "#/components/schemas/InvalidAuth",
+        });
+      });
     });
 
     it("versioning/added", async () => {
--- packages/openapi3/test/http-specs/utils/spec-snapshot-testing.ts
+++ packages/openapi3/test/http-specs/utils/spec-snapshot-testing.ts
@@ -1,13 +1,27 @@
-import { CompilerHost, NodeHost, compile, joinPaths, resolvePath } from "@typespec/compiler";
+import {
+  CompilerHost,
+  NodeHost,
+  compile,
+  getDirectoryPath,
+  joinPaths,
+  resolvePath,
+} from "@typespec/compiler";
 import { expectDiagnosticEmpty } from "@typespec/compiler/testing";
+import { fail, strictEqual } from "assert";
 import { readdirSync } from "fs";
-import { RunnerTestFile, RunnerTestSuite, afterAll, it } from "vitest";
+import { mkdir, readFile, readdir, rm, writeFile } from "fs/promises";
+import { RunnerTestFile, RunnerTestSuite, afterAll, beforeAll, it } from "vitest";
 import { OpenAPI3EmitterOptions } from "../../../src/lib.js";
 import { worksFor } from "./../../works-for.js";
+
+const shouldUpdateSnapshots = process.env.RECORD === "true";
 
 export interface SpecSnapshotTestOptions {
   /**  Spec root directory. */
   specDir: string;
+
+  /** Output directory for snapshots. */
+  outputDir: string;
 
   /** Folders to exclude from testing. */
   exclude?: string[];
@@ -15,31 +29,86 @@ export interface SpecSnapshotTestOptions {
 
 export interface TestContext {
   runCount: number;
+  registerSnapshot(filename: string): void;
 }
 export function defineSpecTests(config: SpecSnapshotTestOptions) {
   const specs = resolveSpecs(config);
+  let existingSnapshots: string[];
+  const writtenSnapshots: string[] = [];
   const context = {
     runCount: 0,
+    registerSnapshot(filename: string) {
+      writtenSnapshots.push(filename);
+    },
   };
+  beforeAll(async () => {
+    existingSnapshots = await readFilesInDirRecursively(config.outputDir);
+  });
 
   afterAll(async function (context: Readonly<RunnerTestSuite | RunnerTestFile>) {
     if (context.tasks.some((x) => x.mode === "skip")) {
       return; // Not running the full test suite, so don't bother checking snapshots.
     }
+    const missingSnapshots = new Set<string>(existingSnapshots);
+    for (const writtenSnapshot of writtenSnapshots) {
+      missingSnapshots.delete(writtenSnapshot);
+    }
+    if (missingSnapshots.size > 0) {
+      if (shouldUpdateSnapshots) {
+        for (const file of [...missingSnapshots].map((x) => joinPaths(config.outputDir, x))) {
+          await rm(file);
+        }
+      } else {
+        const snapshotList = [...missingSnapshots].map((x) => `  ${x}`).join("\n");
+        fail(
+          `The following snapshot are still present in the output dir but were not generated:\n${snapshotList}\n Run with RECORD=true to regenerate them.`,
+        );
+      }
+    }
   });
   worksFor(["3.0.0", "3.1.0"], ({ openApiForFile }) => {
-    specs.forEach((spec) => defineSpecTest(context, spec, openApiForFile));
+    specs.forEach((spec) => defineSpecTest(context, config, spec, openApiForFile));
   });
 }
 
 function defineSpecTest(
   context: TestContext,
+  config: SpecSnapshotTestOptions,
   spec: Spec,
   openApiForFile: (spec: Spec) => Promise<typeof openApiForFile>,
 ) {
   it(spec.name, async () => {
     context.runCount++;
-    await openApiForFile(spec);
+    const results = await openApiForFile(spec);
+    if (shouldUpdateSnapshots) {
+      //await cleanUpDir(config.outputDir, Object.entries(results));
+
+      for (const [snapshotPath, content] of Object.entries(results)) {
+        const outputPath = resolvePath(config.outputDir, snapshotPath);
+        try {
+          await mkdir(getDirectoryPath(outputPath), { recursive: true });
+          await writeFile(outputPath, JSON.stringify(content));
+          context.registerSnapshot(resolvePath(spec.name, snapshotPath));
+        } catch (e) {
+          throw new Error(`Failure to write snapshot: "${outputPath}"\n Error: ${e}`);
+        }
+      }
+    } else {
+      for (const [snapshotPath, content] of Object.entries(results)) {
+        const outputPath = resolvePath(config.outputDir, snapshotPath);
+        let existingContent;
+        try {
+          existingContent = await readFile(outputPath);
+        } catch (e: unknown) {
+          if (isEnoentError(e)) {
+            fail(`Snapshot "${outputPath}" is missing. Run with RECORD=true to regenerate it.`);
+          }
+          throw e;
+        }
+        context.registerSnapshot(resolvePath(spec.name, snapshotPath));
+        strictEqual(JSON.stringify(content), existingContent.toString());
+      }
+    }
   });
 }
 
@@ -57,6 +126,30 @@ function createSpecTestHost(): SpecSnapshotTestHost {
       outputs.set(path, content);
     },
   };
+}
+
+async function readFilesInDirRecursively(dir: string): Promise<string[]> {
+  let entries;
+  try {
+    entries = await readdir(dir, { withFileTypes: true });
+  } catch (e) {
+    if (isEnoentError(e)) {
+      return [];
+    } else {
+      throw new Error(`Failed to read dir "${dir}"\n Error: ${e}`);
+    }
+  }
+  const files: string[] = [];
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      for (const file of await readFilesInDirRecursively(resolvePath(dir, entry.name))) {
+        files.push(resolvePath(entry.name, file));
+      }
+    } else {
+      files.push(entry.name);
+    }
+  }
+  return files;
 }
 
 interface Spec {
@@ -89,6 +182,10 @@ function resolveSpecs(config: SpecSnapshotTestOptions): Spec[] {
   }
 }
 
+function isEnoentError(e: unknown): e is { code: "ENOENT" } {
+  return typeof e === "object" && e !== null && "code" in e;
+}
+
 export async function openApiForFile(spec: Spec, options: OpenAPI3EmitterOptions = {}) {
   const host = createSpecTestHost();
   const program = await compile(host, spec.fullPath, {
@@ -97,10 +194,10 @@ export async function openApiForFile(spec: Spec, options: OpenAPI3EmitterOptions
     options: { "@typespec/openapi3": { ...options, "file-type": "json" } },
   });
   expectDiagnosticEmpty(program.diagnostics);
-  const openApiVersion = options["openapi-versions"]?.[0];
+  const openApiVersion = options["openapi-versions"]?.[0] ?? "3.0.0";
   const output: any = {};
   for (const [path, content] of host.outputs.entries()) {
-    const snapshotPath = resolvePath(spec.name, openApiVersion, path);
+    const snapshotPath = resolvePath(openApiVersion, spec.name, path);
     output[snapshotPath] = JSON.parse(content);
   }
   return output;
