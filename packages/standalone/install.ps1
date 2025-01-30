617895830104573b074e020ecb8ebb1bf10f74a5
Timothee Guerin <tiguerin@microsoft.com>
2025-01-30 15:03:14 -0800

    fix

--- packages/standalone/install.ps1
+++ packages/standalone/install.ps1
@@ -76,7 +76,9 @@ function Get-Env {
 function New-TemporaryDirectory {
     $tmp = [System.IO.Path]::GetTempPath() # Not $env:TEMP, see https://stackoverflow.com/a/946017
     $name = (New-Guid).ToString("N")
-    New-Item -ItemType Directory -Path (Join-Path $tmp $name)
+    $fullPath = Join-Path $tmp $name
+    New-Item -ItemType Directory -Path $fullPath
+    return $fullPath
 }
 
 function Find-Latest-Version {
