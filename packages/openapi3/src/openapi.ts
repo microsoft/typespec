b25d6e93942e0a75f92dee42dcf7d737d4755249
Timothee Guerin <tiguerin@microsoft.com>
2025-02-05 12:56:12 -0800

    missing some deprecation

--- packages/openapi3/src/openapi.ts
+++ packages/openapi3/src/openapi.ts
@@ -383,6 +383,7 @@ function createOAPIEmitter(
       case "Scalar":
         return ignoreDiagnostics(
           program.checker.isTypeAssignableTo(
+            // eslint-disable-next-line @typescript-eslint/no-deprecated
             type.projectionBase ?? type,
             program.checker.getStdType("string"),
             type,
