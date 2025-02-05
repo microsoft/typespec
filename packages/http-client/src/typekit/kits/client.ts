503fb4bc46d46334ba5571ababe74de455a426ab
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2025-02-04 21:18:22 -0800

    Add @typespec/http-client

---
+++ packages/http-client/src/typekit/kits/client.ts
@@ -0,0 +1,216 @@
+import {
+  Interface,
+  isTemplateDeclaration,
+  isTemplateDeclarationOrInstance,
+  Namespace,
+  NoTarget,
+  Operation,
+} from "@typespec/compiler";
+import { defineKit } from "@typespec/compiler/experimental/typekit";
+import {
+  getHttpService,
+  getServers,
+  HttpServiceAuthentication,
+  resolveAuthentication,
+} from "@typespec/http";
+import "@typespec/http/experimental/typekit";
+import { InternalClient } from "../../interfaces.js";
+import { reportDiagnostic } from "../../lib.js";
+import { createBaseConstructor, getConstructors } from "../../utils/client-helpers.js";
+import { NameKit } from "./utils.js";
+
+interface ClientKit extends NameKit<InternalClient> {
+  /**
+   * Get the parent of a client
+   * @param type The client to get the parent of
+   */
+  getParent(type: InternalClient | Namespace | Interface): InternalClient | undefined;
+  /**
+   * Flatten a client into a list of clients. This will include the client and all its sub clients recursively.
+   * @param client The client to flatten
+   */
+  flat(client: InternalClient): InternalClient[];
+  /**
+   * Get a client from a single namespace / interface
+   */
+  getClient(namespace: Namespace | Interface): InternalClient;
+  /**
+   * Get the constructor for a client. Will return the base intersection of all possible constructors.
+   *
+   * If you'd like to look at overloads, call `this.operation.getOverloads` on the result of this function.
+   *
+   * @param client The client we're getting constructors for
+   */
+  getConstructor(client: InternalClient): Operation;
+
+  /**
+   * Whether the client is publicly initializable
+   */
+  isPubliclyInitializable(client: InternalClient): boolean;
+
+  /**
+   * Return the methods on the client
+   *
+   * @param client the client to get the methods for
+   */
+  listServiceOperations(client: InternalClient): Operation[];
+
+  /**
+   * Get the url template of a client, given its constructor as well */
+  getUrlTemplate(client: InternalClient, constructor: Operation): string;
+  /**
+   * Determines is both clients have the same constructor
+   */
+  haveSameConstructor(a: InternalClient, b: InternalClient): Boolean;
+  /**
+   * Resolves the authentication schemes for a client
+   * @param client
+   */
+  getAuth(client: InternalClient): HttpServiceAuthentication;
+}
+
+interface TypeKit {
+  client: ClientKit;
+}
+
+declare module "@typespec/compiler/experimental/typekit" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Typekit extends TypeKit {}
+}
+
+function getClientName(name: string): string {
+  return name.endsWith("Client") ? name : `${name}Client`;
+}
+
+export const clientCache = new Map<Namespace | Interface, InternalClient>();
+export const clientOperationCache = new Map<InternalClient, Operation[]>();
+
+defineKit<TypeKit>({
+  client: {
+    getParent(client) {
+      const type = client.kind === "Client" ? client.type : client;
+      if (type.namespace && type.namespace !== this.program.getGlobalNamespaceType()) {
+        return this.client.getClient(type.namespace);
+      }
+
+      return undefined;
+    },
+    flat(client) {
+      const clientStack = [client];
+      const clients: InternalClient[] = [];
+      while (clientStack.length > 0) {
+        const currentClient = clientStack.pop();
+        if (currentClient) {
+          clients.push(currentClient);
+          clientStack.push(...this.clientLibrary.listClients(currentClient));
+        }
+      }
+      return clients;
+    },
+    getClient(namespace) {
+      if (!namespace) {
+        reportDiagnostic(this.program, {
+          code: "undefined-namespace-for-client",
+          target: NoTarget,
+        });
+      }
+      if (clientCache.has(namespace)) {
+        return clientCache.get(namespace)!;
+      }
+
+      const client = {
+        kind: "Client",
+        name: getClientName(namespace.name),
+        service: namespace,
+        type: namespace,
+      } as InternalClient;
+
+      clientCache.set(namespace, client);
+      return client;
+    },
+    getConstructor(client) {
+      const constructors = getConstructors(client);
+      if (constructors.length === 1) {
+        return constructors[0];
+      }
+      return createBaseConstructor(client, constructors);
+    },
+    getName(client) {
+      return client.name;
+    },
+    isPubliclyInitializable(client) {
+      return client.type.kind === "Namespace";
+    },
+    listServiceOperations(client) {
+      if (clientOperationCache.has(client)) {
+        return clientOperationCache.get(client)!;
+      }
+
+      if (client.type.kind === "Interface" && isTemplateDeclaration(client.type)) {
+        // Skip template interface operations
+        return [];
+      }
+
+      const operations: Operation[] = [];
+
+      for (const clientOperation of client.type.operations.values()) {
+        // Skip templated operations
+        if (isTemplateDeclarationOrInstance(clientOperation)) {
+          continue;
+        }
+
+        operations.push(clientOperation);
+      }
+
+      clientOperationCache.set(client, operations);
+
+      return operations;
+    },
+    getUrlTemplate(client, constructor) {
+      const params = this.operation.getClientSignature(client, constructor);
+      const endpointParams = params
+        .filter(
+          (p) =>
+            this.modelProperty.getName(p) === "endpoint" || this.modelProperty.isHttpPathParam(p),
+        )
+        .map((p) => p.name)
+        .sort();
+      if (endpointParams.length === 1) {
+        return "{endpoint}";
+      }
+      // here we have multiple templated arguments to an endpoint
+      const servers = getServers(this.program, client.service) || [];
+      for (const server of servers) {
+        const serverParams = [...server.parameters.values()].map((p) => p.name).sort();
+        if (JSON.stringify(serverParams) === JSON.stringify(endpointParams)) {
+          // this is the server we want
+          return server.url;
+        }
+      }
+      return "{endpoint}";
+    },
+    haveSameConstructor(a, b) {
+      const aConstructor = this.client.getConstructor(a);
+      const bConstructor = this.client.getConstructor(b);
+
+      const bParams = [...bConstructor.parameters.properties.values()];
+      const aParams = [...aConstructor.parameters.properties.values()];
+
+      if (bParams.length !== aParams.length) {
+        return false;
+      }
+
+      for (let i = 0; i < aParams.length; i++) {
+        if (bParams[i].type !== aParams[i].type) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+    getAuth(client) {
+      const [httpService] = getHttpService(this.program, client.service);
+      return resolveAuthentication(httpService);
+    },
+  },
+});
---
+++ packages/http-client/test/typekit/client.test.ts
@@ -0,0 +1,465 @@
+import { Interface, Namespace } from "@typespec/compiler";
+import { BasicTestRunner } from "@typespec/compiler/testing";
+import { $ } from "@typespec/compiler/experimental/typekit";
+import { ok } from "assert";
+import { beforeEach, describe, expect, it } from "vitest";
+import "../../src/typekit/index.js";
+import { createTypespecHttpClientLibraryTestRunner } from "../test-host.js";
+
+let runner: BasicTestRunner;
+
+beforeEach(async () => {
+  runner = await createTypespecHttpClientLibraryTestRunner();
+});
+
+describe("isSameConstructor", () => {
+  it("should return true for the same client", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService;
+      `)) as { DemoService: Namespace };
+
+    const client = $.client.getClient(DemoService);
+
+    expect($.client.haveSameConstructor(client, client)).toBeTruthy();
+  });
+
+  it("should return false for the clients with different constructors", async () => {
+    const { DemoService, SubClient } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        @useAuth(ApiKeyAuth<ApiKeyLocation.header, "x-ms-api-key">)
+        @test namespace SubClient {
+        }
+      }
+      `)) as { DemoService: Namespace; SubClient: Namespace };
+
+    const client = $.client.getClient(DemoService);
+    const subClient = $.client.getClient(SubClient);
+
+    expect($.client.haveSameConstructor(client, subClient)).toBeFalsy();
+  });
+
+  it("should return false for the clients with different constructors", async () => {
+    const { DemoService, SubClient } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        @useAuth(ApiKeyAuth<ApiKeyLocation.header, "x-ms-api-key">)
+        @test namespace SubClient {
+        }
+      }
+      `)) as { DemoService: Namespace; SubClient: Namespace };
+
+    const client = $.client.getClient(DemoService);
+    const subClient = $.client.getClient(SubClient);
+
+    expect($.client.haveSameConstructor(client, subClient)).toBeFalsy();
+  });
+
+  it.skip("should return true when subclient doesn't override the client params", async () => {
+    const { DemoService, SubClient } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @useAuth(ApiKeyAuth<ApiKeyLocation.header, "x-ms-api-key">)
+      @test namespace DemoService {
+        @test namespace SubClient {
+        }
+      }
+      `)) as { DemoService: Namespace; SubClient: Namespace };
+
+    const demoClient = $.client.getClient(DemoService)
+    const subClient = $.client.getClient(SubClient)
+
+
+    expect($.client.haveSameConstructor(demoClient, subClient)).toBeTruthy();
+  });
+
+  it("should return false for the clients with different constructor", async () => {
+    const { DemoService, SubClient } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        @test namespace SubClient {
+        }
+      }
+      `)) as { DemoService: Namespace; SubClient: Namespace };
+
+    const client = $.client.getClient(DemoService);
+    const subClient = $.client.getClient(SubClient);
+
+    expect($.client.haveSameConstructor(client, subClient)).toBeTruthy();
+  });
+});
+
+describe("getClient", () => {
+  it("should get a client from the globalNamespace", async () => {
+    (await runner.compile(`
+       op foo(): void;
+      `)) as { DemoService: Namespace };
+
+    const namespace = $.program.getGlobalNamespaceType();
+    const client = $.client.getClient(namespace);
+
+    expect(client.name).toEqual("Client");
+  });
+
+  it("should get the client", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService;
+      `)) as { DemoService: Namespace };
+
+    const client = $.client.getClient(DemoService);
+
+    expect(client.name).toEqual("DemoServiceClient");
+    expect(client.service).toEqual(DemoService);
+    expect(client.type).toEqual(DemoService);
+  });
+
+  it("should preserve client object identity", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService;
+      `)) as { DemoService: Namespace };
+
+    const client1 = $.client.getClient(DemoService);
+    const client2 = $.client.getClient(DemoService);
+    expect(client1).toBe(client2);
+  });
+
+  it("should get a flattened list of clients", async () => {
+    const { DemoService, BarBaz } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        @test namespace Foo {
+           @test namespace FooBaz {}
+        }
+
+        @test namespace Bar {
+           @test interface BarBaz {}
+        }
+      }
+      `)) as { DemoService: Namespace; BarBaz: Interface };
+
+    const client = $.client.getClient(DemoService);
+    const flatClients = $.client.flat(client);
+    const barBaz = $.client.getClient(BarBaz);
+    expect(flatClients).toHaveLength(5);
+    const barBazClient = flatClients.find((c) => c.type === barBaz.type);
+    expect(barBazClient).toBeDefined();
+  });
+});
+
+describe("getConstructor", () => {
+  describe("credential parameter", () => {
+    it("none", async () => {
+      const { DemoService } = (await runner.compile(`
+        @service({
+          title: "Widget Service",
+        })
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+      // no overloads, should just be one
+      expect($.operation.getOverloads(client, constructor)).toHaveLength(0);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(1);
+      expect(params[0].name).toEqual("endpoint");
+      expect($.scalar.isString(params[0].type)).toBeTruthy();
+    });
+    it("apikey", async () => {
+      const { DemoService } = (await runner.compile(`
+        @service({
+          title: "Widget Service",
+        })
+        @useAuth(ApiKeyAuth<ApiKeyLocation.header, "x-ms-api-key">)
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+      // no constructor overloads, should just be one
+      expect($.operation.getOverloads(client, constructor)).toHaveLength(0);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(2);
+      expect(params[0].name).toEqual("endpoint");
+      expect($.scalar.isString(params[0].type)).toBeTruthy();
+      const credParam = params[1];
+      expect(credParam.name).toEqual("credential");
+      ok($.literal.isString(credParam.type));
+      expect(credParam.type.value).toEqual("apiKey");
+    });
+
+    it("bearer", async () => {
+      const { DemoService } = (await runner.compile(`
+        @service({
+          title: "Widget Service",
+        })
+        @useAuth(OAuth2Auth<[{
+          type: OAuth2FlowType.implicit;
+          authorizationUrl: "https://login.microsoftonline.com/common/oauth2/authorize";
+          scopes: ["https://security.microsoft.com/.default"];
+        }]>)
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+      // no constructor overloads, should just be one
+      expect($.operation.getOverloads(client, constructor)).toHaveLength(0);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(2);
+      expect(params[0].name).toEqual("endpoint");
+      expect($.scalar.isString(params[0].type)).toBeTruthy();
+      const credParam = params[1];
+      expect(credParam.name).toEqual("credential");
+      ok($.literal.isString(credParam.type));
+      expect(credParam.type.value).toEqual("oauth2");
+    });
+  });
+  describe("endpoint", () => {
+    it("no servers", async () => {
+      const { DemoService } = (await runner.compile(`
+        @service({
+          title: "Widget Service",
+        })
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+      // no overloads, should just be one
+      expect($.operation.getOverloads(client, constructor)).toHaveLength(0);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(1);
+      expect(params[0].name).toEqual("endpoint");
+      expect($.scalar.isString(params[0].type)).toBeTruthy();
+    });
+    it("one server, no params", async () => {
+      const { DemoService } = (await runner.compile(`
+        @server("https://example.com", "The service endpoint")
+        @service({
+          title: "Widget Service",
+        })
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+      expect($.operation.getOverloads(client, constructor)).toHaveLength(0);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(1);
+      expect(params[0].name).toEqual("endpoint");
+      const clientDefaultValue = $.modelProperty.getClientDefaultValue(client, params[0]);
+      ok(clientDefaultValue?.valueKind === "StringValue");
+      expect(clientDefaultValue.value).toEqual("https://example.com");
+    });
+    it("one server with parameter", async () => {
+      const { DemoService } = (await runner.compile(`
+        @server("https://example.com/{name}/foo", "My service url", { name: string })
+        @service({
+          title: "Widget Service",
+        })
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+
+      // base operation
+      expect(constructor.returnType).toEqual($.program.checker.voidType);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(2);
+      const endpointParam = params.find((p) => p.name === "endpoint");
+      ok(endpointParam);
+      expect(endpointParam.optional).toBeTruthy();
+
+      const nameParam = params.find((p) => p.name === "name");
+      ok(nameParam);
+      expect(nameParam.optional).toBeTruthy();
+
+      // should have two overloads, one for completely overriding endpoint, one for just the parameter name
+      expect($.operation.getOverloads(client, constructor)).toHaveLength(2);
+
+      // parameter name overload
+      const paramNameOverload = $.operation
+        .getOverloads(client, constructor)
+        .find((o) => $.operation.getClientSignature(client, o).find((p) => p.name === "name"));
+      ok(paramNameOverload);
+
+      const paramNameOverloadParams = $.operation.getClientSignature(client, paramNameOverload);
+      expect(paramNameOverloadParams).toHaveLength(1);
+      expect(paramNameOverloadParams[0].name).toEqual("name");
+      expect(paramNameOverloadParams[0].optional).toBeFalsy();
+
+      expect(paramNameOverload.returnType).toEqual($.program.checker.voidType);
+
+      // endpoint overload
+      const endpointOverload = $.operation
+        .getOverloads(client, constructor)
+        .find((o) => $.operation.getClientSignature(client, o).find((p) => p.name === "endpoint"));
+      ok(endpointOverload);
+
+      const endpointOverloadParams = $.operation.getClientSignature(client, endpointOverload);
+      expect(endpointOverloadParams).toHaveLength(1);
+      expect(endpointOverloadParams[0].name).toEqual("endpoint");
+      expect(endpointOverloadParams[0].optional).toBeFalsy();
+
+      expect(endpointOverload.returnType).toEqual($.program.checker.voidType);
+    });
+    it("multiple servers", async () => {
+      const { DemoService } = (await runner.compile(`
+        @server("https://example.com", "The service endpoint")
+        @server("https://example.org", "The service endpoint")
+        @service({
+          title: "Widget Service",
+        })
+        @test namespace DemoService;
+        `)) as { DemoService: Namespace };
+      const client = $.clientLibrary.listClients(DemoService)[0];
+      const constructor = $.client.getConstructor(client);
+
+      // base operation
+      expect(constructor.returnType).toEqual($.program.checker.voidType);
+      const params = $.operation.getClientSignature(client, constructor);
+      expect(params).toHaveLength(1);
+      const endpointParam = params.find((p) => p.name === "endpoint");
+      ok(endpointParam);
+      expect(endpointParam.optional).toBeFalsy();
+      // TODO: i'm getting a ProxyRef to a String type instead of an actual string type, so $.isString is failing
+      ok(endpointParam.type.kind === "Scalar" && endpointParam.type.name === "string");
+
+      // should have two overloads, one for each server
+      const overloads = $.operation.getOverloads(client, constructor);
+      expect(overloads).toHaveLength(2);
+
+      // .com overload
+      const comOverload = overloads[0];
+      const comOverloadParams = $.operation.getClientSignature(client, comOverload);
+      expect(comOverloadParams).toHaveLength(1);
+    });
+  });
+});
+
+describe("isPubliclyInitializable", () => {
+  it("namespace", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService;
+      `)) as { DemoService: Namespace };
+
+    const responses = $.clientLibrary.listClients(DemoService);
+    expect(responses).toHaveLength(1);
+    expect(responses[0].name).toEqual("DemoServiceClient");
+    expect($.client.isPubliclyInitializable(responses[0])).toBeTruthy();
+  });
+  it("nested namespace", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        namespace NestedService {};
+      }
+      `)) as { DemoService: Namespace };
+
+    const responses = $.clientLibrary.listClients(DemoService);
+    expect(responses).toHaveLength(1);
+    expect(responses[0].name).toEqual("DemoServiceClient");
+    expect($.client.isPubliclyInitializable(responses[0])).toBeTruthy();
+
+    const subclients = $.clientLibrary.listClients(responses[0]);
+    expect(subclients).toHaveLength(1);
+    expect(subclients[0].name).toEqual("NestedServiceClient");
+    expect($.client.isPubliclyInitializable(subclients[0])).toBeTruthy();
+  });
+  it("nested interface", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        interface NestedInterface {};
+      }
+      `)) as { DemoService: Namespace };
+
+    const responses = $.clientLibrary.listClients(DemoService);
+    expect(responses).toHaveLength(1);
+    expect(responses[0].name).toEqual("DemoServiceClient");
+    expect($.client.isPubliclyInitializable(responses[0])).toBeTruthy();
+
+    const subclients = $.clientLibrary.listClients(responses[0]);
+    expect(subclients).toHaveLength(1);
+    expect(subclients[0].name).toEqual("NestedInterfaceClient");
+    expect($.client.isPubliclyInitializable(subclients[0])).toBeFalsy();
+  });
+});
+
+describe("listServiceOperations", () => {
+  it("should list only operations defined in the spec", async () => {
+    await runner.compile(`
+      op foo(): void;
+     `);
+
+    const namespace = $.program.getGlobalNamespaceType();
+    const client = $.client.getClient(namespace);
+
+    const operations = $.client.listServiceOperations(client);
+
+    expect(operations).toHaveLength(1);
+    expect(operations[0].name).toEqual("foo");
+  });
+
+  it("no operations", async () => {
+    const { DemoService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService;
+      `)) as { DemoService: Namespace };
+    const client = $.clientLibrary.listClients(DemoService)[0];
+    const operations = $.client.listServiceOperations(client);
+    expect(operations).toHaveLength(0);
+  });
+  it("nested namespace", async () => {
+    const { DemoService, NestedService } = (await runner.compile(`
+      @service({
+        title: "Widget Service",
+      })
+      @test namespace DemoService {
+        @route("demo")
+        op demoServiceOp(): void;
+        @test namespace NestedService {
+          @route("nested")
+          op nestedServiceOp(): void;
+        };
+      }
+      `)) as { DemoService: Namespace; NestedService: Namespace };
+
+    const demoServiceClient = $.clientLibrary.listClients(DemoService)[0];
+    expect($.client.listServiceOperations(demoServiceClient)).toHaveLength(1);
+    expect($.client.listServiceOperations(demoServiceClient)[0].name).toEqual("demoServiceOp");
+
+    const nestedServiceClient = $.clientLibrary.listClients(NestedService)[0];
+    expect($.client.listServiceOperations(nestedServiceClient)).toHaveLength(1);
+    expect($.client.listServiceOperations(nestedServiceClient)[0].name).toEqual("nestedServiceOp");
+  });
+});
