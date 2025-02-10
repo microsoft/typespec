3ee3f3183b63f791f9a9dd6367a25f6598035d4c
jolov <jolov@microsoft.com>
2025-02-09 20:06:03 -0800

    reset module

--- packages/http-client-csharp/emitter/test/Unit/dotnet-sdk-validation.test.ts
+++ packages/http-client-csharp/emitter/test/Unit/dotnet-sdk-validation.test.ts
@@ -1,7 +1,3 @@
-vi.mock("../../src/lib/utils.js", () => ({
-  execAsync: vi.fn(),
-}));
-
 import { Program } from "@typespec/compiler";
 import { TestHost } from "@typespec/compiler/testing";
 import { strictEqual } from "assert";
@@ -10,17 +6,6 @@ import { _validateDotNetSdk } from "../../src/emitter.js";
 import { Logger, LoggerLevel } from "../../src/index.js";
 import { execAsync } from "../../src/lib/utils.js";
 import { createEmitterTestHost, typeSpecCompile } from "./utils/test-util.js";
-const loggerMock = {
-  info: vi.fn(),
-  warn: vi.fn(),
-  error: vi.fn(),
-  log: vi.fn(),
-};
-
-vi.mock("../../src/index.js", () => ({
-  Logger: vi.fn().mockImplementation(() => loggerMock),
-  LoggerLevel: { INFO: "info" },
-}));
 
 describe("Test _validateDotNetSdk", () => {
   let runner: TestHost;
@@ -39,19 +24,18 @@ describe("Test _validateDotNetSdk", () => {
       `,
       runner,
     );
-
-    vi.clearAllMocks();
-  });
-
-  afterEach(() => {
-    vi.restoreAllMocks();
+    vi.resetModules();
+    vi.mock("../../src/lib/utils.js", () => ({
+      execAsync: vi.fn(),
+    }));
+    // vi.restoreAllMocks();
   });
 
   it("should return false and report diagnostic when dotnet SDK is not installed.", async () => {
     /* mock the scenario that dotnet SDK is not installed, so execAsync will throw exception with error ENOENT */
     const error: any = new Error("ENOENT: no such file or directory");
     error.code = "ENOENT";
-    (execAsync as Mock).mockRejectedValue(error);
+    (execAsync as Mock).mockRejectedValueOnce(error);
     const logger = new Logger(program, LoggerLevel.INFO);
     const result = await _validateDotNetSdk(program, minVersion, logger);
     expect(result).toBe(false);
@@ -68,12 +52,12 @@ describe("Test _validateDotNetSdk", () => {
 
   it("should return true for installed SDK version whose major equals min supported version", async () => {
     /* mock the scenario that the installed SDK version whose major equals min supported version */
-    (execAsync as Mock).mockResolvedValue({
-      exitCode: 0,
-      stdio: "",
-      stdout: "8.0.204",
-      stderr: "",
-      proc: { pid: 0, output: "", stdout: "", stderr: "", stdin: "" },
+    (execAsync as Mock).mockResolvedValueOnce({
+        exitCode: 0,
+        stdio: "",
+        stdout: "8.0.204",
+        stderr: "",
+        proc: { pid: 0, output: "", stdout: "", stderr: "", stdin: "" },
     });
     const logger = new Logger(program, LoggerLevel.INFO);
     const result = await _validateDotNetSdk(program, minVersion, logger);
@@ -84,12 +68,12 @@ describe("Test _validateDotNetSdk", () => {
 
   it("should return true for installed SDK version whose major greaters than min supported version", async () => {
     /* mock the scenario that the installed SDK version whose major greater than min supported version */
-    (execAsync as Mock).mockResolvedValue({
-      exitCode: 0,
-      stdio: "",
-      stdout: "9.0.102",
-      stderr: "",
-      proc: { pid: 0, output: "", stdout: "", stderr: "", stdin: "" },
+    (execAsync as Mock).mockResolvedValueOnce({
+        exitCode: 0,
+        stdio: "",
+        stdout: "9.0.102",
+        stderr: "",
+        proc: { pid: 0, output: "", stdout: "", stderr: "", stdin: "" },
     });
     const logger = new Logger(program, LoggerLevel.INFO);
     const result = await _validateDotNetSdk(program, minVersion, logger);
@@ -100,12 +84,12 @@ describe("Test _validateDotNetSdk", () => {
 
   it("should return false and report diagnostic for invalid .NET SDK version", async () => {
     /* mock the scenario that the installed SDK version whose major less than min supported version */
-    (execAsync as Mock).mockResolvedValue({
-      exitCode: 0,
-      stdio: "",
-      stdout: "5.0.408",
-      stderr: "",
-      proc: { pid: 0, output: "", stdout: "", stderr: "", stdin: "" },
+    (execAsync as Mock).mockResolvedValueOnce({
+        exitCode: 0,
+        stdio: "",
+        stdout: "5.0.408",
+        stderr: "",
+        proc: { pid: 0, output: "", stdout: "", stderr: "", stdin: "" },
     });
     const logger = new Logger(program, LoggerLevel.INFO);
     const result = await _validateDotNetSdk(program, minVersion, logger);
