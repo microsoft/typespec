49dc4d3f704d922d09d466428b63a63813c3db99
Yuchao Yan <yuchaoyan@microsoft.com>
2025-02-12 15:14:13 +0800

    fix pylint issue

--- packages/http-client-python/generator/pygen/codegen/serializers/model_serializer.py
+++ packages/http-client-python/generator/pygen/codegen/serializers/model_serializer.py
@@ -328,7 +328,7 @@ class DpgModelSerializer(_ModelSerializer):
         # there is 4 spaces indentation so original line length limit 120 - 4 = 116
         pylint_disable = (
             " # pylint: disable=line-too-long"
-            if len(generated_code) <= 116 < (len(generated_code) + len(type_ignore))
+            if (len(generated_code) <= 116 < (len(generated_code) + len(type_ignore))) or (len(generated_code) > 116)
             else ""
         )
         return f"{generated_code}{type_ignore}{pylint_disable}"
