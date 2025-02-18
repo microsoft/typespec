95fd7533ac4004d744c3c058a6614dcae946f984
Arcturus Zhang <dapzhang@microsoft.com>
2025-02-18 17:27:28 +0800

    Revert "clean up"
    This reverts commit 25cf9b525faff7395a1c62687e39c76ea976bfd4.

--- packages/http-client-csharp/emitter/src/lib/utils.ts
+++ packages/http-client-csharp/emitter/src/lib/utils.ts
@@ -108,3 +108,38 @@ async function processJsonRpc(context: CSharpEmitterContext, message: string): P
     return undefined;
   }
 }
+
+export async function execAsync(
+  command: string,
+  args: string[] = [],
+  options: SpawnOptions = {},
+): Promise<{ exitCode: number; stdio: string; stdout: string; stderr: string; proc: any }> {
+  const child = spawn(command, args, options);
+
+  return new Promise((resolve, reject) => {
+    child.on("error", (error) => {
+      reject(error);
+    });
+    const stdio: Buffer[] = [];
+    const stdout: Buffer[] = [];
+    const stderr: Buffer[] = [];
+    child.stdout?.on("data", (data) => {
+      stdout.push(data);
+      stdio.push(data);
+    });
+    child.stderr?.on("data", (data) => {
+      stderr.push(data);
+      stdio.push(data);
+    });
+
+    child.on("exit", (exitCode) => {
+      resolve({
+        exitCode: exitCode ?? -1,
+        stdio: Buffer.concat(stdio).toString(),
+        stdout: Buffer.concat(stdout).toString(),
+        stderr: Buffer.concat(stderr).toString(),
+        proc: child,
+      });
+    });
+  });
+}
