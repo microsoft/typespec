d277438c5bf6559550a27e66df8b253634180159
jolov <jolov@microsoft.com>
2025-02-18 20:28:39 -0800

    Respect fully qualified namespace when matching

--- packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator/test/Providers/ModelProviders/TestData/ClientCustomizationTests/CanRemoveConstructors/MockInputClient.cs
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator/test/Providers/ModelProviders/TestData/ClientCustomizationTests/CanRemoveConstructors/MockInputClient.cs
@@ -1,11 +1,25 @@
 using System.Collections.Generic;
 using UnbrandedTypeSpec;
 
-namespace Sample;
-
-[CodeGenSuppress("MockInputClient"]
-[CodeGenSuppress("MockInputClient", typeof(bool))]
-[CodeGenSuppress("MockInputClient", typeof(bool), typeof(int))]
-public partial class MockInputClient
+namespace Sample
 {
+    [CodeGenSuppress("MockInputClient"]
+    [CodeGenSuppress("MockInputClient", typeof(bool))]
+    [CodeGenSuppress("MockInputClient", typeof(bool), typeof(int))]
+    [CodeGenSuppress("MockInputClient", typeof(Foo))]
+    [CodeGenSuppress("MockInputClient", typeof(BarNamespace.Foo))]
+    public partial class MockInputClient
+    {
+    }
+
+    public class Foo
+    {
+    }
+}
+
+namespace BarNamespace
+{
+    public class Foo
+    {
+    }
 }
