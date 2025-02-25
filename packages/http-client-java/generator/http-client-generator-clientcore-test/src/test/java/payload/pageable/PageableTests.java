59e1d382cdc4cdadd0c1981c156074a6541573e5
Weidong Xu <weidxu@microsoft.com>
2025-02-25 15:09:20 +0800

    add test for page2

--- packages/http-client-java/generator/http-client-generator-clientcore-test/src/test/java/payload/pageable/PageableTests.java
+++ packages/http-client-java/generator/http-client-generator-clientcore-test/src/test/java/payload/pageable/PageableTests.java
@@ -4,13 +4,17 @@
 package payload.pageable;
 
 import io.clientcore.core.http.models.PagedIterable;
+import io.clientcore.core.http.models.PagingOptions;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PageableTests {
 
     private final ServerDrivenPaginationClient client = new PageableClientBuilder().buildServerDrivenPaginationClient();
-    private final ServerDrivenPaginationContinuationTokenClient tokenClient = new PageableClientBuilder().buildServerDrivenPaginationContinuationTokenClient();
+    private final ServerDrivenPaginationContinuationTokenClient tokenClient
+        = new PageableClientBuilder().buildServerDrivenPaginationContinuationTokenClient();
 
     @Test
     public void testNextLink() {
@@ -31,6 +35,15 @@ public class PageableTests {
         Assertions.assertEquals(4, pagedIterable.stream().count());
 
         pagedIterable = tokenClient.requestQueryResponseHeader();
-        Assertions.assertEquals(4, pagedIterable.stream().count());
+        Assertions.assertEquals(List.of("1", "2", "3", "4"),
+            pagedIterable.stream().map(Pet::getId).collect(Collectors.toList()));
+
+        // query 2nd page
+        pagedIterable = tokenClient.requestQueryResponseHeader();
+        Assertions.assertEquals(List.of("3", "4"),
+            pagedIterable.streamByPage(new PagingOptions().setContinuationToken("page2"))
+                .flatMap(page -> page.getValue().stream())
+                .map(Pet::getId)
+                .collect(Collectors.toList()));
     }
 }
