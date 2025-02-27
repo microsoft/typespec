fa8281e31a37fa9d27f45961761f31c08b768cf4
RodgeFu <wenjiefu@microsoft.com>
2025-02-27 13:05:32 +0800

    update update-vscode-telemetry-key script

--- packages/typespec-vscode/scripts/update-telemetry-key.js
+++ packages/typespec-vscode/scripts/update-telemetry-key.js
@@ -5,27 +5,33 @@ import { fileURLToPath } from "url";
 const newKey = process.argv[2];
 if (!newKey) {
   console.log("One argument for telemetry-key to use is expected. Exit without updating anything");
+  process.exit(1);
 } else {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const targetPackageJsonFile = path.resolve(__dirname, "../package.json");
   console.log(`Updating package.json at ${targetPackageJsonFile}`);
   const packageJson = JSON.parse(fs.readFileSync(targetPackageJsonFile, "utf8"));
-  console.log(`original package.json: \n` + JSON.stringify(packageJson, null, 2));
-  const oldKey = packageJson.telemetryKey;
-  console.log(`Original telemetry key: ${oldKey}`);
+  const oldKey = packageJson.telemetryKey ?? "";
+  const keyToString = (key) =>
+    `'${key.substring(0, 8)}...${key.substring(Math.max(0, oldKey.length - 17))}'(length: ${key.length})`;
 
-  const getLastSegOfKey = (key) => key.substring(Math.max(0, newKey.length - 12));
-
-  console.log(
-    `Updating telemetry key from ...${getLastSegOfKey(oldKey)} to ...${getLastSegOfKey(newKey)}`,
-  );
+  console.log(`Updating telemetry key from ${keyToString(oldKey)} to ${keyToString(newKey)}`);
   packageJson.telemetryKey = newKey;
 
   fs.writeFileSync(targetPackageJsonFile, JSON.stringify(packageJson, null, 2));
 
-  const newPackageJsonString = fs.readFileSync(targetPackageJsonFile, "utf8");
-  console.log(`package.json updated to: \n` + JSON.stringify(newPackageJsonString, null, 2));
-
-  console.log("package.json updated successfully");
+  // double verify the updated result
+  const newPackageJson = JSON.parse(fs.readFileSync(targetPackageJsonFile, "utf8"));
+  const updatedKey = newPackageJson.telemetryKey ?? "";
+  if (updatedKey !== newKey) {
+    console.error(
+      `Failed to update telemetry key in package.json. Actual = ${keyToString(updatedKey)}; Expected = ${keyToString(newKey)}`,
+    );
+    process.exit(2);
+  } else {
+    console.log(
+      `telemetryKey in package.json updated to '${keyToString(updatedKey)}' successfully`,
+    );
+  }
 }
