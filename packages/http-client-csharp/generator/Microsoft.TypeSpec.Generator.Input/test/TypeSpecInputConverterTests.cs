3f3b3931069973b572273dd972a4260337bf4dc5
jolov <jolov@microsoft.com>
2025-03-05 12:07:53 -0800

    add test

--- packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.Input/test/TypeSpecInputConverterTests.cs
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.Input/test/TypeSpecInputConverterTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.TypeSpec.Generator.Tests.Common;
 using NUnit.Framework;
 
@@ -6,10 +7,31 @@ namespace Microsoft.TypeSpec.Generator.Input.Tests
     public class TypeSpecInputConverterTests
     {
         [Test]
-        public void LoadsTypeSpecInput()
+        public void LoadsTypeSpecPagingInput()
         {
             var inputLibrary = new InputLibrary(Helpers.GetAssetFileOrDirectoryPath(false));
             var inputNamespace = inputLibrary.Load();
+
+            var nextLinkOperation = inputNamespace.Clients.First().Operations.FirstOrDefault(x => x.Name == "ListWithNextLink");
+            Assert.IsNotNull(nextLinkOperation);
+            Assert.IsNotNull(nextLinkOperation!.Paging);
+
+            var nextLink = nextLinkOperation.Paging!.NextLink;
+            Assert.IsNotNull(nextLink);
+            Assert.AreEqual(1, nextLink!.ResponseSegments.Count);
+            Assert.AreEqual("next", nextLink.ResponseSegments[0]);
+            Assert.AreEqual(ResponseLocation.Body, nextLink.ResponseLocation);
+
+            var continuationOperation = inputNamespace.Clients.First().Operations.FirstOrDefault(x => x.Name == "ListWithContinuationToken");
+            Assert.IsNotNull(continuationOperation);
+            Assert.IsNotNull(continuationOperation!.Paging);
+
+            var continuation = continuationOperation.Paging!.ContinuationToken;
+            Assert.IsNotNull(continuation);
+            Assert.AreEqual(1, continuation!.ResponseSegments.Count);
+            Assert.AreEqual("nextToken", continuation.ResponseSegments[0]);
+            Assert.AreEqual(ResponseLocation.Header, continuation.ResponseLocation);
+            Assert.AreEqual("token", continuation.Parameter.Name);
         }
     }
 }
