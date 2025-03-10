d53f7b69664e69cdc2e3d2158a8a4da7db1a864f
Allen Zhang <allenzhang@live.com>
2025-03-10 11:45:10 -0700

    Update changelog and tests

--- packages/http/test/typekit/http-request.test.ts
+++ packages/http/test/typekit/http-request.test.ts
@@ -174,7 +174,7 @@ describe("HttpRequest Get Parameters", () => {
     const { createFoo } = (await runner.compile(`
       @test model Foo {
         @path(#{allowReserved: true}) id: string;
-        @header requestId: string[];
+        @header(#{explode: true}) requestId: string[];
         @query(#{explode: true}) data: string[];
       }
 
@@ -193,6 +193,7 @@ describe("HttpRequest Get Parameters", () => {
     const dataProperty = query!.properties.get("data");
 
     expect($.modelProperty.getHttpHeaderOptions(requestIdProperty!)).toStrictEqual({
+      explode: true,
       name: "request-id",
       type: "header",
     });
