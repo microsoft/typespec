7151538fae83feccac56999ecb6608ba457fb832
Wei Hu <huwe@microsoft.com>
2025-03-10 12:09:24 +0800

    fix format

--- packages/http-client-csharp/emitter/src/index.ts
+++ packages/http-client-csharp/emitter/src/index.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 export { configurationFileName, tspOutputFileName } from "./constants.js";
-export { $onEmit, writeCodeModel, emit } from "./emitter.js";
+export { $onEmit, emit, writeCodeModel } from "./emitter.js";
 export { createModel } from "./lib/client-model-builder.js";
 export { createDiagnostic, getTracer, reportDiagnostic } from "./lib/lib.js";
 export { LoggerLevel } from "./lib/logger-level.js";
