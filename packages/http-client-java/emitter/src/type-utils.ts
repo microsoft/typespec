40d17c8d6d6c769d0fcaa5fc2f30cc0fce67063a
Weidong Xu <weidxu@microsoft.com>
2025-03-11 22:46:15 +0800

    try use a deprecated property

--- packages/http-client-java/emitter/src/type-utils.ts
+++ packages/http-client-java/emitter/src/type-utils.ts
@@ -350,8 +350,7 @@ export function getPropertySerializedName(property: SdkBodyModelPropertyType): s
   return (
     property.serializationOptions.json?.name ??
     property.serializationOptions.multipart?.name ??
-    property.__raw?.name ??
-    property.name
+    property.serializedName
   );
 }
 
