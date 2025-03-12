bac70dea36cc27cbaab6558de5ddba996f1b5e9c
jolov <jolov@microsoft.com>
2025-03-12 12:57:00 -0700

    exclude js tests

--- packages/http-client-csharp/emitter/vitest.config.ts
+++ packages/http-client-csharp/emitter/vitest.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import { defaultTypeSpecVitestConfig } from "../../../vitest.workspace.js";
 
-export default mergeConfig(defaultTypeSpecVitestConfig, defineConfig({}));
+export default mergeConfig(defaultTypeSpecVitestConfig, defineConfig({
+    test: {
+        exclude: ["node_modules", "dist/**/*.test.js"],
+    }
+}));
+
