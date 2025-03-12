31bad8f169bbdcdb6c8e662dbe5dd44797b25da0
Wei Hu <huwe@microsoft.com>
2025-03-12 11:19:34 +0800

    format

--- packages/http-client-csharp/emitter/src/update-code-model.ts
+++ packages/http-client-csharp/emitter/src/update-code-model.ts
@@ -2,7 +2,10 @@ import { CodeModel } from "./type/code-model.js";
 
 export type CodeModelUpdate = (model: CodeModel) => CodeModel;
 
-export function updateCodeModel(codeModel: CodeModel, update: CodeModelUpdate | undefined) : CodeModel {
+export function updateCodeModel(
+  codeModel: CodeModel,
+  update: CodeModelUpdate | undefined,
+): CodeModel {
   if (update) {
     return update(codeModel);
   }
