df9ad6ffca0a3c7f40b9b47a1974d661a0297bbb
Weidong Xu <weidxu@microsoft.com>
2025-03-12 09:59:58 +0800

    add emitter/src to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -72,7 +72,11 @@ const allFilesConfig = tsEslint.config({
  */
 export function getTypeScriptProjectRules(root) {
   return tsEslint.config({
-    files: ["**/packages/*/src/**/*.ts", "**/packages/*/src/**/*.tsx"],
+    files: [
+      "**/packages/*/src/**/*.ts",
+      "**/packages/*/src/**/*.tsx",
+      "**/packages/*/emitter/src/**/*.ts",
+    ],
     ignores: ["**/packages/http-client-csharp/**/*", "**/packages/http-client-python/**/*"], // Ignore isolated modules
     plugins: {},
     languageOptions: {
