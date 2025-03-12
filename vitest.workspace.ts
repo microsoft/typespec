f153af6550e257d7059ed848890d05a2d0a99a34
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-03-12 15:09:27 -0700

    Update vitest.workspace.ts
    Co-authored-by: Timothee Guerin <timothee.guerin@outlook.com>

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -18,7 +18,7 @@ export const defaultTypeSpecVitestConfig = defineConfig({
     outputFile: {
       junit: "./test-results.xml",
     },
-    exclude: ["node_modules", "dist/**/*.test.js"],
+    exclude: ["node_modules", "dist/**/*.test.*"],
   },
   server: {
     watch: {
