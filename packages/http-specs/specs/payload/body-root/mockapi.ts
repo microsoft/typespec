0ee5abc2cb62f7fa4d9dd481f210ffa28e992603
Mary Gao <yanmeigao1210@gmail.com>
2025-03-17 13:30:45 +0800

    add body root cases

---
+++ packages/http-specs/specs/payload/body-root/mockapi.ts
@@ -0,0 +1,81 @@
+import { passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
+
+// Mock API scenarios
+export const Scenarios: Record<string, ScenarioMockApi> = {};
+
+// Input only scenario
+Scenarios.Payload_BodyRoot_InputOnly = passOnSuccess({
+  uri: "/input-only/alice",
+  method: "post",
+  request: {
+    body: {
+      age: 30,
+      gender: "female",
+    },
+    headers: {
+      "x-client-id": "123",
+    },
+  },
+  response: { status: 204 },
+});
+
+// Input and output scenario renamed to use RoundTripModel
+Scenarios.Payload_BodyRoot_InputAndOutput = passOnSuccess({
+  uri: "/input-and-output/alice",
+  method: "post",
+  request: {
+    body: {
+      age: 30,
+      gender: "female",
+    },
+    headers: {
+      "x-client-id": "123",
+    },
+  },
+  response: {
+    status: 200,
+    body: {
+      name: "alice",
+      age: 30,
+      gender: "female",
+    },
+    headers: {
+      "x-client-id": "123",
+    },
+  },
+});
+
+// Output only scenario
+Scenarios.Payload_BodyRoot_OutputOnly = passOnSuccess({
+  uri: "/output-only/alice",
+  method: "get",
+  response: {
+    status: 200,
+    body: {
+      name: "alice",
+      age: 30,
+      gender: "female",
+    },
+    headers: {
+      "x-client-id": "123",
+    },
+  },
+});
+
+// Optional query scenario
+Scenarios.Payload_BodyRoot_OptionalQuery = passOnSuccess({
+  uri: "/optional-param",
+  method: "get",
+  request: {
+    query: {
+      orderby: "asc",
+    },
+    headers: {
+      "x-client-id": "123",
+    },
+  },
+  response: {
+    status: 200,
+    body: ["cat", "dog"],
+  },
+});
