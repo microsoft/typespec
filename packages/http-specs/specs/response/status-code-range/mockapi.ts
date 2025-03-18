40edbcd82f85991f1cc37f3c1c7d6f4d79f5c96f
jolov <jolov@microsoft.com>
2025-03-18 16:59:52 -0700

    Fix

--- packages/http-specs/specs/response/status-code-range/mockapi.ts
+++ packages/http-specs/specs/response/status-code-range/mockapi.ts
@@ -1,10 +1,12 @@
-import { json, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
+import { json, passOnCode, passOnSuccess, ScenarioMockApi } from "@typespec/spec-api";
 
 export const Scenarios: Record<string, ScenarioMockApi> = {};
 
-Scenarios.Response_StatusCodeRange_errorResponseStatusCodeInRange = passOnSuccess({
+Scenarios.Response_StatusCodeRange_errorResponseStatusCodeInRange = passOnCode(494, {
   uri: "/response/status-code-range/error-response-status-code-in-range",
   method: "get",
+  request: {
+  },
   response: {
     status: 494,
     body: json({
@@ -15,9 +17,11 @@ Scenarios.Response_StatusCodeRange_errorResponseStatusCodeInRange = passOnSucces
   kind: "MockApiDefinition",
 });
 
-Scenarios.Response_StatusCodeRange_errorResponseStatusCode404 = passOnSuccess({
+Scenarios.Response_StatusCodeRange_errorResponseStatusCode404 = passOnCode(404, {
   uri: "/response/status-code-range/error-response-status-code-404",
   method: "get",
+  request: {
+  },
   response: {
     status: 404,
     body: json({
