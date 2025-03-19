20a401df6223a09328995a708c5cd254a90758d8
Yuchao Yan <yuchaoyan@microsoft.com>
2025-03-19 13:08:39 +0800

    replace some deprecated api

--- packages/http-client-python/emitter/src/utils.ts
+++ packages/http-client-python/emitter/src/utils.ts
@@ -205,7 +205,7 @@ export function emitParamBase<TServiceOperation extends SdkServiceOperation>(
   method?: SdkServiceMethod<TServiceOperation>,
 ): ParamBase {
   let type = getType(context, parameter.type);
-  if (parameter.isApiVersionParam) {
+  if (parameter.kind === "apiVersion") {
     if (parameter.clientDefaultValue) {
       type = getSimpleTypeResult({
         type: "constant",
@@ -220,7 +220,7 @@ export function emitParamBase<TServiceOperation extends SdkServiceOperation>(
     addedOn: getAddedOn(context, parameter),
     clientName: camelToSnakeCase(parameter.name),
     inOverload: false,
-    isApiVersion: parameter.isApiVersionParam,
+    isApiVersion: parameter.kind === "apiVersion",
     isContinuationToken: isContinuationToken(parameter, method),
     type,
   };
