a6b3d50524fd534ce89db867844a3410d61bfd3e
Timothee Guerin <tiguerin@microsoft.com>
2025-03-21 13:44:23 -0700

    Remove winston from spector (#6572)
    Library is quite overkill for this simple tool and contains huge stack
    of dependencies with some with vulnerabilities

--- packages/spector/src/logger.ts
+++ packages/spector/src/logger.ts
@@ -1,10 +1,41 @@
-import winston from "winston";
+/* eslint-disable no-console */
+import pc from "picocolors";
 
-export const logger = winston.createLogger({
+const levels = {
+  debug: 10,
+  info: 20,
+  warn: 30,
+  error: 30,
+};
+
+type Level = keyof typeof levels;
+const levelDisplay: Record<Level, string> = {
+  debug: pc.blue("debug"),
+  info: pc.green("info"),
+  warn: pc.yellow("warn"),
+  error: pc.red("error"),
+};
+
+interface Logger {
+  level: Level;
+  debug: (message: string, ...data: unknown[]) => void;
+  info: (message: string, ...data: unknown[]) => void;
+  warn: (message: string, ...data: unknown[]) => void;
+  error: (message: string, ...data: unknown[]) => void;
+}
+
+export const logger: Logger = {
   level: "info",
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-    }),
-  ],
-});
+  debug: log("debug"),
+  info: log("info"),
+  warn: log("warn"),
+  error: log("error"),
+};
+
+function log(level: Level) {
+  return (message: string, ...data: unknown[]) => {
+    if (levels[level] >= levels[logger.level]) {
+      console.log(levelDisplay[level], message, ...data);
+    }
+  };
+}
