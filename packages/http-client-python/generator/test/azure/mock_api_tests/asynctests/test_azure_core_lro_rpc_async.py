009e78f1cc4b0218eb7147e5f32e26c8a59efeb5
iscai-msft <isabellavcai@gmail.com>
2025-03-21 15:30:32 -0400

    hopefully fix azure-core-lro-rpc testing folder

--- packages/http-client-python/generator/test/azure/mock_api_tests/asynctests/test_azure_core_lro_rpc_async.py
+++ packages/http-client-python/generator/test/azure/mock_api_tests/asynctests/test_azure_core_lro_rpc_async.py
@@ -4,8 +4,8 @@
 # license information.
 # --------------------------------------------------------------------------
 import pytest
-from azurecore.lro.rpc.aio import RpcClient
-from azurecore.lro.rpc import models
+from azure.core.lro.rpc.aio import RpcClient
+from azure.core.lro.rpc import models
 
 
 @pytest.fixture
