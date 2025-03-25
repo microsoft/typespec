fb5c19ef22dba3ed9ed6fe720cde6d9d1537e1ea
Allen Zhang <allenzhang@live.com>
2025-03-24 23:23:34 -0700

    revert spector code change

--- packages/spector/src/server/server.ts
+++ packages/spector/src/server/server.ts
@@ -1,7 +1,5 @@
 import { RequestExt } from "@typespec/spec-api";
 import bodyParser from "body-parser";
-import { ChildProcess, spawn } from "child_process";
-import { EventEmitter } from "events";
 import express, { ErrorRequestHandler, RequestHandler, Response } from "express";
 import { Server, ServerResponse } from "http";
 import morgan from "morgan";
@@ -99,74 +97,3 @@ const getAddress = (server: Server): string => {
   const address = server?.address();
   return typeof address === "string" ? "pipe " + address : "port " + address?.port;
 };
-
-interface ServerConfig {
-  name: string;
-  scaffoldCommand: string;
-  startCommand: string;
-  workingDirectory: string;
-}
-
-export class CustomServerManager extends EventEmitter {
-  private serverProcess: ChildProcess | null = null;
-
-  constructor(private config: ServerConfig) {
-    super();
-  }
-
-  public async scaffold(): Promise<void> {
-    logger.info(`Scaffolding ${this.config.name}...`);
-    await this.executeCommand(this.config.scaffoldCommand);
-    logger.info(`${this.config.name} scaffolding completed.`);
-  }
-
-  public start(): void {
-    if (this.serverProcess) {
-      logger.warn(`${this.config.name} is already running.`);
-      return;
-    }
-
-    logger.info(`Starting ${this.config.name}...`);
-    const [command, ...args] = this.config.startCommand.split(" ");
-    this.serverProcess = spawn(command, args, {
-      cwd: this.config.workingDirectory,
-      stdio: "inherit",
-    });
-
-    this.serverProcess.on("close", (code) => {
-      logger.info(`${this.config.name} exited with code ${code}`);
-      this.serverProcess = null;
-      this.emit("stopped", code);
-    });
-
-    this.emit("started");
-  }
-
-  public stop(): void {
-    if (!this.serverProcess) {
-      logger.warn(`${this.config.name} is not running.`);
-      return;
-    }
-
-    logger.info(`Stopping ${this.config.name}...`);
-    this.serverProcess.kill();
-  }
-
-  private executeCommand(command: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const [cmd, ...args] = command.split(" ");
-      const process = spawn(cmd, args, {
-        cwd: this.config.workingDirectory,
-        stdio: "inherit",
-      });
-
-      process.on("close", (code) => {
-        if (code === 0) {
-          resolve();
-        } else {
-          reject(new Error(`Command "${command}" exited with code ${code}`));
-        }
-      });
-    });
-  }
-}
