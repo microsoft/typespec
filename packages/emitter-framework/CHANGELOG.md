3c5e279aaaa18ae1ce82272e05701d6a2a683af0
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2025-03-28 06:16:53 +0000

    Fix instance member issues in request

--- packages/emitter-framework/CHANGELOG.md
+++ packages/emitter-framework/CHANGELOG.md
@@ -11,12 +11,8 @@
 - [#6178](https://github.com/microsoft/typespec/pull/6178) Improvements on the TestHarness
 - [#6460](https://github.com/microsoft/typespec/pull/6460) Update dependency structure for EmitterFramework, HttpClient and JS Emitter
 
-
-
-
 ## 0.2.0
 
 ### Features
 
 - [#5996](https://github.com/microsoft/typespec/pull/5996) Adding Emitter Framework and Http Client packages
-
--- packages/http-client-js/src/components/http-request-parameters-expression.tsx
+++ packages/http-client-js/src/components/http-request-parameters-expression.tsx
@@ -1,10 +1,11 @@
 import * as ay from "@alloy-js/core";
 import * as ts from "@alloy-js/typescript";
 import { useTransformNamePolicy } from "@typespec/emitter-framework";
-import { HttpProperty } from "@typespec/http";
-import { getDefaultValue } from "../utils/parameters.jsx";
+import { HttpOperation, HttpProperty } from "@typespec/http";
+import { getDefaultValue, hasDefaultValue } from "../utils/parameters.jsx";
 import { JsonTransform } from "./transforms/json/json-transform.jsx";
 export interface HttpRequestParametersExpressionProps {
+  httpOperation: HttpOperation;
   optionsParameter: ay.Children;
   parameters?: HttpProperty[];
   children?: ay.Children;
@@ -28,33 +29,37 @@ export function HttpRequestParametersExpression(props: HttpRequestParametersExpr
   const members = (
     <ay.For each={props.parameters} line comma>
       {(httpProperty) => {
-        const parameter = httpProperty.property;
-
+        // const itemRef: ay.Children = parameter.optional ? ay.code`${optionsParamRef}?` : null;
         const defaultValue = getDefaultValue(httpProperty);
-        const paramItemRef: ay.Children = transformNamer.getApplicationName(parameter);
-
-        const paramRef = ay.code`${optionsParamRef}?.${paramItemRef}`;
+        const propertyExpression = buildMemberChainExpression(
+          props.httpOperation,
+          httpProperty,
+          optionsParamRef,
+        );
+        const transportParamName = transformNamer.getTransportName(httpProperty.property);
 
         if (defaultValue) {
-          const defaultAssignment = defaultValue ? ` ?? ${defaultValue}` : "";
-          const headerValue = (
+          const paramValue = (
             <>
-              {paramRef}
-              {defaultAssignment}
+              {propertyExpression.fullExpression}
+              {` ?? ${defaultValue}`}
             </>
           );
-          const name = transformNamer.getTransportName(parameter);
-          const paramAssignment = <ts.ObjectProperty name={`"${name}"`} value={headerValue} />;
-          return paramAssignment;
+          return <ts.ObjectProperty name={`"${transportParamName}"`} value={paramValue} />;
         }
 
-        const itemRef: ay.Children = parameter.optional ? ay.code`${optionsParamRef}?` : null;
-        if (parameter.optional) {
+        if (propertyExpression.isNullish) {
           return ay.code`
-        ...(${paramRef} && {${(<JsonTransform itemRef={itemRef} type={parameter} target="transport" />)}})
+        ...(${propertyExpression.fullExpression} && {${(<JsonTransform itemRef={propertyExpression.leadingExpression} type={httpProperty.property} target="transport" />)}})
       `;
         } else {
-          return <JsonTransform itemRef={itemRef} type={parameter} target="transport" />;
+          return (
+            <JsonTransform
+              itemRef={propertyExpression.leadingExpression}
+              type={httpProperty.property}
+              target="transport"
+            />
+          );
         }
       }}
     </ay.For>
@@ -63,4 +68,115 @@ export function HttpRequestParametersExpression(props: HttpRequestParametersExpr
   parameters.push(members);
 
   return <ts.ObjectExpression>{parameters}</ts.ObjectExpression>;
+}
+
+interface MemberChainExpression {
+  propertyName: string;
+  isNullish: boolean;
+  fullExpression: ay.Children;
+  leadingExpression?: ay.Children;
+}
+
+/**
+ * Builds the member chain expression for potentially nested HTTP parameters.
+ * This logic will be simplified and made more robust once a couple of changes land in Alloy.
+ *    - Support refkeys on interface members
+ *    - Support for conditional chaining on ts.MemberChainExpression
+ *    - We'd eventually get an api like this <InstancePropertyExpression instance={refkey(parameter) staticMember={refkey(property)}} />
+ * @param httpOperation - The HttpOperation containing parameters.
+ * @param httpProperty - The current HttpProperty to build an access path for.
+ * @param optionsParamRef - Reference to the user's main options parameter.
+ * @returns The full nested expression, leading expression, nullish flag, and final property name.
+ */
+function buildMemberChainExpression(
+  httpOperation: HttpOperation,
+  httpProperty: HttpProperty,
+  optionsParamRef?: ay.Children,
+): MemberChainExpression {
+  const accessSegments: ay.Children[] = []; // Renamed from 'chain' for clarity
+  let isNullish = httpProperty.property.optional === true;
+  const namePolicy = ay.useNamePolicy();
+  const propertyApplicationName = namePolicy.getName(httpProperty.property.name, "property");
+
+  // Handle client operation parameters, these are the parameters that are in the signature of the operation
+  if (httpProperty.path.length === 1) {
+    if (httpProperty.property.optional || hasDefaultValue(httpProperty)) {
+      return {
+        propertyName: propertyApplicationName,
+        isNullish,
+        fullExpression: ay.code`${optionsParamRef}?.${propertyApplicationName}`,
+        leadingExpression: ay.code`${optionsParamRef}`,
+      };
+    }
+
+    return {
+      propertyName: propertyApplicationName,
+      isNullish,
+      fullExpression: propertyApplicationName,
+    };
+  }
+
+  // There are Http Parameters that might be nested in other parameters, we need to figure out how to access them
+  // from the client parameter.
+  let currentParentPath = httpProperty.path.slice(0, -1);
+  let isFirstNested = true; // Renamed from 'isFirst' for clarity
+  while (currentParentPath.length) {
+    const parentProperty = findHttpPropertyByPath(httpOperation, currentParentPath);
+    if (!parentProperty) break;
+
+    let joiner = ".";
+    if (parentProperty.property.optional) {
+      joiner = "?.";
+      isNullish = true;
+    }
+
+    const parentName = namePolicy.getName(parentProperty.property.name, "property");
+    accessSegments.unshift(joiner);
+
+    if (isFirstNested && typeof parentName === "string") {
+      // For the first nested property in the chain, use direct name
+      isFirstNested = false;
+      accessSegments.unshift(ay.code`${parentName}`);
+    } else {
+      // For deeper nested properties, use bracket notation
+      accessSegments.unshift(`[${JSON.stringify(parentName)}]`);
+    }
+
+    currentParentPath = currentParentPath.slice(0, -1);
+  }
+
+  return {
+    propertyName: propertyApplicationName,
+    isNullish,
+    fullExpression: (
+      <>
+        {ay.mapJoin(
+          () => accessSegments,
+          (element) => element,
+        )}
+        {propertyApplicationName}
+      </>
+    ),
+    leadingExpression: (
+      <>
+        {ay.mapJoin(
+          () => accessSegments.slice(0, -1),
+          (element) => element,
+        )}
+      </>
+    ),
+  };
+}
+
+function findHttpPropertyByPath(
+  httpOperation: HttpOperation,
+  path: (string | number)[],
+): HttpProperty | undefined {
+  for (const property of httpOperation.parameters.properties) {
+    // Check if the property path matches the given path
+    if (JSON.stringify(property.path) === JSON.stringify(path)) {
+      return property;
+    }
+  }
+  return undefined;
 }
