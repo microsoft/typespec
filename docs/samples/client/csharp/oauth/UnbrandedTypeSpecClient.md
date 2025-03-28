950c17076b51db6b2159a288665011af5a77839a
Nisha Bhatia <Nisha.Bhatia@microsoft.com>
2025-03-28 01:28:50 -0700

    wip

---
+++ docs/samples/client/csharp/oauth/UnbrandedTypeSpecClient.md
@@ -0,0 +1,35 @@
+### Create custom client with unbranded token
+
+#### To create custom client with Uri and ApiKeyCredential
+```csharp
+/// <summary>
+/// Initializes a new instance of the <see cref="UnbrandedTypeSpecClient"/> class.
+/// </summary>
+/// <param name="uri">The URI of the service.</param>
+/// <param name="credential">The ApiKeyCredential.</param>
+public FooClient(Uri endpoint, ApiKeyCredential keyCredential)
+public UnbrandedTypeSpecClient(Uri endpoint, ApiKeyCredential keyCredential)
+{
+}
+```
+
+A code sample demonstrating creating a custom client with an ApiKeyCredential can be found [here](https://github.com/microsoft/typespec/blob/main/packages/http-client-csharp/generator/TestProjects/Local/Unbranded-TypeSpec/src/Generated/UnbrandedTypeSpecClient.cs#L37). 
+
+#### To create custom client with Uri and AuthenticationTokenProvider
+```csharp
+/// <summary>
+/// Initializes a new instance of the <see cref="UnbrandedTypeSpecClient"/> class.
+/// </summary>
+/// <param name="uri">The URI of the service.</param>
+/// <param name="credential">The authentication token provider.</param>
+public UnbrandedTypeSpecClient(Uri uri, AuthenticationTokenProvider credential)
+{
+    var options = new ClientPipelineOptions();
+    Pipeline = ClientPipeline.Create(options,
+    perCallPolicies: ReadOnlySpan<PipelinePolicy>.Empty,
+    perTryPolicies: [new OAuth2BearerTokenAuthenticationPolicy(credential, flows)],
+    beforeTransportPolicies: ReadOnlySpan<PipelinePolicy>.Empty);
+}
+```
+
+TODO: insert link here after PR gets merged
--- packages/http-client-csharp/generator/TestProjects/Local/Unbranded-TypeSpec/src/Custom/UnbrandedTypeSpecClient.cs
+++ packages/http-client-csharp/generator/TestProjects/Local/Unbranded-TypeSpec/src/Custom/UnbrandedTypeSpecClient.cs
@@ -6,7 +6,6 @@ using System;
 using System.ClientModel;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
-using System.IO;
 
 namespace UnbrandedTypeSpec
 {
@@ -15,9 +14,10 @@ namespace UnbrandedTypeSpec
     /// </summary>
     public partial class UnbrandedTypeSpecClient
     {
+        // If generated, flows would come from the service level OAuth2 flow definitions as shown in the Oauth2 Scopes section of this: https://typespec.io/docs/libraries/http/authentication/
         private readonly Dictionary<string, object>[] flows = [
             new Dictionary<string, object> {
-                { GetTokenOptions.ScopesPropertyName, new string[] { "baselineScope" } },
+                { GetTokenOptions.ScopesPropertyName, new string[] { "defaultScope" } },
                 { GetTokenOptions.TokenUrlPropertyName , "https://myauthserver.com/token"},
                 { GetTokenOptions.RefreshUrlPropertyName, "https://myauthserver.com/refresh"}
             }
@@ -27,14 +27,43 @@ namespace UnbrandedTypeSpec
         /// Initializes a new instance of the <see cref="UnbrandedTypeSpecClient"/> class.
         /// </summary>
         /// <param name="uri">The URI of the service.</param>
-        /// <param name="credential">The authentication token provider.</param>
-        public UnbrandedTypeSpecClient(Uri uri, AuthenticationTokenProvider credential)
+        /// <param name="authTokenProvider">The authentication token provider.</param>
+        public UnbrandedTypeSpecClient(Uri uri, AuthenticationTokenProvider authTokenProvider)
         {
             var options = new ClientPipelineOptions();
             Pipeline = ClientPipeline.Create(options,
             perCallPolicies: ReadOnlySpan<PipelinePolicy>.Empty,
-            perTryPolicies: [new OAuth2BearerTokenAuthenticationPolicy(credential, flows)],
+            perTryPolicies: [new OAuth2BearerTokenAuthenticationPolicy(authTokenProvider, flows)],
             beforeTransportPolicies: ReadOnlySpan<PipelinePolicy>.Empty);
+        }
+
+        /// <summary>
+        /// [Protocol Method] Return bye
+        /// <item>
+        /// <description> This <see href="https://aka.ms/azsdk/net/protocol-methods">protocol method</see> allows explicit creation of the request and processing of the response for advanced scenarios. </description>
+        /// </item>
+        /// </summary>
+        /// <param name="options"> The request options, which can override default behaviors of the client pipeline on a per-call basis. </param>
+        /// <exception cref="ClientResultException"> Service returned a non-success status code. </exception>
+        /// <returns> The response returned from the service. </returns>
+        public virtual ClientResult SayBye(RequestOptions options)
+        {
+            using PipelineMessage message = CreateSayByeRequest(options);
+            return ClientResult.FromResponse(Pipeline.ProcessMessage(message, options));
+        }
+
+        internal PipelineMessage CreateSayByeRequest(RequestOptions options)
+        {
+            PipelineMessage message = Pipeline.CreateMessage();
+            message.ResponseClassifier = PipelineMessageClassifier200;
+            PipelineRequest request = message.Request;
+            request.Method = "GET";
+            ClientUriBuilder uri = new ClientUriBuilder();
+            uri.Reset(_endpoint);
+            uri.AppendPath("/bye", false);
+            request.Headers.Set("Accept", "application/json");
+            message.Apply(options);
+            return message;
         }
     }
 }
