c22e4e663e75d13e202014157c0236791df90009
Arcturus Zhang <dapzhang@microsoft.com>
2025-04-03 17:55:14 +0800

    update

--- packages/http-client-csharp/emitter/src/index.ts
+++ packages/http-client-csharp/emitter/src/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+export { writeCodeModel } from "./code-model-writer.js";
 export { configurationFileName, tspOutputFileName } from "./constants.js";
 export { $onEmit } from "./emitter.js";
 export { createModel } from "./lib/client-model-builder.js";
