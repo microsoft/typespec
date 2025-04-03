f12aa340994026d1dd71c860328f54299ba20a30
Weidong Xu <weidxu@microsoft.com>
2025-04-03 19:09:59 +0800

    add one more test

--- packages/http-client-java/emitter/test/validate.test.ts
+++ packages/http-client-java/emitter/test/validate.test.ts
@@ -3,12 +3,13 @@ import { findJavaVersion, getJavaMajorVersion } from "../src/validate.js";
 
 describe("validate", () => {
   it("findJavaVersion", () => {
+    expect(findJavaVersion("javac 1.8.0_422")).toBe("1.8.0");
     expect(findJavaVersion("javac 21.0.3")).toBe("21.0.3");
     expect(findJavaVersion("javac 24")).toBe("24");
-    expect(findJavaVersion("javac 1.8.0_422")).toBe("1.8.0");
   });
 
   it("getJavaMajorVersion", () => {
+    expect(getJavaMajorVersion("1.8.0")).toBe(8);
     expect(getJavaMajorVersion("21.0.3")).toBe(21);
     expect(getJavaMajorVersion("24")).toBe(24);
   });
