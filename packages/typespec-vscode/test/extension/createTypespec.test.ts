a0bf33c4e85073e7d37e16d70072c85ddfc67ded
Tiancheng Zhang (WICRESOFT NORTH AMERICA LTD) <v-zhangtianc@microsoft.com>
2025-04-03 16:21:19 +0800

    completed create/import test case

---
+++ packages/typespec-vscode/test/extension/createTypespec.test.ts
@@ -0,0 +1,84 @@
+import { afterEach, beforeEach } from "vitest"
+import {
+  contrastResult,
+  start,
+  selectFolder,
+  preContrastResult,
+  installExtensionForFile,
+  closeVscode,
+  installExtension,
+} from "./common/commonSteps"
+import { screenshotSelf, test } from "./common/utils"
+import fs from "node:fs"
+import path from "node:path"
+import {
+  inputProjectName,
+  selectEmitters,
+  selectTemplate,
+} from "./common/createSteps"
+
+beforeEach(() => {
+  const dir = path.resolve(__dirname, "./CreateTypespecProject")
+  if (fs.existsSync(dir)) {
+    for (const file of fs.readdirSync(dir)) {
+      const filePath = path.resolve(dir, file)
+      fs.rmSync(filePath, { recursive: true, force: true })
+    }
+  } else {
+    fs.mkdirSync(dir, { recursive: true })
+  }
+})
+
+test("CreateTypespec-Generic REST API", async ({ launch }) => {
+  const workspacePath = path.resolve(__dirname, "./CreateTypespecProject")
+  const { page } = await launch({
+    workspacePath,
+  })
+  // await installExtensionForFile(
+  //   page,
+  //   path.resolve(__dirname, "../../extension.vsix")
+  // )
+
+  await installExtension(page)
+
+  await start(page, {
+    folderName: "CreateTypespecProject",
+    command: "Create Typespec Project",
+  })
+  await selectFolder()
+  await selectTemplate(page, "Generic REST API")
+  await inputProjectName(page)
+  await selectEmitters(page, ["OpenAPI"])
+  await preContrastResult(
+    page,
+    "Project created!",
+    "Failed to create project Successful",
+    [10, 10]
+  )
+  await closeVscode(page)
+  await contrastResult(
+    [
+      ".gitignore",
+      "main.tsp",
+      "node_modules",
+      "package-lock.json",
+      "package.json",
+      "tspconfig.yaml",
+    ],
+    workspacePath
+  )
+})
+
+// test("CreateTypespec-Special scenarios-button", async ({ launch }) => {
+//   const { page } = await launch({ workspacePath: "./test" })
+//   await installExtension(page)
+
+//   await page
+//     .getByLabel(/Explorer/)
+//     .first()
+//     .click()
+//   await page.getByRole("button", { name: "Create TypeSpec Project" }).click()
+//   await selectFolder()
+//   await notEmptyFolderContinue(page)
+//   await closeVscode(page)
+// })
