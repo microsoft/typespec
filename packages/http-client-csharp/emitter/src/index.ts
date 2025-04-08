3455e7c1bd2efc033436427c9d9f1faa211b0c0f
Arcturus Zhang <dapzhang@microsoft.com>
2025-04-08 09:54:30 +0800

    add some comments decribing this is only for autorest.csharp

--- packages/http-client-csharp/emitter/src/index.ts
+++ packages/http-client-csharp/emitter/src/index.ts
@@ -4,6 +4,8 @@
 export { writeCodeModel } from "./code-model-writer.js";
 export { configurationFileName, tspOutputFileName } from "./constants.js";
 export { $onEmit } from "./emitter.js";
+// we export `createModel` only for autorest.csharp because it uses the emitter to generate the code model file but not calling the dll here
+// we could remove this export when in the future we deprecate autorest.csharp
 export { createModel } from "./lib/client-model-builder.js";
 export { $lib, createDiagnostic, getTracer, reportDiagnostic } from "./lib/lib.js";
 export { LoggerLevel } from "./lib/logger-level.js";
@@ -15,6 +17,8 @@ export {
   resolveOptions,
 } from "./options.js";
 export { setSDKContextOptions } from "./sdk-context-options.js";
+// we export `createCSharpEmitterContext` only for autorest.csharp because it uses the emitter to generate the code model file but not calling the dll here
+// we could remove this export when in the future we deprecate autorest.csharp
 export { CSharpEmitterContext, createCSharpEmitterContext } from "./sdk-context.js";
 export { CodeModel } from "./type/code-model.js";
 export { InputClient, InputModelType } from "./type/input-type.js";
