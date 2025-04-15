15f95a649fb935bb07cacb0b45df6d0aa2d2843c
anuchandy <anuamd@hotmail.com>
2025-04-15 12:58:37 -0700

    Removing toJson/fromJson from code models, given models always uses yaml

--- packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/extension/model/SmartLocation.java
+++ packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/extension/model/SmartLocation.java
@@ -3,17 +3,12 @@
 
 package com.microsoft.typespec.http.client.generator.core.extension.model;
 
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
 import java.util.List;
 
 /**
  * Represents a smart location.
  */
-public class SmartLocation implements JsonSerializable<SourceLocation> {
+public class SmartLocation {
     private List<Object> path;
 
     /**
@@ -38,36 +33,5 @@ public class SmartLocation implements JsonSerializable<SourceLocation> {
      */
     public void setPath(List<Object> path) {
         this.path = path;
-    }
-
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        return jsonWriter.writeStartObject().writeArrayField("path", path, JsonWriter::writeUntyped).writeEndObject();
-    }
-
-    /**
-     * Deserializes a SmartLocation instance from the JSON data.
-     *
-     * @param jsonReader The JSON reader to deserialize from.
-     * @return A SmartLocation instance deserialized from the JSON data.
-     * @throws IOException If an error occurs during deserialization.
-     */
-    public static SmartLocation fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            SmartLocation smartLocation = new SmartLocation();
-
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("path".equals(fieldName)) {
-                    smartLocation.path = reader.readArray(JsonReader::readUntyped);
-                } else {
-                    reader.skipChildren();;
-                }
-            }
-
-            return smartLocation;
-        });
     }
 }
--- packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/extension/model/SourceLocation.java
+++ packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/extension/model/SourceLocation.java
@@ -3,16 +3,10 @@
 
 package com.microsoft.typespec.http.client.generator.core.extension.model;
 
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
-
 /**
  * Represents a source location.
  */
-public class SourceLocation implements JsonSerializable<SourceLocation> {
+public class SourceLocation {
     private String document;
     private SmartLocation position;
 
@@ -56,41 +50,5 @@ public class SourceLocation implements JsonSerializable<SourceLocation> {
      */
     public void setPosition(SmartLocation position) {
         this.position = position;
-    }
-
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        return jsonWriter.writeStartObject()
-            .writeStringField("document", document)
-            .writeJsonField("position", position)
-            .writeEndObject();
-    }
-
-    /**
-     * Deserializes a SourceLocation instance from the JSON data.
-     *
-     * @param jsonReader The JSON reader to deserialize from.
-     * @return A SourceLocation instance deserialized from the JSON data.
-     * @throws IOException If an error occurs during deserialization.
-     */
-    public static SourceLocation fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            SourceLocation sourceLocation = new SourceLocation();
-
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("document".equals(fieldName)) {
-                    sourceLocation.document = reader.getString();
-                } else if ("position".equals(fieldName)) {
-                    sourceLocation.position = SmartLocation.fromJson(reader);
-                } else {
-                    reader.skipChildren();
-                }
-            }
-
-            return sourceLocation;
-        });
     }
 }
