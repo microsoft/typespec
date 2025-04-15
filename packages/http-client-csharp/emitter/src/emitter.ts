5700245916a26134f22fc1d10d033b3543fe306d
Wei Hu <huwe@microsoft.com>
2025-04-15 09:03:53 +0800

    cleanup

--- packages/http-client-csharp/emitter/src/emitter.ts
+++ packages/http-client-csharp/emitter/src/emitter.ts
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import {
-  createSdkContext,
-  CreateSdkContextOptions,
-  SdkContext,
-} from "@azure-tools/typespec-client-generator-core";
+import { createSdkContext, SdkContext } from "@azure-tools/typespec-client-generator-core";
 import {
   EmitContext,
   getDirectoryPath,
