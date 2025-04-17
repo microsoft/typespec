e512d40c0c990df08d532a953312573285fc9076
Maor Leger <maorleger@users.noreply.github.com>
2025-04-17 13:14:33 -0700

    implement interface-method (#13)

---
+++ packages/emitter-framework/src/typescript/components/interface-method.tsx
@@ -0,0 +1,42 @@
+import { splitProps } from "@alloy-js/core/jsx-runtime";
+import * as ts from "@alloy-js/typescript";
+import { Operation } from "@typespec/compiler";
+import { buildParameterDescriptors, getReturnType } from "../utils/operation.js";
+import { TypeExpression } from "./type-expression.jsx";
+
+export interface InterfaceMethodProps extends Omit<ts.InterfaceMethodProps, "name"> {
+  type?: Operation;
+  name?: string;
+  parametersMode?: "prepend" | "append" | "replace";
+}
+
+export function InterfaceMethod(props: InterfaceMethodProps) {
+  const [efProps, updateProps, forwardProps] = splitProps(
+    props,
+    ["type"],
+    ["returnType", "parameters"],
+  );
+
+  if (!efProps.type) {
+    return <ts.InterfaceMethod {...forwardProps} {...updateProps} name="" />;
+  }
+
+  const name = props.name
+    ? props.name
+    : ts.useTSNamePolicy().getName(efProps.type.name, "function");
+  const returnType = props.returnType ?? <TypeExpression type={getReturnType(efProps.type)} />;
+  const allParameters = buildParameterDescriptors(efProps.type.parameters, {
+    params: props.parameters,
+    mode: props.parametersMode,
+  });
+
+  return (
+    <ts.InterfaceMethod
+      {...forwardProps}
+      name={name}
+      returnType={returnType}
+      parameters={allParameters}
+      {...updateProps}
+    />
+  );
+}
---
+++ packages/emitter-framework/test/typescript/components/interface-method.test.tsx
@@ -0,0 +1,75 @@
+import { Output, render } from "@alloy-js/core";
+import { d } from "@alloy-js/core/testing";
+import { InterfaceDeclaration, SourceFile } from "@alloy-js/typescript";
+import { Operation } from "@typespec/compiler";
+import { BasicTestRunner } from "@typespec/compiler/testing";
+import { beforeEach, describe, it } from "vitest";
+import { InterfaceMethod } from "../../../src/typescript/components/interface-method.jsx";
+import { assertFileContents } from "../../utils.js";
+import { createEmitterFrameworkTestRunner } from "../test-host.js";
+
+let runner: BasicTestRunner;
+
+beforeEach(async () => {
+  runner = await createEmitterFrameworkTestRunner();
+});
+
+describe("interface methods with a `type` prop", () => {
+  let runner: BasicTestRunner;
+
+  beforeEach(async () => {
+    runner = await createEmitterFrameworkTestRunner();
+  });
+
+  it("creates a interface member", async () => {
+    const { getName } = (await runner.compile(`
+      @test op getName(id: string): string;
+    `)) as { getName: Operation };
+
+    const res = render(
+      <Output>
+        <SourceFile path="test.ts">
+          <InterfaceDeclaration name="basicInterface">
+            <InterfaceMethod type={getName} name={getName.name} />
+          </InterfaceDeclaration>
+        </SourceFile>
+      </Output>,
+    );
+
+    assertFileContents(
+      res,
+      d`
+        interface basicInterface {
+          getName(id: string): string
+        }
+      `,
+    );
+  });
+
+  it("creates an async interface function", async () => {
+    const { getName } = (await runner.compile(`
+      @test op getName(id: string): string;
+    `)) as { getName: Operation };
+
+    const res = render(
+      <Output>
+        <SourceFile path="test.ts">
+          <InterfaceDeclaration name="basicInterface">
+            <InterfaceMethod async type={getName} />
+          </InterfaceDeclaration>
+        </SourceFile>
+      </Output>,
+    );
+
+    assertFileContents(
+      res,
+      d`
+        interface basicInterface {
+          getName(id: string): Promise<string>
+        }
+      `,
+    );
+  });
+
+  it.todo("can append extra parameters with raw params provided", async () => {});
+});
