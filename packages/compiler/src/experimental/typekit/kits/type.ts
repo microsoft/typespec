138db0f4ef0cc263ecbd8def686ae445d73f5a70
Maor Leger <maorleger@microsoft.com>
2025-04-21 16:47:28 +0000

    typeguard

--- packages/compiler/src/experimental/typekit/kits/type.ts
+++ packages/compiler/src/experimental/typekit/kits/type.ts
@@ -36,7 +36,7 @@ export interface TypeTypekit {
    * Checks if a type is decorated with `@error`
    * @param type The type to check.
    */
-  isError(type: Type): asserts type is Model;
+  isError(type: Type): type is Model;
   /**
    * Get the name of this type in the specified encoding.
    */
