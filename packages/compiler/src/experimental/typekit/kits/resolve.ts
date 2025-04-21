e9b295454dd94f7e5fcad3cd56403b7adb10166f
Christopher Radek <Christopher.Radek@microsoft.com>
2025-04-21 14:41:56 -0700

    make core typekits diagnosable and add tests

--- packages/compiler/src/experimental/typekit/kits/resolve.ts
+++ packages/compiler/src/experimental/typekit/kits/resolve.ts
@@ -1,6 +1,6 @@
-import { Diagnostic, Type } from "../../../core/types.js";
+import { Type } from "../../../core/types.js";
 import { createDiagnosable, Diagnosable } from "../create-diagnosable.js";
-import { defineKit, Typekit } from "../define-kit.js";
+import { defineKit } from "../define-kit.js";
 
 export type ResolveKit = Diagnosable<(reference: string) => Type | undefined>;
 
@@ -21,10 +21,7 @@ declare module "../define-kit.js" {
 }
 
 defineKit<TypekitExtension>({
-  resolve: createDiagnosable(function (
-    this: Typekit,
-    reference: string,
-  ): [Type | undefined, readonly Diagnostic[]] {
+  resolve: createDiagnosable(function (reference) {
     // Directly use the program's resolveTypeReference method
     return this.program.resolveTypeReference(reference);
   }),
