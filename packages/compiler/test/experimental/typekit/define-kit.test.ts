04484e02af609f95ef7f1bda145da7a36e22e5e1
Christopher Radek <Christopher.Radek@microsoft.com>
2025-04-23 16:33:52 -0700

    fix test

--- packages/compiler/test/experimental/typekit/define-kit.test.ts
+++ packages/compiler/test/experimental/typekit/define-kit.test.ts
@@ -12,3 +12,27 @@ it("can define a kit", async () => {
 
   expect(($(program) as any).test()).toBe(true);
 });
+
+it("supports nested kits", async () => {
+  const { program } = await createContextMock();
+  defineKit({
+    __nestedTest: {
+      get foo() {
+        return this.program.checker.anyType;
+      },
+      nested: {
+        get foo() {
+          return this.program.checker.anyType;
+        },
+        bar() {
+          return this.program.checker.anyType;
+        },
+      } as any,
+    } as any,
+  });
+  const tk = $(program);
+
+  expect((tk as any).__nestedTest.foo).toBe(program.checker.anyType);
+  expect((tk as any).__nestedTest.nested.foo).toBe(program.checker.anyType);
+  expect((tk as any).__nestedTest.nested.bar()).toBe(program.checker.anyType);
+});
