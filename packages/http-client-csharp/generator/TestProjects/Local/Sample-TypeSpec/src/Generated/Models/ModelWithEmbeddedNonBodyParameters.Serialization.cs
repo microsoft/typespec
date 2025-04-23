8f1436cc167ebd0859415984b371d2740b448253
m-nash <64171366+m-nash@users.noreply.github.com>
2025-04-22 19:45:47 -0700

    regen after changes

--- packages/http-client-csharp/generator/TestProjects/Local/Sample-TypeSpec/src/Generated/Models/ModelWithEmbeddedNonBodyParameters.Serialization.cs
+++ packages/http-client-csharp/generator/TestProjects/Local/Sample-TypeSpec/src/Generated/Models/ModelWithEmbeddedNonBodyParameters.Serialization.cs
@@ -112,7 +112,7 @@ namespace SampleTypeSpec
             switch (format)
             {
                 case "J":
-                    return ModelReaderWriter.Write(this, options);
+                    return ModelReaderWriter.Write(this, options, SampleTypeSpecContext.Default);
                 default:
                     throw new FormatException($"The model {nameof(ModelWithEmbeddedNonBodyParameters)} does not support writing '{options.Format}' format.");
             }
