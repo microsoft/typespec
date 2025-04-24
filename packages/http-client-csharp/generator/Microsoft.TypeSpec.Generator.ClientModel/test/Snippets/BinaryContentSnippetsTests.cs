bc002ebf664ecf1c7f83f74d710eb85a023425e6
jolov <jolov@microsoft.com>
2025-04-24 16:09:04 -0700

    Use RequestContentApi when constructing request content

---
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/test/Snippets/BinaryContentSnippetsTests.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.TypeSpec.Generator.ClientModel.Tests
+{
+    public class BinaryContentSnippetsTests
+    {
+
+    }
+}
