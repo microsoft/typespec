7d36a9257e48fde7a406aef779d5c88a0d8ec20a
Maor Leger <maorleger@microsoft.com>
2025-05-02 15:33:04 +0000

    update doccomments

--- packages/emitter-framework/src/typescript/utils/refkey.ts
+++ packages/emitter-framework/src/typescript/utils/refkey.ts
@@ -8,10 +8,10 @@ const refKeyPrefix = Symbol.for("emitter-framework:typescript");
  *
  * @remarks
  *
- * The underlying `refkey` function is called with the {@link refKeyPrefix} symbol as the first argument.
+ * The underlying refkey function is called with the {@link refKeyPrefix} symbol as the first argument.
  *
- * @param args The arguments to pass to `refkey`.
- * @returns A `Refkey` object that can be used to identify the value.
+ * @param args The parameters of the refkey.
+ * @returns A refkey object that can be used to identify the value.
  */
 export function efRefkey(...args: unknown[]) {
   if (args.length === 0) {
@@ -24,9 +24,9 @@ export function efRefkey(...args: unknown[]) {
  * Creates a refkey for a declaration by combining the provided refkey with an internal
  * refkey generated from the provided arguments.
  *
- * @param refKey The refkey provided by the user to be passed as is.
- * @param args The arguments to create a new refkey with.
- * @returns An array of refkeys that can be passed to a Alloy declaration.
+ * @param refkey The refkey provided by the user to be passed as is.
+ * @param args The parameters of the refkey.
+ * @returns An array of refkeys that can be passed to an Alloy declaration.
  */
 export function declarationRefkey(refkey?: Refkey | Refkey[], ...args: unknown[]) {
   if (refkey) {
