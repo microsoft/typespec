a55e3744100f7b8ae2c51acf562023c80efb94a3
Weidong Xu <weidxu@microsoft.com>
2025-05-07 15:44:04 +0800

    Default is 0

--- packages/http-client-java/emitter/src/code-model-builder.ts
+++ packages/http-client-java/emitter/src/code-model-builder.ts
@@ -712,7 +712,7 @@ export class CodeModelBuilder {
         );
         const parentAccessorPublic = Boolean(
           subClient.clientInitialization.initializedBy & InitializedByFlags.Parent ||
-            subClient.clientInitialization.initializedBy & InitializedByFlags.Default,
+            subClient.clientInitialization.initializedBy === InitializedByFlags.Default,
         );
         codeModelClient.addSubClient(codeModelSubclient, buildMethodPublic, parentAccessorPublic);
       }
