072868a5068714829671601f0dd108740939a5ed
Maor Leger <maorleger@microsoft.com>
2025-05-15 20:59:11 +0000

    Refactor

--- packages/emitter-framework/src/typescript/components/union-expression.tsx
+++ packages/emitter-framework/src/typescript/components/union-expression.tsx
@@ -1,7 +1,8 @@
 import * as ay from "@alloy-js/core";
 import { Children } from "@alloy-js/core";
 import * as ts from "@alloy-js/typescript";
-import { Enum, EnumMember, Union, UnionVariant } from "@typespec/compiler";
+import { compilerAssert, Enum, EnumMember, Union, UnionVariant } from "@typespec/compiler";
+import { Typekit } from "@typespec/compiler/typekit";
 import { useTsp } from "../../core/context/tsp-context.js";
 import { TypeExpression } from "./type-expression.jsx";
 
@@ -17,47 +18,13 @@ export function UnionExpression({ type, children }: UnionExpressionProps) {
     UnionVariant | EnumMember
   >;
 
-  let discriminatedUnion = undefined;
-  if ($.union.is(type)) {
-    discriminatedUnion = $.union.getDiscriminatedUnion(type);
-  }
-
   const variants = (
     <ay.For joiner={" | "} each={items}>
-      {(_, value) => {
-        if ($.enumMember.is(value)) {
-          return <ts.ValueExpression jsValue={value.value ?? value.name} />;
-        }
-
-        if (discriminatedUnion?.options.envelope === "object") {
-          const discriminatorPropertyName = discriminatedUnion.options.discriminatorPropertyName;
-          const envelopePropertyName = discriminatedUnion.options.envelopePropertyName;
-
-          const envelope = $.model.create({
-            properties: {
-              [discriminatorPropertyName]: $.modelProperty.create({
-                name: discriminatedUnion.options.discriminatorPropertyName,
-                type: $.literal.createString(value.name as string),
-              }),
-              [envelopePropertyName]: $.modelProperty.create({
-                name: discriminatedUnion.options.envelopePropertyName,
-                type: value.type,
-              }),
-            },
-          });
-
-          return <TypeExpression type={envelope} />;
-        } else if (discriminatedUnion?.options.envelope === "none") {
-          // this is a discriminated union with no envelope
-          // we need a model where the discriminator and the rest of the values are side-by-side
-          return <TypeExpression type={value.type} />;
-        } else {
-          return <TypeExpression type={value.type} />;
-        }
-      }}
+      {(_, value) => renderVariant($, value)}
     </ay.For>
   );
 
+  // Handle additional children if present
   if (children || (Array.isArray(children) && children.length)) {
     return (
       <>
@@ -67,4 +34,94 @@ export function UnionExpression({ type, children }: UnionExpressionProps) {
   }
 
   return variants;
+}
+
+/**
+ * Renders a union variant or enum member based on its type
+ */
+function renderVariant($: Typekit, value: UnionVariant | EnumMember) {
+  if ($.enumMember.is(value)) {
+    return <ts.ValueExpression jsValue={value.value ?? value.name} />;
+  }
+
+  const discriminatedUnion = $.union.getDiscriminatedUnion(value.union);
+  switch (discriminatedUnion?.options.envelope) {
+    case "object":
+      return (
+        <ObjectEnvelope
+          discriminatorPropertyName={discriminatedUnion.options.discriminatorPropertyName}
+          envelopePropertyName={discriminatedUnion.options.envelopePropertyName}
+          type={value}
+        />
+      );
+    case "none":
+      return (
+        <NoneEnvelope
+          discriminatorPropertyName={discriminatedUnion.options.discriminatorPropertyName}
+          type={value}
+        />
+      );
+    default:
+      // not a discriminated union
+      return <TypeExpression type={value.type} />;
+  }
+}
+
+interface ObjectEnvelopeProps {
+  type: UnionVariant;
+  discriminatorPropertyName: string;
+  envelopePropertyName: string;
+}
+
+/**
+ * Renders a discriminated union with "object" envelope style
+ * where model properties are nested inside an envelope
+ */
+function ObjectEnvelope(props: ObjectEnvelopeProps) {
+  const { $ } = useTsp();
+
+  const envelope = $.model.create({
+    properties: {
+      [props.discriminatorPropertyName]: $.modelProperty.create({
+        name: props.discriminatorPropertyName,
+        type: $.literal.createString(props.type.name as string),
+      }),
+      [props.envelopePropertyName]: $.modelProperty.create({
+        name: props.envelopePropertyName,
+        type: props.type.type,
+      }),
+    },
+  });
+
+  return <TypeExpression type={envelope} />;
+}
+
+interface NoneEnvelopeProps {
+  type: UnionVariant;
+  discriminatorPropertyName: string;
+}
+
+/**
+ * Renders a discriminated union with "none" envelope style
+ * where discriminator property sits alongside model properties
+ */
+function NoneEnvelope(props: NoneEnvelopeProps) {
+  const { $ } = useTsp();
+
+  compilerAssert(
+    $.model.is(props.type.type),
+    "Expected all union variants to be models when using a discriminated union with no envelope",
+  );
+
+  const model = $.model.create({
+    properties: {
+      [props.discriminatorPropertyName]: $.modelProperty.create({
+        name: props.discriminatorPropertyName,
+        type: $.literal.createString(props.type.name as string),
+      }),
+      ...Object.fromEntries(props.type.type.properties),
+    },
+  });
+
+  return <TypeExpression type={model} />;
 }
