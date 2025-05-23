e37b8ce70aaeaff757ddfc272af8cb3abdc0a8e5
Timothee Guerin <tiguerin@microsoft.com>
2025-05-23 09:27:35 -0700

    Remove templateArguments property (#7461)
    fix #7456
    
    That property was deprecated and removed for 1.0 but seems like due to
    the cast to `as any` we missed the actual assignment part

--- packages/compiler/src/core/checker.ts
+++ packages/compiler/src/core/checker.ts
@@ -140,6 +140,7 @@ import {
   TemplateParameterDeclarationNode,
   TemplateableNode,
   TemplatedType,
+  TemplatedTypeBase,
   Tuple,
   TupleExpressionNode,
   Type,
@@ -6395,14 +6396,10 @@ export function finishTypeForProgram<T extends Type>(program: Program, typeDef: 
   return finishTypeForProgramAndChecker(program, program.checker.typePrototype, typeDef);
 }
 
-function linkMapper<T extends Type>(typeDef: T, mapper?: TypeMapper) {
+function linkMapper<T extends Type & TemplatedTypeBase>(typeDef: T, mapper?: TypeMapper) {
   if (mapper) {
-    compilerAssert(
-      !(typeDef as any).templateArguments,
-      "Mapper provided but template arguments already set.",
-    );
-    (typeDef as any).templateMapper = mapper;
-    (typeDef as any).templateArguments = mapper.args;
+    compilerAssert(!typeDef.templateMapper, "Mapper provided but template arguments already set.");
+    typeDef.templateMapper = mapper;
   }
 }
 
