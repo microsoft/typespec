e0077ad1b4f86925428c7a172b910f04fc842ab6
Weidong Xu <weidxu@microsoft.com>
2025-06-06 10:49:31 +0800

    remove deploymentstacks

--- packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/model/clientmodel/examplemodel/FluentClientMethodExample.java
+++ packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/model/clientmodel/examplemodel/FluentClientMethodExample.java
@@ -101,8 +101,6 @@ public class FluentClientMethodExample implements FluentMethodExample {
                 serviceClientReference = "managementLockClient()";
             } else if (tag.contains("changes")) {
                 serviceClientReference = "resourceChangeClient()";
-            } else if (tag.contains("deploymentstacks")) {
-                serviceClientReference = "deploymentStackClient()";
             } else if (tag.contains("databoundaries")) {
                 serviceClientReference = "dataBoundaryClient()";
             } else if (!tag.contains("resources")) {
