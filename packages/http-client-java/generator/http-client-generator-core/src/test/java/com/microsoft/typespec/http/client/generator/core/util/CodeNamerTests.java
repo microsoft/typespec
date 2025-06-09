93a22eec86346aff3c4258fcecb8c0cfcbe9f45d
Weidong Xu <weidxu@microsoft.com>
2025-06-09 16:55:18 +0800

    http-client-java, bug fix on enum variable name (#7590)
    follow up on https://github.com/microsoft/typespec/pull/7448

--- packages/http-client-java/generator/http-client-generator-core/src/test/java/com/microsoft/typespec/http/client/generator/core/util/CodeNamerTests.java
+++ packages/http-client-java/generator/http-client-generator-core/src/test/java/com/microsoft/typespec/http/client/generator/core/util/CodeNamerTests.java
@@ -10,6 +10,7 @@ public class CodeNamerTests {
 
     @Test
     public void testEnumMemberName() {
+        // numbers to words
         Assertions.assertEquals("NINE_NINE_ONE", CodeNamer.getEnumMemberName("991"));
 
         Assertions.assertEquals("TCP", CodeNamer.getEnumMemberName("tcp"));
@@ -18,26 +19,33 @@ public class CodeNamerTests {
 
         Assertions.assertEquals("CONTENT_TYPE", CodeNamer.getEnumMemberName("content_type"));
 
+        // unicode letter
+        Assertions.assertEquals("Ā" + "ALL" + "Ā", CodeNamer.getEnumMemberName("\u0100all\u0101"));
+
+        // special characters removed
         Assertions.assertEquals("MICROSOFT_APP_CONFIGURATION_CONFIGURATION_STORES",
             CodeNamer.getEnumMemberName("Microsoft.AppConfiguration/configurationStores"));
-
-        Assertions.assertEquals("ASTERISK", CodeNamer.getEnumMemberName("*"));
-
-        Assertions.assertEquals("ALL", CodeNamer.getEnumMemberName("$all"));
-
-        Assertions.assertEquals("ALL", CodeNamer.getEnumMemberName("all*"));
 
         Assertions.assertEquals("SYSTEM_ASSIGNED_USER_ASSIGNED",
             CodeNamer.getEnumMemberName("SystemAssigned, UserAssigned"));
 
+        Assertions.assertEquals("ALL", CodeNamer.getEnumMemberName("$all"));
+        Assertions.assertEquals("ALL", CodeNamer.getEnumMemberName("all*"));
+
+        // contains only special characters
+        Assertions.assertEquals("ASTERISK", CodeNamer.getEnumMemberName("*"));
+
+        Assertions.assertEquals("COLON", CodeNamer.getEnumMemberName("__:"));
+
+        // underscore alone cannot be variable name
+        Assertions.assertEquals("UNDERSCORE", CodeNamer.getEnumMemberName("_"));
+        Assertions.assertEquals("UNDERSCORE", CodeNamer.getEnumMemberName("___"));
+
+        // underscore removed, and numbers to words
         Assertions.assertEquals("ONE_ZEROMINUTELY", CodeNamer.getEnumMemberName("_10minutely"));
 
-        Assertions.assertEquals("DOLLAR_SIGN_100_GIFT_CARD", CodeNamer.getEnumMemberName("$100 Gift Card"));
-
-        Assertions.assertEquals("PERCENT_SIGN_50_OFF", CodeNamer.getEnumMemberName("%50 Off"));
-
-        Assertions.assertEquals("NUMBER_SIGN_10_TRENDING", CodeNamer.getEnumMemberName("#10 Trending"));
-
-        Assertions.assertEquals("_", CodeNamer.getEnumMemberName("_"));
+        // special characters removed, and numbers to words
+        Assertions.assertEquals("ONE_ZERO_ZERO_GIFT_CARD", CodeNamer.getEnumMemberName("$100 Gift Card"));
+        Assertions.assertEquals("ONE_ZERO_ZERO_GIFT_CARD", CodeNamer.getEnumMemberName("$$100 Gift Card"));
     }
 }
