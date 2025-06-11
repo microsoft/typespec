ecf06e2072ed61c72a69dc018f7a05e44a03d306
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2025-06-11 16:36:36 -0400

    Completely remove usage of Eclipse language server (#7552)
    Completes the removal of Eclipse language server usage in the Java
    generator.

--- packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/customization/LibraryCustomization.java
+++ packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/customization/LibraryCustomization.java
@@ -6,14 +6,25 @@ package com.microsoft.typespec.http.client.generator.core.customization;
 /**
  * The top level customization for an AutoRest generated client library.
  */
-public interface LibraryCustomization {
+public final class LibraryCustomization {
+    private final Editor editor;
+
+    LibraryCustomization(Editor editor) {
+        this.editor = editor;
+    }
+
     /**
      * Gets the package level customization for a Java package in the client library.
      *
      * @param packageName the fully qualified name of the package
      * @return the package level customization.
      */
-    PackageCustomization getPackage(String packageName);
+    public PackageCustomization getPackage(String packageName) {
+        if (!editor.packageExists(packageName)) {
+            throw new IllegalArgumentException(packageName + " does not exist");
+        }
+        return new PackageCustomization(editor, packageName);
+    }
 
     /**
      * Gets the class level customization for a Java class in the client library.
@@ -22,12 +33,16 @@ public interface LibraryCustomization {
      * @param className the simple name of the class
      * @return the class level customization
      */
-    ClassCustomization getClass(String packageName, String className);
+    public ClassCustomization getClass(String packageName, String className) {
+        return getPackage(packageName).getClass(className);
+    }
 
     /**
      * Gets the raw editor containing the current files being edited and eventually emitted to the disk.
      *
      * @return the raw editor
      */
-    Editor getRawEditor();
+    public Editor getRawEditor() {
+        return editor;
+    }
 }
--- packages/http-client-java/generator/http-client-generator/src/main/java/com/microsoft/typespec/http/client/generator/model/EmitterOptions.java
+++ packages/http-client-java/generator/http-client-generator/src/main/java/com/microsoft/typespec/http/client/generator/model/EmitterOptions.java
@@ -31,7 +31,6 @@ public class EmitterOptions implements JsonSerializable<EmitterOptions> {
     private Boolean includeApiViewProperties = true;
     private String packageVersion;
     private Boolean useObjectForUnknown = false;
-    private Boolean useEclipseLanguageServer = true;
     private Map<String, PollingSettings> polling = new HashMap<>();
     private String modelsSubpackage;
     private String apiVersion;
@@ -89,10 +88,6 @@ public class EmitterOptions implements JsonSerializable<EmitterOptions> {
 
     public Boolean getUseObjectForUnknown() {
         return useObjectForUnknown;
-    }
-
-    public Boolean getUseEclipseLanguageServer() {
-        return useEclipseLanguageServer;
     }
 
     public List<String> getServiceVersions() {
@@ -197,8 +192,6 @@ public class EmitterOptions implements JsonSerializable<EmitterOptions> {
                 options.includeApiViewProperties = reader.getNullable(EmitterOptions::getBoolean);
             } else if ("use-object-for-unknown".equals(fieldName)) {
                 options.useObjectForUnknown = reader.getNullable(EmitterOptions::getBoolean);
-            } else if ("use-eclipse-language-server".equals(fieldName)) {
-                options.useEclipseLanguageServer = reader.getNullable(EmitterOptions::getBoolean);
             } else if ("polling".equals(fieldName)) {
                 options.polling = reader.readMap(PollingSettings::fromJson);
             } else if ("arm".equals(fieldName)) {
