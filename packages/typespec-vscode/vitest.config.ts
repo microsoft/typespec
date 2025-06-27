2382136ef5319a0a956783535e9b06af921035a9
v-zhocai <v-zhocai@microsoft.com>
2025-06-27 13:46:32 +0800

    Add test case for TypeSpec Extension

--- packages/typespec-vscode/vitest.config.ts
+++ packages/typespec-vscode/vitest.config.ts
@@ -1,31 +1,11 @@
 import { defineConfig, mergeConfig } from "vitest/config";
-import path from "node:path";
 import { defaultTypeSpecVitestConfig } from "../../vitest.workspace.js";
 
-export default defineConfig({
-  projects: [
-    mergeConfig(
-      defaultTypeSpecVitestConfig,
-      defineConfig({
-        name: "unit",
-        test: {
-          include: ["test/unit/**/*.test.ts"],
-        },
-      }),
-    ),
-    defineConfig({
-      name: "extension",
-      test: {
-        include: ["test/extension/**/*.test.ts"],
-        testTimeout: process.env.CI ? 240_000 : Number.POSITIVE_INFINITY,
-        fileParallelism: false,
-        env: {
-          VSCODE_E2E_EXTENSION_PATH: "./",
-          VSCODE_E2E_TRACE: "on",
-        },
-        globalSetup: [path.resolve(__dirname, "test/extension/common/downloadSetup.ts")],
-        retry: 1,
-      },
-    }),
-  ],
-});
+export default mergeConfig(
+  defaultTypeSpecVitestConfig,
+  defineConfig({
+    test: {
+      include: ["test/unit/**/*.test.ts"],
+    },
+  }),
+);
