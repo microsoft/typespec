55ecd5302359cc319427c0664da415c3fb83d370
Jorge Rangel <jorgerangel@microsoft.com>
2025-07-02 12:22:02 -0500

    feat: make codegen singleton public

--- packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/src/ScmCodeModelGenerator.cs
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/src/ScmCodeModelGenerator.cs
@@ -15,7 +15,7 @@ namespace Microsoft.TypeSpec.Generator.ClientModel
     public class ScmCodeModelGenerator : CodeModelGenerator
     {
         private static ScmCodeModelGenerator? _instance;
-        internal static ScmCodeModelGenerator Instance => _instance ?? throw new InvalidOperationException("ScmCodeModelGenerator is not loaded.");
+        internal static new ScmCodeModelGenerator Instance => _instance ?? throw new InvalidOperationException("ScmCodeModelGenerator is not loaded.");
 
         private ScmOutputLibrary? _scmOutputLibrary;
         public override OutputLibrary OutputLibrary => _scmOutputLibrary ??= new();
