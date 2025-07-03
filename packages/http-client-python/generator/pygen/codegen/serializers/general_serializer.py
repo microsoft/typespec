9e45cc10fc0e436ae2b455085c5690fd2f13098c
iscai-msft <isabellavcai@gmail.com>
2025-07-03 12:21:05 -0400

    bump azure-core min version

--- packages/http-client-python/generator/pygen/codegen/serializers/general_serializer.py
+++ packages/http-client-python/generator/pygen/codegen/serializers/general_serializer.py
@@ -20,7 +20,7 @@ VERSION_MAP = {
     "msrest": "0.7.1",
     "isodate": "0.6.1",
     "azure-mgmt-core": "1.5.0",
-    "azure-core": "1.30.0",
+    "azure-core": "1.35.0",
     "typing-extensions": "4.6.0",
     "corehttp": "1.0.0b6",
 }
