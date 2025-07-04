65db2bdf285b781995e827aea71f9cc63a4d4a2e
ChenxiJiang333 <119990644+ChenxiJiang333@users.noreply.github.com>
2025-07-04 14:29:31 +0800

    Update test_response_status_code_range_async.py

--- packages/http-client-python/generator/test/generic_mock_api_tests/asynctests/test_response_status_code_range_async.py
+++ packages/http-client-python/generator/test/generic_mock_api_tests/asynctests/test_response_status_code_range_async.py
@@ -6,7 +6,6 @@
 import pytest
 from response.statuscoderange.aio import StatusCodeRangeClient
 from response.statuscoderange.models import ErrorInRange, NotFoundError
-from azure.core.exceptions import HttpResponseError, ResourceNotFoundError
 
 
 @pytest.fixture
@@ -17,7 +16,7 @@ async def client():
 
 @pytest.mark.asyncio
 async def test_error_response_status_code_in_range(client: StatusCodeRangeClient):
-    with pytest.raises(HttpResponseError) as exc_info:
+    with pytest.raises(Exception) as exc_info:
         await client.error_response_status_code_in_range()
 
     error = exc_info.value.model
@@ -29,7 +28,7 @@ async def test_error_response_status_code_in_range(client: StatusCodeRangeClient
 
 @pytest.mark.asyncio
 async def test_error_response_status_code_404(client: StatusCodeRangeClient):
-    with pytest.raises(ResourceNotFoundError) as exc_info:
+    with pytest.raises(Exception) as exc_info:
         await client.error_response_status_code404()
 
     error = exc_info.value.model
