0674f602fd9435d5c2e150e58e4938e81dbd31dd
Timothee Guerin <tiguerin@microsoft.com>
2025-07-07 16:17:49 -0700

    .

--- packages/json-schema/src/get-json-schema-for-type/get-json-schema-for-type.test.ts
+++ packages/json-schema/src/get-json-schema-for-type/get-json-schema-for-type.test.ts
@@ -1,28 +1,27 @@
 import { Tester } from "#test/tester.js";
 import { t } from "@typespec/compiler/testing";
-import { describe, expect, it } from "vitest";
+import { expect, it } from "vitest";
 import { getJsonSchemaForType } from "./get-json-schema-for-type.js";
 
-describe("getJsonSchemaForType", () => {
-  it("get a simple contained schema for a model", async () => {
-    const { Test, program } = await Tester.compile(t.code`
+it("get a simple contained schema for a model", async () => {
+  const { Test, program } = await Tester.compile(t.code`
       model ${t.model("Test")} {
         prop: string;
       }  
     `);
 
-    const result = getJsonSchemaForType(program, Test);
-    expect(result).toEqual({
-      type: "object",
-      properties: {
-        prop: { type: "string" },
-      },
-      required: ["prop"],
-    });
+  const result = getJsonSchemaForType(program, Test);
+  expect(result).toEqual({
+    type: "object",
+    properties: {
+      prop: { type: "string" },
+    },
+    required: ["prop"],
   });
+});
 
-  it("include nested models", async () => {
-    const { Test, program } = await Tester.compile(t.code`
+it("include nested models", async () => {
+  const { Test, program } = await Tester.compile(t.code`
       model ${t.model("Test")} {
         other: Other;
       }
@@ -30,24 +29,24 @@ describe("getJsonSchemaForType", () => {
       model Other { prop: string; }
     `);
 
-    const result = getJsonSchemaForType(program, Test);
-    expect(result).toEqual({
-      type: "object",
-      properties: {
-        other: {
-          type: "object",
-          properties: {
-            prop: { type: "string" },
-          },
-          required: ["prop"],
+  const result = getJsonSchemaForType(program, Test);
+  expect(result).toEqual({
+    type: "object",
+    properties: {
+      other: {
+        type: "object",
+        properties: {
+          prop: { type: "string" },
         },
+        required: ["prop"],
       },
-      required: ["other"],
-    });
+    },
+    required: ["other"],
   });
+});
 
-  it("support self references", async () => {
-    const { Test, program } = await Tester.compile(t.code`
+it("support self references", async () => {
+  const { Test, program } = await Tester.compile(t.code`
       model ${t.model("Test")} {
         node: Node;
       }
@@ -56,19 +55,18 @@ describe("getJsonSchemaForType", () => {
       model Node { parent?: Node; }
     `);
 
-    const result = getJsonSchemaForType(program, Test);
-    expect(result).toEqual({
-      type: "object",
-      properties: {
-        node: {
-          description: "Node model",
-          type: "object",
-          properties: {
-            parent: { description: "Node model" },
-          },
+  const result = getJsonSchemaForType(program, Test);
+  expect(result).toEqual({
+    type: "object",
+    properties: {
+      node: {
+        description: "Node model",
+        type: "object",
+        properties: {
+          parent: { description: "Node model" },
         },
       },
-      required: ["node"],
-    });
+    },
+    required: ["node"],
   });
 });
