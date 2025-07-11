f7868df6630a2318a697144da31b9d4edb3b2b82
jolov <jolov@microsoft.com>
2025-07-11 15:49:40 -0700

    Add duration-constant encoding support

---
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.Input/src/Extensions/DurationKnownEncodingExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.TypeSpec.Generator.Input.Extensions
+{
+    internal static class DurationKnownEncodingExtensions
+    {
+        public static bool TryParse(string? value, [NotNullWhen(true)] out DurationKnownEncoding? result)
+        {
+            result = value?.ToLowerInvariant() switch
+            {
+                "iso8601" => DurationKnownEncoding.Iso8601,
+                "seconds" => DurationKnownEncoding.Seconds,
+                "duration-constant" => DurationKnownEncoding.Constant,
+                "constant" => DurationKnownEncoding.Constant,
+                _ => null
+            };
+
+            return result.HasValue;
+        }
+    }
+}
---
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.Input/test/DurationKnownEncodingTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.TypeSpec.Generator.Input.Extensions;
+using NUnit.Framework;
+
+namespace Microsoft.TypeSpec.Generator.Input.Tests
+{
+    public class DurationKnownEncodingTests
+    {
+        [TestCase("duration-constant", DurationKnownEncoding.Constant)]
+        [TestCase("constant", DurationKnownEncoding.Constant)]
+        [TestCase("iso8601", DurationKnownEncoding.Iso8601)]
+        [TestCase("seconds", DurationKnownEncoding.Seconds)]
+        public void TryParseEachEncoding(string encoding, DurationKnownEncoding expectedEncoding)
+        {
+            var result = DurationKnownEncodingExtensions.TryParse(encoding, out var parsedEncoding);
+            Assert.IsTrue(result);
+            Assert.AreEqual(expectedEncoding, parsedEncoding);
+        }
+
+        [Test]
+        public void TryParseInvalidEncoding()
+        {
+            var result = DurationKnownEncodingExtensions.TryParse("invalid-encoding", out var parsedEncoding);
+            Assert.IsFalse(result);
+            Assert.IsNull(parsedEncoding);
+        }
+    }
+}
