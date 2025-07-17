8ff0231253ab31f7567871175c3b2e8a47709756
Yuchao Yan <yuchaoyan@microsoft.com>
2025-07-17 05:17:45 +0000

    update

--- packages/http-client-python/generator/test/azure/mock_api_tests/asynctests/test_azure_client_generator_core_override_async.py
+++ packages/http-client-python/generator/test/azure/mock_api_tests/asynctests/test_azure_client_generator_core_override_async.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+import inspect
 import pytest
 from specs.azure.clientgenerator.core.override.aio import OverrideClient
 
@@ -21,3 +22,22 @@ async def test_reorder_parameters(client: OverrideClient):
 @pytest.mark.asyncio
 async def test_group_parameters(client: OverrideClient):
     await client.group_parameters.group(param1="param1", param2="param2")
+
+
+def test_reorder_parameters_unit_async(client: OverrideClient):
+    # make sure signature name of `reorder_parameters` are ["param1", "param2"]
+    # Get the reorder method from the reorder_parameters operation
+    reorder_method = client.reorder_parameters.reorder
+
+    # Inspect the method signature
+    sig = inspect.signature(reorder_method)
+
+    # Get parameter names excluding 'self' and '**kwargs'
+    param_names = [
+        param_name
+        for param_name, param in sig.parameters.items()
+        if param_name not in ("self", "kwargs") and param.kind != param.VAR_KEYWORD
+    ]
+
+    # Assert that the parameter names are exactly ["param1", "param2"]
+    assert param_names == ["param1", "param2"], f"Expected parameter names ['param1', 'param2'], but got {param_names}"
