55d29af05c6669ea92e2c78c663b3d07bb13081d
Will Temple <witemple@microsoft.com>
2025-07-17 11:40:37 -0400

    Also expose libraries

--- packages/playground/src/react/debug.ts
+++ packages/playground/src/react/debug.ts
@@ -8,9 +8,30 @@ import type { BrowserHost } from "../types.js";
 /* eslint-disable no-console */
 
 export interface DebugGlobals {
+  /**
+   * The current TypeSpec program.
+   */
   program: Program | undefined;
+
+  /**
+   * The compiler host.
+   */
   host: BrowserHost | undefined;
+
+  /**
+   * The TypeSpec compiler API.
+   */
+  compiler: typeof import("@typespec/compiler") | undefined;
+
+  /**
+   * Global Typekit instance bound to the current program.
+   */
   $$: Typekit | undefined;
+
+  /**
+   * A map of all loaded TypeSpec libraries.
+   */
+  libs: Record<string, unknown> | undefined;
 }
 
 /**
@@ -23,7 +44,9 @@ export function printDebugInfo() {
   );
   console.info(" - `program`: The current TypeSpec program.");
   console.info(" - `host`: The current compiler host.");
+  console.info(" - `compiler`: The TypeSpec compiler API.");
   console.info(" - `$$`: A Typekit instance bound to the current program.");
+  console.info(" - `libs`: A map of all loaded TypeSpec libraries.");
 }
 
 /**
@@ -31,4 +54,11 @@ export function printDebugInfo() {
  */
 export function debugGlobals(): DebugGlobals {
   return window as unknown as DebugGlobals;
+}
+
+/**
+ * Gets the global debug libraries
+ */
+export function debugLibs(): Record<string, unknown> {
+  return ((window as unknown as DebugGlobals).libs ??= {});
 }
