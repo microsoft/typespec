d28afae6feb381cdd6b6c035f001307d4ff00e52
Timothee Guerin <tiguerin@microsoft.com>
2025-07-17 12:55:23 -0700

    migrate new tester

--- packages/emitter-framework/src/typescript/components/union/declaration.test.tsx
+++ packages/emitter-framework/src/typescript/components/union/declaration.test.tsx
@@ -1,383 +1,264 @@
-import { render } from "@alloy-js/core";
-import { d } from "@alloy-js/core/testing";
+import { Output } from "#core/index.js";
+import { Tester } from "#test/test-host.js";
+import type { Children } from "@alloy-js/core";
 import { SourceFile } from "@alloy-js/typescript";
-import type { Enum, Model, Union } from "@typespec/compiler";
-import type { BasicTestRunner } from "@typespec/compiler/testing";
-import { beforeEach, describe, it } from "vitest";
-import { createEmitterFrameworkTestRunner } from "../../../../test/typescript/test-host.js";
-import { assertFileContents } from "../../../../test/utils.js";
-import { Output } from "../../../core/components/output.jsx";
+import { t, type TesterInstance } from "@typespec/compiler/testing";
+import { beforeEach, expect, it } from "vitest";
 import { InterfaceDeclaration } from "../../index.js";
 import { UnionDeclaration } from "./declaration.jsx";
 
-let runner: BasicTestRunner;
+let tester: TesterInstance;
+
+function Wrapper(props: { children: Children }) {
+  return (
+    <Output program={tester.program}>
+      <SourceFile path="test.ts">{props.children}</SourceFile>
+    </Output>
+  );
+}
 
 beforeEach(async () => {
-  runner = await createEmitterFrameworkTestRunner();
+  tester = await Tester.createInstance();
 });
 
-describe("Union not bound to Typespec Types", () => {
-  it("creates a union declaration", async () => {
-    await runner.compile(``);
-    const res = render(
-      <Output program={runner.program}>
-        <SourceFile path="test.ts">
-          <UnionDeclaration name="MyUnion">"red" | "blue"</UnionDeclaration>
-        </SourceFile>
-      </Output>,
-    );
-
-    assertFileContents(
-      res,
-      d`
-        type MyUnion = "red" | "blue";
-      `,
-    );
-  });
+it("creates a union declaration (not bound to Typespec Types)", async () => {
+  await tester.compile("");
+  expect(
+    <Wrapper>
+      <UnionDeclaration name="MyUnion">"red" | "blue"</UnionDeclaration>
+    </Wrapper>,
+  ).toRenderTo(`type MyUnion = "red" | "blue";`);
 });
 
-describe("Union bound to Typespec Types", () => {
-  describe("Bound to Union", () => {
-    it("creates a union declaration", async () => {
-      const { TestUnion } = (await runner.compile(`
-        namespace DemoService;
-        @test union TestUnion {
-          one: "one",
-          two: "two"
-        }
-      `)) as { TestUnion: Union };
+it("creates a union declaration (bound to union)", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    union ${t.union("TestUnion")} {
+      one: "one",
+      two: "two"
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestUnion} />
+    </Wrapper>,
+  ).toRenderTo(`type TestUnion = "one" | "two";`);
+});
 
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <UnionDeclaration type={TestUnion} />
-          </SourceFile>
-        </Output>,
-      );
+it("creates a union declaration with JSDoc", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    /**
+     * Test Union
+     */
+    union ${t.union("TestUnion")} {
+      one: "one",
+      two: "two"
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestUnion} />
+    </Wrapper>,
+  ).toRenderTo(`/**
+ * Test Union
+ */
+type TestUnion = "one" | "two";`);
+});
 
-      assertFileContents(
-        res,
-        d`
-          type TestUnion = "one" | "two";
-        `,
-      );
-    });
+it("creates a union declaration with name override", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    union ${t.union("TestUnion")} {
+      one: "one",
+      two: "two"
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration export type={TestUnion} name="MyUnion" />
+    </Wrapper>,
+  ).toRenderTo(`export type MyUnion = "one" | "two";`);
+});
 
-    it("creates a union declaration with JSDoc", async () => {
-      const { TestUnion } = (await runner.compile(`
-        namespace DemoService;
-        /**
-         * Test Union
-         */
-        @test union TestUnion {
-          one: "one",
-          two: "two"
-        }
-      `)) as { TestUnion: Union };
+it("creates a union declaration with extra children", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    union ${t.union("TestUnion")} {
+      one: "one",
+      two: "two"
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestUnion}>"three"</UnionDeclaration>
+    </Wrapper>,
+  ).toRenderTo(`type TestUnion = "one" | "two" | "three";`);
+});
 
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <UnionDeclaration type={TestUnion} />
-          </SourceFile>
-        </Output>,
-      );
+it("renders a discriminated union", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    @discriminated
+    union ${t.union("TestUnion")} {
+      one: { oneItem: true },
+      two: true
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestUnion} />
+    </Wrapper>,
+  ).toRenderTo(`type TestUnion = {
+  kind: "one";
+  value: {
+    oneItem: true;
+  };
+} | {
+  kind: "two";
+  value: true;
+};`);
+});
 
-      assertFileContents(
-        res,
-        d`
-          /**
-           * Test Union
-           */
-          type TestUnion = "one" | "two";
-        `,
-      );
-    });
+it("renders a discriminated union with custom properties", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    @discriminated(#{ discriminatorPropertyName: "dataKind", envelopePropertyName: "data" })
+    union ${t.union("TestUnion")} {
+      one: { oneItem: true },
+      two: true
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestUnion} />
+    </Wrapper>,
+  ).toRenderTo(`type TestUnion = {
+  dataKind: "one";
+  data: {
+    oneItem: true;
+  };
+} | {
+  dataKind: "two";
+  data: true;
+};`);
+});
 
-    it("creates a union declaration with name override", async () => {
-      const { TestUnion } = (await runner.compile(`
-        namespace DemoService;
-        @test union TestUnion {
-          one: "one",
-          two: "two"
-        }
-      `)) as { TestUnion: Union };
+it("renders a discriminated union with named models", async () => {
+  const { Pet, Cat, Dog } = await tester.compile(t.code`
+    namespace DemoService;
+    model ${t.model("Cat")} {
+      name: string;
+      meow: boolean;
+    }
+    model ${t.model("Dog")} {
+      name: string;
+      bark: boolean;
+    }
+    @discriminated
+    union ${t.union("Pet")} {
+      cat: Cat,
+      dog: Dog,
+    }
+  `);
+  expect(
+    <Wrapper>
+      <InterfaceDeclaration type={Cat} />
+      <hbr />
+      <InterfaceDeclaration type={Dog} />
+      <hbr />
+      <UnionDeclaration type={Pet} />
+    </Wrapper>,
+  ).toRenderTo(`interface Cat {
+  name: string;
+  meow: boolean;
+}
+interface Dog {
+  name: string;
+  bark: boolean;
+}
+type Pet = {
+  kind: "cat";
+  value: Cat;
+} | {
+  kind: "dog";
+  value: Dog;
+};`);
+});
 
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <UnionDeclaration export type={TestUnion} name="MyUnion" />
-          </SourceFile>
-        </Output>,
-      );
+it("renders named discriminated union (no envelope)", async () => {
+  const { Pet, Cat, Dog } = await tester.compile(t.code`
+    namespace DemoService;
+    model ${t.model("Cat")} {
+      name: string;
+      meow: boolean;
+    }
+    model ${t.model("Dog")} {
+      name: string;
+      bark: boolean;
+    }
+    @discriminated(#{ envelope: "none", discriminatorPropertyName: "dataKind" })
+    union ${t.union("Pet")} {
+      cat: Cat,
+      dog: Dog,
+    }
+  `);
+  expect(
+    <Wrapper>
+      <InterfaceDeclaration type={Cat} />
+      <hbr />
+      <InterfaceDeclaration type={Dog} />
+      <hbr />
+      <UnionDeclaration type={Pet} />
+    </Wrapper>,
+  ).toRenderTo(`interface Cat {
+  name: string;
+  meow: boolean;
+}
+interface Dog {
+  name: string;
+  bark: boolean;
+}
+type Pet = {
+  dataKind: "cat"
+} & Cat | {
+  dataKind: "dog"
+} & Dog;`);
+});
 
-      assertFileContents(
-        res,
-        d`
-          export type MyUnion = "one" | "two";
-        `,
-      );
-    });
+it("renders anonymous discriminated union (no envelope)", async () => {
+  const { TestUnion } = await tester.compile(t.code`
+    namespace DemoService;
+    @discriminated(#{ envelope: "none", discriminatorPropertyName: "dataKind" })
+    union ${t.union("TestUnion")} {
+      one: { oneItem: true },
+      two: { secondItem: false }
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestUnion} />
+    </Wrapper>,
+  ).toRenderTo(`type TestUnion = {
+  dataKind: "one";
+  oneItem: true;
+} | {
+  dataKind: "two";
+  secondItem: false;
+};`);
+});
 
-    it("creates a union declaration with extra children", async () => {
-      const { TestUnion } = (await runner.compile(`
-        namespace DemoService;
-        @test union TestUnion {
-          one: "one",
-          two: "two"
-        }
-      `)) as { TestUnion: Union };
-
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <UnionDeclaration type={TestUnion}>"three"</UnionDeclaration>
-          </SourceFile>
-        </Output>,
-      );
-
-      assertFileContents(
-        res,
-        d`
-          type TestUnion = "one" | "two" | "three";
-        `,
-      );
-    });
-
-    describe("Discriminated Union", () => {
-      it("renders a discriminated union", async () => {
-        const { TestUnion } = (await runner.compile(`
-          namespace DemoService;
-          @discriminated
-          @test union TestUnion {
-            one: { oneItem: true },
-            two: true
-          }
-        `)) as { TestUnion: Union };
-
-        const res = render(
-          <Output program={runner.program}>
-            <SourceFile path="test.ts">
-              <UnionDeclaration type={TestUnion} />
-            </SourceFile>
-          </Output>,
-        );
-
-        assertFileContents(
-          res,
-          d`
-            type TestUnion = {
-              kind: "one";
-              value: {
-                oneItem: true;
-              };
-            } | {
-              kind: "two";
-              value: true;
-            };
-        `,
-        );
-      });
-    });
-
-    it("renders a discriminated union with custom properties", async () => {
-      const { TestUnion } = (await runner.compile(`
-        namespace DemoService;
-          @discriminated(#{ discriminatorPropertyName: "dataKind", envelopePropertyName: "data" })
-          @test union TestUnion {
-            one: { oneItem: true },
-            two: true
-          }
-        `)) as { TestUnion: Union };
-
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <UnionDeclaration type={TestUnion} />
-          </SourceFile>
-        </Output>,
-      );
-      assertFileContents(
-        res,
-        d`
-          type TestUnion = {
-            dataKind: "one";
-            data: {
-              oneItem: true;
-            };
-          } | {
-            dataKind: "two";
-            data: true;
-          };
-        `,
-      );
-    });
-
-    it("renders a discriminated union with named models", async () => {
-      const { Pet, Cat, Dog } = (await runner.compile(`
-          namespace DemoService;
-          @test model Cat {
-            name: string;
-            meow: boolean;
-          }
-
-          @test model Dog {
-            name: string;
-            bark: boolean;
-          }
-
-          @discriminated
-          @test union Pet {
-            cat: Cat,
-            dog: Dog,
-          }
-        `)) as { Pet: Union; Cat: Model; Dog: Model };
-
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <InterfaceDeclaration type={Cat} />
-            <hbr />
-            <InterfaceDeclaration type={Dog} />
-            <hbr />
-            <UnionDeclaration type={Pet} />
-          </SourceFile>
-        </Output>,
-      );
-      assertFileContents(
-        res,
-        d`
-          interface Cat {
-            name: string;
-            meow: boolean;
-          }
-          interface Dog {
-            name: string;
-            bark: boolean;
-          }
-          type Pet = {
-            kind: "cat";
-            value: Cat;
-          } | {
-            kind: "dog";
-            value: Dog;
-          };
-        `,
-      );
-    });
-
-    describe("Discriminated Union with no envelope", () => {
-      it("renders named discriminated union", async () => {
-        const { Pet, Cat, Dog } = (await runner.compile(`
-          namespace DemoService;
-
-          @test model Cat {
-            name: string;
-            meow: boolean;
-          }
-
-          @test model Dog {
-            name: string;
-            bark: boolean;
-          }
-
-          @discriminated(#{ envelope: "none", discriminatorPropertyName: "dataKind" })
-          @test union Pet {
-            cat: Cat,
-            dog: Dog,
-          }
-        `)) as { Pet: Union; Cat: Model; Dog: Model };
-
-        const res = render(
-          <Output program={runner.program}>
-            <SourceFile path="test.ts">
-              <InterfaceDeclaration type={Cat} />
-              <hbr />
-              <InterfaceDeclaration type={Dog} />
-              <hbr />
-              <UnionDeclaration type={Pet} />
-            </SourceFile>
-          </Output>,
-        );
-
-        assertFileContents(
-          res,
-          d`
-            interface Cat {
-              name: string;
-              meow: boolean;
-            }
-            interface Dog {
-              name: string;
-              bark: boolean;
-            }
-            type Pet = {
-              dataKind: "cat"
-            } & Cat | {
-              dataKind: "dog"
-            } & Dog;
-          `,
-        );
-      });
-
-      it("renders anonymous discriminated union", async () => {
-        const { TestUnion } = (await runner.compile(`
-          namespace DemoService;
-          @discriminated(#{ envelope: "none", discriminatorPropertyName: "dataKind" })
-          @test union TestUnion {
-            one: { oneItem: true },
-            two: { secondItem: false }
-          }
-        `)) as { TestUnion: Union };
-
-        const res = render(
-          <Output program={runner.program}>
-            <SourceFile path="test.ts">
-              <UnionDeclaration type={TestUnion} />
-            </SourceFile>
-          </Output>,
-        );
-
-        assertFileContents(
-          res,
-          d`
-            type TestUnion = {
-              dataKind: "one";
-              oneItem: true;
-            } | {
-              dataKind: "two";
-              secondItem: false;
-            };
-          `,
-        );
-      });
-    });
-  });
-
-  describe("Bound to Enum", () => {
-    it("creates a union declaration", async () => {
-      const { TestEnum } = (await runner.compile(`
-        namespace DemoService;
-        @test enum TestEnum {
-          one: "one",
-          two: "two"
-        }
-      `)) as { TestEnum: Enum };
-
-      const res = render(
-        <Output program={runner.program}>
-          <SourceFile path="test.ts">
-            <UnionDeclaration type={TestEnum} />
-          </SourceFile>
-        </Output>,
-      );
-
-      assertFileContents(
-        res,
-        d`
-          type TestEnum = "one" | "two";
-        `,
-      );
-    });
-  });
+it("creates a union declaration (bound to enum)", async () => {
+  const { TestEnum } = await tester.compile(t.code`
+    namespace DemoService;
+    enum ${t.enum("TestEnum")} {
+      one: "one",
+      two: "two"
+    }
+  `);
+  expect(
+    <Wrapper>
+      <UnionDeclaration type={TestEnum} />
+    </Wrapper>,
+  ).toRenderTo(`type TestEnum = "one" | "two";`);
 });
