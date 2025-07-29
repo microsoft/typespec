b58b956e0cf5bfa5aee383db168fec11ca194201
Timothee Guerin <tiguerin@microsoft.com>
2025-07-29 13:10:25 -0700

    fix

--- packages/emitter-framework/test/typescript/utils.tsx
+++ packages/emitter-framework/test/typescript/utils.tsx
@@ -1,7 +1,7 @@
-import { Output } from "#core/components/output.jsx";
 import type { Children } from "@alloy-js/core/jsx-runtime";
 import { SourceFile } from "@alloy-js/typescript";
 import type { Program } from "@typespec/compiler";
+import { Output } from "../../src/core/components/output.jsx";
 
 export function TestFile(props: { program: Program; children: Children }) {
   return (
