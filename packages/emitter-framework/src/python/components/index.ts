1699ed1eff41b31275d5eba065a91ea95b25ff28
swatikumar <swatikumar@pinterest.com>
2025-07-31 12:22:45 -0400

    Add python scaffolding

---
+++ packages/emitter-framework/src/python/components/index.ts
@@ -0,0 +1,5 @@
+// Python components will be exported here as they are created
+// Example future exports:
+// export * from "./class-declaration.js";
+// Empty export to make this a valid module until components are added
+export {};
