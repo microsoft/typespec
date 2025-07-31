627932ba454768dbb3e49be778b1cce287358c03
Weidong Xu <weidxu@microsoft.com>
2025-07-31 09:43:10 +0800

    bump java dep

--- packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/model/projectmodel/Project.java
+++ packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/model/projectmodel/Project.java
@@ -59,15 +59,15 @@ public class Project {
         // azure
         AZURE_CLIENT_SDK_PARENT("com.azure", "azure-client-sdk-parent", "1.7.0"),
         AZURE_CLIENT_SDK_PARENT_V2("com.azure.v2", "azure-client-sdk-parent", "2.0.0-beta.1"),
-        AZURE_CORE("com.azure", "azure-core", "1.55.4"),
+        AZURE_CORE("com.azure", "azure-core", "1.55.5"),
         AZURE_CORE_V2("com.azure.v2", "azure-core", "2.0.0-beta.1"),
-        AZURE_CORE_MANAGEMENT("com.azure", "azure-core-management", "1.18.0"),
-        AZURE_CORE_HTTP_NETTY("com.azure", "azure-core-http-netty", "1.15.12"),
-        AZURE_CORE_TEST("com.azure", "azure-core-test", "1.27.0-beta.9"),
-        AZURE_IDENTITY("com.azure", "azure-identity", "1.16.2"),
-        AZURE_CORE_EXPERIMENTAL("com.azure", "azure-core-experimental", "1.0.0-beta.61"),
+        AZURE_CORE_MANAGEMENT("com.azure", "azure-core-management", "1.18.1"),
+        AZURE_CORE_HTTP_NETTY("com.azure", "azure-core-http-netty", "1.15.13"),
+        AZURE_CORE_TEST("com.azure", "azure-core-test", "1.27.0-beta.10"),
+        AZURE_IDENTITY("com.azure", "azure-identity", "1.16.3"),
+        AZURE_CORE_EXPERIMENTAL("com.azure", "azure-core-experimental", "1.0.0-beta.62"),
 
-        CLIENTCORE("io.clientcore", "core", "1.0.0-beta.10");
+        CLIENTCORE("io.clientcore", "core", "1.0.0-beta.11");
 
         private final String groupId;
         private final String artifactId;
