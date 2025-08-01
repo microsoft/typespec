f80679f417b5e5c052cb2c4e25983005aa72d9a8
Will Temple <witemple@microsoft.com>
2025-08-01 17:03:23 -0400

    Implement array spector tests

---
+++ packages/http-server-js/test/e2e/http/type/array/main.test.e2e.ts
@@ -0,0 +1,78 @@
+import { afterEach, assert, beforeEach, describe, expect, it } from "vitest";
+import { createArrayRouter } from "../../../generated/type/array/src/generated/http/router.js";
+import { startServer, testRouterOptions } from "../../../helpers.js";
+import { runScenario } from "../../../spector.js";
+
+import { Temporal } from "temporal-polyfill";
+import { HTTP_RESPONDER, HttpResponder } from "../../../../../src/helpers/http.js";
+import { HttpContext } from "../../../../../src/http/index.js";
+
+class ArrayImpl<T> {
+  #assert: (l: T, r: T) => void;
+
+  constructor(public readonly value: T[], assert: (l: T, r: T) => void = (l, r) => l === r) {
+    this.#assert = assert;
+  }
+
+  async get() {
+    return this.value;
+  }
+
+  async put(_: HttpContext, body: T[]) {
+    assert.equal(body.length, this.value.length);
+
+    for (let i = 0; i < body.length; i++) {
+      this.#assert(this.value[i], body[i]);
+    }
+
+    return;
+  }
+}
+
+describe("Type.Array", () => {
+  let serverAbortController: AbortController;
+  beforeEach(() => {
+    serverAbortController = new AbortController();
+  });
+  afterEach(() => {
+    serverAbortController.abort();
+  });
+
+  it("passes all scenarios", async () => {
+    const router = createArrayRouter(
+      new ArrayImpl([1, 2]),
+      new ArrayImpl([0x7FFFFFFFFFFFFFFFn, -0x7FFFFFFFFFFFFFFFn]),
+      new ArrayImpl([true, false]),
+      new ArrayImpl(["hello", ""]),
+      new ArrayImpl([43.125]),
+      new ArrayImpl([Temporal.Instant.from("2022-08-26T18:38:00Z")], (l, r) => {
+        assert.equal(l.toString(), r.toString());
+      }),
+      new ArrayImpl([Temporal.Duration.from("P123DT22H14M12.011S")], (l, r) => {
+        assert.equal(l.toString(), r.toString());
+      }),
+      // TODO: this impl is not correct, and the tester does not handle it correctly either, because it is a hybrid 
+      // array with additional string properties. It should probably be serialized as `{"0":1,"1":"hello","k3":null}`
+      new ArrayImpl([1, "hello", null]),
+      new ArrayImpl([{property: "hello"}, {property: "world"}], (l, r) => {
+        assert.equal(l.property, r.property);
+      }),
+      new ArrayImpl([1.25, null, 3.0]),
+      new ArrayImpl([1, null, 3]),
+      new ArrayImpl([true, null, false]),
+      new ArrayImpl(["hello", null, "world"]),
+      new ArrayImpl([{property: "hello"}, null, {property: "world"}], (l, r) => {
+        if (l === null) {
+          assert.equal(r, null);
+        } else {
+          assert.isNotNull(r);
+          assert.equal(l.property, r.property);
+        }
+      }),
+      testRouterOptions,
+    );
+    const baseUrl = await startServer(router, serverAbortController.signal);
+    const { status } = await runScenario("type/array/**/*", baseUrl);
+    expect(status).toBe("pass");
+  });
+});
