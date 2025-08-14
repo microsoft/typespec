89482e632f24272a90926d1a8ae3305702567644
Vincent Biret <vincentbiret@hotmail.com>
2025-08-14 18:59:15 -0400

    feat: adds support for importing servers (#8201)
    fixes #8193
    
    ---------
    
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/openapi3/src/cli/actions/convert/generators/generate-service-info.ts
+++ packages/openapi3/src/cli/actions/convert/generators/generate-service-info.ts
@@ -1,9 +1,13 @@
-import { TypeSpecServiceInfo } from "../interfaces.js";
+import { TypeSpecServer, TypeSpecServiceInfo } from "../interfaces.js";
 import { generateDocs } from "../utils/docs.js";
 import { generateNamespaceName } from "../utils/generate-namespace-name.js";
 import { toTspValues } from "../utils/tsp-values.js";
+import { generateServers } from "./generate-servers.js";
 
-export function generateServiceInformation(serviceInfo: TypeSpecServiceInfo): string {
+export function generateServiceInformation(
+  serviceInfo: TypeSpecServiceInfo,
+  servers: TypeSpecServer[] = [],
+): string {
   const definitions: string[] = [];
 
   const { name, doc, ...info } = serviceInfo;
@@ -17,6 +21,10 @@ export function generateServiceInformation(serviceInfo: TypeSpecServiceInfo): st
 
   if (doc) {
     definitions.push(generateDocs(doc));
+  }
+
+  if (servers.length > 0) {
+    definitions.push(generateServers(servers));
   }
 
   definitions.push(`namespace ${generateNamespaceName(name)};`);
