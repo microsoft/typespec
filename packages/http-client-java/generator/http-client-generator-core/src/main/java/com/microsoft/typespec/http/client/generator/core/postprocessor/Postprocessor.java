microsoft/typespec

commit b720b087a88efd5f94ad7160a5d4a4acdac68b50

http-client-java, bug fix duplicate resource name (#8176)
see https://github.com/Azure/autorest.java/pull/3182 for detail

packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/transformer/ResourceTypeNormalization.java
@@ -63,8 +63,10 @@ class ResourceTypeNormalization {
         objectSchemas.forEach(compositeType -> {
             Optional<ObjectSchema> parentType = getObjectParent(compositeType);
             if (parentType.isPresent()) {
-                getSchemaResourceType(parentType.get())
-                    .ifPresent(type -> adaptForParentSchema(compositeType, parentType.get(), type));
+                getSchemaResourceType(parentType.get()).ifPresent(type -> {
+                    renameSchemaOnResourceType(parentType.get(), type);
+                    adaptForParentSchema(compositeType, parentType.get(), type);
+                });
 
                 if (FluentType.SYSTEM_DATA.getName().equals(Utils.getJavaName(parentType.get()))) {
                     adaptAsSystemData(compositeType);
@@ -231,6 +233,12 @@ class ResourceTypeNormalization {
         }
     }
 
+    /**
+     * Check the object schema, determine its resource type.
+     *
+     * @param compositeType the object schema to check
+     * @return the resource type
+     */
     private static Optional<ResourceType> getSchemaResourceType(ObjectSchema compositeType) {
         ResourceType type = null;
 
@@ -259,6 +267,28 @@ class ResourceTypeNormalization {
         }
 
         return Optional.ofNullable(type);
+    }
+
+    /**
+     * Rename the schema based on the resource type.
+     *
+     * @param compositeType the object schema to rename
+     * @param type the resource type
+     */
+    private static void renameSchemaOnResourceType(ObjectSchema compositeType, ResourceType type) {
+        switch (type) {
+            case SUB_RESOURCE:
+                compositeType.getLanguage().getJava().setName(ResourceTypeName.SUB_RESOURCE);
+                break;
+
+            case PROXY_RESOURCE:
+                compositeType.getLanguage().getJava().setName(ResourceTypeName.PROXY_RESOURCE);
+                break;
+
+            case RESOURCE:
+                compositeType.getLanguage().getJava().setName(ResourceTypeName.RESOURCE);
+                break;
+        }
     }
 
     private static void adaptForParentSchema(ObjectSchema compositeType, ObjectSchema parentType, ResourceType type) {

commit 48315fe5ecb7038b25e36527b261a8532e359456

http-client-java, bug fix on parent resource rename (#8190)
fix problem caused by https://github.com/microsoft/typespec/pull/8176

It causes problem on `ExtensionResource`, that `systemData` is not
included in the result resource. However it is not reproducible from
azure-http-specs test.

The current solution is to limit this `rename` to `AutoGenerated` class
alone.

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/postprocessor/Postprocessor.java
@@ -143,6 +143,7 @@ public class Postprocessor {
 
     public static Class<? extends Customization> loadCustomizationClassFromJavaCode(String filePath,
         String baseDirectory, Logger logger) {
+        final Path originCustomizationFile = Paths.get(filePath);
         Path customizationFile = Paths.get(filePath);
         if (!customizationFile.isAbsolute()) {
             if (baseDirectory != null) {
@@ -155,7 +156,7 @@ public class Postprocessor {
             return loadCustomizationClass(customizationFile.getFileName().toString().replace(".java", ""), code);
         } catch (IOException e) {
             logger.error("Cannot read customization from base directory {} and file {}", baseDirectory,
-                customizationFile);
+                originCustomizationFile);
             return null;
         }
     }

packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/model/ResourceType.java
@@ -9,7 +9,7 @@ public enum ResourceType {
     PROXY_RESOURCE(ResourceTypeName.PROXY_RESOURCE),
     SUB_RESOURCE(ResourceTypeName.SUB_RESOURCE);
 
-    private String className;
+    private final String className;
 
     ResourceType(String className) {
         this.className = className;

packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/transformer/ResourceTypeNormalization.java
@@ -64,7 +64,7 @@ class ResourceTypeNormalization {
             Optional<ObjectSchema> parentType = getObjectParent(compositeType);
             if (parentType.isPresent()) {
                 getSchemaResourceType(parentType.get()).ifPresent(type -> {
-                    renameSchemaOnResourceType(parentType.get(), type);
+                    correctDeduplicatedName(parentType.get());
                     adaptForParentSchema(compositeType, parentType.get(), type);
                 });
 
@@ -270,24 +270,16 @@ class ResourceTypeNormalization {
     }
 
     /**
-     * Rename the schema based on the resource type.
+     * Correct the schema name based on the resource type.
      *
-     * @param compositeType the object schema to rename
-     * @param type the resource type
+     * @param compositeType the object schema to correct
      */
-    private static void renameSchemaOnResourceType(ObjectSchema compositeType, ResourceType type) {
-        switch (type) {
-            case SUB_RESOURCE:
-                compositeType.getLanguage().getJava().setName(ResourceTypeName.SUB_RESOURCE);
-                break;
-
-            case PROXY_RESOURCE:
-                compositeType.getLanguage().getJava().setName(ResourceTypeName.PROXY_RESOURCE);
-                break;
-
-            case RESOURCE:
-                compositeType.getLanguage().getJava().setName(ResourceTypeName.RESOURCE);
-                break;
+    private static void correctDeduplicatedName(ObjectSchema compositeType) {
+        final String deduplicateSuffix = "AutoGenerated";
+        final String javaName = Utils.getJavaName(compositeType);
+        if (javaName.contains(deduplicateSuffix)) {
+            int index = javaName.indexOf(deduplicateSuffix);
+            compositeType.getLanguage().getJava().setName(javaName.substring(0, index));
         }
     }
 