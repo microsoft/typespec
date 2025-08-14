d94ec3f4b4526234c67da81b148f620c2e7befe7
Vincent Biret <vibiret@microsoft.com>
2025-08-14 12:43:07 -0400

    chore: refactoring of external docs for readability
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/openapi3/src/cli/actions/convert/generators/generate-tags.ts
+++ packages/openapi3/src/cli/actions/convert/generators/generate-tags.ts
@@ -1,12 +1,24 @@
-import { TypeSpecTagMetadata } from "../interfaces.js";
+import { TypeSpecExternalDocs, TypeSpecTagMetadata } from "../interfaces.js";
+
+function generateExternalDocs(externalDocs?: TypeSpecExternalDocs): string {
+  if (!externalDocs) {
+    return "";
+  }
+
+  const externalDocsUrl = externalDocs.url ? `url: "${externalDocs.url}"` : "";
+  const externalDocsDescription = externalDocs.description ? `description: "${externalDocs.description}"` : "";
+  
+  if (!externalDocsUrl && !externalDocsDescription) {
+    return "";
+  }
+
+  return `externalDocs: #{${[externalDocsUrl, externalDocsDescription].filter(x => !!x).join(", ")}}`;
+}
 
 export function generateTags(tags: TypeSpecTagMetadata[]): string {
   const definitions = tags.map((tag) => {
     const description = tag.description ? `description: "${tag.description}"` : "";
-    const externalDocsUrl = tag.externalDocs?.url ? `url: "${tag.externalDocs.url}"` : "";
-    const externalDocsDescription = tag.externalDocs?.description ? `description: "${tag.externalDocs.description}"` : "";
-    const externalDocs = externalDocsUrl || externalDocsDescription ?
-      `externalDocs: #{${[externalDocsUrl, externalDocsDescription].filter(x => !!x).join(", ")}}` : "";
+    const externalDocs = generateExternalDocs(tag.externalDocs);
     const tagMetadata = description || externalDocs ? `, #{${[description, externalDocs].filter(x => !!x).join(", ")}}` : "";
     return `@tagMetadata("${tag.name}"${tagMetadata})`;
   });
