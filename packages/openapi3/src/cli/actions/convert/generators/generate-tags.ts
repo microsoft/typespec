4a85ec9f1c54820a76795ad6d04a2015394cd51d
Vincent Biret <vibiret@microsoft.com>
2025-08-14 12:53:46 -0400

    chore: formatting
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/openapi3/src/cli/actions/convert/generators/generate-tags.ts
+++ packages/openapi3/src/cli/actions/convert/generators/generate-tags.ts
@@ -6,20 +6,25 @@ function generateExternalDocs(externalDocs?: TypeSpecExternalDocs): string {
   }
 
   const externalDocsUrl = externalDocs.url ? `url: "${externalDocs.url}"` : "";
-  const externalDocsDescription = externalDocs.description ? `description: "${externalDocs.description}"` : "";
-  
+  const externalDocsDescription = externalDocs.description
+    ? `description: "${externalDocs.description}"`
+    : "";
+
   if (!externalDocsUrl && !externalDocsDescription) {
     return "";
   }
 
-  return `externalDocs: #{${[externalDocsUrl, externalDocsDescription].filter(x => !!x).join(", ")}}`;
+  return `externalDocs: #{${[externalDocsUrl, externalDocsDescription].filter((x) => !!x).join(", ")}}`;
 }
 
 export function generateTags(tags: TypeSpecTagMetadata[]): string {
   const definitions = tags.map((tag) => {
     const description = tag.description ? `description: "${tag.description}"` : "";
     const externalDocs = generateExternalDocs(tag.externalDocs);
-    const tagMetadata = description || externalDocs ? `, #{${[description, externalDocs].filter(x => !!x).join(", ")}}` : "";
+    const tagMetadata =
+      description || externalDocs
+        ? `, #{${[description, externalDocs].filter((x) => !!x).join(", ")}}`
+        : "";
     return `@tagMetadata("${tag.name}"${tagMetadata})`;
   });
 
--- packages/openapi3/src/cli/actions/convert/transforms/transform-tags.ts
+++ packages/openapi3/src/cli/actions/convert/transforms/transform-tags.ts
@@ -5,9 +5,12 @@ export function transformTags(tags: OpenAPI3Tag[]): TypeSpecTagMetadata[] {
   return tags.map((tag) => ({
     name: tag.name,
     description: tag.description,
-    externalDocs: tag.externalDocs?.url || tag.externalDocs?.description ? {
-      url: tag.externalDocs.url,
-      description: tag.externalDocs.description
-    } : undefined
+    externalDocs:
+      tag.externalDocs?.url || tag.externalDocs?.description
+        ? {
+            url: tag.externalDocs.url,
+            description: tag.externalDocs.description,
+          }
+        : undefined,
   }));
 }
