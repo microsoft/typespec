759b61dcb66610c68841d54a76cd242685b19ad7
Vincent Biret <vibiret@microsoft.com>
2025-08-18 15:49:22 -0400

    fix: symbol definition in inline data definition

--- packages/openapi3/src/cli/actions/convert/utils/decorators.ts
+++ packages/openapi3/src/cli/actions/convert/utils/decorators.ts
@@ -1,3 +1,4 @@
+import { printIdentifier } from "@typespec/compiler";
 import { ExtensionKey } from "@typespec/openapi";
 import { Extensions, OpenAPI3Parameter, OpenAPI3Schema, Refable } from "../../../../types.js";
 import { TSValue, TypeSpecDecorator } from "../interfaces.js";
@@ -83,7 +84,7 @@ export function normalizeObjectValueToTSValueExpression(value: any): string {
   if (typeof value === "object" && !Array.isArray(value)) {
     return `#{${Object.entries(value)
       .map(([key, v]) => {
-        return `${key}: ${normalizeObjectValueToTSValueExpression(v)}`;
+        return `${printIdentifier(key, "disallow-reserved")}: ${normalizeObjectValueToTSValueExpression(v)}`;
       })
       .join(", ")}}`;
   } else if (Array.isArray(value)) {
