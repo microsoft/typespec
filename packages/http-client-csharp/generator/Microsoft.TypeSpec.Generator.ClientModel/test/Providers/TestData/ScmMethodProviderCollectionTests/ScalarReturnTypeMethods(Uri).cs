e9a7e74ec35c3a11e3b5653dbc6930a70f18112e
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-08-26 12:06:25 -0700

    Construct response from scalar result (#8286)

--- packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/test/Providers/TestData/ScmMethodProviderCollectionTests/ScalarReturnTypeMethods(Uri).cs
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/test/Providers/TestData/ScmMethodProviderCollectionTests/ScalarReturnTypeMethods(Uri).cs
@@ -1,4 +1,4 @@
 ﻿global::System.ClientModel.ClientResult result = this.GetScalar(cancellationToken.CanBeCanceled ? new global::System.ClientModel.Primitives.RequestOptions { CancellationToken = cancellationToken } : null);
 using global::System.Text.Json.JsonDocument document = global::System.Text.Json.JsonDocument.Parse(result.GetRawResponse().Content);
 global::System.Text.Json.JsonElement element = document.RootElement;
-return new global::System.Uri(element.GetString());
+return global::System.ClientModel.ClientResult.FromValue(new global::System.Uri(element.GetString()), result.GetRawResponse());
