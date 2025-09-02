6b8db4758d76dac8b58df2e1ef25815f3f830bd2
RodgeFu <wenjiefu@microsoft.com>
2025-09-02 17:53:33 +0800

    fix build break

--- packages/playground/src/services.ts
+++ packages/playground/src/services.ts
@@ -59,7 +59,7 @@ export async function registerMonacoLanguage(host: BrowserHost) {
       const model = monaco.editor.getModel(monaco.Uri.parse(url));
       return model ? textDocumentForModel(model) : undefined;
     },
-    sendDiagnostics() {},
+    async sendDiagnostics() {},
     log: (log) => {
       switch (log.level) {
         case "error":
