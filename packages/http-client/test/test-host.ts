265267df2d1e0b837d15b5d333e3b346fd545b1b
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2025-09-11 17:12:35 -0700

    add tests

--- packages/http-client/test/test-host.ts
+++ packages/http-client/test/test-host.ts
@@ -2,7 +2,7 @@ import { resolvePath } from "@typespec/compiler";
 import { createTester } from "@typespec/compiler/testing";
 
 export const Tester = createTester(resolvePath(import.meta.dirname, ".."), {
-  libraries: ["@typespec/http"],
+  libraries: ["@typespec/http", "@typespec/http-client"],
 })
-  .import("@typespec/http")
-  .using("Http");
+  .import("@typespec/http", "@typespec/http-client")
+  .using("Http", "HttpClient");
