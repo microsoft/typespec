55ed75af0af38a5b80ae08a4ab58abf9e3a816a3
Yuchao Yan <yuchaoyan@microsoft.com>
2025-09-15 14:11:57 +0800

    fix ci for autorest

--- packages/http-client-python/generator/pygen/codegen/serializers/builder_serializer.py
+++ packages/http-client-python/generator/pygen/codegen/serializers/builder_serializer.py
@@ -1383,13 +1383,15 @@ class _PagingOperationSerializer(_OperationSerializer[PagingOperationType]):
                 cont_token_property = "None"
             elif self.code_model.options["models-mode"] == "msrest":
                 cont_token_property = f"deserialized.{next_link_name} or None"
-            else:
+            elif builder.next_link_is_nested:
                 next_link_name_array = next_link_name.split(".")
                 access = (
                     "".join([f'.get("{i}", {{}})' for i in next_link_name_array[:-1]])
                     + f'.get("{next_link_name_array[-1]}")'
                 )
                 cont_token_property = f"deserialized{access} or None"
+            else:
+                cont_token_property = f'deserialized.get("{next_link_name}") or None'
         list_type = "AsyncList" if self.async_mode else "iter"
         retval.append(f"    return {cont_token_property}, {list_type}(list_of_elem)")
         return retval
