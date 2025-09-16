d6d6ee16289780ae3796cfd9c5f632d9677b79e6
Weidong Xu <weidxu@microsoft.com>
2025-09-16 13:43:32 +0800

    update test

--- packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/clientgenerator/core/clientlocation/ClientLocationClientTests.java
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/clientgenerator/core/clientlocation/ClientLocationClientTests.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.Test;
 
 public final class ClientLocationClientTests {
 
+    private final ClientLocationClientBuilder builder = new ClientLocationClientBuilder().storageAccount("testaccount");
+
     @Test
     public void testMoveToExistingSubClient() {
-        MoveToExistingSubUserOperationsClient userClient
-            = new ClientLocationClientBuilder().buildMoveToExistingSubUserOperationsClient();
+        MoveToExistingSubUserOperationsClient userClient = builder.buildMoveToExistingSubUserOperationsClient();
         userClient.getUser();
 
-        MoveToExistingSubAdminOperationsClient adminClient
-            = new ClientLocationClientBuilder().buildMoveToExistingSubAdminOperationsClient();
+        MoveToExistingSubAdminOperationsClient adminClient = builder.buildMoveToExistingSubAdminOperationsClient();
         // deleteUser moved to MoveToExistingSubAdminOperationsClient
         adminClient.deleteUser();
         adminClient.getAdminInfo();
@@ -24,35 +24,33 @@ public final class ClientLocationClientTests {
 
     @Test
     public void testMoveToNewSubClient() {
-        MoveToNewSubProductOperationsClient productClient
-            = new ClientLocationClientBuilder().buildMoveToNewSubProductOperationsClient();
+        MoveToNewSubProductOperationsClient productClient = builder.buildMoveToNewSubProductOperationsClient();
         productClient.listProducts();
 
-        ArchiveOperationsClient archiveClient = new ClientLocationClientBuilder().buildArchiveOperationsClient();
+        ArchiveOperationsClient archiveClient = builder.buildArchiveOperationsClient();
         // archiveProduct moved to ArchiveOperationsClient
         archiveClient.archiveProduct();
     }
 
     @Test
     public void testMoveToRootClient() {
-        MoveToRootResourceOperationsClient resourceClient
-            = new ClientLocationClientBuilder().buildMoveToRootResourceOperationsClient();
+        MoveToRootResourceOperationsClient resourceClient = builder.buildMoveToRootResourceOperationsClient();
         resourceClient.getResource();
 
-        ClientLocationClient rootClient = new ClientLocationClientBuilder().buildClient();
+        ClientLocationClient rootClient = builder.buildClient();
         // getHealthStatus moved to root client
         rootClient.getHealthStatus();
     }
 
     @Test
     public void testMoveMethodParameterToClient() {
-        MoveMethodParameterToBlobOperationsClient blobClient
-            = new ClientLocationClientBuilder().buildMoveMethodParameterToBlobOperationsClient();
+        MoveMethodParameterToBlobOperationsClient blobClient = builder.buildMoveMethodParameterToBlobOperationsClient();
 
         // Test the scenario: GET /blob?storageAccount=testaccount&container=testcontainer&blob=testblob.txt
         // Expected response: {"id": "blob-001", "name": "testblob.txt", "size": 1024, "path":
         // "/testcontainer/testblob.txt"}
-        Blob blob = blobClient.getBlob("testaccount", "testcontainer", "testblob.txt");
+        // "testaccount" moved to client
+        Blob blob = blobClient.getBlob("testcontainer", "testblob.txt");
 
         // Verify the Blob model structure
         Assertions.assertNotNull(blob);
