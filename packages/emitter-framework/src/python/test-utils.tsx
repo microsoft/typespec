d671338261844721507107a384b7d65cd329fa30
Maurício Gardini <mauriciogardini@users.noreply.github.com>
2025-09-30 18:18:57 -0300

    Add `EnumDeclaration` implementation (#46)
    * Add `EnumDeclaration` implementation
    
    Add EnumDeclaration implementation to convert TSP enums into Python enums.
    
    While looking at the TS implementation, we noticed that it also handles Unions, converting them into enums as well, so we added that to the implementation as well.
    
    * Make changes to reflect alloy changes
    
    ---------
    
    Co-authored-by: Steve Rice <srice@pinterest.com>

--- packages/emitter-framework/src/python/test-utils.tsx
+++ packages/emitter-framework/src/python/test-utils.tsx
@@ -1,18 +1,18 @@
 import { Output } from "#core/components/index.js";
 import { type Children } from "@alloy-js/core";
-import { createPythonNamePolicy, SourceFile } from "@alloy-js/python";
+import * as py from "@alloy-js/python";
 import type { Program } from "@typespec/compiler";
 import { datetimeModule, decimalModule, typingModule } from "./builtins.js";
 
 export function getOutput(program: Program, children: Children[]): Children {
-  const policy = createPythonNamePolicy();
+  const policy = py.createPythonNamePolicy();
   return (
     <Output
       program={program}
-      externals={[datetimeModule, decimalModule, typingModule]}
+      externals={[datetimeModule, decimalModule, typingModule, py.enumModule]}
       namePolicy={policy}
     >
-      <SourceFile path="test.py">{children}</SourceFile>
+      <py.SourceFile path="test.py">{children}</py.SourceFile>
     </Output>
   );
 }
