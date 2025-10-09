39fa98eb0198e0153d90526f6fc072a8645d19e6
Weidong Xu <weidxu@microsoft.com>
2025-10-09 11:06:41 +0800

    add test but disabled

---
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/versioning/previewversion/PreviewVersionTests.java
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azure.versioning.previewversion;
+
+import azure.versioning.previewversion.models.UpdateWidgetColorRequest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class PreviewVersionTests {
+
+    private static final String widgetId = "widget-123";
+
+    @Disabled
+    @Test
+    public void testPreviewClient() {
+        PreviewVersionClient client = new PreviewVersionClientBuilder().buildClient();
+
+        client.getWidget(widgetId);
+
+        client.updateWidgetColor(widgetId, new UpdateWidgetColorRequest().setColor("red"));
+    }
+
+    @Disabled
+    @Test
+    public void testStableClient() {
+        PreviewVersionClient client
+            = new PreviewVersionClientBuilder().serviceVersion(PreviewVersionServiceVersion.V2024_06_01).buildClient();
+
+        client.listWidgets("test", null);
+    }
+}
