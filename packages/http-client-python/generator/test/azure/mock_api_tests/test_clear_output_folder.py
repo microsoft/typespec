6124c496f1a9442468c5024042d88e9360de7f2c
Yuchao Yan <yuchaoyan@microsoft.com>
2025-10-11 13:42:10 +0800

    add clear output folder logic for tsp

---
+++ packages/http-client-python/generator/test/azure/mock_api_tests/test_clear_output_folder.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from pathlib import Path
+
+GENERATED_PATH = Path(__file__).parent.resolve() / "generated"
+
+
+def test_clear_output_folder():
+    assert not (
+        GENERATED_PATH / "authentication-api-key/authentication/apiKey/_operations/to_be_deleted.py"
+    ).exists(), "File to_be_deleted.py should be deleted after regeneration"
