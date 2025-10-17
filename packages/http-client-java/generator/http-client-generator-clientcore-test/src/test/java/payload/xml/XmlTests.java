a8645eac167a290f0ed82cdcf8e46e8764ae8506
Weidong Xu <weidxu@microsoft.com>
2025-10-17 09:44:35 +0800

    enable xml test

--- packages/http-client-java/generator/http-client-generator-clientcore-test/src/test/java/payload/xml/XmlTests.java
+++ packages/http-client-java/generator/http-client-generator-clientcore-test/src/test/java/payload/xml/XmlTests.java
@@ -6,6 +6,8 @@ package payload.xml;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public final class XmlTests {
@@ -16,9 +18,9 @@ public final class XmlTests {
 
         client.put(new SimpleModel("foo", 123));
 
-//        SimpleModel model = client.get();
-//        Assertions.assertEquals("foo", model.getName());
-//        Assertions.assertEquals(123, model.getAge());
+        SimpleModel model = client.get();
+        Assertions.assertEquals("foo", model.getName());
+        Assertions.assertEquals(123, model.getAge());
     }
 
     @Test
@@ -27,9 +29,9 @@ public final class XmlTests {
 
         client.put(new ModelWithSimpleArrays(List.of("red", "green", "blue"), List.of(1, 2)));
 
-//        ModelWithSimpleArrays model = client.get();
-//        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
-//        Assertions.assertEquals(List.of(1, 2), model.getCounts());
+        ModelWithSimpleArrays model = client.get();
+        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
+        Assertions.assertEquals(List.of(1, 2), model.getCounts());
     }
 
     @Test
@@ -38,12 +40,12 @@ public final class XmlTests {
 
         client.put(new ModelWithArrayOfModel(List.of(new SimpleModel("foo", 123), new SimpleModel("bar", 456))));
 
-//        ModelWithArrayOfModel model = client.get();
-//        Assertions.assertEquals(2, model.getItems().size());
-//        Assertions.assertEquals("foo", model.getItems().get(0).getName());
-//        Assertions.assertEquals(123, model.getItems().get(0).getAge());
-//        Assertions.assertEquals("bar", model.getItems().get(1).getName());
-//        Assertions.assertEquals(456, model.getItems().get(1).getAge());
+        ModelWithArrayOfModel model = client.get();
+        Assertions.assertEquals(2, model.getItems().size());
+        Assertions.assertEquals("foo", model.getItems().get(0).getName());
+        Assertions.assertEquals(123, model.getItems().get(0).getAge());
+        Assertions.assertEquals("bar", model.getItems().get(1).getName());
+        Assertions.assertEquals(456, model.getItems().get(1).getAge());
     }
 
     @Test
@@ -52,8 +54,8 @@ public final class XmlTests {
 
         client.put(new ModelWithOptionalField("widget"));
 
-//        ModelWithOptionalField model = client.get();
-//        Assertions.assertEquals("widget", model.getItem());
+        ModelWithOptionalField model = client.get();
+        Assertions.assertEquals("widget", model.getItem());
     }
 
     @Test
@@ -62,10 +64,10 @@ public final class XmlTests {
 
         client.put(new ModelWithAttributes(123, "foo", true));
 
-//        ModelWithAttributes model = client.get();
-//        Assertions.assertEquals(123, model.getId1());
-//        Assertions.assertEquals("foo", model.getId2());
-//        Assertions.assertEquals(true, model.isEnabled());
+        ModelWithAttributes model = client.get();
+        Assertions.assertEquals(123, model.getId1());
+        Assertions.assertEquals("foo", model.getId2());
+        Assertions.assertEquals(true, model.isEnabled());
     }
 
     @Test
@@ -74,9 +76,9 @@ public final class XmlTests {
 
         client.put(new ModelWithUnwrappedArray(List.of("red", "green", "blue"), List.of(1, 2)));
 
-//        ModelWithUnwrappedArray model = client.get();
-//        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
-//        Assertions.assertEquals(List.of(1, 2), model.getCounts());
+        ModelWithUnwrappedArray model = client.get();
+        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
+        Assertions.assertEquals(List.of(1, 2), model.getCounts());
     }
 
     @Test
@@ -85,9 +87,9 @@ public final class XmlTests {
 
         client.put(new ModelWithRenamedArrays(List.of("red", "green", "blue"), List.of(1, 2)));
 
-//        ModelWithRenamedArrays model = client.get();
-//        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
-//        Assertions.assertEquals(List.of(1, 2), model.getCounts());
+        ModelWithRenamedArrays model = client.get();
+        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
+        Assertions.assertEquals(List.of(1, 2), model.getCounts());
     }
 
     @Test
@@ -96,11 +98,11 @@ public final class XmlTests {
 
         client.put(new ModelWithRenamedFields(new SimpleModel("foo", 123), new SimpleModel("bar", 456)));
 
-//        ModelWithRenamedFields model = client.get();
-//        Assertions.assertEquals("foo", model.getInputData().getName());
-//        Assertions.assertEquals(123, model.getInputData().getAge());
-//        Assertions.assertEquals("bar", model.getOutputData().getName());
-//        Assertions.assertEquals(456, model.getOutputData().getAge());
+        ModelWithRenamedFields model = client.get();
+        Assertions.assertEquals("foo", model.getInputData().getName());
+        Assertions.assertEquals(123, model.getInputData().getAge());
+        Assertions.assertEquals("bar", model.getOutputData().getName());
+        Assertions.assertEquals(456, model.getOutputData().getAge());
     }
 
     @Test
@@ -109,8 +111,8 @@ public final class XmlTests {
 
         client.put(new ModelWithEmptyArray(Collections.emptyList()));
 
-//        ModelWithEmptyArray model = client.get();
-//        Assertions.assertTrue(model.getItems().isEmpty());
+        ModelWithEmptyArray model = client.get();
+        Assertions.assertTrue(model.getItems() == null || model.getItems().isEmpty());
     }
 
     @Test
@@ -119,9 +121,9 @@ public final class XmlTests {
 
         client.put(new ModelWithText("foo", "\n  This is some text.\n"));
 
-//        ModelWithText model = client.get();
-//        Assertions.assertEquals("foo", model.getLanguage());
-//        Assertions.assertEquals("\n  This is some text.\n", model.getContent());
+        ModelWithText model = client.get();
+        Assertions.assertEquals("foo", model.getLanguage());
+        Assertions.assertEquals("\n  This is some text.\n", model.getContent());
     }
 
     @Test
@@ -130,10 +132,10 @@ public final class XmlTests {
 
         client.put(new ModelWithDictionary(Map.of("Color", "blue", "Count", "123", "Enabled", "false")));
 
-//        ModelWithDictionary model = client.get();
-//        Assertions.assertEquals("blue", model.getMetadata().get("Color"));
-//        Assertions.assertEquals("123", model.getMetadata().get("Count"));
-//        Assertions.assertEquals("false", model.getMetadata().get("Enabled"));
+        ModelWithDictionary model = client.get();
+        Assertions.assertEquals("blue", model.getMetadata().get("Color"));
+        Assertions.assertEquals("123", model.getMetadata().get("Count"));
+        Assertions.assertEquals("false", model.getMetadata().get("Enabled"));
     }
 
     @Test
@@ -142,9 +144,9 @@ public final class XmlTests {
 
         client.put(new ModelWithEncodedNames(new SimpleModel("foo", 123), List.of("red", "green", "blue")));
 
-//        ModelWithEncodedNames model = client.get();
-//        Assertions.assertEquals("foo", model.getModelData().getName());
-//        Assertions.assertEquals(123, model.getModelData().getAge());
-//        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
+        ModelWithEncodedNames model = client.get();
+        Assertions.assertEquals("foo", model.getModelData().getName());
+        Assertions.assertEquals(123, model.getModelData().getAge());
+        Assertions.assertEquals(List.of("red", "green", "blue"), model.getColors());
     }
 }
