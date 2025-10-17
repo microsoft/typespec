43d5420b990ea58c88dfbd82c928b07a07bb7efb
Timothee Guerin <tiguerin@microsoft.com>
2025-10-17 10:39:56 -0700

    Fix doc multi line in parameters

--- packages/openapi3/test/tsp-openapi3/servers.test.ts
+++ packages/openapi3/test/tsp-openapi3/servers.test.ts
@@ -36,3 +36,47 @@ it("render multi line description", async () => {
 
   await validateTsp(tsp);
 });
+it("render multi line description for parameters", async () => {
+  const tsp = await renderTypeSpecForOpenAPI3({
+    servers: [
+      {
+        url: "https://example.com/api/{region}",
+        variables: {
+          region: {
+            default: "us",
+            description: "Some\nMulti\nline\nDescription",
+          },
+        },
+      },
+    ],
+  });
+
+  expect(tsp).toMatchInlineSnapshot(`
+    "import "@typespec/http";
+    import "@typespec/openapi";
+    import "@typespec/openapi3";
+
+    using Http;
+    using OpenAPI;
+
+    @service(#{ title: "Test Service" })
+    @info(#{ version: "1.0.0" })
+    @server(
+      "https://example.com/api/{region}",
+      "",
+      {
+        /**
+         * Some
+         * Multi
+         * line
+         * Description
+         */
+        region: string = "us",
+      }
+    )
+    namespace TestService;
+    "
+  `);
+
+  await validateTsp(tsp);
+});
