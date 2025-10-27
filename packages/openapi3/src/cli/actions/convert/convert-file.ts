baf11bc3a31b97db5d2edb2c84aee03e4a973842
Vincent Biret <vibiret@microsoft.com>
2025-10-27 14:27:01 -0400

    chore: removes dereference to avoid inlining of schemas
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/openapi3/src/cli/actions/convert/convert-file.ts
+++ packages/openapi3/src/cli/actions/convert/convert-file.ts
@@ -1,7 +1,7 @@
 import { bundle } from "@scalar/json-magic/bundle";
 import { fetchUrls, parseJson, parseYaml } from "@scalar/json-magic/bundle/plugins/browser";
 import { readFiles } from "@scalar/json-magic/bundle/plugins/node";
-import { dereference } from "@scalar/openapi-parser";
+import { OpenAPI } from "@scalar/openapi-types";
 import { formatTypeSpec, resolvePath } from "@typespec/compiler";
 import { OpenAPI3Document } from "../../../types.js";
 import { CliHost } from "../../types.js";
@@ -14,15 +14,14 @@ import { createContext } from "./utils/context.js";
 export async function convertAction(host: CliHost, args: ConvertCliArgs) {
   // attempt to read the file
   const fullPath = resolvePath(process.cwd(), args.path);
-  const data = await bundle(fullPath, {
+  const data = (await bundle(fullPath, {
     plugins: [readFiles(), fetchUrls(), parseYaml(), parseJson()],
     treeShake: false,
-  });
-  const { schema } = await dereference(data);
-  if (!schema) {
+  })) as OpenAPI.Document;
+  if (!data) {
     throw new Error("Failed to dereference OpenAPI document");
   }
-  const context = createContext(schema as OpenAPI3Document, console, args.namespace);
+  const context = createContext(data as OpenAPI3Document, console, args.namespace);
   const program = transform(context);
   let mainTsp: string;
   try {
