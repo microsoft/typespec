25fbc505451aed2a62fff741ea108fdd70c07cab
RodgeFu <wenjiefu@microsoft.com>
2025-10-30 14:16:30 +0800

    update private fields

--- packages/compiler/src/server/update-manager.ts
+++ packages/compiler/src/server/update-manager.ts
@@ -254,26 +254,26 @@ export function debounceThrottle<T, P>(
 }
 
 class DeferredPromise<T> {
-  private promise: Promise<T>;
-  private resolve!: (value: T) => void;
-  private reject!: (reason?: any) => void;
+  #promise: Promise<T>;
+  #resolve!: (value: T) => void;
+  #reject!: (reason?: any) => void;
 
   constructor() {
-    this.promise = new Promise<T>((res, rej) => {
-      this.resolve = res;
-      this.reject = rej;
+    this.#promise = new Promise<T>((res, rej) => {
+      this.#resolve = res;
+      this.#reject = rej;
     });
   }
 
   getPromise(): Promise<T> {
-    return this.promise;
+    return this.#promise;
   }
 
   resolvePromise(value: T) {
-    this.resolve(value);
+    this.#resolve(value);
   }
 
   rejectPromise(reason?: any) {
-    this.reject(reason);
+    this.#reject(reason);
   }
 }
