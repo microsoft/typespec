c3771ebe0e28fd352de4f96e3ebbe1e0c76d5971
Yuchao Yan <yuchaoyan@microsoft.com>
2025-11-04 10:12:32 +0000

    fix ci

--- packages/http-client-python/generator/test/unittests/test_docstring.py
+++ packages/http-client-python/generator/test/unittests/test_docstring.py
@@ -35,8 +35,10 @@ def string_to_hash_id(input_string):
 
 
 def test_docstring_generation():
+    import azure.docstring
+
     with open(
-        _FILE_PATH.parent.parent / "azure/generated/docstring/docstring/models/_models.py", "r", encoding="utf-8"
+        _FILE_PATH.parent.parent / "azure/generated/docstring/azure/docstring/models/_models.py", "r", encoding="utf-8"
     ) as f:
         content = f.read()
         hash_id = string_to_hash_id(content)
