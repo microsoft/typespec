bb883df168406146dfcfa472ff586e61f8616718
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-11-10 15:16:16 -0800

    Do not apply propagation for properties with no wire info (#8970)
    Fix to https://github.com/microsoft/typespec/pull/8958

---
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/test/Providers/MrwSerializationTypeDefinitions/TestData/DynamicModelSerializationTests/PropagateCustomizedDynamicPropertyMixed/DynamicModel.cs
@@ -0,0 +1,16 @@
+﻿using SampleTypeSpec;
+
+namespace Sample.Models
+{
+    public partial class DynamicModel
+    {
+        [CodeGenMember("Prop1")]
+        public Foo Prop2 { get; set; }
+
+        public Foo SomePropertyWithNoWireInfo { get; set; }
+    }
+
+    public partial class Foo
+    {
+    }
+}
