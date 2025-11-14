4533aa484611f47af35f3def4a89b3f1378a2efa
Zhonglei Ma <v-zhongleima@microsoft.com>
2025-11-14 10:22:07 +0800

    updated

--- packages/compiler/src/server/serverlib.ts
+++ packages/compiler/src/server/serverlib.ts
@@ -165,20 +165,7 @@ export function createServer(
   const signatureHelpUpdateManager = new UpdateManager<CompileResult | undefined>(
     "signature-help",
     log,
-    () => [
-      {
-        frequencyInWindow: 20,
-        delay: 100,
-      },
-      {
-        frequencyInWindow: 10,
-        delay: 80,
-      },
-      {
-        frequencyInWindow: 0,
-        delay: 50,
-      },
-    ],
+    () => 0,
   );
 
   const compileService = createCompileService({
