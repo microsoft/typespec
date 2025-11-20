8fe8c23d79866af28c0b4397f3f75cc01f5109e0
Timothee Guerin <tiguerin@microsoft.com>
2025-11-20 17:18:57 -0500

    Upgrade dependencies 2025 (#9046)
    Azure pr https://github.com/Azure/typespec-azure/pull/3546

--- packages/compiler/src/init/prompts.ts
+++ packages/compiler/src/init/prompts.ts
@@ -4,11 +4,13 @@ import pc from "picocolors";
 export function checkbox<Value>(config: Parameters<typeof inquirerCheckbox<Value>>[0]) {
   return inquirerCheckbox({
     ...config,
-    instructions: pc.gray(
-      ` (Press ${pc.cyan("space")} to select, ${pc.cyan("a")} to toggle all, ${pc.cyan("i")} to invert selection and ${pc.cyan("enter")} to proceed.)`,
-    ),
     theme: {
       ...config.theme,
+      style: {
+        keysHelpTip: pc.gray(
+          ` (Press ${pc.cyan("space")} to select, ${pc.cyan("a")} to toggle all, ${pc.cyan("i")} to invert selection and ${pc.cyan("enter")} to proceed.)`,
+        ),
+      },
       icon: {
         unchecked: pc.cyan(" ◯"),
         checked: pc.green(" ◉"),
