ad3c909513bf857fa903303fee81ea84b7993ad4
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2025-11-21 12:54:56 -0500

    Finish regeneration

--- packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/template/FluentLiveTestsTemplate.java
+++ packages/http-client-java/generator/http-client-generator-mgmt/src/main/java/com/microsoft/typespec/http/client/generator/mgmt/template/FluentLiveTestsTemplate.java
@@ -3,7 +3,6 @@
 
 package com.microsoft.typespec.http.client.generator.mgmt.template;
 
-import com.azure.core.util.CoreUtils;
 import com.microsoft.typespec.http.client.generator.core.model.clientmodel.ClassType;
 import com.microsoft.typespec.http.client.generator.core.model.clientmodel.examplemodel.ExampleHelperFeature;
 import com.microsoft.typespec.http.client.generator.core.model.javamodel.JavaFile;
@@ -13,6 +12,7 @@ import com.microsoft.typespec.http.client.generator.mgmt.model.clientmodel.Fluen
 import com.microsoft.typespec.http.client.generator.mgmt.model.clientmodel.FluentLiveTestCase;
 import com.microsoft.typespec.http.client.generator.mgmt.model.clientmodel.FluentLiveTestStep;
 import com.microsoft.typespec.http.client.generator.mgmt.model.clientmodel.FluentLiveTests;
+import io.clientcore.core.utils.CoreUtils;
 import java.util.ArrayList;
 
 public class FluentLiveTestsTemplate {
