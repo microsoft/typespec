microsoft/typespec

commit 51ed31e2a421143b806d90e059b7cf1cb2885af0

Overhaul handling of V1 vs V2 vs ClientCore code generation (#9059)
This PR overhauls code generation for different flavors of V1, V2, and
ClientCore by centralizing almost all branching logic for dependency
imports into `Annotation` and `ClassType`. This is done by having all
usages of Azure Core and ClientCore types be imported using `Annotation`
and `ClassType`, and moving forward any new imports being class usages
being added for Azure Core or ClientCore types should go through those
centralized handlers.

Additionally, dependency references to azure-core and
azure-core-management were removed and all usages of types from those
packages are done manually when creating the specific `Annotation` or
`ClassType`. This shrinks the shipped generator size by a few MB but
more importantly starts moving towards a spot where the generator isn't
tightly coupled with a specific generation target. `io.clientcore:core`
is still a hard dependency as it's needed for implementation of the
generator itself.

Since, `azure-core` and `azure-core-management` are no longer
dependencies, the dependency on Reactor Core was also removed.

---------

Co-authored-by: Weidong Xu <weidxu@microsoft.com>

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/model/clientmodel/GenericType.java
@@ -3,9 +3,7 @@
 
 package com.microsoft.typespec.http.client.generator.core.model.clientmodel;
 
-import com.microsoft.typespec.http.client.generator.core.extension.plugin.JavaSettings;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,7 +11,7 @@ import java.util.stream.Collectors;
  * A generic type that is used by the client.
  */
 public class GenericType implements IType {
-    public static final GenericType FLUX_BYTE_BUFFER = Flux(ClassType.BYTE_BUFFER);
+    public static final GenericType FLUX_BYTE_BUFFER = flux(ClassType.BYTE_BUFFER);
     /**
      * The main non-generic type of this generic type.
      */
@@ -40,87 +38,58 @@ public class GenericType implements IType {
     }
 
     public GenericType(String packageKeyword, String name, String jsonToken, IType... typeArguments) {
-        if (!JavaSettings.getInstance().isAzureV1()) {
-            if (Objects.equals(packageKeyword + "." + name, com.azure.core.http.rest.Response.class.getName())) {
-                packageKeyword = "io.clientcore.core.http";
-            } else {
-                packageKeyword = packageKeyword.replace(ExternalPackage.AZURE_CORE_PACKAGE_NAME,
-                    ExternalPackage.CLIENTCORE_PACKAGE_NAME);
-            }
-        }
-
         this.name = name;
         this.packageName = packageKeyword;
         this.typeArguments = typeArguments;
         this.jsonToken = jsonToken;
     }
 
-    public static GenericType Flux(IType typeArgument) {
-        return new GenericType("reactor.core.publisher", "Flux", typeArgument);
+    public static GenericType flux(IType typeArgument) {
+        return genericType(ClassType.FLUX, typeArgument);
     }
 
-    public static GenericType Mono(IType typeArgument) {
-        return new GenericType("reactor.core.publisher", "Mono", typeArgument);
+    public static GenericType mono(IType typeArgument) {
+        return genericType(ClassType.MONO, typeArgument);
     }
 
-    public static GenericType OperationStatus(IType typeArgument) {
-        return new GenericType("com.microsoft.azure.v3", "OperationStatus", typeArgument);
+    public static GenericType response(IType bodyType) {
+        return genericType(ClassType.RESPONSE, bodyType);
     }
 
-    public static GenericType Page(IType elementType) {
-        return new GenericType("com.microsoft.azure.v3", "Page", elementType);
+    public static GenericType restResponse(IType headersType, IType bodyType) {
+        return genericType(ClassType.RESPONSE_BASE, headersType, bodyType);
     }
 
-    public static GenericType PagedList(IType elementType) {
-        return new GenericType("com.microsoft.azure.v3", "PagedList", elementType);
+    public static GenericType pagedResponse(IType bodyType) {
+        return genericType(ClassType.PAGED_RESPONSE, bodyType);
     }
 
-    public static GenericType Response(IType bodyType) {
-        return new GenericType(ClassType.RESPONSE.getPackage(), ClassType.RESPONSE.getName(), bodyType);
+    public static GenericType pagedFlux(IType bodyType) {
+        return genericType(ClassType.PAGED_FLUX, bodyType);
     }
 
-    public static GenericType RestResponse(IType headersType, IType bodyType) {
-        return new GenericType("com.azure.core.http.rest", "ResponseBase", headersType, bodyType);
+    public static GenericType pagedIterable(IType bodyType) {
+        return genericType(ClassType.PAGED_ITERABLE, bodyType);
     }
 
-    public static GenericType PagedResponse(IType bodyType) {
-        if (JavaSettings.getInstance().isAzureV1()) {
-            return new GenericType("com.azure.core.http.rest", "PagedResponse", bodyType);
-        } else {
-            return new GenericType("io.clientcore.core.http.paging", "PagedResponse", bodyType);
-        }
+    public static GenericType function(IType inputType, IType outputType) {
+        return genericType(ClassType.FUNCTION, inputType, outputType);
     }
 
-    public static GenericType PagedFlux(IType bodyType) {
-        return new GenericType("com.azure.core.http.rest", "PagedFlux", bodyType);
+    public static GenericType pollerFlux(IType pollResultType, IType finalResultType) {
+        return genericType(ClassType.POLLER_FLUX, pollResultType, finalResultType);
     }
 
-    public static GenericType PagedIterable(IType bodyType) {
-        if (JavaSettings.getInstance().isAzureV1()) {
-            return new GenericType("com.azure.core.http.rest", "PagedIterable", bodyType);
-        } else {
-            return new GenericType("io.clientcore.core.http.paging", "PagedIterable", bodyType);
-        }
+    public static GenericType syncPoller(IType pollResultType, IType finalResultType) {
+        return genericType(ClassType.SYNC_POLLER, pollResultType, finalResultType);
     }
 
-    public static GenericType Function(IType inputType, IType outputType) {
-        return new GenericType("java.util", "Function", inputType, outputType);
+    public static GenericType pollResult(IType pollResultType) {
+        return genericType(ClassType.POLL_RESULT, pollResultType);
     }
 
-    public static GenericType PollerFlux(IType pollResultType, IType finalResultType) {
-        return new GenericType("com.azure.core.util.polling", "PollerFlux", pollResultType, finalResultType);
-    }
-
-    public static GenericType SyncPoller(IType pollResultType, IType finalResultType) {
-        return new GenericType("com.azure.core.util.polling", "SyncPoller", pollResultType, finalResultType);
-    }
-
-    public static GenericType AzureVNextPoller(IType pollResultType, IType finalResultType) {
-        return new GenericType("com.azure.v2.core.http.polling", "Poller", pollResultType, finalResultType);
-    }
-
-    public static GenericType PollResult(IType pollResultType) {
-        return new GenericType("com.azure.core.management.polling", "PollResult", pollResultType);
+    private static GenericType genericType(ClassType wrapper, IType... types) {
+        return new GenericType(wrapper.getPackage(), wrapper.getName(), types);
     }
 
     public final String getName() {
@@ -230,45 +199,14 @@ public class GenericType implements IType {
     }
 
     public final String convertToClientType(String expression) {
-        if (this == getClientType()) {
-            return expression;
-        }
-
-        IType[] wireTypeArguments = getTypeArguments();
-        IType[] clientTypeArguments = Arrays.stream(wireTypeArguments).map(IType::getClientType).toArray(IType[]::new);
-
-        for (int i = 0; i < clientTypeArguments.length; ++i) {
-            if (clientTypeArguments[i] != wireTypeArguments[i]) {
-                if (this instanceof ListType) {
-                    expression
-                        = String.format("%1$s.stream().map(el -> %2$s).collect(java.util.stream.Collectors.toList())",
-                            expression, wireTypeArguments[i].convertToClientType("el"));
-                } else if (this instanceof IterableType) {
-                    expression = String.format(
-                        "java.util.stream.StreamSupport.stream(%1$s.spliterator(), false).map"
-                            + "(el -> %2$s).collect(java.util.stream.Collectors.toList())",
-                        expression, wireTypeArguments[i].convertToClientType("el"));
-                } else if (this instanceof MapType) {
-                    // Key is always String in Swagger 2
-                    expression = String.format(
-                        "%1$s.entrySet().stream().collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, el -> %2$s))",
-                        expression, wireTypeArguments[i].convertToClientType("el.getValue()"));
-                } else if (this.getPackage().equals("io.reactivex")) {
-                    expression = String.format("%1$s.map(el => %2$s)", expression,
-                        wireTypeArguments[0].convertToClientType("el"));
-                } else {
-                    throw new UnsupportedOperationException(
-                        String.format("Instance %1$s of generic type %2$s not supported for conversion to client type.",
-                            expression, toString()));
-                }
-                break;
-            }
-        }
-
-        return expression;
+        return convert(expression, true);
     }
 
     public final String convertFromClientType(String expression) {
+        return convert(expression, false);
+    }
+
+    private String convert(String expression, boolean isToClient) {
         if (this == getClientType()) {
             return expression;
         }
@@ -279,26 +217,30 @@ public class GenericType implements IType {
         for (int i = 0; i < clientTypeArguments.length; ++i) {
             if (clientTypeArguments[i] != wireTypeArguments[i]) {
                 if (this instanceof ListType) {
+                    String mapping = isToClient
+                        ? wireTypeArguments[i].convertToClientType("el")
+                        : wireTypeArguments[i].convertFromClientType("el");
                     expression
                         = String.format("%1$s.stream().map(el -> %2$s).collect(java.util.stream.Collectors.toList())",
-                            expression, wireTypeArguments[i].convertFromClientType("el"));
+                            expression, mapping);
                 } else if (this instanceof IterableType) {
-                    expression = String.format(
-                        "java.util.stream.StreamSupport.stream(%1$s.spliterator(), false).map"
-                            + "(el -> %2$s).collect(java.util.stream.Collectors.toList())",
-                        expression, wireTypeArguments[i].convertFromClientType("el"));
+                    String mapping = isToClient
+                        ? wireTypeArguments[i].convertToClientType("el")
+                        : wireTypeArguments[i].convertFromClientType("el");
+                    expression = String.format("java.util.stream.StreamSupport.stream(%1$s.spliterator(), false).map"
+                        + "(el -> %2$s).collect(java.util.stream.Collectors.toList())", expression, mapping);
                 } else if (this instanceof MapType) {
+                    String mapping = isToClient
+                        ? wireTypeArguments[i].convertToClientType("el")
+                        : wireTypeArguments[i].convertFromClientType("el");
                     // Key is always String in Swagger 2
                     expression = String.format(
                         "%1$s.entrySet().stream().collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, el -> %2$s))",
-                        expression, wireTypeArguments[i].convertFromClientType("el.getValue()"));
-                } else if (this.getPackage().equals("io.reactivex")) {
-                    expression = String.format("%1$s.map(el => %2$s)", expression,
-                        wireTypeArguments[0].convertFromClientType("el"));
+                        expression, mapping);
                 } else {
                     throw new UnsupportedOperationException(String.format(
-                        "Instance %1$s of generic type %2$s not supported for conversion from client type.", expression,
-                        toString()));
+                        "Instance %1$s of generic type %2$s not supported for conversion %3$s client type.", expression,
+                        this, isToClient ? "to" : "from"));
                 }
                 break;
             }

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/template/TemplateHelper.java
@@ -3,7 +3,6 @@
 
 package com.microsoft.typespec.http.client.generator.core.template;
 
-import com.azure.core.util.CoreUtils;
 import com.microsoft.typespec.http.client.generator.core.Javagen;
 import com.microsoft.typespec.http.client.generator.core.extension.model.codemodel.Scheme;
 import com.microsoft.typespec.http.client.generator.core.extension.plugin.JavaSettings;
@@ -13,6 +12,7 @@ import com.microsoft.typespec.http.client.generator.core.model.clientmodel.Pipel
 import com.microsoft.typespec.http.client.generator.core.model.clientmodel.SecurityInfo;
 import com.microsoft.typespec.http.client.generator.core.model.javamodel.JavaBlock;
 import io.clientcore.core.serialization.json.JsonWriter;
+import io.clientcore.core.utils.CoreUtils;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/template/util/ModelTemplateHeaderHelper.java
@@ -3,9 +3,6 @@
 
 package com.microsoft.typespec.http.client.generator.core.template.util;
 
-import com.azure.core.http.HttpHeaderName;
-import com.azure.core.http.HttpHeaders;
-import com.azure.core.util.CoreUtils;
 import com.microsoft.typespec.http.client.generator.core.extension.plugin.JavaSettings;
 import com.microsoft.typespec.http.client.generator.core.model.clientmodel.ArrayType;
 import com.microsoft.typespec.http.client.generator.core.model.clientmodel.ClassType;
@@ -22,6 +19,8 @@ import com.microsoft.typespec.http.client.generator.core.model.javamodel.JavaMod
 import com.microsoft.typespec.http.client.generator.core.model.javamodel.JavaVisibility;
 import com.microsoft.typespec.http.client.generator.core.template.ModelTemplate;
 import com.microsoft.typespec.http.client.generator.core.util.CodeNamer;
+import io.clientcore.core.http.models.HttpHeaderName;
+import io.clientcore.core.utils.CoreUtils;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -31,7 +30,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * Utility class for {@link ModelTemplate} that handles generating {@link HttpHeaders} deserialization to POJOs.
+ * Utility class for {@link ModelTemplate} that handles generating {@code HttpHeaders} deserialization to POJOs.
  */
 public final class ModelTemplateHeaderHelper {
     private static final Map<String, String> HEADER_TO_KNOWN_HTTPHEADERNAME;
@@ -39,21 +38,6 @@ public final class ModelTemplateHeaderHelper {
 
     static {
         Map<String, String> headerToKnownHttpHeaderName = new TreeMap<>(String::compareToIgnoreCase);
-        for (Field httpHeaderNameConstant : HttpHeaderName.class.getDeclaredFields()) {
-            if (httpHeaderNameConstant.getType() != HttpHeaderName.class
-                || !isPublicConstant(httpHeaderNameConstant.getModifiers())) {
-                continue;
-            }
-
-            try {
-                HttpHeaderName httpHeaderName = (HttpHeaderName) httpHeaderNameConstant.get(null);
-                String constantName = httpHeaderNameConstant.getName();
-                headerToKnownHttpHeaderName.put(httpHeaderName.getCaseInsensitiveName(), constantName);
-            } catch (IllegalAccessException ignored) {
-                // Do nothing.
-            }
-        }
-
         Map<String, String> clientCoreHeaderToKnownHttpHeaderName = new TreeMap<>(String::compareToIgnoreCase);
         for (Field httpHeaderNameConstant : io.clientcore.core.http.models.HttpHeaderName.class.getDeclaredFields()) {
             if (httpHeaderNameConstant.getType() != io.clientcore.core.http.models.HttpHeaderName.class
@@ -65,18 +49,31 @@ public final class ModelTemplateHeaderHelper {
                 io.clientcore.core.http.models.HttpHeaderName httpHeaderName
                     = (io.clientcore.core.http.models.HttpHeaderName) httpHeaderNameConstant.get(null);
                 String constantName = httpHeaderNameConstant.getName();
+                headerToKnownHttpHeaderName.put(httpHeaderName.getCaseInsensitiveName(), constantName);
                 clientCoreHeaderToKnownHttpHeaderName.put(httpHeaderName.getCaseInsensitiveName(), constantName);
             } catch (IllegalAccessException ignored) {
                 // Do nothing.
             }
         }
 
+        // ClientCore and azure-core have the same HttpHeaderNames for the most part, so just use ClientCore's
+        // HttpHeaderNames for both maps. Then add the few extra azure-core headers here.
+        // If HttpHeaderNames are missed from azure-core, they will be handled as unknown headers, which has effectively
+        // no difference from this optimization other than code bloat.
+        headerToKnownHttpHeaderName.put("Azure-AsyncOperation", "AZURE_ASYNC_OPERATION");
+        headerToKnownHttpHeaderName.put("retry-after-ms", "RETRY_AFTER_MS");
+        headerToKnownHttpHeaderName.put("x-ms-client-id", "X_MS_CLIENT_ID");
+        headerToKnownHttpHeaderName.put("x-ms-client-request-id", "X_MS_CLIENT_REQUEST_ID");
+        headerToKnownHttpHeaderName.put("x-ms-date", "X_MS_DATE");
+        headerToKnownHttpHeaderName.put("x-ms-request-id", "X_MS_REQUEST_ID");
+        headerToKnownHttpHeaderName.put("x-ms-retry-after-ms", "X_MS_RETRY_AFTER_MS");
+
         HEADER_TO_KNOWN_HTTPHEADERNAME = Collections.unmodifiableMap(headerToKnownHttpHeaderName);
         CLIENTCORE_HEADER_TO_KNOWN_HTTPHEADERNAME = Collections.unmodifiableMap(clientCoreHeaderToKnownHttpHeaderName);
     }
 
     /**
-     * Adds an {@link HttpHeaders}-based constructor to a model.
+     * Adds an {@code HttpHeaders}-based constructor to a model.
      *
      * @param classBlock The class block for the model.
      * @param model The model itself.
@@ -127,7 +124,6 @@ public final class ModelTemplateHeaderHelper {
         // match the init logic of HEADER_TO_KNOWN_HTTPHEADERNAME
         String caseInsensitiveName = HttpHeaderName.fromString(headerName).getCaseInsensitiveName();
         if (JavaSettings.getInstance().isAzureV1()) {
-
             if (HEADER_TO_KNOWN_HTTPHEADERNAME.containsKey(caseInsensitiveName)) {
                 // known name
                 return "HttpHeaderName." + HEADER_TO_KNOWN_HTTPHEADERNAME.get(caseInsensitiveName);

commit 6e5651f38e7890aefeadc9aef64b9108feb1182d

http-client-java, bug fix follow https://github.com/microsoft/typespec/pull/9059 (#9103)
a few more issue found in
https://github.com/Azure/autorest.java/pull/3224 (after the resolution
of eclipse lib dependencies)

fixes
-
https://github.com/microsoft/typespec/pull/9103/commits/3a7c20274e3c0aad340f515f642c4590890f2707
-
https://github.com/microsoft/typespec/pull/9103/commits/557ef6b947e144cc6370126a872a03eee47c8003
(ran an agent to check if other possible errors -- it says no :-))
-
https://github.com/microsoft/typespec/pull/9103/commits/2c7c94f2ac617ecc0867007408e696262582d91a

All of them just to revert bugs introduced from
https://github.com/microsoft/typespec/pull/9059

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/model/clientmodel/ClassType.java
@@ -88,7 +88,7 @@ public class ClassType implements IType {
     public static final ClassType ACCESS_TOKEN = withClientCoreReplacement("com.azure.core.credential.AccessToken",
         "io.clientcore.core.credentials.oauth.AccessToken");
     public static final ClassType AZURE_KEY_CREDENTIAL
-        = new Builder(false).packageName("com.azure.core.credential").name("AccessToken").build();
+        = new Builder(false).packageName("com.azure.core.credential").name("AzureKeyCredential").build();
     public static final ClassType KEY_CREDENTIAL = withClientCoreReplacement("com.azure.core.credential.KeyCredential",
         "io.clientcore.core.credentials.KeyCredential");
     public static final ClassType TOKEN_CREDENTIAL = withClientCoreAndVNextReplacement(

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/model/clientmodel/GenericType.java
@@ -231,8 +231,8 @@ public class GenericType implements IType {
                         + "(el -> %2$s).collect(java.util.stream.Collectors.toList())", expression, mapping);
                 } else if (this instanceof MapType) {
                     String mapping = isToClient
-                        ? wireTypeArguments[i].convertToClientType("el")
-                        : wireTypeArguments[i].convertFromClientType("el");
+                        ? wireTypeArguments[i].convertToClientType("el.getValue()")
+                        : wireTypeArguments[i].convertFromClientType("el.getValue()");
                     // Key is always String in Swagger 2
                     expression = String.format(
                         "%1$s.entrySet().stream().collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, el -> %2$s))",

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/template/TemplateHelper.java
@@ -36,11 +36,8 @@ public final class TemplateHelper {
     }
 
     public static String getByteCloneExpression(String propertyName) {
-        return JavaSettings.getInstance().isAzureV1() ? "CoreUtils.clone(" + propertyName + ")" : propertyName; // TODO:
-                                                                                                                // generic
-                                                                                                                // not
-                                                                                                                // having
-                                                                                                                // CoreUtils
+        // TODO: generic not having CoreUtils
+        return JavaSettings.getInstance().isAzureV1() ? "CoreUtils.clone(" + propertyName + ")" : propertyName;
     }
 
     public static void createHttpPipelineMethod(JavaSettings settings, String defaultCredentialScopes,

packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/template/util/ModelTemplateHeaderHelper.java
@@ -60,7 +60,8 @@ public final class ModelTemplateHeaderHelper {
         // HttpHeaderNames for both maps. Then add the few extra azure-core headers here.
         // If HttpHeaderNames are missed from azure-core, they will be handled as unknown headers, which has effectively
         // no difference from this optimization other than code bloat.
-        headerToKnownHttpHeaderName.put("Azure-AsyncOperation", "AZURE_ASYNC_OPERATION");
+        headerToKnownHttpHeaderName.put("Azure-AsyncOperation", "AZURE_ASYNCOPERATION");
+        headerToKnownHttpHeaderName.put("Operation-Location", "OPERATION_LOCATION");
         headerToKnownHttpHeaderName.put("retry-after-ms", "RETRY_AFTER_MS");
         headerToKnownHttpHeaderName.put("x-ms-client-id", "X_MS_CLIENT_ID");
         headerToKnownHttpHeaderName.put("x-ms-client-request-id", "X_MS_CLIENT_REQUEST_ID");