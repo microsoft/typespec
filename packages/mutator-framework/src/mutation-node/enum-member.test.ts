5085863f307f8cadaaa5d36285174b6ee3491e99
Fiona <fthompson@pinterest.com>
2025-12-03 16:15:48 -0500

    Add more comprehensive enum mutation node tests

--- packages/mutator-framework/src/mutation-node/enum-member.test.ts
+++ packages/mutator-framework/src/mutation-node/enum-member.test.ts
@@ -2,25 +2,43 @@ import { t, type TesterInstance } from "@typespec/compiler/testing";
 import { beforeEach, expect, it } from "vitest";
 import { Tester } from "../../test/test-host.js";
 import { getSubgraph } from "../../test/utils.js";
+
 let runner: TesterInstance;
 beforeEach(async () => {
   runner = await Tester.createInstance();
 });
 
 it("handles mutation of member values", async () => {
-  const { program, Foo, a } = await runner.compile(t.code`
+  const { program, Foo, member1 } = await runner.compile(t.code`
       enum ${t.enum("Foo")} {
-        ${t.enumMember("a")}: "valueA";
-        b: "valueB";
+        ${t.enumMember("member1")}: "value1";
+        member2: "value2";
       }
     `);
 
   const subgraph = getSubgraph(program);
   const fooNode = subgraph.getNode(Foo);
-  const aNode = subgraph.getNode(a);
-  aNode.mutate((clone) => (clone.value = "valueARenamed"));
-  expect(aNode.isMutated).toBe(true);
+  const member1Node = subgraph.getNode(member1);
+  member1Node.mutate((clone) => (clone.value = "value1Renamed"));
+  expect(member1Node.isMutated).toBe(true);
   expect(fooNode.isMutated).toBe(true);
-  expect(fooNode.mutatedType.members.get("a") === aNode.mutatedType).toBe(true);
-  expect(aNode.mutatedType.value).toBe("valueARenamed");
+  expect(fooNode.mutatedType.members.get("member1") === member1Node.mutatedType).toBe(true);
+  expect(member1Node.mutatedType.value).toBe("value1Renamed");
+});
+
+it("handles mutation of member with numeric values", async () => {
+  const { program, Foo, member1 } = await runner.compile(t.code`
+      enum ${t.enum("Foo")} {
+        ${t.enumMember("member1")}: 1;
+        member2: 2;
+      }
+    `);
+
+  const subgraph = getSubgraph(program);
+  const fooNode = subgraph.getNode(Foo);
+  const member1Node = subgraph.getNode(member1);
+  member1Node.mutate((clone) => (clone.value = 100));
+  expect(member1Node.isMutated).toBe(true);
+  expect(fooNode.isMutated).toBe(true);
+  expect(member1Node.mutatedType.value).toBe(100);
 });
--- packages/mutator-framework/src/mutation-node/enum.test.ts
+++ packages/mutator-framework/src/mutation-node/enum.test.ts
@@ -2,25 +2,61 @@ import { t, type TesterInstance } from "@typespec/compiler/testing";
 import { beforeEach, expect, it } from "vitest";
 import { Tester } from "../../test/test-host.js";
 import { getSubgraph } from "../../test/utils.js";
+
 let runner: TesterInstance;
 beforeEach(async () => {
   runner = await Tester.createInstance();
 });
 
 it("handles mutation of members", async () => {
-  const { program, Foo, a } = await runner.compile(t.code`
+  const { program, Foo, member1 } = await runner.compile(t.code`
       enum ${t.enum("Foo")} {
-        ${t.enumMember("a")};
-        b;
+        ${t.enumMember("member1")};
+        member2;
       }
     `);
 
   const subgraph = getSubgraph(program);
   const fooNode = subgraph.getNode(Foo);
-  const aNode = subgraph.getNode(a);
-  aNode.mutate((clone) => (clone.name = "aRenamed"));
-  expect(aNode.isMutated).toBe(true);
+  const member1Node = subgraph.getNode(member1);
+  member1Node.mutate();
+  expect(member1Node.isMutated).toBe(true);
   expect(fooNode.isMutated).toBe(true);
-  expect(fooNode.mutatedType.members.get("a") === undefined).toBeTruthy();
-  expect(fooNode.mutatedType.members.get("aRenamed") === aNode.mutatedType).toBeTruthy();
+  expect(fooNode.mutatedType.members.get("member1") === member1Node.mutatedType).toBe(true);
+});
+
+it("handles mutation of members with name change", async () => {
+  const { program, Foo, member1 } = await runner.compile(t.code`
+      enum ${t.enum("Foo")} {
+        ${t.enumMember("member1")};
+        member2;
+      }
+    `);
+
+  const subgraph = getSubgraph(program);
+  const fooNode = subgraph.getNode(Foo);
+  const member1Node = subgraph.getNode(member1);
+  member1Node.mutate((clone) => (clone.name = "member1Renamed"));
+  expect(member1Node.isMutated).toBe(true);
+  expect(fooNode.isMutated).toBe(true);
+  expect(fooNode.mutatedType.members.get("member1")).toBeUndefined();
+  expect(fooNode.mutatedType.members.get("member1Renamed") === member1Node.mutatedType).toBe(true);
+});
+
+it("handles deletion of members", async () => {
+  const { program, Foo, member1 } = await runner.compile(t.code`
+      enum ${t.enum("Foo")} {
+        ${t.enumMember("member1")};
+        member2;
+      }
+    `);
+
+  const subgraph = getSubgraph(program);
+  const fooNode = subgraph.getNode(Foo);
+  const member1Node = subgraph.getNode(member1);
+  member1Node.delete();
+  expect(member1Node.isDeleted).toBe(true);
+  expect(fooNode.isMutated).toBe(true);
+  expect(fooNode.mutatedType.members.get("member1")).toBeUndefined();
+  expect(fooNode.mutatedType.members.size).toBe(1);
 });
