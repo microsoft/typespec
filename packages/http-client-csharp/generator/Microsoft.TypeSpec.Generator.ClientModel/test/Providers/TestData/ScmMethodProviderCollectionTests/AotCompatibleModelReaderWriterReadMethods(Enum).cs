4ce60cba9dda24ca2e0b2fcc96ad7d80d8fb84d5
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2025-12-04 22:36:37 -0800

    Add unit tests for new AOT safe output

---
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/test/Providers/TestData/ScmMethodProviderCollectionTests/AotCompatibleModelReaderWriterReadMethods(Enum).cs
@@ -0,0 +1,2 @@
+global::System.ClientModel.ClientResult result = this.GetData(cancellationToken.ToRequestOptions());
+return global::System.ClientModel.ClientResult.FromValue(global::System.ClientModel.Primitives.ModelReaderWriter.Read<global::Sample.Models.TestEnum>(result.GetRawResponse().Content, global::Sample.ModelSerializationExtensions.WireOptions, global::Sample.SampleContext.Default), result.GetRawResponse());
---
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator.ClientModel/test/Providers/TestData/ScmMethodProviderCollectionTests/AotCompatibleModelReaderWriterReadMethods(Int32).cs
@@ -0,0 +1,2 @@
+global::System.ClientModel.ClientResult result = this.GetData(cancellationToken.ToRequestOptions());
+return global::System.ClientModel.ClientResult.FromValue(global::System.ClientModel.Primitives.ModelReaderWriter.Read<int>(result.GetRawResponse().Content, global::Sample.ModelSerializationExtensions.WireOptions, global::Sample.SampleContext.Default), result.GetRawResponse());
