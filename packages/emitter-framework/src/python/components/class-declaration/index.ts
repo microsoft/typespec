275c68b2910853e2d854766fbc0326b46e3f3768
Mauricio Gardini <mgardini@pinterest.com>
2025-12-16 21:37:09 +0000

    Add subpath exports

---
+++ packages/emitter-framework/src/python/components/class-declaration/index.ts
@@ -0,0 +1,6 @@
+export * from "./class-bases.js";
+export * from "./class-body.js";
+export * from "./class-declaration.js";
+export * from "./class-extends.js";
+export * from "./class-member.js";
+export * from "./class-method.js";
--- packages/emitter-framework/src/python/index.ts
+++ packages/emitter-framework/src/python/index.ts
@@ -1,2 +1,3 @@
 export * from "./builtins.js";
 export * from "./components/index.js";
+export * from "./utils/index.js";
