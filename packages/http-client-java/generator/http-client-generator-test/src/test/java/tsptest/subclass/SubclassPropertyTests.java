993951fca9b12333a0dbc30867e8a58b5b42cb47
Weidong Xu <weidxu@microsoft.com>
2026-01-11 18:16:28 +0800

    update test

--- packages/http-client-java/generator/http-client-generator-test/src/test/java/tsptest/subclass/SubclassPropertyTests.java
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/tsptest/subclass/SubclassPropertyTests.java
@@ -6,17 +6,28 @@ package tsptest.subclass;
 import com.azure.core.util.BinaryData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tsptest.subclass.models.PropertyChangedToConstant;
 import tsptest.subclass.models.PropertyChangedToRequired;
 
 public class SubclassPropertyTests {
 
     @Test
-    public void testPropertyInSubclass() {
+    public void testPropertyChangedToRequired() {
         PropertyChangedToRequired model = new PropertyChangedToRequired("value");
         String json = BinaryData.fromObject(model).toString();
-        Assertions.assertEquals("{\"generatedKeyName\":\"value\"}", json);
+        Assertions.assertEquals("{\"propertyChangedToRequired\":\"value\"}", json);
 
         model = BinaryData.fromString(json).toObject(PropertyChangedToRequired.class);
         Assertions.assertEquals("value", model.getPropertyChangedToRequired());
+    }
+
+    @Test
+    public void testPropertyChangedToConstant() {
+        PropertyChangedToConstant model = new PropertyChangedToConstant();
+        String json = BinaryData.fromObject(model).toString();
+        Assertions.assertEquals("{\"propertyChangedToConstant\":\"constantValue\"}", json);
+
+        model = BinaryData.fromString(json).toObject(PropertyChangedToConstant.class);
+        Assertions.assertEquals("constantValue", model.getPropertyChangedToConstant());
     }
 }
