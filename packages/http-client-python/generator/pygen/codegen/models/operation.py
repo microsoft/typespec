65cbfa1a26252df13bd4d91e0e9187675ad5fc49
iscai-msft <isabellavcai@gmail.com>
2026-01-28 14:00:48 -0500

    fix stop iteration

--- packages/http-client-python/generator/pygen/codegen/models/operation.py
+++ packages/http-client-python/generator/pygen/codegen/models/operation.py
@@ -451,8 +451,13 @@ class OperationBase(  # pylint: disable=too-many-public-methods,too-many-instanc
                 xml_non_default_errors = any(
                     xml_serializable(str(e.default_content_type)) for e in self.non_default_errors
                 )
-                default_error = next(e for e in self.exceptions if "default" in e.status_codes and e.type)
-                if xml_non_default_errors or xml_serializable(str(default_error.default_content_type)):
+                try:
+                    default_error = next(e for e in self.exceptions if "default" in e.status_codes and e.type)
+                except StopIteration:
+                    default_error = None
+                if xml_non_default_errors or (
+                    default_error and xml_serializable(str(default_error.default_content_type))
+                ):
                     file_import.add_submodule_import(relative_path, "_failsafe_deserialize_xml", ImportType.LOCAL)
                 else:
                     file_import.add_submodule_import(relative_path, "_failsafe_deserialize", ImportType.LOCAL)
