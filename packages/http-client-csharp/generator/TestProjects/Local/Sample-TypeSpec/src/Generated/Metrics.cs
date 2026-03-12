650560b6ecff36b56c9d058e37e5767c34ec6776
jolov <jolov@microsoft.com>
2026-03-12 08:09:21 -0700

    regen

--- packages/http-client-csharp/generator/TestProjects/Local/Sample-TypeSpec/src/Generated/Metrics.cs
+++ packages/http-client-csharp/generator/TestProjects/Local/Sample-TypeSpec/src/Generated/Metrics.cs
@@ -56,6 +56,7 @@ namespace SampleTypeSpec
             Argument.AssertNotNullOrEmpty(metricsNamespace, nameof(metricsNamespace));
 
             Argument.AssertNotNull(endpoint, nameof(endpoint));
+            Argument.AssertNotNullOrEmpty(metricsNamespace, nameof(metricsNamespace));
 
             options ??= new SampleTypeSpecClientOptions();
 
