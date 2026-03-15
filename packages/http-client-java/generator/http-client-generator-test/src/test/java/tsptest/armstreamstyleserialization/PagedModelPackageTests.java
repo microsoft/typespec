806b1ab4a2c581b201769b07b81c0800b8f2de1c
Weidong Xu <weidxu@microsoft.com>
2026-03-15 17:17:30 +0800

    test case

---
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/tsptest/armstreamstyleserialization/PagedModelPackageTests.java
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package tsptest.armstreamstyleserialization;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tsptest.armstreamstyleserialization.fluent.models.ListResultSummary2Inner;
+import tsptest.armstreamstyleserialization.implementation.models.ListResult;
+import tsptest.armstreamstyleserialization.implementation.models.ListResultSummary;
+import tsptest.armstreamstyleserialization.models.ListResult2;
+
+public class PagedModelPackageTests {
+
+    @Test
+    public void testPagedModelPackage() {
+        Assertions.assertTrue(isInImplementationModels(ListResult.class));
+        Assertions.assertTrue(isInImplementationModels(ListResultSummary.class));
+
+        // ListResult2 not in implementation, because its child ListResultSummary2(Inner) is PUBLIC
+        Assertions.assertFalse(isInImplementationModels(ListResult2.class));
+        Assertions.assertFalse(isInImplementationModels(ListResultSummary2Inner.class));
+    }
+
+    private static boolean isInImplementationModels(Class<?> clazz) {
+        return clazz.getPackage().getName().endsWith(".implementation.models");
+    }
+}
