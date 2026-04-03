8485c63f7277017fe1a3f1be6b30a7dae72be29c
Weidong Xu <weidxu@microsoft.com>
2026-04-03 12:15:18 +0800

    format

--- packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/util/WireTypeClientTypeConverter.java
+++ packages/http-client-java/generator/http-client-generator-core/src/main/java/com/microsoft/typespec/http/client/generator/core/util/WireTypeClientTypeConverter.java
@@ -10,7 +10,6 @@ import com.microsoft.typespec.http.client.generator.core.model.clientmodel.IType
 import com.microsoft.typespec.http.client.generator.core.model.clientmodel.PrimitiveType;
 import io.clientcore.core.utils.Base64Uri;
 import io.clientcore.core.utils.DateTimeRfc1123;
-
 import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.Instant;
@@ -170,7 +169,8 @@ public class WireTypeClientTypeConverter {
         try {
             if (wireType == ClassType.DATE_TIME_RFC_1123) {
                 if (literalValue.length() != 29 || !literalValue.endsWith("GMT")) {
-                    /* hack
+                    /*
+                     * hack
                      * DateTimeException from DateTimeRfc1123 would log a same error message as generator,
                      * which trigger the error message from emitter which fails the emitter.
                      * We need this to be a warning, not an error.
