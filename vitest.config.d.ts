606a285e1b90428662b54a8520e435f19ed92bec
Vincent Biret <vibiret@microsoft.com>
2026-05-27 08:32:36 -0400

    chore: removes extraneous files

--- vitest.config.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Default Config For all TypeSpec projects using vitest.
- */
-export declare const defaultTypeSpecVitestConfig: import("vite").UserConfig;
-declare const _default: Record<string, any>;
-export default _default;
-//# sourceMappingURL=vitest.config.d.ts.map
