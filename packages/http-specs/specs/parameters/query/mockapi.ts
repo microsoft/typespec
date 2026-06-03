42946e0a34967564a407779c216a984e60888c7f
iscai-msft <isabellavcai@gmail.com>
2026-06-03 12:06:35 -0400

    switch to special char

--- packages/http-specs/specs/parameters/query/mockapi.ts
+++ packages/http-specs/specs/parameters/query/mockapi.ts
@@ -14,8 +14,8 @@ Scenarios.Parameters_Query_Constant_post = passOnSuccess({
   kind: "MockApiDefinition",
 });
 
-Scenarios.Parameters_Query_Prefix_dollarSign = passOnSuccess({
-  uri: "/parameters/query/prefix/dollar-sign",
+Scenarios.Parameters_Query_SpecialChar_dollarSign = passOnSuccess({
+  uri: "/parameters/query/special-char/dollar-sign",
   method: "get",
   request: {
     query: { $filter: "status eq 'active'" },
