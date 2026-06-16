f676243819f0c222a0c57b8cc1ab3ba9198e2ce4
Wei Hu <huwe@microsoft.com>
2026-06-16 07:04:08 +0000

    Update model root visibility test

--- packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator/test/Providers/ModelProviders/ModelProviderTests.cs
+++ packages/http-client-csharp/generator/Microsoft.TypeSpec.Generator/test/Providers/ModelProviders/ModelProviderTests.cs
@@ -1449,7 +1449,7 @@ namespace Microsoft.TypeSpec.Generator.Tests.Providers.ModelProviders
         }
 
         [Test]
-        public void PublicModelsAreIncludedInAdditionalRootTypes()
+        public void PublicModelsAreNotIncludedInAdditionalRootTypes()
         {
             var inputModel = InputFactory.Model(
                 "MockInputModel",
@@ -1462,7 +1462,7 @@ namespace Microsoft.TypeSpec.Generator.Tests.Providers.ModelProviders
             Assert.IsNotNull(modelProvider);
 
             var rootTypes = CodeModelGenerator.Instance.AdditionalRootTypes;
-            Assert.IsTrue(rootTypes.Contains("Sample.Models.MockInputModel"));
+            Assert.IsFalse(rootTypes.Contains("Sample.Models.MockInputModel"));
         }
 
         [Test]
