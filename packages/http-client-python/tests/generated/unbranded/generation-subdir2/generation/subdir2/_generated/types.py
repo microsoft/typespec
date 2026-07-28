ca080d681dbecc600102bd493390b4ff82b42d2d
Yuchao Yan <yuchaoyan@microsoft.com>
2026-07-28 05:32:52 +0000

    regenerate

---
+++ packages/http-client-python/tests/generated/unbranded/generation-subdir2/generation/subdir2/_generated/types.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+
+from typing import TYPE_CHECKING, Union
+from typing_extensions import Required, TypedDict
+
+if TYPE_CHECKING:
+    from . import _unions
+    from .models import EnumV1, EnumV2
+
+
+class ModelV1(TypedDict, total=False):
+    """ModelV1.
+
+    :ivar prop: Required.
+    :vartype prop: str
+    :ivar enumProp: Required. Known values are: "enumMemberV1" and "enumMemberV2".
+    :vartype enumProp: Union[str, "EnumV1"]
+    :ivar unionProp: Required. Is either a str type or a int type.
+    :vartype unionProp: "_unions.UnionV1"
+    """
+
+    prop: Required[str]
+    """Required."""
+    enumProp: Required[Union[str, "EnumV1"]]
+    """Required. Known values are: \"enumMemberV1\" and \"enumMemberV2\"."""
+    unionProp: Required["_unions.UnionV1"]
+    """Required. Is either a str type or a int type."""
+
+
+class ModelV2(TypedDict, total=False):
+    """ModelV2.
+
+    :ivar prop: Required.
+    :vartype prop: str
+    :ivar enumProp: Required. "enumMember"
+    :vartype enumProp: Union[str, "EnumV2"]
+    :ivar unionProp: Required. Is either a str type or a int type.
+    :vartype unionProp: "_unions.UnionV2"
+    """
+
+    prop: Required[str]
+    """Required."""
+    enumProp: Required[Union[str, "EnumV2"]]
+    """Required. \"enumMember\""""
+    unionProp: Required["_unions.UnionV2"]
+    """Required. Is either a str type or a int type."""
