dfb4c030d6b4c82df321324feb6803ca280be001
Timothee Guerin <tiguerin@microsoft.com>
2026-07-30 12:36:21 -0400

    fix: keep typekit barrel re-exports as value exports
    oxlint's consistent-type-exports converted `export * from ./operation.js`
    (and model / model-property) to `export type *`. Those kit modules only
    export types but run `defineKit(...)` at import time to register typekit
    methods; `export type *` drops the module so registration never happens,
    causing `$.operation.getClientSignature is not a function` at emit time.

--- packages/http-client/src/typekit/kits/index.ts
+++ packages/http-client/src/typekit/kits/index.ts
@@ -1,5 +1,5 @@
 export * from "./client-library.js";
 export * from "./client.js";
-export type * from "./model-property.js";
-export type * from "./model.js";
-export type * from "./operation.js";
+export * from "./model-property.js";
+export * from "./model.js";
+export * from "./operation.js";
