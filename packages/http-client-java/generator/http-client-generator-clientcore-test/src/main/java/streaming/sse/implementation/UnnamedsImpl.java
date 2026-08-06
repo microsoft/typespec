6ebff389e9efa1101a8911bcdc6b0ebf0e3e2313
Weidong Xu <weidxu@microsoft.com>
2026-08-06 13:55:52 +0800

    http-client-java, bump dep part1 (#11573)

--- packages/http-client-java/generator/http-client-generator-clientcore-test/src/main/java/streaming/sse/implementation/UnnamedsImpl.java
+++ packages/http-client-java/generator/http-client-generator-clientcore-test/src/main/java/streaming/sse/implementation/UnnamedsImpl.java
@@ -1,11 +1,11 @@
-package parameters.query.implementation;
+package streaming.sse.implementation;
 
 import io.clientcore.core.annotations.ReturnType;
 import io.clientcore.core.annotations.ServiceInterface;
 import io.clientcore.core.annotations.ServiceMethod;
+import io.clientcore.core.http.annotations.HeaderParam;
 import io.clientcore.core.http.annotations.HostParam;
 import io.clientcore.core.http.annotations.HttpRequestInformation;
-import io.clientcore.core.http.annotations.QueryParam;
 import io.clientcore.core.http.annotations.UnexpectedResponseExceptionDetail;
 import io.clientcore.core.http.models.HttpMethod;
 import io.clientcore.core.http.models.HttpResponseException;
@@ -13,21 +13,22 @@ import io.clientcore.core.http.models.RequestContext;
 import io.clientcore.core.http.models.Response;
 import io.clientcore.core.http.pipeline.HttpPipeline;
 import io.clientcore.core.instrumentation.Instrumentation;
+import io.clientcore.core.models.binarydata.BinaryData;
 import java.lang.reflect.InvocationTargetException;
 
 /**
- * An instance of this class provides access to all the operations defined in SpecialChars.
+ * An instance of this class provides access to all the operations defined in Unnameds.
  */
-public final class SpecialCharsImpl {
+public final class UnnamedsImpl {
     /**
      * The proxy service used to perform REST calls.
      */
-    private final SpecialCharsService service;
+    private final UnnamedsService service;
 
     /**
      * The service client containing this operation class.
      */
-    private final QueryClientImpl client;
+    private final SseClientImpl client;
 
     /**
      * The instance of instrumentation to report telemetry.
@@ -35,27 +36,26 @@ public final class SpecialCharsImpl {
     private final Instrumentation instrumentation;
 
     /**
-     * Initializes an instance of SpecialCharsImpl.
+     * Initializes an instance of UnnamedsImpl.
      * 
      * @param client the instance of the service client containing this operation class.
      */
-    SpecialCharsImpl(QueryClientImpl client) {
-        this.service = SpecialCharsService.getNewInstance(client.getHttpPipeline());
+    UnnamedsImpl(SseClientImpl client) {
+        this.service = UnnamedsService.getNewInstance(client.getHttpPipeline());
         this.client = client;
         this.instrumentation = client.getInstrumentation();
     }
 
     /**
-     * The interface defining all the services for QueryClientSpecialChars to be used by the proxy service to perform
-     * REST calls.
+     * The interface defining all the services for SseClientUnnameds to be used by the proxy service to perform REST
+     * calls.
      */
-    @ServiceInterface(name = "QueryClientSpecialChars", host = "{endpoint}")
-    public interface SpecialCharsService {
-        static SpecialCharsService getNewInstance(HttpPipeline pipeline) {
+    @ServiceInterface(name = "SseClientUnnameds", host = "{endpoint}")
+    public interface UnnamedsService {
+        static UnnamedsService getNewInstance(HttpPipeline pipeline) {
             try {
-                Class<?> clazz = Class.forName("parameters.query.implementation.SpecialCharsServiceImpl");
-                return (SpecialCharsService) clazz.getMethod("getNewInstance", HttpPipeline.class)
-                    .invoke(null, pipeline);
+                Class<?> clazz = Class.forName("streaming.sse.implementation.UnnamedsServiceImpl");
+                return (UnnamedsService) clazz.getMethod("getNewInstance", HttpPipeline.class).invoke(null, pipeline);
             } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException
                 | InvocationTargetException e) {
                 throw new RuntimeException(e);
@@ -65,28 +65,28 @@ public final class SpecialCharsImpl {
 
         @HttpRequestInformation(
             method = HttpMethod.GET,
-            path = "/parameters/query/special-char/dollar-sign",
-            expectedStatusCodes = { 204 })
+            path = "/streaming/sse/unnamed/receive",
+            expectedStatusCodes = { 200 })
         @UnexpectedResponseExceptionDetail
-        Response<Void> dollarSign(@HostParam("endpoint") String endpoint, @QueryParam("$filter") String filter,
+        Response<BinaryData> receive(@HostParam("endpoint") String endpoint, @HeaderParam("Accept") String accept,
             RequestContext requestContext);
     }
 
     /**
-     * The dollarSign operation.
+     * The receive operation.
      * 
-     * @param filter The filter parameter.
      * @param requestContext The context to configure the HTTP request before HTTP client sends it.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws HttpResponseException thrown if the service returns an error.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return the {@link Response}.
+     * @return the response body along with {@link Response}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Response<Void> dollarSignWithResponse(String filter, RequestContext requestContext) {
-        return this.instrumentation.instrumentWithResponse("Parameters.Query.SpecialChar.dollarSign", requestContext,
+    public Response<BinaryData> receiveWithResponse(RequestContext requestContext) {
+        return this.instrumentation.instrumentWithResponse("Streaming.Sse.Unnamed.receive", requestContext,
             updatedContext -> {
-                return service.dollarSign(this.client.getEndpoint(), filter, updatedContext);
+                final String accept = "text/event-stream";
+                return service.receive(this.client.getEndpoint(), accept, updatedContext);
             });
     }
 }
