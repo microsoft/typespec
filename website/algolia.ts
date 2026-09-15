9bc7d662964347b49d077b1a530bd042307b6db7
Timothee Guerin <tiguerin@microsoft.com>
2026-09-15 13:10:46 -0400

    more fixes

--- website/algolia.ts
+++ website/algolia.ts
@@ -3,5 +3,5 @@ export default {
   appId: "V3T9EUVLJR",
   // This is the search API KEY this can be public https://support.algolia.com/hc/en-us/articles/18966776061329-Can-the-search-API-key-be-public
   apiKey: "bae16ae67ddbe24e700ac20d192ad20f",
-  indexName: "typespec",
+  indices: ["typespec"],
 };
